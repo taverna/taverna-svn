9f7e420ec2e817479d4791ecc3feb95f06d07906
dmarvin <dmarvin@bf327186-88b3-11dd-a302-d386e5130c1c>
2003-04-12 13:19:55 +0000

    Basic Taverna base tasks and custom tasks for common soaplab services
    git-svn-id: http://taverna.googlecode.com/svn@38 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/taverna1.0/src/uk/ac/soton/itinnovation/taverna/enactor/entities/PortTask.java
@@ -0,0 +1,133 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// © University of Southampton IT Innovation Centre, 2002
+//
+// Copyright in this library belongs to the IT Innovation Centre of
+// 2 Venture Road, Chilworth Science Park, Southampton SO16 7NP, UK.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1
+// of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation Inc, 59 Temple Place, Suite 330, Boston MA 02111-1307 USA.
+//
+//      Created By          :   Darren Marvin
+//      Created Date        :   2003/4/9
+//      Created for Project :   MYGRID
+//      Dependencies        :
+//
+//      Last commit info    :   $Author: dmarvin $
+//                              $Date: 2003-04-12 13:19:55 $
+//                              $Revision: 1.1 $
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+package uk.ac.soton.itinnovation.taverna.enactor.entities;
+
+import uk.ac.soton.itinnovation.mygrid.workflow.enactor.core.entities.graph.GraphNode;
+import uk.ac.soton.itinnovation.mygrid.workflow.enactor.core.serviceprovidermanager.ServiceSelectionCriteria;
+
+import uk.ac.soton.itinnovation.mygrid.workflow.enactor.io.Part;
+
+/**
+ * GraphNode that represents a port on a Processor.
+ */
+public class PortTask extends TavernaTask{
+	
+	public static int IN = 0;
+	public static int OUT = 1;
+
+	private org.embl.ebi.escience.scufl.Port port; 
+	private int type;
+	private Part dataPacket = null;
+		
+	public PortTask(org.embl.ebi.escience.scufl.Port port) {
+		super(port.getProcessor().getName() + ":" + port.getName());
+		this.port = port;
+		if(port instanceof org.embl.ebi.escience.scufl.InputPort)
+			type = 0;
+		else 
+			type = 1;
+	}
+
+	/**
+	 * Obtains the type of this Port, either input (0) or output (1).
+	 * @return type
+	 */
+	public int type() {
+		return type;
+	}
+
+	/**
+	 * Obtains the original XScufl definition Port 
+	 * @return XScufl definition port
+	 */
+	public org.embl.ebi.escience.scufl.Port getScuflPort() {
+		return port;
+	}
+
+	/**
+	 * Checks to see if data is available on this port
+	 * @return true if available
+	 */
+	public boolean dataAvailable() {
+		if(dataPacket==null) {
+			return false;
+		}
+		else
+			return true;
+	}
+
+	/**
+	 * Blocking method that waits for data to arrive
+	 * and then supplies it
+	 * @return holder for data
+	 */
+	public synchronized Part waitForData() {
+		while(dataPacket==null) {
+			try{
+				wait();
+			} catch(InterruptedException ex) {
+			}
+		}
+		return dataPacket;
+	}
+
+	/**
+	 * Sets a reference to the actual data holder
+	 * @param data holder for data
+	 */
+	public synchronized void setData(Part p) {
+		dataPacket = p;
+		//set data on child porttasks too
+		GraphNode[] chds = getChildren();
+		for(int i=0;i<chds.length;i++) {
+			if(chds[i] instanceof PortTask) {
+				PortTask pT = (PortTask) chds[i];
+				pT.setData(p);
+			}
+		}
+		notifyAll();
+	}
+
+	/**
+     * Forces class to clean itself up a bit.
+     */
+    public void cleanUpConcreteTask() {}
+
+	/**
+     * Retrieves the selection criteria associated with this task. This criteria
+     * determines the service provider for a particular service.
+     */
+    public ServiceSelectionCriteria getServiceSelectionCriteria() {
+        return null;
+    }
+} 
---
+++ trunk/taverna1.0/src/uk/ac/soton/itinnovation/taverna/enactor/entities/TavernaTaskFactory.java
@@ -0,0 +1,84 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// © University of Southampton IT Innovation Centre, 2002
+//
+// Copyright in this library belongs to the IT Innovation Centre of
+// 2 Venture Road, Chilworth Science Park, Southampton SO16 7NP, UK.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1
+// of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation Inc, 59 Temple Place, Suite 330, Boston MA 02111-1307 USA.
+//
+//      Created By          :   Darren Marvin
+//      Created Date        :   2003/04/08
+//      Created for Project :   MYGRID
+//      Dependencies        :
+//
+//      Last commit info    :   $Author: dmarvin $
+//                              $Date: 2003-04-12 13:19:55 $
+//                              $Revision: 1.1 $
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+package uk.ac.soton.itinnovation.taverna.enactor.entities;
+
+
+//java imports
+
+//third party imports
+import org.apache.log4j.Logger;
+
+//local imports
+
+
+
+/**
+ * Creates the correct concrete task for a given WSDL identifier
+ * Future versions should load a task based on the workflow for a particular service.
+ */
+public class TavernaTaskFactory {
+
+	private static Logger logger = Logger.getLogger(TavernaTaskFactory.class);
+
+    /**
+     * Retrieves a concrete instance of a taverna task 
+	 *
+     * @param String identifier for the required application
+     */
+    public static TavernaTask getConcreteTavernaTask(String taskID,String processerName) throws UnsupportedTavernaProcessorException {
+        //obtain the class of this taskID, this could be shifted to configuration later
+		String taskClassName = null;
+		if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/edit::seqret"))
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.SeqretTask";
+		else if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/nucleic_gene_finding::getorf"))
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.GetOrfTask";
+		else if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/nucleic_translation::transeq"))
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.TranSeqTask";
+		else if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/alignment_multiple::emma"))
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.EmmaSeqTask";
+		else if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/nucleic_profiles::prophecy")) 
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.ProphecyTask";
+		else if(taskID.equals("http://industry.ebi.ac.uk/soap/soaplab/nucleic_profiles::prophet")) 
+			taskClassName = "uk.ac.soton.itinnovation.taverna.enactorentities.ProphetTask";
+		else {
+			logger.error("Don't know how to deal with processor with soaplab wsdl '" + taskID + "'");
+			throw new UnsupportedTavernaProcessorException("Don't know how to deal with processor with soaplab wsdl '" + taskID + "'");
+		}
+		try {
+            return (TavernaTask) Class.forName(taskClassName).newInstance();
+        } catch (Exception ex) {
+            logger.error("Don't know how to deal with processor with soaplab wsdl '" + taskID + "'",ex);
+			throw new UnsupportedTavernaProcessorException("Couldn't load implementation for processor with soaplab wsdl '" + taskID + "'");
+        }
+    }
+}
