4891ebe755123448275afad3b7c247a320ff05eb
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2003-09-23 14:25:40 +0000

    Starting to put together the various bits of code for baclava
    git-svn-id: http://taverna.googlecode.com/svn@242 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/taverna1.0/src/org/embl/ebi/escience/baclava/handlers/StringHandler.java
@@ -0,0 +1,22 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.baclava.handlers;
+
+import org.jdom.*;
+import org.embl.ebi.escience.baclava.*;
+
+/**
+ * Handle String objects
+ * @author Tom Oinn
+ */
+public class StringHandler implements XMLHandler {
+
+    public Object objectFromElement(Element theElement) {
+	// The element in this case ...
+	return null;
+    }
+
+}
