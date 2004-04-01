ff9d66c61aac46e22f61c0f78fbb5b2ed16fe9c0
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2004-04-01 14:31:35 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-1-beta-9'.
    git-svn-id: http://taverna.googlecode.com/svn@463 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/release-0-1-beta-9/taverna1.0/src/uk/ac/mrc/hgmp/taverna/retsina/IScuflProcessor.java
@@ -0,0 +1,13 @@
+package uk.ac.mrc.hgmp.taverna.retsina;
+
+/**
+ * This is the interface that all processors
+ * in the Scufl language implement. This is currently
+ * used for containers that can pass parameter names
+ * etc around within the graph editor and translator
+ * programs.
+ * @author Tom Oinn
+ */
+public interface IScuflProcessor {
+
+}
