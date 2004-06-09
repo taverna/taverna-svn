8a9be34a3788e508767d2f252fca0d4266d25b44
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2004-06-09 11:22:28 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-1-beta-10'.
    git-svn-id: http://taverna.googlecode.com/svn@569 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/release-0-1-beta-10/taverna1.0/src/org/embl/ebi/escience/scufl/provenance/process/TimeoutExceeded.java
@@ -0,0 +1,16 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scufl.provenance.process;
+
+/**
+ * Event corresponding to an invocation timout
+ * @author Tom Oinn
+ */
+public class TimeoutExceeded extends ProcessEvent {
+    
+    // No detail here
+    
+}
---
+++ tags/release-0-1-beta-10/taverna1.0/src/org/embl/ebi/escience/scuflui/renderers/TextRtf.java
@@ -0,0 +1,43 @@
+package org.embl.ebi.escience.scuflui.renderers;
+
+import org.embl.ebi.escience.baclava.DataThing;
+
+import javax.swing.*;
+import java.util.regex.Pattern;
+
+/**
+ *
+ *
+ * @author Matthew Pocock
+ */
+public class TextRtf
+        extends AbstractRenderer.ByPattern
+{
+    public TextRtf()
+    {
+        super("RTF",
+              new ImageIcon(ClassLoader.getSystemResource(
+                "org/embl/ebi/escience/baclava/icons/text.png")),
+              Pattern.compile(".*text/rtf.*"));
+    }
+
+    protected boolean canHandle(RendererRegistry renderers,
+                                Object userObject,
+                                String mimeType)
+    {
+        return super.canHandle(renderers, userObject, mimeType) &&
+                userObject instanceof String;
+    }
+
+    public boolean isTerminal()
+    {
+        return true;
+    }
+
+    public JComponent getComponent(RendererRegistry renderers,
+                                   DataThing dataThing)
+    {
+        return new JEditorPane(
+                "text/html", (String) dataThing.getDataObject());
+    }
+}
