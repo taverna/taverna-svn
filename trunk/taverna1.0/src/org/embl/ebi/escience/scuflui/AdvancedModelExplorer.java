d35236b73cdda0d68ca1e5025cc74609ab3c58fe
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2004-07-28 12:35:03 +0000

    Split off the html generation code into its own class, tidied colours and other layout type issues
    git-svn-id: http://taverna.googlecode.com/svn@639 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflui/AdvancedModelExplorer.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflui/AdvancedModelExplorer.java
@@ -317,192 +317,46 @@ public class AdvancedModelExplorer extends JPanel
     }
 
     private void updateTabForSummary() {
-	URL worldIcon = null;
-	try {
-	    Class c = Class.forName("org.embl.ebi.escience.scuflui.workbench.Workbench");
-	    worldIcon = c.getResource("openurl.gif");
-	}
-	catch (Exception e) {
-	    //
-	}
 	propertiesPanel.removeAll();
-	propertiesPanel.setLayout(new BoxLayout(propertiesPanel, BoxLayout.PAGE_AXIS));
-	Processor[] processors = model.getProcessors();
-	Map resources = new HashMap();
-	for (int i = 0; i<processors.length; i++) {
-	    String resourceHost = processors[i].getResourceHost();
-	    if (resourceHost != Processor.ENACTOR) {
-		if (resources.containsKey(resourceHost) == false) {
-		    resources.put(resourceHost, new ArrayList());
-		}
-		java.util.List processorForResource = (java.util.List)resources.get(resourceHost);
-		processorForResource.add(processors[i]);
-	    }
-	}
-	StringBuffer sb = new StringBuffer();
-	sb.append("<html><head><STYLE TYPE=\"text/css\">");
-	sb.append("body {\n");
-	sb.append("  background-color: #eeeeee;\n");
-	sb.append("font-family: Helvetica, Arial, sans-serif;\n");
-	sb.append("font-size: 12pt;\n");
-	sb.append("}\n");
-	sb.append("</STYLE></head><body>");
-	sb.append("<h2>Workflow information</h2>");
-	sb.append("This report applies to the workflow titled '<font color=\"red\">"+model.getDescription().getTitle()+"</font>' authored by '<font color=\"red\">"+model.getDescription().getAuthor()+"</font>' and with LSID '<font color=\"red\">"+model.getDescription().getLSID()+"</font>'. The textual description, if any is shown below :<blockquote><em>"+model.getDescription().getText()+"</em></blockquote>");
-	sb.append("<h2>Resource usage report</h2>");
-	sb.append("This display shows the various external resources used by the current workflow. It does not show resources such as local operations or string constants which are run within the enactment engine. Services are categorized by resource host and type, and the name of the instance of each service shown to the right.");
-	sb.append("<table border=\"1\" width=\"100%\" bgcolor=\"white\">");
-	for (Iterator i = resources.keySet().iterator(); i.hasNext();) {
-	    String hostName = (String)i.next();
-	    java.util.List usageList = (java.util.List)resources.get(hostName);
-	    sb.append("<tr><td valign=\"top\" bgcolor=\"#eeeeff\"colspan=\"3\"><img src=\""+worldIcon.toString()+"\"> <code><b>"+hostName+"</b></code></td></tr>\n");
-	    // Do all web service installations within this host
-	    Set temp = new HashSet();
-	    for (Iterator j = usageList.iterator(); j.hasNext(); ) {
-		Object o = j.next();
-		if (o instanceof WSDLBasedProcessor) {
-		    temp.add(o);
-		}
-	    }
-	    WSDLBasedProcessor[] wp = (WSDLBasedProcessor[])temp.toArray(new WSDLBasedProcessor[0]);
-	    Map wsLocations = new HashMap();
-	    for (int j = 0; j < wp.length; j++) {
-		String wsdlLocation = "";
-		try {
-		    URL wsdlURL = new URL(wp[j].getWSDLLocation());
-		    wsdlLocation = wsdlURL.getFile();
-		}
-		catch (MalformedURLException mue) {
-		    //
-		}
-		if (wsLocations.containsKey(wsdlLocation)==false) {
-		    wsLocations.put(wsdlLocation, new HashMap());
-		}
-		Map operationToProcessorName = (Map)wsLocations.get(wsdlLocation);
-		String operationName = wp[j].getOperationName();
-		if (operationToProcessorName.containsKey(operationName)==false) {
-		    operationToProcessorName.put(operationName, new HashSet());
-		}
-		Set processorNames = (Set)operationToProcessorName.get(operationName);
-		processorNames.add(wp[j].getName());
-	    }
-	    for (Iterator j = wsLocations.keySet().iterator(); j.hasNext();) {
-		// Top level iterator over all service locations.
-		String location = (String)j.next();
-		Map operationToProcessorName = (Map)wsLocations.get(location);
-		int rows = 2+operationToProcessorName.size();
-		sb.append("<tr>");
-		sb.append("<td width=\"80\" valign=\"top\" rowspan=\""+rows+"\" bgcolor=\"#55bb22\">Web&nbsp;service</td>");
-		sb.append("<td colspan=\"2\" bgcolor=\"#55bb22\">WSDL Defined at <font color=\"purple\">"+location+"</font></td>");
-		sb.append("</tr>");
-		sb.append("<tr><td bgcolor=\"#eeeedd\">Operation name</td><td bgcolor=\"#eeeedd\">Processors</td></tr>");
-		for (Iterator k = operationToProcessorName.keySet().iterator(); k.hasNext();) {
-		    String operationName = (String)k.next();
-		    Set processorNames = (Set)operationToProcessorName.get(operationName);
-		    sb.append("<tr>");
-		    sb.append("<td><font color=\"purple\">"+operationName+"</font></td>");
-		    sb.append("<td>");
-		    for (Iterator l = processorNames.iterator(); l.hasNext();) {
-			sb.append((String)l.next());
-			if (l.hasNext()) {
-			    sb.append(", ");
+	propertiesPanel.setLayout(new BoxLayout(propertiesPanel, BoxLayout.PAGE_AXIS));	
+	final String htmlSummary = WorkflowSummaryAsHTML.getSummary(model);
+	JToolBar toolbar = new JToolBar();
+	toolbar.setFloatable(false);
+	toolbar.setRollover(true);
+	toolbar.setMaximumSize(new Dimension(2000,30));
+	toolbar.setBorderPainted(true);
+	JButton saveHTML = new JButton(Workbench.saveIcon);
+	saveHTML.setPreferredSize(new Dimension(25,25));
+	toolbar.add(new JLabel(" Save HTML description "));
+	toolbar.add(saveHTML);
+	propertiesPanel.add(toolbar, BorderLayout.PAGE_START);
+	saveHTML.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    try {
+			Preferences prefs = Preferences.userNodeForPackage(AdvancedModelExplorer.class);
+			String curDir = prefs.get("currentDir", System.getProperty("user.home"));
+			fc.resetChoosableFileFilters();
+			fc.setFileFilter(new ExtensionFileFilter(new String[]{"html","htm"}));
+			fc.setCurrentDirectory(new File(curDir));
+			int returnVal = fc.showSaveDialog(AdvancedModelExplorer.this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+			    prefs.put("currentDir", fc.getCurrentDirectory().toString());
+			    File file = fc.getSelectedFile();
+			    PrintWriter out = new PrintWriter(new FileWriter(file));
+			    out.println(htmlSummary);
+			    out.flush();
+			    out.close();
 			}
 		    }
-		    sb.append("</td></tr>");
-		}
-	    }
-
-	    // Do all soaplab installations within this host
-	    temp = new HashSet();
-	    for  (Iterator j = usageList.iterator(); j.hasNext(); ) {
-		Object o = j.next();
-		if (o instanceof SoaplabProcessor) {
-		    temp.add(o);
-		}
-	    }
-	    SoaplabProcessor sp[] = (SoaplabProcessor[])temp.toArray(new SoaplabProcessor[0]);
-	    Map soaplabLocations = new HashMap();
-	    for (int j = 0; j < sp.length; j++) {
-		String soaplabLocation = sp[j].getServicePath();
-		if (soaplabLocations.containsKey(soaplabLocation)==false) {
-		    soaplabLocations.put(soaplabLocation, new HashMap());
-		}
-		Map nameToProcessorNames = (Map)soaplabLocations.get(soaplabLocation);
-		String appName = sp[j].getCategory()+"::<font color=\"purple\">"+sp[j].getAppName()+"</font>";
-		if (nameToProcessorNames.containsKey(appName)==false) {
-		    nameToProcessorNames.put(appName, new HashSet());
-		}
-		Set processorNames = (Set)nameToProcessorNames.get(appName);
-		processorNames.add(sp[j].getName());
-	    }
-	    for (Iterator j = soaplabLocations.keySet().iterator(); j.hasNext(); ) {
-		String location = (String)j.next();
-		Map nameToProcessorName = (Map)soaplabLocations.get(location);
-		int rows = 2+nameToProcessorName.size();
-		sb.append("<tr>");
-		sb.append("<td width=\"80\" valign=\"top\" rowspan=\""+rows+"\" bgcolor=\"#ffff88\">Soaplab</td>");
-		sb.append("<td colspan=\"2\" bgcolor=\"ffff88\">Service rooted at <font color=\"purple\">"+location+"</font></td>");
-		sb.append("</tr>");
-		sb.append("<tr><td bgcolor=\"#eeeedd\">App category and name</td><td bgcolor=\"#eeeedd\">Processors</td></tr>");
-		for (Iterator k = nameToProcessorName.keySet().iterator(); k.hasNext();) {
-		    String appName = (String)k.next();
-		    Set processorNames = (Set)nameToProcessorName.get(appName);
-		    sb.append("<tr>");
-		    sb.append("<td>"+appName+"</td>");
-		    sb.append("<td>");
-		    for (Iterator l = processorNames.iterator(); l.hasNext();) {
-			sb.append((String)l.next());
-			if (l.hasNext()) {
-			    sb.append(", ");
-			}
+		    catch (Exception ex) {
+			JOptionPane.showMessageDialog(AdvancedModelExplorer.this,
+						      "Problem saving workflow : \n"+ex.getMessage(),
+						      "Error!",
+						      JOptionPane.ERROR_MESSAGE);
 		    }
-		    sb.append("</td></tr>");
 		}
-	    }
-
-	    for (Iterator j = usageList.iterator(); j.hasNext(); ) {
-		Processor p = (Processor)j.next();
-		if (p instanceof WSDLBasedProcessor == false &&
-		    p instanceof SoaplabProcessor == false) {
-		    sb.append("<tr>");
-		    /**
-		       if (p instanceof SoaplabProcessor) {
-		       sb.append("<td bgcolor=\"#ffff88\">Soaplab</td>");
-		       SoaplabProcessor sp = (SoaplabProcessor)p;
-		       sb.append("<td>"+sp.getCategory()+"::<font color=\"purple\">"+sp.getAppName()+"</font></td>");
-		       }
-		    */
-		    /**
-		       else if (p instanceof WSDLBasedProcessor) {
-		       sb.append("<td bgcolor=\"#55bb22\">Web&nbsp;service</td>");
-		       String targetOperation = ((WSDLBasedProcessor)p).getOperationName();
-		       String wsdlLocation = ((WSDLBasedProcessor)p).getWSDLLocation();
-		       String wsdl = "";
-		       try {
-		       URL wsdlURL = new URL(wsdlLocation);
-		       wsdl = wsdlURL.getFile();
-		       }
-		       catch (MalformedURLException mue) {
-		       //
-		       }
-		       sb.append("<td><font color=\"purple\">"+targetOperation+"</font>&nbsp;in&nbsp;"+wsdl+"</td>");
-		       }
-		    */
-		    if (p instanceof BiomobyProcessor) {
-			sb.append("<td bgcolor=\"#ff9900\">Biomoby</td>");
-			BiomobyProcessor bp = (BiomobyProcessor)p;
-			sb.append("<td><font color=\"purple\">"+bp.getServiceName()+"</font>&nbsp;in&nbsp;"+bp.getEndpoint().getFile()+"</td>");
-		    }
-		    else {
-			sb.append("<td colspan=\"2\">Unknown&nbsp;type</td>");
-		    }
-		    sb.append("<td>"+p.getName()+"</td>");
-		    sb.append("</tr>\n");
-		}
-	    }
-	}
-	sb.append("</table></body></html>");
-	JEditorPane ed = new JEditorPane("text/html",sb.toString());
+	    });
+	JEditorPane ed = new JEditorPane("text/html",htmlSummary);
 	ed.setEditable(false);
 	JScrollPane edPane = new JScrollPane(ed);
 	propertiesPanel.add(edPane);
