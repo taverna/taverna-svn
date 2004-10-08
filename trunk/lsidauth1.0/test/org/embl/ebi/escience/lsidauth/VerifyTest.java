6df356dd0d800cc8629ab9e5096d2d3d65fba7cb
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2004-10-08 11:17:29 +0000

    Added test for incorrect secret key
    git-svn-id: http://taverna.googlecode.com/svn@723 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/lsidauth1.0/test/org/embl/ebi/escience/lsidauth/VerifyTest.java
+++ trunk/lsidauth1.0/test/org/embl/ebi/escience/lsidauth/VerifyTest.java
@@ -9,7 +9,11 @@ import junit.framework.TestCase;
  */
 public class VerifyTest extends TestCase {
     
-    public void testVerify() {
+    /**
+     * Test that a correct password is recognized and an incorrect
+     * one rejected by the comparePasswords method
+     */
+    public void testMismatchedPasswords() {
 	byte[] sampleKey = "asdkjhsdkjfhsdf".getBytes();
 	LSIDAuth.init(sampleKey);
 	LSIDAuth auth = LSIDAuth.getInstance();
@@ -23,8 +27,32 @@ public class VerifyTest extends TestCase {
 					      "AnotherSecretPassword"));
 	}
 	catch (Exception ex) {
+	    ex.printStackTrace();
 	    fail(ex.getMessage());
 	}
     }
-
+    
+    /**
+     * Test that the correct password is rejected if a different
+     * secret key is used in the initialization of the library
+     */
+    public void testMismatchedSecretKeys() {
+	byte[] sampleKey1 = "TheCorrectKey".getBytes();
+	byte[] sampleKey2 = "TheWrongKey".getBytes();
+	try {
+	    LSIDAuth.init(sampleKey1);
+	    String cryptedPassword = LSIDAuth.getInstance().createPassword("SecretPassword");
+	    assertTrue("Correct password rejected!",
+		       LSIDAuth.getInstance().comparePasswords(Base64.decode(cryptedPassword),
+							       "SecretPassword"));
+	    LSIDAuth.init(sampleKey2);
+	    assertFalse("Password still matches after new secret key!",
+			LSIDAuth.getInstance().comparePasswords(Base64.decode(cryptedPassword),
+								"SecretPassword"));
+	}
+	catch (Exception ex) {
+	    ex.printStackTrace();
+	    fail(ex.getMessage());
+	}
+    }
 }
