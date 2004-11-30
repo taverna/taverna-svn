4004d0c02cfd16f4c4235e534bceb667e3c8534c
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2004-11-30 17:18:17 +0000

    Processor now tracks changes in attributes and sequences
    git-svn-id: http://taverna.googlecode.com/svn@833 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/biomart/BiomartProcessor.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/biomart/BiomartProcessor.java
@@ -19,6 +19,7 @@ public class BiomartProcessor extends Processor {
     private BiomartConfigBean info;
     private String dataSourceName;
     private Query query = null;
+    private QueryListener queryListener;
 
     public BiomartProcessor(ScuflModel model,
 			    String processorName,
@@ -39,6 +40,35 @@ public class BiomartProcessor extends Processor {
 	    pce.initCause(ex);
 	    throw pce;
 	}
+	// Register a query change listener to trap changes to the query object and
+	// fire them off as minor model events
+	queryListener = new QueryAdaptor() {
+		public void attributeAdded(Query sourceQuery, int index, Attribute attribute) {
+		    update();
+		}
+		public void attributeRemoved(Query sourceQuery, int index, Attribute attribute) {
+		    update();
+		}
+		public void sequenceDescriptionChanged(Query sourceQuery, 
+						       SequenceDescription sd1, 
+						       SequenceDescription sd2) {
+		    update();
+		}
+		private void update() {
+		    try {
+			buildPortsFromQuery();
+		    }
+		    catch (Exception ex) {
+			ex.printStackTrace();
+		    }
+		}
+	    };
+	query.addQueryChangeListener(queryListener);
+	
+    }
+    
+    protected void finalize() throws Throwable {
+	query.removeQueryChangeListener(queryListener);
     }
 
     BiomartConfigBean getConfig() {
@@ -61,11 +91,49 @@ public class BiomartProcessor extends Processor {
 	throws PortCreationException,
 	       DuplicatePortNameException {
 	Attribute[] attributes = query.getAttributes();
+	Set attributeNames = new HashSet();
+	// Create new ports corresponding to attributes
 	for (int i = 0; i < attributes.length; i++) {
 	    String fieldName = attributes[i].getField();
-	    Port newPort = new OutputPort(this, fieldName);
-	    newPort.setSyntacticType("l('text/plain')");
-	    addPort(newPort);
+	    attributeNames.add(fieldName);
+	    try {
+		locatePort(fieldName);
+	    }
+	    catch (UnknownPortException upe) {
+		Port newPort = new OutputPort(this, fieldName);
+		newPort.setSyntacticType("l('text/plain')");
+		addPort(newPort);
+	    }
+	}
+	// Create new port for the sequence if defined
+	String sequencePortName = "sequenceexport";
+	if (query.getSequenceDescription() != null) {
+	    attributeNames.add(sequencePortName);
+	    // Try to find the sequence port
+	    OutputPort sequencePort = null;
+	    try {
+		sequencePort = (OutputPort)locatePort(sequencePortName);
+		// Exists, move it to the end of the output port list
+		if (ports.indexOf(sequencePort) < ports.size()-1) {
+		    ports.remove(sequencePort);
+		    ports.add(sequencePort);
+		    fireModelEvent(new MinorScuflModelEvent(this, "Shifted sequence output to end of port list"));
+		}
+	    }
+	    catch (UnknownPortException upe) {
+		Port newPort = new OutputPort(this, sequencePortName);
+		newPort.setSyntacticType("l('text/plain')");
+		addPort(newPort);
+	    }
+	}
+
+	// Remove any ports which don't have attributes with corresponding field names
+	OutputPort[] ports = getOutputPorts();
+	for (int i = 0; i < ports.length; i++) {
+	    Port outputPort = ports[i];
+	    if (attributeNames.contains(outputPort.getName()) == false) {
+		removePort(outputPort);
+	    }
 	}
     }
     
