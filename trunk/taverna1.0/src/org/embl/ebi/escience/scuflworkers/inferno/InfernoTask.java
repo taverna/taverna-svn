e80f3a627b236cfc9e33c5c11caa799ec3057ed7
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-01-20 16:12:05 +0000

    First cut at inferno functionality, not tested!
    git-svn-id: http://taverna.googlecode.com/svn@895 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/inferno/InfernoTask.java
@@ -0,0 +1,138 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scuflworkers.inferno;
+
+import org.embl.ebi.escience.scufl.*;
+import org.embl.ebi.escience.baclava.DataThing;
+import org.embl.ebi.escience.scuflworkers.ProcessorTaskWorker;
+import uk.ac.soton.itinnovation.taverna.enactor.entities.TaskExecutionException;
+import uk.ac.soton.itinnovation.taverna.enactor.entities.ProcessorTask;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+import uk.ac.rdg.resc.jstyx.client.StyxClientSession;
+import uk.ac.rdg.resc.jstyx.client.CStyxFile;
+import uk.ac.rdg.resc.jstyx.client.StyxFileInputStream;
+import uk.ac.rdg.resc.jstyx.client.StyxFileInputStreamReader;
+import uk.ac.rdg.resc.jstyx.client.StyxFileOutputStream;
+import uk.ac.rdg.resc.jstyx.client.StyxFileOutputStreamWriter;
+import uk.ac.rdg.resc.jstyx.StyxException;
+
+import java.util.*;
+
+/**
+ * Invokes a single operation within an Inferno SGS
+ * @author Tom Oinn
+ */
+public class InfernoTask implements ProcessorTaskWorker {
+    
+    private InfernoProcessor processor;
+    
+    public InfernoTask(Processor p) {
+	this.processor = (InfernoProcessor)p;
+    }
+
+    public Map execute(Map inputMap, ProcessorTask parentTask) 
+	throws TaskExecutionException {
+	Map results = new HashMap();
+	try {
+	    // Connect to the Styx session
+	    StyxClientSession session = StyxClientSession.createSession(processor.getHost(), processor.getPort());
+	    session.connect();
+	    
+	    // Create a new instance of the service
+	    CStyxFile cloneFile = new CStyxFile(session, processor.getService()+"/clone");
+	    StyxFileInputStream cloneIn = new StyxFileInputStream(cloneFile);
+	    BufferedReader bufCloneIn = new BufferedReader(new StyxFileInputStreamReader(cloneIn));
+	    String instanceID = bufCloneIn.readLine();
+	    bufCloneIn.close();
+	    
+	    // Flag to indicate whether the input has been defined, attempts
+	    // to set any input (stream, text or binary literals) when this
+	    // is true will throw an exception
+	    boolean definedInput = false;
+	    
+	    if (inputMap.containsKey("refIn")) {
+		ensureFalse(definedInput);
+		// Set a URL to read data to stdin from
+		CStyxFile urlFile = new CStyxFile(session, processor.getService()+"/"+instanceID+"/io/inurl");
+		StyxFileOutputStream urlOut = new StyxFileOutputStream(urlFile);
+		BufferedWriter bufUrlOut = new BufferedWriter(new StyxFileOutputStreamWriter(urlOut));
+		// Get the URL value from the datathing in the input
+		String inputURL = (String)((DataThing)inputMap.get("refIn")).getDataObject();
+		bufUrlOut.write(inputURL);
+		bufUrlOut.flush();
+		bufUrlOut.close();
+		definedInput = true;
+	    }
+	    
+	    if (inputMap.containsKey("stringIn") ||
+		inputMap.containsKey("binaryIn")) {
+		CStyxFile inFile = new CStyxFile(session, processor.getService()+"/"+instanceID+"/io/in");
+		StyxFileOutputStream valueOut = new StyxFileOutputStream(inFile);
+		if (inputMap.containsKey("stringIn")) {
+		    // Send the value of the stringIn input into the /io/in file
+		    ensureFalse(definedInput);
+		    BufferedWriter bufValueOut = new BufferedWriter(new StyxFileOutputStreamWriter(valueOut));
+		    String stringValue = (String)((DataThing)inputMap.get("stringIn")).getDataObject();
+		    bufValueOut.write(stringValue);
+		    bufValueOut.flush();
+		    bufValueOut.close();
+		    definedInput = true;
+		}
+		if (inputMap.containsKey("binaryIn")) {
+		    // Send the byte[] value of the binaryIn input into the /io/in file
+		    ensureFalse(definedInput);
+		    BufferedOutputStream bos = new BufferedOutputStream(valueOut);
+		    byte[] binaryValue = (byte[])((DataThing)inputMap.get("binaryIn")).getDataObject();
+		    bos.write(binaryValue);
+		    bos.flush();
+		    bos.close();
+		    definedInput = true;
+		}
+	    }
+	    
+	    // Start the service
+	    CStyxFile ctlFile = new CStyxFile(session, processor.getService()+"/"+instanceID+"/ctl");
+	    StyxFileOutputStream ctlOut = new StyxFileOutputStream(ctlFile);
+	    BufferedWriter bufCtlOut = new BufferedWriter(new StyxFileOutputStreamWriter(ctlOut));
+	    bufCtlOut.write("start");
+	    bufCtlOut.flush();
+	    bufCtlOut.close();
+	    
+	    // Write the base URL out
+	    String baseURL = "styx://"+processor.getHost()+":"+processor.getPort()+"/"+processor.getService()+"/"+instanceID+"/";
+	    
+	    results.put("baseURL", new DataThing(baseURL));
+	    results.put("refStdOut", new DataThing(baseURL+"io/out"));
+	    results.put("refStdErr", new DataThing(baseURL+"io/err"));
+
+	    return results;
+	}
+	catch (Exception ex) {
+	    if (ex instanceof TaskExecutionException) {
+		throw (TaskExecutionException)ex;
+	    }
+	    TaskExecutionException tee = new TaskExecutionException("Problem invoking inferno SGS : "+ex.getMessage());
+	    tee.initCause(ex);
+	    throw tee;
+	}
+    }
+
+    static void ensureFalse(boolean flag) 
+	throws TaskExecutionException {
+	if (flag == true) {
+	    throw new TaskExecutionException("Input may consist of at most one of url, string value or binary value");
+	}
+    }
+
+}
