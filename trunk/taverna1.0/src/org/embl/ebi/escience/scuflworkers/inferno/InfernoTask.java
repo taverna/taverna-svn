3eeca85948e0ee4cb97beb08ffe9a9590ae428b3
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-01-20 16:52:55 +0000

    Added proper invocation handling for results, still not tested though!
    git-svn-id: http://taverna.googlecode.com/svn@897 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/inferno/InfernoTask.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/inferno/InfernoTask.java
@@ -8,8 +8,8 @@ package org.embl.ebi.escience.scuflworkers.inferno;
 import org.embl.ebi.escience.scufl.*;
 import org.embl.ebi.escience.baclava.DataThing;
 import org.embl.ebi.escience.scuflworkers.ProcessorTaskWorker;
-import uk.ac.soton.itinnovation.taverna.enactor.entities.TaskExecutionException;
-import uk.ac.soton.itinnovation.taverna.enactor.entities.ProcessorTask;
+import uk.ac.soton.itinnovation.taverna.enactor.entities.*;
+import uk.ac.soton.itinnovation.freefluo.core.task.*;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -17,6 +17,7 @@ import java.io.BufferedReader;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import uk.ac.rdg.resc.jstyx.client.StyxClientSession;
@@ -45,6 +46,37 @@ public class InfernoTask implements ProcessorTaskWorker {
 	throws TaskExecutionException {
 	Map results = new HashMap();
 	try {
+
+	    // Detect whether the processor outputs for literal values
+	    // are bound in the current workflow
+	    boolean returnString = false;
+	    boolean returnBytes = false;
+	    boolean returnRef = false;
+	    for (Iterator i = parentTask.getChildren().iterator(); i.hasNext();) {
+		Task task = (Task)i.next();
+		if (task instanceof PortTask) {
+		    PortTask outputPortTask = (PortTask)task;
+		    String portName = outputPortTask.getScuflPort().getName();
+		    if (portName.equals("stringOut")) {
+			returnString = true;
+		    }
+		    if (portName.equals("binaryOut")) {
+			returnBytes = true;
+		    }
+		    if (portName.equals("refStdOut") ||
+			portName.equals("refStdErr")) {
+			returnRef = true;
+		    }
+		}
+	    }
+
+	    if (returnRef & (returnBytes | returnString)) {
+		throw new TaskExecutionException("Cannot return both references and literal values, invalid workflow.");
+	    }
+	    if (returnBytes & returnString) {
+		throw new TaskExecutionException("Cannot return both string and byte[] values, must only bind to one.");
+	    }
+
 	    // Connect to the Styx session
 	    StyxClientSession session = StyxClientSession.createSession(processor.getHost(), processor.getPort());
 	    session.connect();
@@ -110,12 +142,36 @@ public class InfernoTask implements ProcessorTaskWorker {
 	    bufCtlOut.close();
 	    
 	    // Write the base URL out
-	    String baseURL = "styx://"+processor.getHost()+":"+processor.getPort()+"/"+processor.getService()+"/"+instanceID+"/";
-	    
+	    String baseURL = "styx://"+processor.getHost()+":"+processor.getPort()+"/"+
+		processor.getService()+"/"+instanceID+"/";
 	    results.put("baseURL", new DataThing(baseURL));
-	    results.put("refStdOut", new DataThing(baseURL+"io/out"));
-	    results.put("refStdErr", new DataThing(baseURL+"io/err"));
-
+	    if (returnRef) {
+		results.put("refStdOut", new DataThing(baseURL+"io/out"));
+		results.put("refStdErr", new DataThing(baseURL+"io/err"));
+	    }
+	    else if (returnString | returnBytes) {
+		// Return actual value
+		CStyxFile resultFile = new CStyxFile(session, processor.getService()+"/"+instanceID+"/io/out");
+		StyxFileInputStream sfis = new StyxFileInputStream(resultFile);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int b = 0;
+		while (b >= 0) {
+                    b = sfis.read(); // TODO: read more than 1 byte at a time
+                    if (b >= 0) {
+                        baos.write(b);
+                    }
+                }
+                sfis.close();
+		baos.flush();
+                baos.close();
+		if (returnBytes) {
+		    results.put("binaryOut", new DataThing(baos.toByteArray()));
+		}
+		else {
+		    results.put("stringOut", new DataThing(baos.toString()));		    
+		}
+	    }
+	    
 	    return results;
 	}
 	catch (Exception ex) {
