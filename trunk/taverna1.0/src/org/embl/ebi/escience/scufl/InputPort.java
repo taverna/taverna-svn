8fce10f0612ec16b4ff023ad37704d1300118520
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-01-27 16:32:13 +0000

    Added optional and default value fields
    git-svn-id: http://taverna.googlecode.com/svn@912 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scufl/InputPort.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scufl/InputPort.java
@@ -6,6 +6,7 @@
 package org.embl.ebi.escience.scufl;
 import java.awt.datatransfer.*;
 import java.io.*;
+import org.embl.ebi.escience.baclava.DataThing;
 
 /**
  * A port that consumes data on behalf of a processor
@@ -16,7 +17,46 @@ public class InputPort extends Port implements Serializable, Transferable {
     final public static DataFlavor FLAVOR =
 	new DataFlavor(InputPort.class, "Input Port");
     static DataFlavor[] flavors = { FLAVOR };
+    private DataThing defaultValue;
+    private boolean isOptional = false;
+
+    /**
+     * Does this input port have a default value?
+     */
+    public boolean hasDefaultValue() {
+	return (this.defaultValue != null);
+    }
     
+    /**
+     * Get the default value for this port, or
+     * null if there is no default
+     */
+    public DataThing getDefaultValue() {
+	return this.defaultValue;
+    }
+    
+    /**
+     * Set the default value for this port
+     */
+    public void setDefaultValue(DataThing defaultValue) {
+	this.defaultValue = defaultValue;
+    }
+
+    /**
+     * Is this input optional?
+     */
+    public boolean isOptional() {
+	return this.isOptional;
+    }
+
+    /**
+     * Set whether the input is optional (default value if
+     * never set is that the input is mandatory)
+     */
+    public void setOptional(boolean optional) {
+	this.isOptional = optional;
+    }
+
     /**
      * Implements transferable interface
      */
