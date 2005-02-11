0ea8f540c4d9e5afee59b6686ecd04785d94aade
phidias <phidias@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-02-11 14:51:02 +0000

    initial commit of test stubs.
    git-svn-id: http://taverna.googlecode.com/svn@962 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/ncbi/LocusLinkWorkerTest.java
@@ -0,0 +1,18 @@
+package net.sourceforge.taverna.scuflworkers.ncbi;
+
+import junit.framework.TestCase;
+
+/**
+ * This class
+ * 
+ * Last edited by $Author: phidias $
+ * 
+ * @author Mark
+ * @version $Revision: 1.1 $
+ */
+public class LocusLinkWorkerTest extends TestCase {
+
+    public void testExecute() {
+    }
+
+}
---
+++ trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/ncbi/NucleotideGBSeqWorkerTest.java
@@ -0,0 +1,18 @@
+package net.sourceforge.taverna.scuflworkers.ncbi;
+
+import junit.framework.TestCase;
+
+/**
+ * This class
+ * 
+ * Last edited by $Author: phidias $
+ * 
+ * @author Mark
+ * @version $Revision: 1.1 $
+ */
+public class NucleotideGBSeqWorkerTest extends TestCase {
+
+    public void testExecute() {
+    }
+
+}
