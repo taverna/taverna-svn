f08936b813fdc0b86982d5e874e49b81b9750162
phidias <phidias@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-03-01 01:13:46 +0000

    *** empty log message ***
    git-svn-id: http://taverna.googlecode.com/svn@998 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/ncbi/EntrezProteinWorkerTest.java
+++ trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/ncbi/EntrezProteinWorkerTest.java
@@ -8,34 +8,34 @@ import net.sourceforge.taverna.baclava.DataThingAdapter;
 import org.embl.ebi.escience.scuflworkers.java.LocalWorker;
 import org.w3c.dom.Element;
 
-import junit.framework.TestCase;
-
 /**
  * This class
  * 
  * Last edited by $Author: phidias $
  * 
  * @author Mark
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.1 $
  */
-public class LocusLinkWorkerTest extends AbstractXmlWorkerTest {
+public class EntrezProteinWorkerTest extends AbstractXmlWorkerTest {
 
     public void testExecute() throws Exception{
-        LocalWorker worker = new LocusLinkWorker();
+        LocalWorker worker = new EntrezProteinWorker();
         Map inputMap = new HashMap();
         DataThingAdapter inAdapter = new DataThingAdapter(inputMap);
-        inAdapter.putString("term","brca2");
+        inAdapter.putString("term","NP_000050");
         
         Map outputMap = worker.execute(inputMap);
         DataThingAdapter outAdapter = new DataThingAdapter(outputMap);
         
-        String results = outAdapter.getString("outputText");
+        String results = outAdapter.getString("resultsXml");
         assertNotNull("The results were null", results);
         System.out.println(results);
         
-        this.writeFile("test_locus_link.xml", results);
+        this.writeFile("test_entrez_prot.xml",results);
         Element root = this.parseXml(results);
         this.testXmlNotEmpty(root);
+        
+
     }
 
 }
