fea9724b148e3c8eef6d7581d55e2bafdf1a3e38
ferris <ferris@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-04-18 13:30:41 +0000

    Made very slightly less simple. Username and password member variables added
    git-svn-id: http://taverna.googlecode.com/svn@1045 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scufl/enactor/implementation/SimpleUserContext.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scufl/enactor/implementation/SimpleUserContext.java
@@ -19,12 +19,21 @@ import org.embl.ebi.escience.scufl.enactor.*;
 public class SimpleUserContext implements UserContext {
     
     private String personLSID, organizationLSID, experimentDesignLSID;
+    private String username = "";
+    private String password = "";
     private static final String PERSON_KEY = "personLSID";
     private static final String ORG_KEY = "organizationLSID";
     private static final String EXP_KEY = "experimentLSID";
     private static final String USERNAME_KEY = "username";
     private static final String PASSWORD_KEY = "password";
     
+    public SimpleUserContext(String personLSID, String organisationLSID,
+                             String experimentDesignLSID, String username,
+                             String password) {
+        this(personLSID, organisationLSID, experimentDesignLSID);
+        this.username = username;
+        this.password = password;
+    }
 
     public SimpleUserContext(String personLSID,
 			     String organizationLSID,
@@ -38,6 +47,8 @@ public class SimpleUserContext implements UserContext {
         this.personLSID = flowContext.get(PERSON_KEY);
         this.organizationLSID = flowContext.get(ORG_KEY);
         this.experimentDesignLSID = flowContext.get(EXP_KEY);
+        this.username = flowContext.get(USERNAME_KEY);
+        this.password = flowContext.get(PASSWORD_KEY);
     }
     
     public String getPersonLSID() {
@@ -53,7 +64,7 @@ public class SimpleUserContext implements UserContext {
     }
 
     public String[] getUserNameAndPassword(String resourceExpression) {
-	return new String[]{"",""};
+	return new String[]{username, password};
     }
     
     public FlowContext toFlowContext() {
@@ -61,6 +72,8 @@ public class SimpleUserContext implements UserContext {
         flowContext.put(PERSON_KEY, personLSID);
         flowContext.put(ORG_KEY, organizationLSID);
         flowContext.put(EXP_KEY, experimentDesignLSID);
+        flowContext.put(USERNAME_KEY, username);
+        flowContext.put(PASSWORD_KEY, password);
         return flowContext;
     }
 }
