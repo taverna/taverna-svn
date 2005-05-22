c20e62f45fe70ae32dd2af9459f27818d4e650df
phidias <phidias@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-05-22 01:33:54 +0000

    *** empty log message ***
    git-svn-id: http://taverna.googlecode.com/svn@1102 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/contrib/src/org/apache/bsf/ExtendedBSFManager.java
+++ trunk/taverna1.0/contrib/src/org/apache/bsf/ExtendedBSFManager.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
  * Last edited by $Author: phidias $
  * 
  * @author Mark
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class ExtendedBSFManager extends BSFManager {
 	static Properties props = new Properties();
@@ -22,14 +22,16 @@ public class ExtendedBSFManager extends BSFManager {
 	static String[] langArray = null;
 	static Map engineMap = new HashMap();
 	static Map abbrevMap = new HashMap();
+	static Map engineImplMap = new HashMap();
 	static {
 
 		try {
-			InputStream is = ExtendedBSFManager.class.getResourceAsStream("BSFManager.properties");
+			InputStream is = ExtendedBSFManager.class
+					.getResourceAsStream("BSFManager.properties");
 			props.load(is);
-			//String langs = props.getProperty("languages");
-			//langArray = langs.split(",");
-			
+			// String langs = props.getProperty("languages");
+			// langArray = langs.split(",");
+
 			Set keys = props.keySet();
 			Iterator it = keys.iterator();
 			String currKey = null;
@@ -37,20 +39,34 @@ public class ExtendedBSFManager extends BSFManager {
 			String language = null;
 			String abbrev = null;
 			String[] entry = null;
-			while(it.hasNext()){
-				currKey = (String)it.next();
-				if(currKey.startsWith("bsf.engine")){
-					engineName = currKey.substring(currKey.indexOf("bsf.engine."));
-					entry = props.getProperty(currKey).split(",");
-					language =entry[0];
-					abbrev = entry[1];
-					engineMap.put(language,engineName);
-					registerScriptingEngine(engineName, language,entry);
-					
-					
+			try {
+				while (it.hasNext()) {
+					currKey = (String) it.next();
+					if (currKey.startsWith("bsf.engine")) {
+						engineName = currKey.substring("bsf.engine.".length());
+						entry = props.getProperty(currKey).split(",");
+						language = entry[0];
+						abbrev = entry[1];
+						engineMap.put(language, engineName);
+
+						registerScriptingEngine(engineName, language, entry);
+						Class engineClass;
+
+						engineClass = Class.forName(engineName);
+
+						BSFEngine engineImpl = (BSFEngine) engineClass
+								.newInstance();
+						engineImplMap.put(language, engineImpl);
+
+					}
 				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
 			}
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -69,11 +85,15 @@ public class ExtendedBSFManager extends BSFManager {
 	 * @return
 	 */
 	public Vector getProcessorNameList() {
-		
+
 		Vector procList = new Vector();
 		procList.addAll(engineMap.keySet());
 
 		return procList;
+	}
+	
+	public static BSFEngine getEngine(String language){
+		return (BSFEngine)engineImplMap.get(language);
 	}
 
 }
