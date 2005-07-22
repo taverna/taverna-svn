8e5edcfc494b0b09a41c614136e8a875dc7f84c6
ijuma82 <ijuma82@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-07-22 15:38:03 +0000

    Initial version.
    git-svn-id: http://taverna.googlecode.com/svn@1269 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/ocula/src/net/sf/taverna/ocula/example/InputHandler.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2005 University of Manchester
+ *
+ *  This file is part of Taverna.  Further information, and the
+ *  latest version, can be found at http://taverna.sf.net
+ * 
+ *  Taverna is in turn part of the myGrid project, more details
+ *  can be found at http://www.mygrid.org.uk
+ *
+ *  Taverna is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  Taverna is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Taverna; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sf.taverna.ocula.example;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Simple class used by the InputExample.
+ * 
+ * @author Ismael Juma (ismael@juma.me.uk)
+ *
+ */
+public class InputHandler {
+
+    public void submit(Map nameText) {
+	for (Iterator it = nameText.keySet().iterator(); it.hasNext(); ) {
+	    String key = (String) it.next();
+	    String value = (String) nameText.get(key);
+	    System.out.println("TextField '" + key + "' has value '" + value + "'");
+	}
+    }
+}
