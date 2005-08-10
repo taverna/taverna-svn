f83ff014b5792748a6599a2fe2c2bb2a631bcc95
ijuma82 <ijuma82@bf327186-88b3-11dd-a302-d386e5130c1c>
2005-08-10 15:23:08 +0000

    Refactor a little and add more tests.
    git-svn-id: http://taverna.googlecode.com/svn@1312 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/ocula/test/net/sf/taverna/ocula/InputTest.java
+++ trunk/ocula/test/net/sf/taverna/ocula/InputTest.java
@@ -8,16 +8,44 @@ import javax.swing.JFrame;
 import junit.framework.TestCase;
 
 public class InputTest extends TestCase {
-    public void testInputExample() throws Exception{
+    
+    private void setUp(String exampleURLString) throws Exception	{
 	JFrame frame = new JFrame();
 	Ocula o = new Ocula();
 	frame.getContentPane().add(o);
 	URL exampleURL = Thread.currentThread().getContextClassLoader().
-	    getResource("net/sf/taverna/ocula/example/InputExample.xml");
+	    getResource(exampleURLString);
 	frame.setSize(new Dimension(400,300));
 	frame.setVisible(true);
 	o.load(exampleURL);
 	Thread.sleep(3000);
     }
+    public void testInputExample() throws Exception{
+	setUp("net/sf/taverna/ocula/example/InputExample.xml");
+    }
     
+    public static void main(String[] args) throws Exception {
+	//new InputTest().testMalformedScript();
+	new InputTest().testUnsupportedElement();
+    }
+    
+    public void testMalformedScript() throws Exception{
+	setUp("net/sf/taverna/ocula/MalformedInputScript.xml");
+    }
+    
+    public void testUnsupportedElement() throws Exception	{
+	setUp("net/sf/taverna/ocula/UnsupportedInputElement.xml");
+    }
+    
+    public void testUnsupportedSeparatorAttribute() throws Exception	{
+	setUp("net/sf/taverna/ocula/UnsupportedSeparatorAttribute.xml");
+    }
+    
+    public void testUnsupportedTextAttribute() throws Exception	{
+	setUp("net/sf/taverna/ocula/UnsupportedTextAttribute.xml");
+    }
+    
+    public void testUnsupportedAttribute() throws Exception	{
+	setUp("net/sf/taverna/ocula/UnsupportedInputAttribute.xml");
+    }
 }
