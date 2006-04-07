f93877ef409c4a045de52f63fbf84c9899352625
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-04-07 10:35:09 +0000

    Common function marked for generalization, and made static
    git-svn-id: http://taverna.googlecode.com/svn@1581 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflui/ScuflModelExplorer.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflui/ScuflModelExplorer.java
@@ -15,7 +15,7 @@ import org.embl.ebi.escience.scufl.*;
 import org.embl.ebi.escience.scufl.view.*;
 
 
-// Utility Imports
+//Utility Imports
 import java.util.Enumeration;
 
 import org.embl.ebi.escience.scuflui.ScuflModelExplorerPopupHandler;
@@ -32,8 +32,8 @@ import java.lang.String;
  * @author Tom Oinn
  */
 public class ScuflModelExplorer extends JTree 
-    implements ScuflModelEventListener,
-	       ScuflUIComponent {
+implements ScuflModelEventListener,
+ScuflUIComponent {
     
     // The ScuflModel that this is a view / controller over
     ScuflModel model = null;
@@ -46,82 +46,84 @@ public class ScuflModelExplorer extends JTree
      * method to actually show data in this component.
      */
     public ScuflModelExplorer() {
-	super();
-	setLargeModel(true);
-	setRowHeight(18);
-	setModel(treeModel);
-	// Only allow single selection (not really important but...)
-	this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-	// Attach the popup menu generator to the tree
-	this.addMouseListener(new ScuflModelExplorerPopupHandler(this));
-	// Show lines in the tree diagram
-	this.putClientProperty("JTree.lineStyle","Angled");
-	ScuflModelExplorerRenderer renderer = new ScuflModelExplorerRenderer();
-	this.setCellRenderer(renderer);
-	//this.addMouseMotionListener(new ScuflModelExplorerDragHandler(this));
-	//this.setDragEnabled(true);
+        super();
+        setLargeModel(true);
+        setRowHeight(18);
+        setModel(treeModel);
+        // Only allow single selection (not really important but...)
+        this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        // Attach the popup menu generator to the tree
+        this.addMouseListener(new ScuflModelExplorerPopupHandler(this));
+        // Show lines in the tree diagram
+        this.putClientProperty("JTree.lineStyle","Angled");
+        ScuflModelExplorerRenderer renderer = new ScuflModelExplorerRenderer();
+        this.setCellRenderer(renderer);
+        //this.addMouseMotionListener(new ScuflModelExplorerDragHandler(this));
+        //this.setDragEnabled(true);
     }
     
     public javax.swing.ImageIcon getIcon() {
-	return ScuflIcons.windowExplorer;
+        return ScuflIcons.windowExplorer;
     }
-
+    
     /**
      * Set the default expansion state, with all processors, data
      * constraints and workflow source and sink ports show, but
      * nothing else.
      */
     public void setDefaultExpansionState() {
-	expandAll(this, new TreePath(this.treeModel.getRoot()), true);
+        expandAll(this, new TreePath(this.treeModel.getRoot()), true);
     }
-    private void expandAll(JTree tree, TreePath parent, boolean expand) {
+    
+    // FIXME Move to general class, and join with ScavengerTreePanel.expandAll
+    private static void expandAll(JTree tree, TreePath parent, boolean expand) {
         // Traverse children
-	// Ignores nodes who's userObject is a Processor type to
-	// avoid overloading the UI with nodes at startup.
+        // Ignores nodes who's userObject is a Processor type to
+        // avoid overloading the UI with nodes at startup.
         TreeNode node = (TreeNode)parent.getLastPathComponent();
         if (node.getChildCount() >= 0 && (((DefaultMutableTreeNode)node).getUserObject() instanceof Processor == false)) {
             for (Enumeration e=node.children(); e.hasMoreElements(); ) {
                 TreeNode n = (TreeNode)e.nextElement();
                 TreePath path = parent.pathByAddingChild(n);
-		if (((DefaultMutableTreeNode)n).getUserObject() instanceof Processor) {
-		}
-		else {
-		    expandAll(tree, path, expand);
-		}
-	    }
+                if (((DefaultMutableTreeNode)n).getUserObject() instanceof Processor) {
+                }
+                else {
+                    expandAll(tree, path, expand);
+                }
+            }
         }
-	// Expansion or collapse must be done bottom-up
+        // Expansion or collapse must be done bottom-up
         if (expand) {
             tree.expandPath(parent);
         } else {
             tree.collapsePath(parent);
         }
     }
-
-
+    
+    
     /**
      * Bind this view onto a ScuflModel instance, this
      * registers the view to receive events and thus keep
      * up to date.
      */
     public void attachToModel(ScuflModel theModel) {
-	this.model = theModel;
-	treeModel.attachToModel(theModel);
-	theModel.addListener(this.treeModel);
-	theModel.addListener(this);
-	
-	setDefaultExpansionState();
+        this.model = theModel;
+        treeModel.attachToModel(theModel);
+        theModel.addListener(this.treeModel);
+        theModel.addListener(this);
+        
+        setDefaultExpansionState();
     }
-
+    
     /**
      * Unbind from the current model, does nothing if 
      * we're not bound to a model.
      */
     public void detachFromModel() {
-	if (this.model != null) {
-	    this.model.removeListener(this);
-	    this.model.removeListener(this.treeModel);
-	}
+        if (this.model != null) {
+            this.model.removeListener(this);
+            this.model.removeListener(this.treeModel);
+        }
     }
     
     /**
@@ -129,13 +131,13 @@ public class ScuflModelExplorer extends JTree
      * to date with any changes in state.
      */    
     public void receiveModelEvent(ScuflModelEvent event) {
-	setDefaultExpansionState();
+        setDefaultExpansionState();
     }
     
     /**
      * Return a preferred name for windows containing this component
      */
     public String getName() {
-	return "Scufl Model Explorer";
+        return "Scufl Model Explorer";
     }
 }
