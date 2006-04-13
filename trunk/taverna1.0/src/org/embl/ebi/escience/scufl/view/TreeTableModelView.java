517aa70f98f566167cb565a941e8ed0d9b52da61
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-04-13 13:50:23 +0000

    General code cleanup
    git-svn-id: http://taverna.googlecode.com/svn@1600 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scufl/view/TreeTableModelView.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scufl/view/TreeTableModelView.java
@@ -5,11 +5,14 @@
  */
 package org.embl.ebi.escience.scufl.view;
 
-import org.embl.ebi.escience.scufl.*;
-import javax.swing.*;
-import javax.swing.tree.*;
-import java.awt.datatransfer.*;
-import org.embl.ebi.escience.treetable.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.embl.ebi.escience.scufl.AlternateProcessor;
+import org.embl.ebi.escience.scufl.InputPort;
+import org.embl.ebi.escience.scufl.Port;
+import org.embl.ebi.escience.scufl.Processor;
+import org.embl.ebi.escience.scufl.ScuflModelEvent;
+import org.embl.ebi.escience.treetable.TreeTableModel;
 
 /**
  * An extension of the TreeModelView to implement the TreeTable interfaces.
@@ -36,44 +39,38 @@ public class TreeTableModelView extends TreeModelView implements TreeTableModel 
 	public Class getColumnClass(int column) {
 		return columnClasses[column];
 	}
-
-	/**
-	 * The interesting bit
-	 */
+	 
 	public Object getValueAt(Object nodeObject, int column) {
 		if (nodeObject == null) {
 			return null;
 		}
 		DefaultMutableTreeNode node = (DefaultMutableTreeNode) nodeObject;
+		if (column == 0) {
+			return node.getUserObject().toString();
+		}
+
+		// OK,  handle the other columns.. only valid for Processor nodes
 		Processor p = null;
 		if (node.getUserObject() instanceof Processor) {
 			p = (Processor) node.getUserObject();
 		} else if (node.getUserObject() instanceof AlternateProcessor) {
 			p = ((AlternateProcessor) node.getUserObject()).getProcessor();
 		}
+		if (p == null) {
+			return null;
+		}
+
 		switch (column) {
-		case 0:
-			return node.getUserObject().toString();
 		case 1:
-			if (p != null) {
-				return new Integer(p.getRetries());
-			}
+			return new Integer(p.getRetries());
 		case 2:
-			if (p != null) {
-				return new Integer(p.getRetryDelay());
-			}
+			return new Integer(p.getRetryDelay());
 		case 3:
-			if (p != null) {
-				return new Double(p.getBackoff());
-			}
-		case 5:
-			if (p != null) {
-				return new Boolean(p.getCritical());
-			}
+			return new Double(p.getBackoff());
 		case 4:
-			if (p != null) {
-				return new Integer(p.getWorkers());
-			}
+			return new Integer(p.getWorkers());
+		case 5:
+			return new Boolean(p.getCritical());
 		}
 		return null;
 	}
@@ -83,12 +80,6 @@ public class TreeTableModelView extends TreeModelView implements TreeTableModel 
 		if (node == null) {
 			return false;
 		}
-		Processor p = null;
-		if (node.getUserObject() instanceof Processor) {
-			p = (Processor) node.getUserObject();
-		} else if (node.getUserObject() instanceof AlternateProcessor) {
-			p = ((AlternateProcessor) node.getUserObject()).getProcessor();
-		}
 		// Always allow 'edits' on column 0, passes mouse events
 		// through to be handled by the tree
 		if (column == 0) {
@@ -97,80 +88,81 @@ public class TreeTableModelView extends TreeModelView implements TreeTableModel 
 			if (node.getUserObject() instanceof Processor) {
 				return true;
 			} else if (node.getUserObject() instanceof Port) {
-				// Check whether this is a top level workflow source or
-				// sink port
+
 				Port port = (Port) node.getUserObject();
-				if (port.isSource() || port.isSink()) {
+				if (port.isNameEditable()) {
+					// It will be possible to edit the actual name of the port
 					return true;
+				} else if (node.getUserObject() instanceof InputPort) {
+					// Allow setting of default value
+					// FIXME: Should not edit default values in this nasty way
+					InputPort ip = (InputPort) node.getUserObject();
+					// FIXME: (Why would getModel return null here?)
+					return (ip.getProcessor().getModel() != null);
 				}
 			}
-			if (node.getUserObject() instanceof InputPort) {
-				InputPort ip = (InputPort) node.getUserObject();
-				if (ip.getProcessor().getModel() != null) {
-					return true;
-				} else {
-					return false;
-				}
-			} else {
-				return false;
-			}
-		}
-		if (p == null) {
 			return false;
 		}
-		return true;
-	}
-
-	public void setValueAt(Object value, Object nodeObject, int column) {
-		// System.out.println("Setting value at column "+column+" to
-		// "+value.toString());
-		DefaultMutableTreeNode node = (DefaultMutableTreeNode) nodeObject;
+		// OK to edit if we have some processor
 		Processor p = null;
 		if (node.getUserObject() instanceof Processor) {
 			p = (Processor) node.getUserObject();
 		} else if (node.getUserObject() instanceof AlternateProcessor) {
 			p = ((AlternateProcessor) node.getUserObject()).getProcessor();
 		}
-		if (p != null
-				|| (column == 0 && node.getUserObject() instanceof InputPort && ((InputPort) node
-						.getUserObject()).isSink() == false)) {
-			switch (column) {
-			case 0:
-				if (p != null) {
-					p.setName((String) value);
-				} else if (node.getUserObject() instanceof InputPort) {
-					InputPort ip = (InputPort) node.getUserObject();
-					ip.setDefaultValue((String) value);
-				}
-				return;
-			case 1:
-				Integer retries = (Integer) value;
-				p.setRetries(retries.intValue());
-				return;
-			case 2:
-				Integer retryDelay = (Integer) value;
-				p.setRetryDelay(retryDelay.intValue());
-				return;
-			case 3:
-				Double backoff = (Double) value;
-				p.setBackoff(backoff.doubleValue());
-				return;
-			case 5:
-				p.setCritical(((Boolean) value).booleanValue());
-				return;
-			case 4:
-				Integer threads = (Integer) value;
-				p.setWorkers(threads.intValue());
-			}
+		return (p != null);
+	}
 
-		} else if (node.getUserObject() instanceof Port) {
-			Port thePort = (Port) node.getUserObject();
-			Processor theProcessor = thePort.getProcessor();
-			ScuflModel model = theProcessor.getModel();
-			// Check that the port is a workflow input or output
-			if (thePort.isSource() || thePort.isSink()) {
-				thePort.setName((String) value);
+	public void setValueAt(Object value, Object nodeObject, int column) {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) nodeObject;
+		// Is it a Port?
+		if (column == 0 && node.getUserObject() instanceof Port) {
+			Port port = (Port) node.getUserObject();
+			// Typical sinks and workflow inputs
+			if (port.isNameEditable()) {
+				port.setName((String) value);
+			} else if (node.getUserObject() instanceof InputPort) {
+				// FIXME: This is a confusing way to set default values
+				((InputPort) port).setDefaultValue((String) value);
 			}
+			return;
+		}
+
+		// OK, then it must be a Processor
+		Processor p = null;
+		if (node.getUserObject() instanceof Processor) {
+			p = (Processor) node.getUserObject();
+		} else if (node.getUserObject() instanceof AlternateProcessor) {
+			p = ((AlternateProcessor) node.getUserObject()).getProcessor();
+		}
+		if (p == null) {
+			// No? Then we have no clue what to do but exit
+			return;
+		}
+
+		switch (column) {
+		case 0:
+			p.setName((String) value);
+			break;
+		case 1:
+			Integer retries = (Integer) value;
+			p.setRetries(retries.intValue());
+			break;
+		case 2:
+			Integer retryDelay = (Integer) value;
+			p.setRetryDelay(retryDelay.intValue());
+			break;
+		case 3:
+			Double backoff = (Double) value;
+			p.setBackoff(backoff.doubleValue());
+			break;
+		case 4:
+			Integer threads = (Integer) value;
+			p.setWorkers(threads.intValue());
+			break;
+		case 5:
+			p.setCritical(((Boolean) value).booleanValue());
+			break;
 		}
 	}
 
