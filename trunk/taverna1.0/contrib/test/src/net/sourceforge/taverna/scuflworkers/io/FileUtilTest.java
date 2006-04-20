341e984341b1268766e459423a113cb2466994a2
phidias <phidias@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-04-20 00:36:25 +0000

    Fixed class name change.
    git-svn-id: http://taverna.googlecode.com/svn@1607 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/io/FileUtilTest.java
+++ trunk/taverna1.0/contrib/test/src/net/sourceforge/taverna/scuflworkers/io/FileUtilTest.java
@@ -3,7 +3,7 @@
  */
 package net.sourceforge.taverna.scuflworkers.io;
 
-import net.sourceforge.taverna.io.FileUtil;
+import net.sourceforge.taverna.io.FileNameUtil;
 import junit.framework.TestCase;
 
 /**
@@ -13,22 +13,22 @@ import junit.framework.TestCase;
 public class FileUtilTest extends TestCase {
 
 	public void testGetExtension() {
-		String result = FileUtil.getExtension("test.xml");
+		String result = FileNameUtil.getExtension("test.xml");
 		assertTrue("The extension was wrong: " + result, result.equals("xml"));
 	}
 
 	public void testGetExtensionFromPath() {
-		String result = FileUtil.getExtension("C:\\whatever\\test.xml");
+		String result = FileNameUtil.getExtension("C:\\whatever\\test.xml");
 		assertTrue("The extension was wrong: " + result, result.equals("xml"));
 	}
 
 	public void testReplaceExtension() {
-		String result = FileUtil.replaceExtension("test.xml","html");
+		String result = FileNameUtil.replaceExtension("test.xml","html");
 		assertTrue("The extension was not replaced properly: " + result,result.equals("test.html"));
 	}
 
 	public void testReplacePathExtension() {
-		String result = FileUtil.replaceExtension("something.xml","html");
+		String result = FileNameUtil.replaceExtension("something.xml","html");
 		assertTrue("The result was wrong: " + result,result.equals("something.html"));
 	}
 
