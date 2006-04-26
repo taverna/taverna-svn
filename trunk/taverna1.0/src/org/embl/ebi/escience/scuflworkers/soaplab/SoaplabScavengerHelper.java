6e8d7a2c201235c7414eed3c671ffc1b7bb5965b
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-04-26 12:43:46 +0000

    Reformatted code style
    git-svn-id: http://taverna.googlecode.com/svn@1614 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabScavengerHelper.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabScavengerHelper.java
@@ -15,46 +15,43 @@ import org.embl.ebi.escience.scuflworkers.ScavengerHelper;
 import org.embl.ebi.escience.scuflworkers.soaplab.SoaplabScavenger;
 import java.lang.String;
 
-
-
 /**
  * Helper for handling Soaplab scavengers.
+ * 
  * @author Tom Oinn
  */
 public class SoaplabScavengerHelper implements ScavengerHelper {
 
-    public String getScavengerDescription() {
-	return "Add new Soaplab scavenger...";
-    }
+	public String getScavengerDescription() {
+		return "Add new Soaplab scavenger...";
+	}
 
-    public ActionListener getListener(ScavengerTree theScavenger) {
-	final ScavengerTree s = theScavenger;
-	return new ActionListener() {
-		public void actionPerformed(ActionEvent ae) {
-		    final String baseURL = (String)JOptionPane.showInputDialog(null,
-									       "Base location for your soaplab installation?",
-									       "Soaplab location",
-									       JOptionPane.QUESTION_MESSAGE,
-									       null,
-									       null,
-									       "http://www.ebi.ac.uk/soaplab/services/");
-		    if (baseURL!=null) {
-			new Thread() {
-			    public void run() {
-				try {
-				    s.addScavenger(new SoaplabScavenger(baseURL));					
+	public ActionListener getListener(ScavengerTree theScavenger) {
+		final ScavengerTree s = theScavenger;
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				final String baseURL = (String) JOptionPane.showInputDialog(
+						null, "Base location for your soaplab installation?",
+						"Soaplab location", JOptionPane.QUESTION_MESSAGE, null,
+						null, "http://www.ebi.ac.uk/soaplab/services/");
+				if (baseURL != null) {
+					new Thread() {
+						public void run() {
+							try {
+								s.addScavenger(new SoaplabScavenger(baseURL));
+							} catch (ScavengerCreationException sce) {
+								JOptionPane
+										.showMessageDialog(null,
+												"Unable to create scavenger!\n"
+														+ sce.getMessage(),
+												"Exception!",
+												JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					}.start();
 				}
-				catch (ScavengerCreationException sce) {
-				    JOptionPane.showMessageDialog(null,
-								  "Unable to create scavenger!\n"+sce.getMessage(),
-								  "Exception!",
-								  JOptionPane.ERROR_MESSAGE);
-				}
-			    }
-			}.start();
-		    }
-		}
-	    };
-    }
-    
+			}
+		};
+	}
+
 }
--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabXMLHandler.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabXMLHandler.java
@@ -18,64 +18,67 @@ import java.net.URL;
 import org.embl.ebi.escience.scuflworkers.soaplab.SoaplabProcessor;
 import java.lang.String;
 
-
-
 /**
  * Handles XML store and load for the soaplab processor
+ * 
  * @author Tom Oinn
  */
 public class SoaplabXMLHandler implements XMLHandler {
 
-    public Element elementForProcessor(Processor p) {
-	SoaplabProcessor slp = (SoaplabProcessor)p;
-	Element spec = new Element("soaplabwsdl",XScufl.XScuflNS);
-	String endpointString = slp.getEndpoint().toString();
-	String[] split = endpointString.split("::");
-	if (split.length == 2) {
-	    endpointString = split[0]+"."+split[1];
+	public Element elementForProcessor(Processor p) {
+		SoaplabProcessor slp = (SoaplabProcessor) p;
+		Element spec = new Element("soaplabwsdl", XScufl.XScuflNS);
+		String endpointString = slp.getEndpoint().toString();
+		String[] split = endpointString.split("::");
+		if (split.length == 2) {
+			endpointString = split[0] + "." + split[1];
+		}
+		spec.setText(endpointString);
+		if (slp.isPollingDefined()) {
+			// Add attributes for polling...
+			spec.setAttribute("interval", slp.getPollingInterval() + "");
+			spec.setAttribute("backoff", slp.getPollingBackoff() + "");
+			spec.setAttribute("maxinterval", slp.getPollingIntervalMax() + "");
+		}
+		return spec;
 	}
-	spec.setText(endpointString);
-	if (slp.isPollingDefined()) {
-	    // Add attributes for polling...
-	    spec.setAttribute("interval",slp.getPollingInterval()+"");
-	    spec.setAttribute("backoff",slp.getPollingBackoff()+"");
-	    spec.setAttribute("maxinterval",slp.getPollingIntervalMax()+"");
-	}
-	return spec;
-    }
 
-    public Element elementForFactory(ProcessorFactory pf){
-	SoaplabProcessorFactory slpf = (SoaplabProcessorFactory)pf;
-	Element spec = new Element("soaplabwsdl",XScufl.XScuflNS);
-	spec.setText(slpf.getEndpoint());
-	return spec;
-    }
+	public Element elementForFactory(ProcessorFactory pf) {
+		SoaplabProcessorFactory slpf = (SoaplabProcessorFactory) pf;
+		Element spec = new Element("soaplabwsdl", XScufl.XScuflNS);
+		spec.setText(slpf.getEndpoint());
+		return spec;
+	}
 
-    public ProcessorFactory getFactory(Element specElement) {
-	String endpoint = specElement.getTextTrim();
-	return new SoaplabProcessorFactory(endpoint);
-    }
-   
-    public Processor loadProcessorFromXML(Element processorNode, ScuflModel model, String name)
-	throws ProcessorCreationException, 
-	       DuplicateProcessorNameException, 
-	       XScuflFormatException {
-	Element soaplab = processorNode.getChild("soaplabwsdl",XScufl.XScuflNS);
-	String endpoint = soaplab.getTextTrim();
-	try {
-	    URL endpointURL = new URL(endpoint);
+	public ProcessorFactory getFactory(Element specElement) {
+		String endpoint = specElement.getTextTrim();
+		return new SoaplabProcessorFactory(endpoint);
 	}
-	catch (MalformedURLException mue) {
-	    throw new XScuflFormatException("The url specified for the soaplab endpoint for '"+name+"' was invalid : "+mue);
+
+	public Processor loadProcessorFromXML(Element processorNode,
+			ScuflModel model, String name) throws ProcessorCreationException,
+			DuplicateProcessorNameException, XScuflFormatException {
+		Element soaplab = processorNode
+				.getChild("soaplabwsdl", XScufl.XScuflNS);
+		String endpoint = soaplab.getTextTrim();
+		try {
+			URL endpointURL = new URL(endpoint);
+		} catch (MalformedURLException mue) {
+			throw new XScuflFormatException(
+					"The url specified for the soaplab endpoint for '" + name
+							+ "' was invalid : " + mue);
+		}
+		SoaplabProcessor theProcessor = new SoaplabProcessor(model, name,
+				endpoint);
+		// Set the polling properties if they're defined, or just use the
+		// defaults
+		// if not.
+		theProcessor.setPolling(Integer.parseInt(soaplab.getAttributeValue(
+				"interval", "0")), Double.parseDouble(soaplab
+				.getAttributeValue("backoff", "1.0")), Integer.parseInt(soaplab
+				.getAttributeValue("intervalmax", "0")));
+
+		return theProcessor;
 	}
-	SoaplabProcessor theProcessor =  new SoaplabProcessor(model, name, endpoint);
-	// Set the polling properties if they're defined, or just use the defaults
-	// if not.
-	theProcessor.setPolling(Integer.parseInt(soaplab.getAttributeValue("interval","0")),
-				Double.parseDouble(soaplab.getAttributeValue("backoff","1.0")),
-				Integer.parseInt(soaplab.getAttributeValue("intervalmax","0")));
-	
-	return theProcessor;
-    }
 
 }
