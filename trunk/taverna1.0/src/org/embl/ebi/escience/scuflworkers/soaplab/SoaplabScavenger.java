93654f214d5d0ff99890c6c2f8ac5fcc63806962
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-04-27 11:02:38 +0000

    Separated out a loadCategories() method that does the same thing for each factory
    git-svn-id: http://taverna.googlecode.com/svn@1617 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabScavenger.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/scuflworkers/soaplab/SoaplabScavenger.java
@@ -6,111 +6,95 @@
 package org.embl.ebi.escience.scuflworkers.soaplab;
 
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.xml.namespace.QName;
-import org.apache.axis.client.Call;
-import org.apache.axis.client.Service;
+
 import org.embl.ebi.escience.scuflui.workbench.Scavenger;
 import org.embl.ebi.escience.scuflui.workbench.ScavengerCreationException;
-
-import org.embl.ebi.escience.scuflworkers.soaplab.SoaplabProcessorFactory;
-import java.lang.Exception;
-import java.lang.Object;
-import java.lang.RuntimeException;
-import java.lang.String;
+import org.embl.ebi.escience.utils.Soap;
 
 /**
  * A Scavenger that knows how to get all the Soaplab services from a specified
  * installation
  * 
- * @author Tom Oinn
+ * @author Tom Oinn, Stian Soiland
  */
 public class SoaplabScavenger extends Scavenger {
 
-	boolean foundAnInstallation = false;
+	/**
+	 * The base URL of the Soaplab service
+	 */
+	String base;
 
 	/**
 	 * Create a new Soaplab scavenger, the base parameter should be the base URL
 	 * of the Soaplab service, i.e. if your AnalysisFactory is at
-	 * http://foo.bar/soap/AnalysisFactory the parameter shuld be
+	 * http://foo.bar/soap/AnalysisFactory the parameter should be
 	 * http://foo.bar/soap/
 	 */
-	public SoaplabScavenger(String base) throws ScavengerCreationException {
-		super("Soaplab @ " + (base.endsWith("/") ? base : base + "/"));
+	public SoaplabScavenger(String theBase) throws ScavengerCreationException {
+		// Making sure there is / at the end of theBase
+		super("Soaplab @ " + (theBase.endsWith("/") ? theBase : theBase + "/"));
+		// Of course we have to do this again since we are not allowed to do
+		// such stuff before super()
+		base = theBase.endsWith("/") ? theBase : theBase + "/";
 		// Get the categories for this installation
-		try {
-			if (!base.endsWith("/")) {
-				base = base + "/";
-				// throw new RuntimeException("The url for soaplab should look
-				// like http://foo/bar/, see the faq for more information");
-			}
-			String[] categories = null;
-			Call call = (Call) new Service().createCall();
-			try {
-				call.setTargetEndpointAddress(base + "AnalysisFactory");
-				call.setOperationName(new QName("getAvailableCategories"));
-				categories = (String[]) (call.invoke(new Object[0]));
-				// Iterate over all the categories, creating new child nodes
-				for (int i = 0; i < categories.length; i++) {
-					DefaultMutableTreeNode category = new DefaultMutableTreeNode(
-							categories[i]);
-					add(category);
-					call = (Call) new Service().createCall();
-					call.setTargetEndpointAddress(base + "AnalysisFactory");
-					call.setOperationName(new QName(
-							"getAvailableAnalysesInCategory"));
-					String[] services = (String[]) (call
-							.invoke(new String[] { categories[i] }));
-					// Iterate over the services
-					for (int j = 0; j < services.length; j++) {
-						SoaplabProcessorFactory f = new SoaplabProcessorFactory(
-								base, services[j]);
-						category.add(new DefaultMutableTreeNode(f));
-					}
-				}
-				foundAnInstallation = true;
-			} catch (Exception e) {
-				// Ignore
-				e.printStackTrace();
-			}
-			try {
-				call.setTargetEndpointAddress(base + "GowlabFactory");
-				call.setOperationName(new QName("getAvailableCategories"));
-				categories = (String[]) (call.invoke(new Object[0]));
-				// Iterate over all the categories, creating new child nodes
-				for (int i = 0; i < categories.length; i++) {
-					DefaultMutableTreeNode category = new DefaultMutableTreeNode(
-							categories[i]);
-					add(category);
-					call = (Call) new Service().createCall();
-					call.setTargetEndpointAddress(base + "GowlabFactory");
-					call.setOperationName(new QName(
-							"getAvailableAnalysesInCategory"));
-					String[] services = (String[]) (call
-							.invoke(new String[] { categories[i] }));
-					// Iterate over the services
-					for (int j = 0; j < services.length; j++) {
-						SoaplabProcessorFactory f = new SoaplabProcessorFactory(
-								base, services[j]);
-						category.add(new DefaultMutableTreeNode(f));
-					}
-				}
-				foundAnInstallation = true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				// Do nothing
-			}
-		} catch (Exception e) {
-			ScavengerCreationException sce = new ScavengerCreationException(e
-					.getMessage());
-			sce.initCause(e);
-			throw sce;
-		}
-		if (foundAnInstallation == false) {
+		boolean foundAnInstallation = loadCategories(base + "AnalysisFactory");		
+		// Yes, bitwise OR is on purpose, to make sure the second
+		// loadCategories() is always run. Do NOT replace with 
+		//   foundInstallation = foundInstallation || getCategories(..)
+		foundAnInstallation |= loadCategories(base + "GowlabFactory");		
+		if (!foundAnInstallation) {
 			// Neither Soaplab nor Gowlab were found, probably a fault
-			ScavengerCreationException sce = new ScavengerCreationException(
-					"Unable to locate a soaplab installation at \n" + base);
-			throw sce;
+			throw new ScavengerCreationException(
+					"Unable to locate a soaplab installation at \n" + base);			
 		}
+	}
+
+	// FIXME: Can callWebService be used by others? Move out to some common library!
+
+
+	/**
+	 * Create the category tree and fill it with SoapLab processor factories as described by categoryBase
+	 * 
+	 * @param categoryBase The base for the category, example "http://www.ebi.ac.uk/soaplab/services/AnalysisFactory"
+	 * @return
+	 */
+	boolean loadCategories(String categoryBase) {
+		boolean foundSome = false;
+		String[] categories;
+		try {
+			categories = (String[]) Soap.callWebService(categoryBase,
+					"getAvailableCategories");
+		} catch (Exception e) {
+			// FIXME: Log failed call properly
+			e.printStackTrace();
+			return false;
+		}
+		// Iterate over all the categories, creating new child nodes
+		for (int i = 0; i < categories.length; i++) {			
+			String[] services;
+			try {
+				services = (String[]) Soap.callWebService(categoryBase,
+						"getAvailableAnalysesInCategory", categories[i]);
+			} catch (Exception e) {
+				// FIXME: Log skipped category
+				continue;
+			}			
+			if (services.length == 0) {
+				// Avoid creating empty treenodes
+				continue;
+			}
+			DefaultMutableTreeNode category = new DefaultMutableTreeNode(
+					categories[i]);			
+			foundSome = true;
+			// Iterate over the services
+			for (int j = 0; j < services.length; j++) {
+				SoaplabProcessorFactory f = new SoaplabProcessorFactory(base,
+						services[j]);
+				category.add(new DefaultMutableTreeNode(f));
+			}
+			this.add(category);
+		}
+		return foundSome;
 	}
 
 }
