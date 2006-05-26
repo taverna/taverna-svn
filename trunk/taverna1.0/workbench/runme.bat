6c893cc010bfa857e0714d8941458113592b6e2a
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-05-26 10:45:43 +0000

    Snapshot release version is now 1.3-SNAPSHOT, so that Taverna don't look like 1.3.2-RC1 when it's really beyond that.
    (This will be larger than any 1.3.x but less (earlier) than any 1.4.x.)
    
    
    git-svn-id: http://taverna.googlecode.com/svn@1724 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/workbench/runme.bat
+++ trunk/taverna1.0/workbench/runme.bat
@@ -4,7 +4,7 @@ PATH=./bin/win32i386;%PATH%
 
 set OPTS=-Xms256m -Xmx512m -Dtaverna.home=. -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client
 
-set CLASSPATH=.;./resources;./conf;./taverna-1.3.2-RC1-launcher.jar
+set CLASSPATH=.;./resources;./conf;./taverna-1.3-SNAPSHOT-launcher.jar
 
 for %%i in ("libext\*.jar") do call catenv.bat %%i
 
