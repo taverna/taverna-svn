dab176d52d3f24f0b4e4dd0a11ed59b49974105e
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-05-31 09:18:04 +0000

    Better logging, removed printStackTrace's
    git-svn-id: http://taverna.googlecode.com/svn@1747 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/org/embl/ebi/escience/baclava/store/BaclavaDataServiceFactory.java
+++ trunk/taverna1.0/src/org/embl/ebi/escience/baclava/store/BaclavaDataServiceFactory.java
@@ -8,16 +8,13 @@ package org.embl.ebi.escience.baclava.store;
 
 import org.apache.log4j.Logger;
 
-import com.sun.jdi.InvocationException;
-
 /*
  * Factory class for creating instances of BaclavaDataService according to the
  * property taverna.datastore.class, if set.
  */
 
 public class BaclavaDataServiceFactory {
-	static Logger log = Logger.getLogger(BaclavaDataServiceFactory.class
-			.getName());
+	static Logger log = Logger.getLogger(BaclavaDataServiceFactory.class.getName());
 
 	/*
 	 * Returns an instance of BaclavaDataService, according to the property
@@ -32,15 +29,11 @@ public class BaclavaDataServiceFactory {
 				Class c = Class.forName(storageClassName);
 				result = (BaclavaDataService) c.newInstance();
 			} catch (ClassNotFoundException e) {
-				log.error("Unable to find class: " + storageClassName
-						+ " defined by taverna.datastore.class");
-				e.printStackTrace();
+				log.error("Unable to find class: " + storageClassName + " defined by taverna.datastore.class");
 			} catch (InstantiationException e) {
-				log.error("InstatiationException when creating "
-						+ storageClassName, e);
+				log.error("InstatiationException when creating " + storageClassName, e);
 			} catch (IllegalAccessException e) {
-				log.error("IllegalAccessException when creating "
-						+ storageClassName, e);
+				log.error("IllegalAccessException when creating " + storageClassName, e);
 			}
 		}
 		return result;
