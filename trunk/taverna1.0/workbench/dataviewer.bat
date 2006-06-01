6f0bfc9d8cca9466d1d01e751e67ade420299922
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-06-01 11:07:20 +0000

    Allows running bat files from other directories as well, using some dark .bat magic
    git-svn-id: http://taverna.googlecode.com/svn@1759 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/workbench/dataviewer.bat
+++ trunk/taverna1.0/workbench/dataviewer.bat
@@ -3,5 +3,4 @@ set MAIN=org.embl.ebi.escience.baclava.tools.DataThingViewer
 set OPTS=-Xms256m -Xmx512m 
 set OPTS=%OPTS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client 
 set OPTS=%OPTS% -Dtaverna.main=%MAIN%
-set OPTS=%OPTS% -Dtaverna.scrollDesktop
-java %OPTS% -jar ./taverna-launcher-1.3-SNAPSHOT.jar %*
+java %OPTS% -jar "%~dp0\taverna-launcher-1.3-SNAPSHOT.jar" %*
--- trunk/taverna1.0/workbench/executeworkflow.bat
+++ trunk/taverna1.0/workbench/executeworkflow.bat
@@ -4,4 +4,4 @@ set OPTS=-Xms256m -Xmx512m
 set OPTS=%OPTS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client 
 set OPTS=%OPTS% -Djava.awt.headless=true
 set OPTS=%OPTS% -Dtaverna.main=%MAIN%
-java %OPTS% -jar ./taverna-launcher-1.3-SNAPSHOT.jar %*
+java %OPTS% -jar "%~dp0\taverna-launcher-1.3-SNAPSHOT.jar" %*
