aaa497cb96e25eb5a32b3d98af4c113a0dcc4b02
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-06-05 13:36:06 +0000

    make and change to a working directory c:/Document and settings/user/Application data/Taverna for storing Provenance
    git-svn-id: http://taverna.googlecode.com/svn@1785 bf327186-88b3-11dd-a302-d386e5130c1c

--- branches/taverna-1-4/taverna1.0/workbench/runme.bat
+++ branches/taverna-1-4/taverna1.0/workbench/runme.bat
@@ -1,4 +1,9 @@
 @echo off
+
+set DATA="%APPDATA%\Taverrna"
+if not exist %DATA% mkdir %DATA%
+cd %DATA%
+
 set OPTS=-Xms256m -Xmx512m 
 set OTPS=%OPTS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client
 java %OPTS% -jar "%~dp0\taverna-launcher-1.4.jar" %*
