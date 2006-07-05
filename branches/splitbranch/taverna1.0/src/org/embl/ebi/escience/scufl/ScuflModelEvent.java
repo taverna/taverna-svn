e4bb9d012863b8f6a77c0340c6fca19b0e64d220
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-07-05 14:01:26 +0000

    This commit was manufactured by cvs2svn to create branch 'splitbranch'.
    git-svn-id: http://taverna.googlecode.com/svn@1856 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ branches/splitbranch/taverna1.0/src/org/embl/ebi/escience/scufl/ScuflModelEvent.java
@@ -0,0 +1,80 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scufl;
+
+/**
+ * Signifies a change in the model that might be of interest to a view
+ * 
+ * @author Tom Oinn
+ */
+public class ScuflModelEvent {
+
+	public static int UNKNOWN = 0;
+
+	public static int LOAD = 1;
+
+	Object source = null;
+
+	private String message = null;
+
+	int eventType = UNKNOWN;
+
+	/**
+	 * Construct a new event, the object generating the event should put itself
+	 * in the source field, and may optionally supply a textual message as well
+	 */
+	public ScuflModelEvent(Object source, String message) {
+		this(source, message, UNKNOWN);
+	}
+
+	/**
+	 * Construct a new event with the specified event type
+	 */
+	public ScuflModelEvent(Object source, String message, int eventType) {
+		this.source = source;
+		if (message != null) {
+			this.message = message;
+		} else {
+			this.message = "ScuflModelEvent : No message provided";
+		}
+		this.eventType = eventType;
+	}
+
+	/**
+	 * Get the message
+	 */
+	public String getMessage() {
+		return this.message;
+	}
+
+	/**
+	 * Get the event type
+	 */
+	public int getEventType() {
+		return this.eventType;
+	}
+
+	/**
+	 * Get the source of the event
+	 */
+	public Object getSource() {
+		return this.source;
+	}
+
+	public String toString() {
+		return this.source.toString() + " :: " + this.message;
+	}
+
+	protected static String getClassName(Object addedObject) {
+		String name = addedObject.getClass().getName();
+		int index = name.lastIndexOf('.');
+		if (index != -1) {
+			name = name.substring(index + 1);
+		}
+		return name;
+	}
+
+}
---
+++ branches/splitbranch/taverna1.0/src/org/embl/ebi/escience/scuflui/dnd/FactorySpecFragment.java
@@ -0,0 +1,37 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scuflui.dnd;
+
+import java.io.Serializable;
+
+import org.jdom.Element;
+
+/**
+ * Contains a JDOM Element and methods to build processors
+ * and processor factories from same, specialized to be
+ * from the Factory objects for purposes of drag specialization
+ * @author Tom Oinn
+ */
+public class FactorySpecFragment extends SpecFragment implements Serializable {
+    
+    private String name;
+
+    public FactorySpecFragment(Element element, String name) {
+	super(element);
+	this.name = name;
+    }
+    
+    /**
+     * Will contain the original node name of the processor
+     * factory that produced this object, the idea being that
+     * the model explorer can use this as the default name
+     * for a drop event
+     */
+    public String getFactoryNodeName() {
+	return this.name;
+    }
+    
+}
