9bea20db31e32e3961dc501939ee6af290dbf0ef
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-07-07 14:23:58 +0000

    Moved files to conform to maven standard directory layout
    git-svn-id: http://taverna.googlecode.com/svn@1882 bf327186-88b3-11dd-a302-d386e5130c1c

--- branches/splitbranch/taverna1.0/src/main/resources/workbench/runme.bat
+++ branches/splitbranch/taverna1.0/src/main/resources/workbench/runme.bat
@@ -2,4 +2,4 @@
 
 set OPTS=-Xms256m -Xmx512m 
 set OTPS=%OPTS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client
-java %OPTS% -jar "%~dp0\taverna-launcher-1.4-SNAPSHOT.jar" %*
+java %OPTS% -jar "%~dp0\lib\taverna-launcher-1.5-SNAPSHOT.jar" %*
