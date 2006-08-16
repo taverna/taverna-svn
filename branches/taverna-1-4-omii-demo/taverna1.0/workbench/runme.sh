de47ad887dff7a7d93ec41490efb334323067f32
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-08-16 10:48:35 +0000

    Changes to add ablilty to call omii secure services
    git-svn-id: http://taverna.googlecode.com/svn@1933 bf327186-88b3-11dd-a302-d386e5130c1c

--- branches/taverna-1-4-omii-demo/taverna1.0/workbench/runme.sh
+++ branches/taverna-1-4-omii-demo/taverna1.0/workbench/runme.sh
@@ -21,4 +21,21 @@ TAVERNA_HOME=`dirname "$PRG"`
 
 cd "$saveddir"
 
-java -Xmx300m -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client -jar $TAVERNA_HOME/taverna-launcher-1.4.jar
+OMII_CLIENT_HOME=@OMII_CLIENT_HOME@
+
+TAVERNA_OMII_CONF=$OMII_CLIENT_HOME/conf
+
+TAVERNA_OMII_JARS=$OMII_CLIENT_HOME/lib/bouncycastle-jce-jdk13-119.jar
+TAVERNA_OMII_JARS=$TAVERNA_OMII_JARS:$OMII_CLIENT_HOME/lib/castor-0.9.7.jar
+TAVERNA_OMII_JARS=$TAVERNA_OMII_JARS:$OMII_CLIENT_HOME/lib/itinnov-grid-utils-omii1.jar
+TAVERNA_OMII_JARS=$TAVERNA_OMII_JARS:$OMII_CLIENT_HOME/lib/opensaml-1.0.1.jar
+TAVERNA_OMII_JARS=$TAVERNA_OMII_JARS:$OMII_CLIENT_HOME/lib/wss4j-itinnov-4.jar
+TAVERNA_OMII_JARS=$TAVERNA_OMII_JARS:$OMII_CLIENT_HOME/lib/xmlsec-1.2.1.jar
+
+TAVERNA_OMII_FILES=$TAVERNA_OMII_CONF:$TAVERNA_OMII_JARS
+
+TAVERNA_OMII_OPTS=-Daxis.ClientConfigFile=$OMII_CLIENT_HOME/conf/default-client-config.wsdd
+TAVERNA_OMII_OPTS="$TAVERNA_OMII_OPTS -Djava.endorsed.dirs=$OMII_CLIENT_HOME/endorsed"
+TAVERNA_OMII_OPTS="$TAVERNA_OMII_OPTS -Dtaverna.path=$TAVERNA_OMII_FILES"
+
+java -Xmx300m -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client $TAVERNA_OMII_OPTS -jar $TAVERNA_HOME/taverna-launcher-1.4-omii-demo.jar
