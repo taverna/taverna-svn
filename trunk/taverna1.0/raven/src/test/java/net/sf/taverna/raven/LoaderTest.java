d5d47230e6f49b648fe113fa9cd6bbfbf7ff4313
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-08-30 08:58:45 +0000

    Updated tests
    git-svn-id: http://taverna.googlecode.com/svn@1984 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/test/java/net/sf/taverna/raven/LoaderTest.java
+++ trunk/taverna1.0/raven/src/test/java/net/sf/taverna/raven/LoaderTest.java
@@ -4,14 +4,18 @@ import static org.apache.commons.io.FileUtils.deleteDirectory;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.net.URLClassLoader;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import net.sf.taverna.raven.repository.Artifact;
 import net.sf.taverna.raven.repository.ArtifactNotFoundException;
 import net.sf.taverna.raven.repository.ArtifactStateException;
+import net.sf.taverna.raven.repository.impl.ArtifactImpl;
 import net.sf.taverna.raven.repository.impl.LocalRepository;
 import junit.framework.TestCase;
 
@@ -48,13 +52,48 @@ public class LoaderTest extends TestCase {
 		assert tempFile.mkdir();
 		return tempFile;
 	}
-	
-	public void testDummy() {
-	
+		
+	/**
+	 * Test dynamic (reflection based) access to a remote raven repository
+	 * without using any reference to the raven API
+	 * @throws MalformedURLException
+	 * @throws ClassNotFoundException
+	 * @throws SecurityException
+	 * @throws NoSuchMethodException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 * @throws InterruptedException 
+	 */
+	public void testDynamic() throws MalformedURLException, ClassNotFoundException, 
+	SecurityException, NoSuchMethodException, IllegalArgumentException, 
+	IllegalAccessException, InvocationTargetException, InterruptedException {
+		ClassLoader c = new URLClassLoader(new URL[]{new URL("http://www.ebi.ac.uk/~tmo/repository/uk/org/mygrid/taverna/raven/1.5-SNAPSHOT/raven-1.5-SNAPSHOT.jar")});
+		Class loaderClass = Class.forName("net.sf.taverna.raven.Loader",true,c);
+		Method m = loaderClass.getDeclaredMethod("doRavenMagic",String.class,File.class,URL[].class,URL.class,String.class,String.class,String.class,int.class,String.class);
+		Class workbenchClass = (Class)m.invoke(null,new Object[]{"1.5-SNAPSHOT",dir,new URL[]{new URL("http://www.ebi.ac.uk/~tmo/repository/"),new URL("http://www.ibiblio.org/maven2/")},
+				new URL("http://www.ebi.ac.uk/~tmo/mygrid/splashscreen.png"),
+				"uk.org.mygrid.taverna",
+				"taverna-workbench",
+				"1.5-SNAPSHOT",
+				10000,
+		"org.embl.ebi.escience.scuflui.workbench.Workbench"});
+		System.out.println(workbenchClass.toString());
+		Method workbenchMain = workbenchClass.getDeclaredMethod("main",String[].class);
+		workbenchMain.invoke(null,new Object[]{new String[0]});
 	}
 	
 	/**
-	public void testWorkbenchBootstrap() throws MalformedURLException, ArtifactNotFoundException, ArtifactStateException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+	 * Test whether we can launch the workbench from a remote raven
+	 * @throws MalformedURLException
+	 * @throws ArtifactNotFoundException
+	 * @throws ArtifactStateException
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws UnsupportedLookAndFeelException
+	 */
+	public void testNonDynamic() throws MalformedURLException, ArtifactNotFoundException, ArtifactStateException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(UIManager
 				.getSystemLookAndFeelClassName());
 		Class workbenchClass = Loader.doRavenMagic(
@@ -69,8 +108,15 @@ public class LoaderTest extends TestCase {
 				10000,
 		"org.embl.ebi.escience.scuflui.workbench.Workbench");
 		System.out.println(workbenchClass.toString());
+		System.out.println("\n\nRepository state dump : \n");
+		LocalRepository r = LocalRepository.getRepository(dir);
+		for (Artifact a : r.getArtifacts()) {
+			ArtifactImpl ai = (ArtifactImpl)a;
+			System.out.println(ai.toString()+"  ##  "+r.getStatus(ai).toString());
+			for (ArtifactImpl dep : ai.getDependencies()) {
+				System.out.println("    "+dep.toString()+"  --  "+r.getStatus(dep).toString());
+			}
+		}
 	}
-	*/
-
 	
 }
