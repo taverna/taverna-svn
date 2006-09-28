6d5bd97a325908f597ee6bceef69457cd9011eaa
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-09-28 20:47:21 +0000

    Uses synthetica look and feel if present
    git-svn-id: http://taverna.googlecode.com/svn@2061 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/zaria/src/test/java/net/sf/taverna/zaria/TestBasePane.java
+++ trunk/taverna1.0/zaria/src/test/java/net/sf/taverna/zaria/TestBasePane.java
@@ -1,6 +1,7 @@
 package net.sf.taverna.zaria;
 
 import java.awt.BorderLayout;
+import java.text.ParseException;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -11,6 +12,31 @@ import junit.framework.TestCase;
 
 public class TestBasePane extends TestCase {
 
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, InterruptedException, ParseException {
+		try {
+			UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaStandardLookAndFeel");
+		}
+		catch (Exception ex) {
+			// don't have synthetica installed, shame. It looks
+			// a lot better.
+		}
+		JFrame myFrame = new JFrame("ZPane test");
+		myFrame.setSize(300,300);
+		ZPane pane = new ZBasePane() {
+
+			@Override
+			public JComponent getComponent(Class theClass) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+		};
+		pane.setEditable(true);
+		myFrame.getContentPane().add(pane, BorderLayout.CENTER);
+		myFrame.setVisible(true);
+		Thread.sleep(1000*30);
+	}
+	
 	public void testBasePane() throws InterruptedException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         /*  Does not run in head-less mode */
 		/**
