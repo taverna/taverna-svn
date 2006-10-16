5848f4476615116093d80301b4b9f28c931f69c9
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-10-16 13:23:27 +0000

    Load mygrid.properties from classpath and user directory.
    mygrid.properties now distributed with taverna-core.jar
    
    Find (and create if needed) $HOME/.taverna (OS-specific)
    
    Write out default config from .jar to $HOME/.taverna/conf/mygrid.properties - but with all active lines ##commented out.
    
    
    Fixes TAV-183.
    
    
    git-svn-id: http://taverna.googlecode.com/svn@2128 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/taverna-core/src/main/java/net/sf/taverna/utils/MyGridConfiguration.java
+++ trunk/taverna1.0/taverna-core/src/main/java/net/sf/taverna/utils/MyGridConfiguration.java
@@ -1,15 +1,26 @@
 package net.sf.taverna.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Enumeration;
-import java.util.Locale;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Properties;
-import java.util.ResourceBundle;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.log4j.Logger;
+import org.python.modules.synchronize;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * myGrid configuration, such as services to load in the workbench or 
@@ -31,50 +42,272 @@ import org.apache.log4j.Logger;
  */
 public class MyGridConfiguration {
 	
-	private static Properties properties = null;
-	private final static String PROPERTIES = "mygrid";
+	static Properties properties = null;
+	private final static String PROPERTIES = "mygrid.properties";
+	private final static String APPLICATION = "Taverna";
+	
+	// Written to the top of the mygrid.properties.orig
+	private final static String HEADER = 
+		"# Default values are shown double-commented like this:\n" +
+    	"# ## mygrid.example = value\n" +
+    	"\n";		
+
+	// For each source properties, this header will be added
+	private final static String SECTION_HEADER = "# Default properties from ";
+	
 
 	private static Logger logger = Logger.getLogger(MyGridConfiguration.class);
 
-	
 	// Can't be instanciated, static methods only
 	private MyGridConfiguration() {}
 	
 	/**
-	 * Get the myGrid properties
+	 * Get the myGrid properties. The properties will be the combination of
+	 * the default properties distributed with Taverna and the user specified properties.
+	 * <p><strong>Note:</strong>
+	 * This method will force creation/updating of local user properties files if needed. 
+	 * This method will cache the loaded properties until flushProperties() is called.
 	 * 
-	 * @return A Properties instance loaded from mygrid.properties
+	 * @return myGrid properties
 	 */
 	synchronized public static Properties getProperties() {
-		if (properties == null) {
-			ResourceBundle rb;
-			try {
-				rb = ResourceBundle.getBundle(PROPERTIES);
-			} catch (Exception e) {
-				logger.error("Can't find " + PROPERTIES + ".properties in classpath", e);
-				System.out.println("Classpath is:");
-				URLClassLoader l = (URLClassLoader) MyGridConfiguration.class.getClassLoader();
-				for (URL u : l.getURLs()) {
-					System.out.println(u);
-				}
-				return null;
-			}
-			Enumeration keys = rb.getKeys();
-			properties = new Properties();
-			while (keys.hasMoreElements()) {
-				String key = (String) keys.nextElement();
-				String value = (String) rb.getString(key);
-				properties.put(key, value);
-			}					
+		// FIXME: Make it possible for Raven to clear this cache
+		if (properties != null) {
+			return properties;
 		}
+		properties = loadDefaultProperties();
+		writeDefaultProperties();
+		properties.putAll(loadUserProperties());
 		return properties;
 	}
 	
 	/**
+	 * Flush cache of properties. Next call to getProperties() or 
+	 * getProperty() will force a reload from classloaders and user files 
+	 *
+	 */
+	synchronized public static void flushProperties() {
+		properties = null;
+	}
+	
+	
+	/**
+	 * Find resources. The resource specified must be reachable by the
+	 * class loader. Note that several hits might be returned.
+	 * <p>
+	 * Example:
+	 * <pre>
+	 * for (URL url: findResources("mygrid.properties") {
+	 * 		..
+	 * }
+	 * </pre>
+	 * 
+	 * @param resourceName
+	 * @return Iterable over URLs
+	 */
+	static Iterable<URL> findResources(final String resourceName) { 
+		/*   Psevdo code:
+		@classmethod
+		def findDefaultResources(cls, resourceName):
+		    loader = cls.class.classLoader or Thread.currentThread().contextClassLoader
+		    try:
+		        resources = loader.getResources(resourceName)
+		    except IOException:
+		        throw NoSuchElementException    
+		    while resources.hasMoreElements():
+		        yield resources.nextElement()
+		*/
+		
+		// 15 layers of Iterable vs. Iterator courtesy of Sun
+		return new Iterable<URL>() {
+			public Iterator<URL> iterator() {
+				// FIXME: Shouldn't we always use context class loader?
+				ClassLoader loader = MyGridConfiguration.class.getClassLoader();
+				if (loader == null) {
+					loader = Thread.currentThread().getContextClassLoader();
+				}
+				Enumeration<URL> tempResources;
+				try {
+					tempResources = loader.getResources(resourceName);
+				} catch (IOException e) {
+					logger.error("Could not find " + resourceName, e);
+					tempResources = null;
+				} 
+				final Enumeration<URL> resources = tempResources;
+				return new Iterator<URL>() {
+					public boolean hasNext() {
+						return resources != null && resources.hasMoreElements();
+					}
+					public URL next() {
+						if (resources == null) {
+							throw new NoSuchElementException();
+						}
+						return resources.nextElement();
+					}
+					public void remove() {
+						throw new NotImplementedException();
+					}					
+				};
+			}
+			
+		};
+	}
+
+	
+	/**
+	 * Get the default myGrid properties as distributed with Taverna.
+	 * <p>
+	 * The property object returned is freshly created (and loaded) for 
+	 * each call and can be modified without affecting
+	 * later calls.
+	 * 
+	 * @return Default properties as provided by classloader
+	 */
+	public static Properties loadDefaultProperties() {
+		Properties properties = new Properties();
+		// Concatinate all resources
+		for (URL resource : findResources(PROPERTIES)) {
+			logger.info("Loading resources from " + resource.toString());
+			try {
+				properties.load(resource.openStream());
+			} catch (IOException e) {
+				logger.warn("Could not read properties from " + resource, e);
+			}
+		}
+		return properties;
+	}
+
+	/**
+	 * Get the user's application directory according to operating system.
+	 * 
+	 * <p>
+	 * On Windows, this will typically be something like:
+	 * 	<pre>
+	 * 	C:\Document and settings\MyUsername\Application Data\MyApplication
+	 * 	</pre>
+	 * while on Mac OS X it will be something like:
+	 * 	<pre>
+	 * 	/Users/MyUsername/Library/Application Support/MyApplication
+	 * 	</pre>
+	 * All other OS'es are assumed to be UNIX-alike, returning something like:
+	 * 	<pre>
+	 * 	/user/myusername/.myapplication
+	 * 	</pre>
+	 * 
+	 * <p>
+	 * If the directory does not already exist, it will be created. 
+	 * </p>
+	 * 
+	 * @return <code>File</code> object representing the OS-specific user
+	 *         directoryfor this application, or <code>null</code> if it 
+	 *         could not be found or created.
+	 */
+	public static File getUserDir() {
+		File home = new File(System.getProperty("user.home"));
+		if (! home.isDirectory()) {
+			logger.error("User home not a valid directory: " + home);
+			return null;
+		}
+		String os = System.getProperty("os.name");
+		logger.debug("OS is " + os);
+		File appHome;
+		
+		if (os.equals("Mac OS X")) {
+			File libDir = new File(home, "Library/Application Support");
+			if (! libDir.isDirectory()) {
+				logger.warn("Could not find Application support directory:" + libDir);
+				// We'll make it, we could be the first one
+				new File(home, "Library").mkdir();
+				libDir.mkdir();
+			}
+			appHome = new File(libDir, APPLICATION);
+			
+		} else if (os.startsWith("Windows")) {
+			String APPDATA = System.getenv("APPDATA");
+			File appData = null;
+			if (APPDATA != null) {
+				appData = new File(APPDATA);
+			}
+			if (appData != null && appData.isDirectory()) {
+				appHome = new File(appData, APPLICATION);
+			} else {
+				logger.warn("Could not find %APPDATA%: " + APPDATA);
+				appHome = new File(home, APPLICATION);
+			}
+			
+		} else {
+			// We'll assume UNIX style is OK
+			appHome = new File(home, "." + APPLICATION.toLowerCase());
+		}
+		
+		if (! appHome.exists()) {
+			if (appHome.mkdir()) {
+				logger.info("Created " + appHome);
+			} else {
+				logger.error("Could not create " + appHome);
+				return null;
+			}
+		}
+		if (! appHome.isDirectory()) {
+			logger.error(APPLICATION + " user home not a valid directory: " 
+					     + appHome);
+			return null;
+		}
+		return appHome;
+	}
+	
+	/**
+	 * Get a subdirectory of the user's application directory.
+	 * <p>
+	 * Like getUserDir(), but one level deeper. For instance, 
+	 * getUserDir("conf") on UNIX would return the file of
+	 * <code>/user/myusername/.myapplication/conf</code> and assure
+	 * that both <code>.myapplication</code> and <code>conf</code> are
+	 * created.
+	 * 
+	 * @see getUserDir()
+	 * @param subDirectory
+	 * @return
+	 */
+	public static File getUserDir(String subDirectory) {
+		File dir = new File(getUserDir(), subDirectory);
+		if (! dir.isDirectory()) {
+			if (dir.mkdir()) {
+				logger.info("Created " + dir);
+			} else {
+				logger.error("Could not create " + dir);
+				return null;
+			}
+		}
+		return dir;
+	}
+	
+	
+	public static Properties loadUserProperties() {
+		Properties userProps = new Properties();
+		File confDir = getUserDir("conf");
+		File propertyFile = new File(confDir, PROPERTIES);
+		if (! propertyFile.isFile()) {
+			return userProps;
+		}
+		try {
+			userProps.load(propertyFile.toURL().openStream());
+		} catch (MalformedURLException e) {
+			logger.error("Invalid property location " + propertyFile, e);
+		} catch (IOException e) {
+			logger.warn("Could not load user properties", e);
+		}
+		return userProps;
+	}
+	
+
+	/**
 	 * 
 	 * Look up a myGrid property.
 	 * <p>
-	 * This is a shorthand for getProperties().getProperty(key).
+	 * This is a shorthand for getProperties().getProperty(key). Use this instead of
+	 * System.getProperty("taverna.X"), which depends on loadMygridProperties() to 
+	 * have been called.
 	 * 
 	 * @param key 
 	 * @return value 
@@ -88,13 +321,14 @@ public class MyGridConfiguration {
 		return props.getProperty(key);
 	}
 
+	
 	/**
 	 * Load the myGrid properties and store them into the System properties.
 	 * <p>
 	 * This is provided for backwards compatibility <em>only</em>, as old code relies 
 	 * on System.getProperties() to be prepopulated with the myGrid configuration.
 	 * <p>
-	 * Do not rely on properties to have been loaded globally, instad use 
+	 * Do not rely on properties to have been loaded globally, instead use 
 	 * MyGridConfiguration.getProperties()
 	 *
 	 * @see MyGridConfiguration.getProperties()
@@ -104,10 +338,94 @@ public class MyGridConfiguration {
 	synchronized public static void loadMygridProperties() {
 		Properties myGridProps = MyGridConfiguration.getProperties();
 		if (myGridProps == null) {
-			logger.warn("mygrid properties not found, expect NullPointerExceptions");
+			logger.warn("mygrid properties not found and initialized, expect lots of NullPointerExceptions");
+			return;
 		}
 		Properties sysProps = System.getProperties();
 		sysProps.putAll(myGridProps);
+	}
+
+	public static void writeDefaultProperties() {
+		File confDir = getUserDir("conf");
+		File propOrig = new File(confDir, PROPERTIES+".orig");
+		File prop = new File(confDir, PROPERTIES);
+		// If it does not exist, we can replace it
+		boolean replaceProp = ! prop.canRead();
+		try {
+			if (propOrig.canRead() && FileUtils.contentEquals(propOrig, prop)) {
+				// Not changed, replace it
+				replaceProp = true;
+			}
+		} catch (IOException e) {
+			logger.warn("Could not compare " + propOrig + " with " + prop, e);
+		}
+		propOrig.delete();  // Delete if exist
+		if (replaceProp) {
+			prop.delete();
+		}
+		
+		// Write out mygrid.properties.orig
+		try {
+			Writer propWriter = new BufferedWriter(new FileWriter(propOrig));
+			propWriter.write(HEADER);
+			for (URL resource : findResources(PROPERTIES)) {
+				writeDefaults(resource, propWriter);
+			}
+			propWriter.close();
+		} catch (IOException e) {
+			logger.error("Could not write default properties to " + propOrig, e);
+			// Delete it as it might be incomplete and should be rewritten
+			propOrig.delete();
+			return;
+		}
+		
+		// Upgrade mygrid.properties unless there exists a user-modified version
+		if (replaceProp && propOrig.isFile()) {
+			try {
+				FileUtils.copyFile(propOrig, prop);
+			} catch (IOException e) {
+				logger.error("Could not copy " + propOrig + " to " + prop, e);
+				// Delete it so that we can try again next time
+				prop.delete();
+				return;
+			}
+			logger.info("Replaced " + prop);
+		} else {
+			logger.info("Did not replace " + prop);
+		}
+	}
+
+	/**
+	 * Write default properties from the given URL.
+	 * <p>
+	 * The source will be appended to the destination writer line by line, 
+	 * except active lines (non-comments) will be double-commented (##) 
+	 * as to indicate the defaults. A header is added including the source URL.
+	 * <p>
+	 * If the source cannot be read, it will be silently ignored with a log message.
+	 * 
+	 * @param source The URL to the source property file to append
+	 * @param destination An already opened Writer instance for appending. Will not be closed on exit.
+	 * @throws IOException If writing to the destination fails.
+	 */
+	private static void writeDefaults(URL source, Writer destination) throws IOException {
+		LineIterator lines;
+		try {
+			lines  = IOUtils.lineIterator(source.openStream(), "utf8");
+		} catch (IOException e) {
+			logger.warn("Could not read " + source, e);
+			return;
+		}
+		
+		destination.write(SECTION_HEADER + source + "\n");
+		while (lines.hasNext()) {
+			String line = lines.nextLine();
+			if (! line.equals("") && ! line.startsWith("#")) {
+				// Comment out non-comments
+				line = "##" + line;
+			}
+			destination.write(line + "\n");
+		}
 	}
 
 }
---
+++ trunk/taverna1.0/taverna-core/src/test/java/net/sf/taverna/utils/MyGridConfigurationTest.java
@@ -0,0 +1,277 @@
+package net.sf.taverna.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import junit.framework.TestCase;
+
+public class MyGridConfigurationTest extends TestCase {
+	
+	private String realHome;
+	private File tempHome;
+	private String realOS;
+	
+	// Property that we can expect to be there
+	final static String property = "taverna.lsid.providerclass";
+	final static String propValue = "org.embl.ebi.escience.baclava.UUIDLSIDProvider";
+
+	public void setUp() throws IOException {
+		realHome = System.getProperty("user.home");
+		realOS = System.getProperty("os.name");
+		tempHome = File.createTempFile("taverna", ".tmp");
+		assertTrue(tempHome.delete());
+		assertTrue(tempHome.mkdir());
+		assertTrue(tempHome.isDirectory());
+		assertEquals(0, tempHome.listFiles().length); //empty
+		System.setProperty("user.home", tempHome.getAbsolutePath());
+		assertNull(MyGridConfiguration.properties);
+	}
+	
+	public void tearDown() throws IOException {
+		System.setProperty("user.home", realHome);
+		System.setProperty("os.name", realOS);
+		//FileUtils.deleteDirectory(tempHome);
+		// Might have been messed up by getProperties calls
+		MyGridConfiguration.properties = null;
+	}
+	
+	public void testMadeTempHome() {
+		String home = System.getProperty("user.home");
+		assertTrue(home.contains(".tmp"));
+	}
+	
+	public void testGetUserDir() {
+		File userDir = MyGridConfiguration.getUserDir();
+		assertTrue(userDir.isDirectory());
+		// Can't check getParent(), would fail on Windows
+		assertTrue(userDir.getAbsolutePath().startsWith(tempHome.getAbsolutePath()));
+		// Ignore T/t
+		assertTrue(userDir.getName().contains("averna"));
+	}
+	
+	/* 
+	 * Test that getUserDir() works as expected on different OS-es
+	 * 
+	 */
+	public void testGetUserDirMultiOS() {
+		System.setProperty("os.name", "Mac OS X");
+		File dir = MyGridConfiguration.getUserDir();
+		File shouldBe = new File(tempHome, "Library/Application Support/Taverna");
+		assertEquals(shouldBe, dir);
+		assertTrue(dir.isDirectory());
+		
+		System.setProperty("os.name", "Windows XP");
+		dir = MyGridConfiguration.getUserDir();
+		String APPDATA = System.getenv("APPDATA");
+		if (APPDATA == null) {
+			// Likely on Non-Windows platform
+			shouldBe = new File(tempHome, "Taverna");
+		} else {
+			shouldBe = new File(APPDATA, "Taverna");
+		}
+		assertEquals(shouldBe, dir);
+		assertTrue(dir.isDirectory());
+	
+		// Anything else is UNIX style
+		System.setProperty("os.name", "Linn0x");
+		dir = MyGridConfiguration.getUserDir();
+		shouldBe = new File(tempHome, ".taverna");
+		assertEquals(shouldBe, dir);
+		assertTrue(dir.isDirectory());
+	}
+	
+	
+	public void testGetUserDirModule() {
+		File moduleDir = MyGridConfiguration.getUserDir("conf");
+		assertTrue(moduleDir.isDirectory());
+		assertEquals("conf", moduleDir.getName());
+	}
+	
+	/**
+	 * Test that mygrid properties are written out to the
+	 * mygrid.properties.orig, and that the defaults are 
+	 * double-commented out
+	 * 
+	 * @throws IOException
+	 */
+	public void testWriteDefaultConfOrig() throws IOException {
+		File propertiesOrig = new File(MyGridConfiguration.getUserDir("conf"), 
+									 "mygrid.properties.orig");
+		assertFalse(propertiesOrig.isFile());
+		MyGridConfiguration.writeDefaultProperties();
+		assertTrue(propertiesOrig.isFile());
+		String content = FileUtils.readFileToString(propertiesOrig, "utf8");
+		// global header
+		assertTrue(content.startsWith("# Default values are shown"));
+		// local header
+		assertTrue(content.contains("\n# Default properties from file:/"));
+		// Should include the original config file, we'll test for parts of that
+		assertTrue(content.contains("\n# Taverna configuration file"));
+		// The defaults should have been included only once (ie. the split will make two parts)
+		assertEquals(2, content.split("Taverna configuration file").length);
+		// Blank lines should be preserved as-is
+		assertTrue(content.contains("\n\n\n# LSID Configuration"));
+		// This default should be there (including any tabs), but double-commented out
+		assertTrue(content.contains("\n##\ttaverna.lsid.providerclass = " +
+				"org.embl.ebi.escience.baclava.UUIDLSIDProvider\n"));
+		// And the end should be as in the original
+		assertTrue(content.endsWith("\n#--------------------------------------------------------------------\n"));
+	}
+	
+	
+	/**
+	 * Test the upgrade mechanism for mygrid.properties and mygrid.properties.orig
+	 * <p>
+	 * In short, if mygrid.properties exist, it will be upgraded only if it matches completely with the old mygrid.properties.orig. 
+	 * mygrid.properties.orig should always be upgraded. 
+	 * 
+	 * @throws IOException
+	 */
+	public void testWriteDefaultConf() throws IOException {
+		File propertiesOrig = new File(MyGridConfiguration.getUserDir("conf"), 
+			"mygrid.properties.orig");
+		File properties = new File(MyGridConfiguration.getUserDir("conf"), 
+			"mygrid.properties");
+		assertFalse(properties.isFile());
+		assertFalse(propertiesOrig.isFile());
+		
+		// Create both files when they don't exist
+		MyGridConfiguration.writeDefaultProperties();
+		assertTrue(properties.isFile());
+		assertTrue(propertiesOrig.isFile());
+		assertTrue(FileUtils.contentEquals(propertiesOrig, properties));
+		
+		// rewrite mygrid.properties
+		properties.delete();
+		MyGridConfiguration.writeDefaultProperties();
+		assertTrue(FileUtils.contentEquals(propertiesOrig, properties));
+
+		// rewrite mygrid.properties.orig
+		propertiesOrig.delete();
+		MyGridConfiguration.writeDefaultProperties();
+		assertTrue(FileUtils.contentEquals(propertiesOrig, properties));
+		
+		// We'll change the mygrid.properties so that it won't be upgraded
+		FileUtils.writeStringToFile(properties, "Changed something", "latin1");
+		String content = FileUtils.readFileToString(properties, "latin1");
+		assertEquals("Changed something", content);
+		// They should now be different
+		assertFalse(FileUtils.contentEquals(propertiesOrig, properties));
+		MyGridConfiguration.writeDefaultProperties();
+		// And it should not have been changed
+		content = FileUtils.readFileToString(properties, "latin1");
+		assertEquals("Changed something", content);
+		assertFalse(FileUtils.contentEquals(propertiesOrig, properties));
+
+		// Unless it is equal to the .orig, then it is probably
+		// just an old version
+		FileUtils.writeStringToFile(propertiesOrig, "Changed something", "latin1");
+		String origContent = FileUtils.readFileToString(propertiesOrig, "latin1");
+		assertEquals("Changed something", origContent);
+		MyGridConfiguration.writeDefaultProperties();
+		assertTrue(FileUtils.contentEquals(propertiesOrig, properties));
+		// And that crap we added should now be gone
+		origContent = FileUtils.readFileToString(propertiesOrig, "latin1");
+		assertTrue(origContent.contains("Taverna"));
+		assertFalse(origContent.contains("Changed something"));
+	}
+	
+	public void testloadUserProperties() throws IOException {
+		Properties empty = new Properties();
+		// Should be empty as the file doesn't even exist yet
+		assertEquals(empty, MyGridConfiguration.loadUserProperties());
+		// Did not touch the global cache, that is only touched by
+		// getProperties();
+		assertNull(MyGridConfiguration.properties);
+
+		// OK, let's get something to work with
+		MyGridConfiguration.writeDefaultProperties();
+		
+		// the default config should be all ## commented out
+		assertEquals(empty, MyGridConfiguration.loadUserProperties());
+		File properties = new File(MyGridConfiguration.getUserDir("conf"), 
+			"mygrid.properties");
+		String content = FileUtils.readFileToString(properties, "latin1");
+		// uncomment those defaults
+		content = content.replace("\n##", "\n");
+		FileUtils.writeStringToFile(properties, content, "latin1");
+		
+		// Now it should no longer be empty
+		assertFalse(empty.equals(MyGridConfiguration.loadUserProperties()));
+		// but it should be the same as the default properties
+		assertEquals(MyGridConfiguration.loadDefaultProperties(),
+					MyGridConfiguration.loadUserProperties());
+		
+		// OK, let's try to override some of those things instead. 
+		// We'll first check that we have a good property to test
+		assertEquals(propValue, 
+				MyGridConfiguration.loadDefaultProperties().get(property));
+		// We'll change that to "fish" and add a new property
+		String testProp = "taverna.test.property";
+		FileUtils.writeStringToFile(properties, 
+				property + " = fish\n" +
+				testProp + " = test\n", 
+				"latin1");
+		Properties user = MyGridConfiguration.loadUserProperties();
+		Properties defaults = MyGridConfiguration.loadDefaultProperties();
+		// Our new property should be there
+		assertEquals("test", user.get(testProp));
+		// but not in the defaults
+		assertNull(defaults.get(testProp));
+		// In defaults, propValue is unchanged
+		assertEquals(propValue, 
+				defaults.get(property));
+		// but the user props should have "fish" instead
+		assertEquals("fish", user.get(property));
+	}
+	
+	public void testFindDefaultResources() {
+		for (URL url : MyGridConfiguration.findResources("fish.properties")) {
+			fail("Should not have found " + url);
+		}
+		boolean foundMygrid = false;
+		for (URL url : MyGridConfiguration.findResources("mygrid.properties")) {
+			if (foundMygrid) {
+				fail("Found more than one mygrid.properties: " + url);
+			}
+			foundMygrid = true;
+			assertTrue(url.getPath().contains("mygrid.properties"));
+		}
+		assertTrue(foundMygrid);
+	}
+	
+	public void testLoadDefaultProperties() {
+		Properties props = MyGridConfiguration.loadDefaultProperties();
+		// Did not touch the global cache, that is only touched by
+		// getProperties();
+		assertNull(MyGridConfiguration.properties);
+		// We'll assume this property to be loaded
+		assertEquals(propValue, props.get(property));
+	}
+	
+	public void testGetPropertiesLoadDefault() {
+		assertNull(MyGridConfiguration.properties);
+		Properties props = MyGridConfiguration.getProperties();
+		assertNotNull(MyGridConfiguration.properties);
+		assertEquals(propValue, props.get(property));
+	}
+	
+	public void testGetPropertiesWithUser() throws IOException {
+		File properties = new File(MyGridConfiguration.getUserDir("conf"), 
+			"mygrid.properties");
+		String testProp = "taverna.test.property";
+		FileUtils.writeStringToFile(properties, 
+				property + " = fish\n" +
+				testProp + " = test\n", 
+				"latin1");
+		Properties props = MyGridConfiguration.getProperties();
+		assertEquals("test", props.get(testProp));
+		assertEquals("fish", props.get(property));
+	}
+	
+}
