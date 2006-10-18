8c212760a191e008bedd0d9d2a62aa2b8ba0dde0
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-10-18 15:21:54 +0000

    Fix WSDLI...TaskTest so that it sets up the TavernaSPIRegistry first.
    git-svn-id: http://taverna.googlecode.com/svn@2147 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/processors/taverna-wsdl-processor/src/test/java/org/embl/ebi/escience/scuflworkers/wsdl/Attic/WSDLInvocationTaskTest.java
+++ trunk/taverna1.0/processors/taverna-wsdl-processor/src/test/java/org/embl/ebi/escience/scuflworkers/wsdl/Attic/WSDLInvocationTaskTest.java
@@ -1,19 +1,35 @@
 package org.embl.ebi.escience.scuflworkers.wsdl;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import junit.framework.TestCase;
+import net.sf.taverna.raven.repository.impl.LocalRepository;
 
 import org.embl.ebi.escience.baclava.DataThing;
 import org.embl.ebi.escience.scufl.Processor;
+import org.embl.ebi.escience.scufl.ScuflException;
+import org.embl.ebi.escience.utils.TavernaSPIRegistry;
 
 import uk.ac.soton.itinnovation.freefluo.core.flow.Flow;
 import uk.ac.soton.itinnovation.freefluo.task.LogLevel;
 import uk.ac.soton.itinnovation.taverna.enactor.entities.ProcessorTask;
+import uk.ac.soton.itinnovation.taverna.enactor.entities.TaskExecutionException;
 
 public class WSDLInvocationTaskTest extends TestCase {
-	public void testExecute() throws Exception {
+	
+	public void setUp() throws IOException {
+		File tmpDir = File.createTempFile("taverna", "raven");
+		assertTrue(tmpDir.delete());
+		LocalRepository tempRepository = LocalRepository.getRepository(tmpDir);
+		assertTrue(tmpDir.isDirectory());
+		TavernaSPIRegistry.setRepository(tempRepository);		
+	}
+	
+
+	public void testExecute() throws ScuflException, TaskExecutionException  {
 		Processor processor = new WSDLBasedProcessor(
 				null,
 				"guid",
