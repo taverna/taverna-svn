5d29644d8f975bd00bf81aa05b0e3b13e3ff78a8
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-10-30 15:31:08 +0000

    Some syntactic code cleanup (like spaces around = in f=value)
    Added some javadoc
    
    Uses VersionComparator for finding latest version (instead of almost-duplicated code)
    
    
    git-svn-id: http://taverna.googlecode.com/svn@2211 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/spi/Profile.java
+++ trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/spi/Profile.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,7 +13,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
@@ -47,12 +47,13 @@ import org.xml.sax.SAXException;
  * </pre>
  * Note that as this is only used by the SPI mechanism there is no need to include dependencies
  * of these artifacts, the only entries required are those which directly contain SPI implementations
+ * 
  * @author Tom Oinn
  *
  */
 public class Profile implements ArtifactFilter {
-
 	private Set<Artifact> artifacts = new HashSet<Artifact>();
+	
 	private boolean strict;
 	
 	private String version;
@@ -60,7 +61,8 @@ public class Profile implements ArtifactFilter {
 	
 	/**
 	 * Create a Profile and initialize it from the specified
-	 * InputStream of XML (see class description)<p>
+	 * InputStream of XML (see class description)
+	 * <p>
 	 * If the strict setting is set to true then the filter
 	 * operation is a straight set intersection of the set to be
 	 * filtered and the set of artifacts within this profile. If
@@ -71,6 +73,7 @@ public class Profile implements ArtifactFilter {
 	 * allows through components which are unknown to the profile and
 	 * can be used to compose the union of multiple profiles by 
 	 * adding each one to the filter chain in turn.
+	 * 
 	 * @param is Inputstream to read XML from
 	 * @param strict only allows exact matches to the profile through
 	 * if true, if false then artifacts which don't exist in the profile
@@ -82,7 +85,7 @@ public class Profile implements ArtifactFilter {
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		Document document;		
-		this.strict=strict;
+		this.strict = strict;
 		
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();			
@@ -97,22 +100,22 @@ public class Profile implements ArtifactFilter {
 			throw new InvalidProfileException("Failed to create XML parser", e);
 		}
 		
-		//determine the version if available		
-		Node profileVersionAttribute=document.getDocumentElement().getAttributes().getNamedItem("version");
-		if (profileVersionAttribute!=null) {
-			version=profileVersionAttribute.getNodeValue();
-		}
-		else {
+		// determine the version if available		
+		Node profileVersionAttribute = document.getDocumentElement().getAttributes().getNamedItem("version");
+		if (profileVersionAttribute != null) {
+			version = profileVersionAttribute.getNodeValue();
+		} else {
 			System.out.println("Profile document contains no version.");
+			version = null;
 		}
 		
-//		determine the name if available		
+		// determine the name if available		
 		Node profileNameAttribute=document.getDocumentElement().getAttributes().getNamedItem("name");
-		if (profileNameAttribute!=null) {
-			name=profileNameAttribute.getNodeValue();
-		}
-		else {
+		if (profileNameAttribute != null) {
+			name = profileNameAttribute.getNodeValue();
+		} else {
 			System.out.println("Profile document contains no name.");
+			name = null;
 		}
 							
 		NodeList nodelist = document.getDocumentElement().getChildNodes();
@@ -124,26 +127,28 @@ public class Profile implements ArtifactFilter {
 				Node anode = atts.getNamedItem("artifactId");
 				Node vnode = atts.getNamedItem("version");
 				if (gnode == null || anode == null || vnode == null) {
-					throw new InvalidProfileException("Entries must contain group, artifact, version");
+					throw new InvalidProfileException("Entries must contain groupId, artifactId, version");
 				}
-				Artifact a = new BasicArtifact(gnode.getNodeValue(),
+				Artifact artifact = new BasicArtifact(gnode.getNodeValue(),
 						anode.getNodeValue(),
 						vnode.getNodeValue());
-				artifacts.add(a);
+				artifacts.add(artifact);
 			}
 		}		
 	}
 	
 	/**
-	 * Returns the version string of the Profile, or 'NO VERSION' if a version is not defined
+	 * Return the version string of the Profile, or 'NO VERSION' if a version is not defined
 	 */
 	public String getVersion() {
-		if (version==null) return "NO VERSION";
-		else return version;
+		if (version == null) {
+			return "NO VERSION";
+		}
+		return version;
 	}
 	
 	/**
-	 * Allows an artifact to be added to the profile at runtime
+	 * Allow an artifact to be added to the profile at runtime
 	 * @param artifact
 	 */
 	public void addArtifact(Artifact artifact) {
@@ -151,7 +156,7 @@ public class Profile implements ArtifactFilter {
 	}
 	
 	/**
-	 * returns the name of the profile, or null if no name is defined
+	 * Return the name of the profile, or null if no name is defined
 	 */
 	public String getName() {
 		return name;
@@ -165,23 +170,25 @@ public class Profile implements ArtifactFilter {
 	}
 
 	/**
-	 * Returns the intersection of the set of Artifacts in this Profile
+	 * Return the intersection of the set of Artifacts in this Profile
 	 * and that presented to this method if strict is true, otherwise
 	 * return the intersection plus all artifacts in the set which have
 	 * no match within the profile when only groupId and artifactId are
 	 * taken into account.
+	 * 
 	 * @return filtered list of Artifact objects
 	 */
-	public Set<Artifact> filter(Set<Artifact> s) {
+	public Set<Artifact> filter(Set<Artifact> intersecting) {
 		Set<Artifact> result = new HashSet<Artifact>();
-		for (Artifact a : s) {
-			if (artifacts.contains(a)) {
+		for (Artifact artifact : intersecting) {
+			if (artifacts.contains(artifact)) {
 				// Exact match to an entry in the profile so include it
-				result.add(a);			
-			}
-			else if (strict == false) {
-				if (containsOtherVersion(a) == false) {
-					result.add(a);					
+				result.add(artifact);
+			} else if (! strict) {
+				// We will include the artifact as long as we don't have it in 
+				// another version
+				if (! containsOtherVersion(artifact)) {
+					result.add(artifact);					
 				}
 			}			
 		}
@@ -189,13 +196,13 @@ public class Profile implements ArtifactFilter {
 	}
 	
 	/**
-	 * @param a Artifact to look for ignoring version information
+	 * @param artifact Artifact to look for ignoring version information
 	 * @return whether there is a matching pair of groupId,artifactId in this profile
 	 */
-	private boolean containsOtherVersion(Artifact a) {
-		for (Artifact a2 : artifacts) {
-			if (a2.getArtifactId().equals(a.getArtifactId()) &&
-					a2.getGroupId().equals(a.getGroupId())) {
+	private boolean containsOtherVersion(Artifact artifact) {
+		for (Artifact existing : artifacts) {
+			if (existing.getArtifactId().equals(artifact.getArtifactId()) &&
+					existing.getGroupId().equals(artifact.getGroupId())) {
 				return true;
 			}
 		}
@@ -203,103 +210,69 @@ public class Profile implements ArtifactFilter {
 	}
 	
 	/**
-	 * Selects the highest version artifact defined in the registry that fits the 
+	 * Select the highest version artifact defined in the registry that fits the 
 	 * artifactId and groupId. Useful for allowing artifacts to be defined without version with
 	 * the profile dictating the version to be used. 
-	 * 
-	 * Version numbers
-     * are treated as being integers under lexicographic ordering
-     * with the '.' character as separator. Presence of a token is
-     * assumed to be an indication of a later version when compared
-     * to absence, i.e. 1.3 &lt; 1.3.1. Where tokens are not
-     * parsable as integer numbers String ordering is applied to both
- 	 * tokens.
+	 * <p>
+	 * Versions are compared as described in VersionComparator.
  	 * 
+ 	 * @see VersionComparator
 	 * @param groupId
 	 * @param artifactId
 	 * @return the Artifact or null if not found
 	 */
 	public Artifact discoverArtifact(String groupId, String artifactId) {
-		Artifact result=null;
-		List<Artifact> matches=new ArrayList<Artifact>();
-		for (Artifact a: artifacts) {
-			if (a.getArtifactId().equals(artifactId) && a.getGroupId().equals(groupId)) {
-				matches.add(a);
+		Artifact result = null;
+		List<Artifact> matches = new ArrayList<Artifact>();
+		for (Artifact artifact: artifacts) {
+			if (artifact.getArtifactId().equals(artifactId) && 
+					artifact.getGroupId().equals(groupId)) {
+				matches.add(artifact);
 			}
 		}
-		if (matches.size()>0) {
-				Comparator<Artifact> comparator=new Comparator<Artifact>(){
-	
-				public int compare(Artifact o1, Artifact o2) {
-					if (o1.getVersion().equals(o2.getVersion()));
-					return lessThan(o1.getVersion(), o2.getVersion()) ? 1 : -2;
-					
-				}
-				
-				private boolean lessThan(String a, String b) {
-					String[] va = a.split("\\.");
-					String[] vb = b.split("\\.");
-					for (int i = 0; i < va.length || i < vb.length ; i++) {
-						if (i == va.length) {
-							return true;
-						}
-						else if (i == vb.length) {
-							return false;
-						}
-						String ca = va[i];
-						String cb = vb[i];
-						
-						try {
-							int ia = Integer.parseInt(ca);
-							int ib = Integer.parseInt(cb);
-							if (ia < ib) {
-								return true;
-							}
-							else if (ia > ib) {
-								return false;
-							}
-						}
-						catch (NumberFormatException nfe) {
-							if (ca.compareTo(cb) > 0) {
-								return false;
-							}
-							else if (ca.compareTo(cb) < 0) {
-								return true;
-							}
-						}
-					}
-					return false;
-				}
-				
-			};
-			Collections.sort(matches,comparator);
-			result=matches.get(0);
+		if (matches.size() > 0) {
+			VersionComparator.sort(matches);
+			// return LAST element (ie. highest version)
+			result = matches.get(matches.size() - 1);
 		}
 		return result;
 	}
 	
-	public void write(OutputStream outputStream) throws Exception {
+	/**
+	 * Generate the XML representation of this profile, write to given output stream.
+	 * 
+	 * @param outputStream Stream to output profile as XML.
+	 * 
+	 * @throws ParserConfigurationException If a DocumentBuilder could not be created
+	 * @throws TransformerFactoryConfigurationError If a Transformer could not be created
+	 * @throws TransformerException If the XML document could not be transformed
+	 */
+	public void write(OutputStream outputStream) throws ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
 		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document doc=builder.newDocument();
-		Element element=doc.createElement("profile");
+		Document doc = builder.newDocument();
+		Element element = doc.createElement("profile");
 		doc.appendChild(element);
-		if (version!=null) element.setAttribute("version",version);
-		if (name!=null) element.setAttribute("name",name);
-		for (Artifact a : getArtifacts()) {
-			String groupId=a.getGroupId();
-			String artifactId=a.getArtifactId();
-			String version=a.getVersion();
+		if (version != null) {
+			element.setAttribute("version",version);
+		}
+		if (name != null) {
+			element.setAttribute("name",name);
+		}
+		for (Artifact artifact : getArtifacts()) {
+			String groupId = artifact.getGroupId();
+			String artifactId = artifact.getArtifactId();
+			String version = artifact.getVersion();
 			
-			Element artifactElement=doc.createElement("artifact");
+			Element artifactElement = doc.createElement("artifact");
 			artifactElement.setAttribute("groupId", groupId);
 			artifactElement.setAttribute("artifactId", artifactId);
 			artifactElement.setAttribute("version", version);
 			element.appendChild(artifactElement);
 		}
 		
-		Transformer transformer=TransformerFactory.newInstance().newTransformer();
-		DOMSource source=new DOMSource(doc);
-		StreamResult dest=new StreamResult(outputStream);
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult dest = new StreamResult(outputStream);
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		transformer.transform(source, dest);		
 	}
