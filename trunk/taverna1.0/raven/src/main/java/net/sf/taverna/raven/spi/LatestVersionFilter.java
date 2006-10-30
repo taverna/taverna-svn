ca3d6c6cc38083d8a6857b27d0db56bd0fbe24e2
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-10-30 14:10:45 +0000

    Refactored LatestVersionFilter to use VersionComparator, which has been extracted and refactored from Profile.
    git-svn-id: http://taverna.googlecode.com/svn@2208 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/spi/LatestVersionFilter.java
+++ trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/spi/LatestVersionFilter.java
@@ -2,79 +2,43 @@ package net.sf.taverna.raven.spi;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import net.sf.taverna.raven.repository.Artifact;
 
 /**
  * Filters the set of artifacts, removing all but the latest
  * version of each groupId,artifactId pair. Version numbers
- * are treated as being integers under lexicographic ordering
- * with the '.' character as separator. Presence of a token is
- * assumed to be an indication of a later version when compared
- * to absence, i.e. 1.3 &lt; 1.3.1. Where tokens are not
- * parsable as integer numbers String ordering is applied to both
- * tokens.
+ * are sorted as in VersionComparator.
+ * 
+ * @see VersionComparator
  * @author Tom Oinn
+ * @author Stian Soiland
+ * 
  */
 public class LatestVersionFilter implements ArtifactFilter {
 
 	public Set<Artifact> filter(Set<Artifact> artifacts) {
+		SortedSet<Artifact> sortedArtifacts = 
+			new TreeSet<Artifact>(VersionComparator.getInstance());
+		sortedArtifacts.addAll(artifacts);
 		Set<Artifact> results = new HashSet<Artifact>();
-		Set<Artifact> work = new HashSet<Artifact>(artifacts);
-		while (work.isEmpty() == false) {
-			Artifact test = work.iterator().next();
-			work.remove(test);
-			Set<Artifact> toRemove = new HashSet<Artifact>();
-			for (Artifact a : work) {
-				if (test.getGroupId().equals(a.getGroupId()) && 
-						test.getArtifactId().equals(a.getArtifactId())) {
-					toRemove.add(a);
-					test = latest(test, a);
-				}
+		Artifact previous = null;
+		for (Artifact artifact : sortedArtifacts) {
+			if (previous != null && (
+				 previous.getGroupId() != artifact.getGroupId() ||
+				 previous.getArtifactId() != artifact.getArtifactId())) {
+				// Different artifact than before, so latest 
+				// is the latest version of the previous artifact
+				results.add(previous);
 			}
-			results.add(test);
-			work.removeAll(toRemove);
+			previous = artifact;
+		}
+		// And add the last artifact (unless our set was empty)
+		if (previous != null) {
+			results.add(previous);
 		}
 		return results;
-	}
-
-	Artifact latest(Artifact a, Artifact b) {
-		return lessThan(a.getVersion(), b.getVersion())?b:a;
-	}
-	
-	boolean lessThan(String a, String b) {
-		String[] va = a.split("\\.");
-		String[] vb = b.split("\\.");
-		for (int i = 0; i < va.length || i < vb.length ; i++) {
-			if (i == va.length) {
-				return true;
-			}
-			else if (i == vb.length) {
-				return false;
-			}
-			String ca = va[i];
-			String cb = vb[i];
-			
-			try {
-				int ia = Integer.parseInt(ca);
-				int ib = Integer.parseInt(cb);
-				if (ia < ib) {
-					return true;
-				}
-				else if (ia > ib) {
-					return false;
-				}
-			}
-			catch (NumberFormatException nfe) {
-				if (ca.compareTo(cb) > 0) {
-					return false;
-				}
-				else if (ca.compareTo(cb) < 0) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
+	}	
 }
