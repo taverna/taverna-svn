395d6f533d13efcb04b4b4136b03f49dc416bbee
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-11-03 15:11:34 +0000

    This commit was manufactured by cvs2svn to create branch 'feta-1-0'.
    git-svn-id: http://taverna.googlecode.com/svn@2254 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ branches/feta-1-0/feta-engine/src/uk/ac/man/cs/img/fetaEngine/webservice/FetaPortTypeBindingImpl.java
@@ -0,0 +1,27 @@
+/**
+ * FetaPortTypeBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC3 Feb 28, 2005 (10:15:14 EST) WSDL2Java emitter.
+ */
+
+package uk.ac.man.cs.img.fetaEngine.webservice;
+
+public class FetaPortTypeBindingImpl implements uk.ac.man.cs.img.fetaEngine.webservice.FetaPortType{
+    public uk.ac.man.cs.img.fetaEngine.webservice.FetaSearchResponseType inquire(uk.ac.man.cs.img.fetaEngine.webservice.FetaCompositeSearchRequestType searchRequest) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public uk.ac.man.cs.img.fetaEngine.webservice.FetaPublishResponseType publishDescription(java.lang.String publicationRequest) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public uk.ac.man.cs.img.fetaEngine.webservice.FetaRemoveResponseType removeDescription(java.lang.String removalRequest) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public java.lang.String freeFormQuery(java.lang.String freeQueryIn) throws java.rmi.RemoteException {
+        return null;
+    }
+
+}
