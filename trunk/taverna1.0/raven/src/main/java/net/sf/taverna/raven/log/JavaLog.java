b0ac44c96530d7de3cf59ce9fb3a192a1a7b1f58
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-11-09 12:15:52 +0000

    TAV-223 Added JavaDoc
    include callingClass name in log message (used to get JavaLog in all messages)
    
    INFO maps to FINE wile DEBUG now maps to FINER. We shouldn't map to INFO as that is printed by Java default.
    
    
    git-svn-id: http://taverna.googlecode.com/svn@2291 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/log/JavaLog.java
+++ trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/log/JavaLog.java
@@ -7,16 +7,45 @@ import java.util.logging.Logger;
 
 /**
  * Implementation of LogInterface for a java.util.logging backend.
+ * To use this implementation, do:
+ * <pre>
+ * Log.setImplementation(new JavaLog());
+ * </pre>
+ * <p> 
+ * To modify java.util.logging settings and get debug messages from Raven, you
+ * can specify a system property
+ * <code>-Djava.util.logging.config.file=blah/bluh/logging.conf</code>
+ * and in <code>logging.conf</code> specify something like:
+ * <pre>
+ * handlers=java.util.logging.ConsoleHandler
+ * #.level= FINEST
+ * net.sf.taverna.level=FINEST
+ * java.util.logging.ConsoleHandler.level = FINEST
+ * java.util.logging.ConsoleHandler.formatter = java.util.logging.SimpleFormatter
+ * </pre>
+ * <p>
+ * The default log level from Java is INFO, which for Raven means anything logged 
+ * by Log.warn(), Log.error() or Log.fatal(). 
+ * The mappings between LogInterface.Priority and levels of java.util.logging are:
+ * <pre>
+ * DEBUG --> FINER
+ * INFO --> FINE
+ * WARN --> WARNING
+ * ERROR --> SEVERE
+ * FATAL --> SEVERE
+ * </pre>
  * 
  * @author Stian Soiland
  *
  */
 public class JavaLog implements LogInterface {
 	private Logger logger;
+	private String callingClass;
 	private static Map<Priority, Level> priorityMap = new HashMap<Priority, Level>();
 	static {
-		priorityMap.put(Priority.DEBUG, Level.FINE);
-		priorityMap.put(Priority.INFO, Level.INFO);
+		priorityMap.put(Priority.DEBUG, Level.FINER);
+		// Not Level.INFO - as that is printed by default
+		priorityMap.put(Priority.INFO, Level.FINE);
 		priorityMap.put(Priority.WARN, Level.WARNING);
 		// java.util.logging don't have anything between WARNING and SEVERE
 		priorityMap.put(Priority.ERROR, Level.SEVERE);
@@ -32,7 +61,8 @@ public class JavaLog implements LogInterface {
 	public JavaLog(Class c) {
 		// We'll use the full classname as our identifier to be
 		// slightly log4j compatible
-		logger = Logger.getLogger(c.getName());
+		callingClass = c.getName();
+		logger = Logger.getLogger(callingClass);
 	}
 	
 	public JavaLog getLogger(Class c) {
@@ -40,10 +70,12 @@ public class JavaLog implements LogInterface {
 	}
 
 	public void log(Priority p, Object msg, Throwable ex) {
+		// We can supply the callingClass, but don't want to start
+		// guessing the calling method
 		if (ex != null) {
-			logger.log(priorityMap.get(p), msg.toString(), ex);
+			logger.logp(priorityMap.get(p), callingClass, "?", msg.toString(), ex);
 		} else {
-			logger.log(priorityMap.get(p), msg.toString());
+			logger.logp(priorityMap.get(p), callingClass, "?", msg.toString());
 		}
 	}
 
