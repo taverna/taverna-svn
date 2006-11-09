d5730c220604a6202774053c97577470f31e815a
edwardkawas <edwardkawas@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-11-09 18:32:04 +0000

    missing the 'lib' part of the path.
    git-svn-id: http://taverna.googlecode.com/svn@2299 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/main/resources/workbench/runme.bat
+++ trunk/taverna1.0/src/main/resources/workbench/runme.bat
@@ -4,4 +4,4 @@ set ARGS=Xmx300m
 set ARGS=%ARGS% -Djava.system.class.loader=net.sf.taverna.tools.BootstrapClassLoader 
 set ARGS=%ARGS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client 
 
-java %ARGS% -jar "%~dp0\taverna-bootstrap-1.5-SNAPSHOT.jar" %*
+java %ARGS% -jar "%~dp0\lib\taverna-bootstrap-1.5-SNAPSHOT.jar" %*
