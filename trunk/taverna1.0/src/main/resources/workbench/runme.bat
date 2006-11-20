8d3a284e60e66b08fe066982563bacfdee65a1b3
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-11-20 14:21:36 +0000

    TAV-267 specify location of dot.exe as a system property
    git-svn-id: http://taverna.googlecode.com/svn@2345 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/main/resources/workbench/runme.bat
+++ trunk/taverna1.0/src/main/resources/workbench/runme.bat
@@ -3,5 +3,6 @@
 set ARGS=-Xmx300m 
 set ARGS=%ARGS% -Djava.system.class.loader=net.sf.taverna.tools.BootstrapClassLoader 
 set ARGS=%ARGS% -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client 
+set ARGS=%ARGS% "-Dtaverna.dotlocation=%~dp0\bin\win32i386\dot.exe"
 
 java %ARGS% -jar "%~dp0\lib\taverna-bootstrap-1.5-SNAPSHOT.jar" %*
