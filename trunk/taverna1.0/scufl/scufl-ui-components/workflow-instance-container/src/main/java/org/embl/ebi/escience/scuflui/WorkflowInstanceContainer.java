1ce123b4ea1caca72bb682ee71c3aeb3bb04d80d
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2006-12-04 14:35:03 +0000

    TAV-301 - avoid empty screen. Shows a tiny little help panel if there are no tabs left.
    git-svn-id: http://taverna.googlecode.com/svn@2434 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/scufl/scufl-ui-components/workflow-instance-container/src/main/java/org/embl/ebi/escience/scuflui/WorkflowInstanceContainer.java
+++ trunk/taverna1.0/scufl/scufl-ui-components/workflow-instance-container/src/main/java/org/embl/ebi/escience/scuflui/WorkflowInstanceContainer.java
@@ -2,17 +2,24 @@ package org.embl.ebi.escience.scuflui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.Action;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import org.embl.ebi.escience.scufl.enactor.WorkflowInstance;
 import org.embl.ebi.escience.scufl.enactor.WorkflowSubmissionException;
+import org.embl.ebi.escience.scuflui.actions.RunWorkflowAction;
 import org.embl.ebi.escience.scuflui.spi.UIComponentSPI;
 import org.embl.ebi.escience.scuflui.spi.WorkflowInstanceSetViewSPI;
 
@@ -27,6 +34,8 @@ import org.embl.ebi.escience.scuflui.spi.WorkflowInstanceSetViewSPI;
 public class WorkflowInstanceContainer extends JPanel implements WorkflowInstanceSetViewSPI {
 
 	private Calendar firstInstance = null;
+
+	private JPanel helpPanel = new HelpPanel();
 	
 	private JTabbedPane tabs = new JTabbedPane();
 	private Map<String, EnactorInvocation> invocations = 
@@ -39,8 +48,33 @@ public class WorkflowInstanceContainer extends JPanel implements WorkflowInstanc
 	
 	public WorkflowInstanceContainer() {
 		super(new BorderLayout());
-		add(tabs, BorderLayout.CENTER);
+		add(helpPanel, BorderLayout.CENTER);
 	}
+	
+	/**
+	 * Check if the tab panel is empty, and if so, replace it
+	 * with the help panel. Otherwise, the tab panel will be
+	 * shown.
+	 *
+	 */
+	private synchronized void checkEmptyTabs() {
+		JComponent pane;
+		if (tabs.getTabCount() == 0) {
+			pane = helpPanel;
+		} else {
+			pane = tabs;
+		}
+		// WARNING: Assumes 0 is the only component
+		if (getComponent(0) != pane) {
+			remove(0);
+			add(pane, BorderLayout.CENTER);
+			revalidate();
+			// Should have been enough with revalidate(), but at least OS X
+			// also need the repaint()..
+			repaint();
+		}
+	}
+	
 	
 	public void removeWorkflowInstance(String modelName) {
 		EnactorInvocation invocation=invocations.get(modelName);
@@ -49,6 +83,7 @@ public class WorkflowInstanceContainer extends JPanel implements WorkflowInstanc
 			invocations.remove(modelName);
 			invocation.onDispose();
 		}
+		checkEmptyTabs();
 	}
 
 	private String instanceTitle(WorkflowInstance instance) {
@@ -70,7 +105,9 @@ public class WorkflowInstanceContainer extends JPanel implements WorkflowInstanc
 			invocations.put(modelName, enactorInvocationPanel);
 
 			tabs.addTab(instanceTitle(instance), 
-					TavernaIcons.windowRun,enactorInvocationPanel);				
+					TavernaIcons.windowRun,
+					enactorInvocationPanel);		
+			checkEmptyTabs();
 			tabs.setSelectedComponent(enactorInvocationPanel);		
 		} catch (WorkflowSubmissionException e) {
 			e.printStackTrace();
@@ -103,5 +140,24 @@ public class WorkflowInstanceContainer extends JPanel implements WorkflowInstanc
 				uic.onDispose();
 			}
 		}
-	}		
+	}	
+	
+	public class HelpPanel extends JPanel {
+		public HelpPanel() {
+			super(new GridBagLayout());
+			GridBagConstraints c = new GridBagConstraints();
+			c.gridx = 0;
+			c.gridy = GridBagConstraints.RELATIVE;
+			
+			JLabel label = new JLabel("There are currently no workflow results.");
+			add(label, c);
+			
+			label = new JLabel("You can try running a workflow.");
+			add(label, c);
+			
+			Action runAction = new RunWorkflowAction(this);
+			runAction.putValue(Action.NAME, "Run current workflow");
+			add(new JButton(runAction), c);
+		}
+	}
 }
