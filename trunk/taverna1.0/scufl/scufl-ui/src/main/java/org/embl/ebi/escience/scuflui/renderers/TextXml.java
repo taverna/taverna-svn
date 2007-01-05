c1954a7fa75286fe38976210f04176b3b81dd707
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-01-05 15:18:20 +0000

    Reformatted before TAV-179
    git-svn-id: http://taverna.googlecode.com/svn@2567 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/scufl/scufl-ui/src/main/java/org/embl/ebi/escience/scuflui/renderers/TextXml.java
+++ trunk/taverna1.0/scufl/scufl-ui/src/main/java/org/embl/ebi/escience/scuflui/renderers/TextXml.java
@@ -1,6 +1,5 @@
 package org.embl.ebi.escience.scuflui.renderers;
 
-
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
@@ -12,45 +11,36 @@ import org.embl.ebi.escience.scuflui.shared.XMLTree;
 
 /**
  * Viewer to display XML as a tree.
- *
+ * 
  * @author Matthew Pocock
  */
-public class TextXml
-        extends AbstractRenderer.ByPattern
-{
-    public TextXml()
-    {
-        super("XML",
-              new ImageIcon(TextXml.class.getClassLoader().getResource(
-                "org/embl/ebi/escience/baclava/icons/text.png")),
-              Pattern.compile(".*text/xml.*"));
-    }
+public class TextXml extends AbstractRenderer.ByPattern {
+	public TextXml() {
+		super("XML", new ImageIcon(TextXml.class.getClassLoader().getResource(
+			"org/embl/ebi/escience/baclava/icons/text.png")),
+			Pattern.compile(".*text/xml.*"));
+	}
 
-    protected boolean canHandle(RendererRegistry renderers,
-                                Object userObject,
-                                String mimeType)
-    {
-        return super.canHandle(renderers, userObject, mimeType) &&
-                userObject instanceof String;
-    }
+	protected boolean canHandle(RendererRegistry renderers, Object userObject,
+		String mimeType) {
+		return super.canHandle(renderers, userObject, mimeType)
+			&& userObject instanceof String;
+	}
 
-    public boolean isTerminal()
-    {
-        return true;
-    }
+	public boolean isTerminal() {
+		return true;
+	}
 
-    public JComponent getComponent(RendererRegistry renderers,
-                                   DataThing dataThing)
-            throws RendererException
-    {
-        DataThing copy = new DataThing(dataThing);
-        copy.getMetadata().setMIMETypes(
-                Arrays.asList(strip(dataThing.getMetadata().getMIMETypes())));
+	public JComponent getComponent(RendererRegistry renderers,
+		DataThing dataThing) throws RendererException {
+		DataThing copy = new DataThing(dataThing);
+		copy.getMetadata().setMIMETypes(
+			Arrays.asList(strip(dataThing.getMetadata().getMIMETypes())));
 
-        try {
-            return new XMLTree((String) dataThing.getDataObject());
-        } catch (Exception ex) {
-          throw new RendererException(ex);
-        }
-    }
- }
+		try {
+			return new XMLTree((String) dataThing.getDataObject());
+		} catch (Exception ex) {
+			throw new RendererException(ex);
+		}
+	}
+}
