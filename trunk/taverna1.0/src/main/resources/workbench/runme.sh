7c72e3884612c32a5e2e1889f49c304a968581d9
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-01-10 12:07:42 +0000

    Added option extra bits for setting up an alternative repository and proxy information.
    git-svn-id: http://taverna.googlecode.com/svn@2593 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/src/main/resources/workbench/runme.sh
+++ trunk/taverna1.0/src/main/resources/workbench/runme.sh
@@ -24,4 +24,11 @@ ARGS="-Xmx300m"
 ARGS="$ARGS -Djava.system.class.loader=net.sf.taverna.tools.BootstrapClassLoader"
 ARGS="$ARGS -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client"
 
+#uncomment and complete the next line and complete to change the directory taverna downloads jars to
+#ARGS="$ARGS -Dtaverna.repository=<directory>"
+
+#uncomment and complete the next line to set http proxy settings, and the 2nd line if authentication is required
+#ARGS="$ARGS -Dhttp.proxyHost=<hostname or ip address> -Dhttp.proxyPort=<port>"
+#ARGS="$ARGS -Dhttp.proxyUser=<username> -Dhttp.proxyPassword=<password>"
+
 java $ARGS -jar $TAVERNA_HOME/taverna-bootstrap-1.5.0.jar $@
