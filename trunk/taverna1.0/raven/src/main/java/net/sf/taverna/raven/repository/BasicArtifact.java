6c00900d7d894f0fb965988d1afd0f632d015ee2
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-01-12 13:18:32 +0000

    Supporting TAV-342. Artifact is javadoc documented that implementors have to implement hashCode() and equals() as in BasicArtifact. Artifact is also made Comparable instead of BasicArtifact, to further highlight the fact.
    Most users should subclass BasicArtifact and get those implemented for free.
    
    This way, we can assume that Artifact instances can be compared to each-other and used in say HashMap or Set without causing trouble. Another solution would have been to change basically 95% of Artifact usage with BasicArtifact usage, and have endless codes saying "if not artifact instanceof BasicArtifact: artifact = new BasicArtifact(artifact)
    
    
    BasicArtifact adds that BasicArtifact(Artifact) constructor for copying an existing artifact.
    
    
    git-svn-id: http://taverna.googlecode.com/svn@2614 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/repository/BasicArtifact.java
+++ trunk/taverna1.0/raven/src/main/java/net/sf/taverna/raven/repository/BasicArtifact.java
@@ -1,18 +1,33 @@
 package net.sf.taverna.raven.repository;
 
 /**
- * Simple base artifact class, a bean with three strings for
- * groupId, artifactId and version. Artifacts are considered
- * as equal if all three strings match.
+ * Simple base artifact class, a bean with three strings for groupId, artifactId
+ * and version. Artifacts are considered as equal if all three strings match.
+ * 
  * @author Tom Oinn
+ * @author Stian Soiland
  */
-public class BasicArtifact implements Artifact, Comparable {
+public class BasicArtifact implements Artifact {
 
 	protected String groupId;
+
 	protected String artifactId;
+
 	protected String version;
 
 	/**
+	 * Construct a BasicArtifact as a copy of another artifact
+	 * 
+	 * @param other
+	 *            Artifact whose group/artifact/version values to copy
+	 */
+	public BasicArtifact(Artifact other) {
+		this(other.getGroupId(), other.getArtifactId(), other.getVersion());
+	}
+
+	/**
+	 * Construct a BasicArtifact from the given values
+	 * 
 	 * @param groupId
 	 * @param artifactId
 	 * @param version
@@ -24,28 +39,38 @@ public class BasicArtifact implements Artifact, Comparable {
 	}
 
 	/**
-	 * @return true if the other object is also an instance
-	 * of Artifact and all fields (artifactId, groupId and version)
-	 * are equal according to string comparison
-	 * @see java.lang.Object#equals(java.lang.Object)
+	 * Compare with another object.
+	 * 
+	 * @return true if the other object is also an instance of Artifact and all
+	 *         fields (artifactId, groupId and version) are equal according to
+	 *         string comparison
+	 * @see Artifact#equals(Object)
+	 * @see Object#equals(Object)
 	 */
+	@Override
 	public boolean equals(Object other) {
-		if (! (other instanceof Artifact)) {
+		if (other == this) {
+			return true;
+		}
+		if (!(other instanceof Artifact)) {
 			return false;
 		}
-		Artifact otherDep = (Artifact)other;
-		return (otherDep.getArtifactId().equals(getArtifactId()) &&
-					otherDep.getGroupId().equals(getGroupId()) &&
-					otherDep.getVersion().equals(getVersion()));
+		Artifact otherDep = (Artifact) other;
+		return (otherDep.getArtifactId().equals(getArtifactId())
+			&& otherDep.getGroupId().equals(getGroupId()) 
+			&& otherDep.getVersion().equals(getVersion()));
 	}
 
-	/** 
-	 * @return hashcode of the concatenation of the three
-	 * string fields
-	 * @see java.lang.Object#hashCode()
+	/**
+	 * Calculate the hash code of the artifact.
+	 * 
+	 * @return hashcode of the concatenation of the three string fields
+	 * @see Artifact#hashCode()
+	 * @see Object#hashCode()
 	 */
+	@Override
 	public int hashCode() {
-		return (getArtifactId()+getGroupId()+getVersion()).hashCode();
+		return (getArtifactId() + getGroupId() + getVersion()).hashCode();
 	}
 
 	public String getArtifactId() {
@@ -62,27 +87,32 @@ public class BasicArtifact implements Artifact, Comparable {
 
 	@Override
 	public String toString() {
-		return getGroupId()+":"+getArtifactId()+":"+getVersion();
+		return getGroupId() + ":" + getArtifactId() + ":" + getVersion();
 	}
-	
-	public int compareTo(Object arg0) {
-		if (arg0 instanceof Artifact) {
-			Artifact other = (Artifact)arg0;
-			int groupIdComp = getGroupId().compareTo(other.getGroupId());
-			if (groupIdComp == 0) {
-				int artifactIdComp = getArtifactId().compareTo(other.getArtifactId());
-				if (artifactIdComp == 0) {
-					return getVersion().compareTo(other.getVersion());
-				}
-				else {
-					return artifactIdComp;
-				}
-			}
-			else {
+
+	/**
+	 * Compare with another object. If the other instance is an Artifact, it
+	 * will be compared by group, artifact and version in that order.
+	 */
+	public int compareTo(Object other) {
+		if (super.equals(other)) {
+			return 0;
+		}
+		if (other instanceof Artifact) {
+			Artifact artifact = (Artifact) other;
+			int groupIdComp = getGroupId().compareTo(artifact.getGroupId());
+			if (groupIdComp != 0) {
 				return groupIdComp;
+			}
+			int artifactIdComp =
+				getArtifactId().compareTo(artifact.getArtifactId());
+			if (artifactIdComp == 0) {
+				return getVersion().compareTo(artifact.getVersion());
+			} else {
+				return artifactIdComp;
 			}
 		}
 		throw new ClassCastException("Cannot compare Artifact to other type");
 	}
-	
+
 }
