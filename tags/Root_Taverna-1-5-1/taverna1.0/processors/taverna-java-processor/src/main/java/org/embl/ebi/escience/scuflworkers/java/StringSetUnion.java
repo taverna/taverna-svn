78fb9f5ba3b7bc78024c973eec515d4c50da2034
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-01-23 15:15:48 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_Taverna-1-5-1'.
    git-svn-id: http://taverna.googlecode.com/svn@2672 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/Root_Taverna-1-5-1/taverna1.0/processors/taverna-java-processor/src/main/java/org/embl/ebi/escience/scuflworkers/java/StringSetUnion.java
@@ -0,0 +1,61 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scuflworkers.java;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.embl.ebi.escience.baclava.DataThing;
+
+import uk.ac.soton.itinnovation.taverna.enactor.entities.TaskExecutionException;
+
+/**
+ * Provide the union of two lists of strings, the result being a string list
+ * containing all strings that occur in either of the input lists.
+ * 
+ * @author Tom Oinn
+ */
+public class StringSetUnion implements LocalWorker {
+
+	public String[] inputNames() {
+		return new String[] { "list1", "list2" };
+	}
+
+	public String[] inputTypes() {
+		return new String[] { LocalWorker.STRING_ARRAY, LocalWorker.STRING_ARRAY };
+	}
+
+	public String[] outputNames() {
+		return new String[] { "union" };
+	}
+
+	public String[] outputTypes() {
+		return new String[] { LocalWorker.STRING_ARRAY };
+	}
+
+	public Map execute(Map inputs) throws TaskExecutionException {
+		try {
+			Set results = new HashSet();
+			Collection list1 = (Collection) ((DataThing) inputs.get("list1")).getDataObject();
+			Collection list2 = (Collection) ((DataThing) inputs.get("list2")).getDataObject();
+			results.addAll(list1);
+			results.addAll(list2);
+			List resultList = new ArrayList();
+			resultList.addAll(results);
+			Map outputs = new HashMap();
+			outputs.put("union", new DataThing(resultList));
+			return outputs;
+		} catch (NullPointerException npe) {
+			throw new TaskExecutionException("Must specify both input lists!");
+		}
+	}
+
+}
---
+++ tags/Root_Taverna-1-5-1/taverna1.0/taverna-contrib/src/main/java/net/sourceforge/taverna/scuflworkers/bsf/BSFProcessor.java
@@ -0,0 +1,109 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ */
+package net.sourceforge.taverna.scuflworkers.bsf;
+
+import java.util.Properties;
+
+import org.embl.ebi.escience.scufl.DuplicatePortNameException;
+import org.embl.ebi.escience.scufl.DuplicateProcessorNameException;
+import org.embl.ebi.escience.scufl.InputPort;
+import org.embl.ebi.escience.scufl.OutputPort;
+import org.embl.ebi.escience.scufl.Port;
+import org.embl.ebi.escience.scufl.PortCreationException;
+import org.embl.ebi.escience.scufl.Processor;
+import org.embl.ebi.escience.scufl.ProcessorCreationException;
+import org.embl.ebi.escience.scufl.ScuflModel;
+import org.embl.ebi.escience.scufl.ScuflModelEvent;
+
+/**
+ * A processor that uses the BSF scripting engine to allow arbitrary scripts to
+ * be invoked on workflow data. In this implementation all inputs and outputs
+ * are strings, if this becomes a serious issue I guess we can change it but for
+ * now this will do.
+ * 
+ * @author mfortner
+ */
+public class BSFProcessor extends Processor implements java.io.Serializable {
+
+	private String theScript = "";
+
+	private String language = "";
+
+	/**
+	 * Construct a new processor with the given model and name, delegates to the
+	 * superclass and sets the script up correctly along with the input and
+	 * output port name arrays.
+	 */
+	public BSFProcessor(ScuflModel model, String name, String script, String[] inputs, String[] outputs)
+			throws ProcessorCreationException, DuplicateProcessorNameException {
+		super(model, name);
+		// Create appropriate inputs and outputs from the arrays
+		try {
+			this.theScript = script;
+			// Iterate over inputs...
+			for (int i = 0; i < inputs.length; i++) {
+				Port p = new InputPort(this, inputs[i]);
+				p.setSyntacticType("'text/plain'");
+				addPort(p);
+			}
+			// Iterate over outputs
+			for (int i = 0; i < outputs.length; i++) {
+				Port p = new OutputPort(this, outputs[i]);
+				p.setSyntacticType("'text/plain'");
+				addPort(p);
+			}
+		} catch (DuplicatePortNameException dpne) {
+			throw new ProcessorCreationException("The supplied specification for the BSF script processor '" + name
+					+ "' contained a duplicate port '" + dpne.getMessage() + "'");
+		} catch (PortCreationException pce) {
+			throw new ProcessorCreationException("An error occured whilst generating ports for the BSF processor "
+					+ pce.getMessage());
+		}
+	}
+
+	/**
+	 * Set the script
+	 */
+	public void setScript(String theScript) {
+		if (theScript != null) {
+			this.theScript = theScript;
+		} else {
+			this.theScript = "";
+		}
+		fireModelEvent(new ScuflModelEvent(this, "Script modified"));
+	}
+
+	/**
+	 * Get the script
+	 */
+	public String getScript() {
+		return this.theScript;
+	}
+
+	/**
+	 * @return Returns the language.
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @param language
+	 *            The language to set.
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * Get the properties for this processor for display purposes
+	 */
+	public Properties getProperties() {
+		Properties props = new Properties();
+		props.put("Script", "See configurator for more information");
+		return props;
+	}
+
+}
