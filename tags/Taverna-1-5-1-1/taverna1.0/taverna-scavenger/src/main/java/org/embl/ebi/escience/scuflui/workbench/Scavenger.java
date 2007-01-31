d4294f690aef1dae502f909c42151ddf7a23c526
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-01-31 14:25:44 +0000

    This commit was manufactured by cvs2svn to create tag 'Taverna-1-5-1-1'.
    git-svn-id: http://taverna.googlecode.com/svn@2714 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/Taverna-1-5-1-1/taverna1.0/taverna-scavenger/src/main/java/org/embl/ebi/escience/scuflui/workbench/Scavenger.java
@@ -0,0 +1,22 @@
+/**
+ * This file is a component of the Taverna project,
+ * and is licensed under the GNU LGPL.
+ * Copyright Tom Oinn, EMBL-EBI
+ */
+package org.embl.ebi.escience.scuflui.workbench;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * A subclass of DefaultMutableTreeNode that should be subclassed to create
+ * particular scavengers for different types of processor
+ * 
+ * @author Tom Oinn
+ */
+@SuppressWarnings("serial")
+public class Scavenger extends DefaultMutableTreeNode {
+
+	public Scavenger(Object userObject) {
+		super(userObject);
+	} 	
+}
