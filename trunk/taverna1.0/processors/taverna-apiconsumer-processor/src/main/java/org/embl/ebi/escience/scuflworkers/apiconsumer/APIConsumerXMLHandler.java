7a4bcdb1361da72f018248ba27429a46587bdc61
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-02-23 12:05:27 +0000

    TAV-412 Support for dependencies for API consumer.
    A new right click menu "Dependencies" is shown, very similar (in fact the same) as the one in TAV-411.
    
    
    git-svn-id: http://taverna.googlecode.com/svn@2780 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/processors/taverna-apiconsumer-processor/src/main/java/org/embl/ebi/escience/scuflworkers/apiconsumer/APIConsumerXMLHandler.java
+++ trunk/taverna1.0/processors/taverna-apiconsumer-processor/src/main/java/org/embl/ebi/escience/scuflworkers/apiconsumer/APIConsumerXMLHandler.java
@@ -12,34 +12,45 @@ import org.embl.ebi.escience.scufl.ScuflModel;
 import org.embl.ebi.escience.scufl.parser.XScuflFormatException;
 import org.embl.ebi.escience.scuflworkers.ProcessorFactory;
 import org.embl.ebi.escience.scuflworkers.XMLHandler;
+import org.embl.ebi.escience.scuflworkers.dependency.DependencyXMLHandler;
 import org.jdom.Element;
 
 /**
  * Handles XML store and load for the APIConsumer processor
+ * 
  * @author Tom Oinn
+ * @author Stian Soiland
  */
 public class APIConsumerXMLHandler implements XMLHandler {
 
-    public Element elementForProcessor(Processor p) {
-	APIConsumerProcessor ap = (APIConsumerProcessor)p;
-	return ap.definition.asXML();
-    }
+	public Element elementForProcessor(Processor p) {
+		APIConsumerProcessor ap = (APIConsumerProcessor) p;
+		Element apiconsumer = ap.definition.asXML();
+		Element dependencies = DependencyXMLHandler.saveDependencies(ap);
+		apiconsumer.addContent(dependencies);
+		return apiconsumer;
+	}
 
-    public Element elementForFactory(ProcessorFactory pf) {
-	APIConsumerProcessorFactory apf = (APIConsumerProcessorFactory)pf;
-	return apf.definition.asXML();
-    }
+	public Element elementForFactory(ProcessorFactory pf) {
+		APIConsumerProcessorFactory apf = (APIConsumerProcessorFactory) pf;
+		// FIXME: Include dependencies
+		return apf.definition.asXML();
+	}
 
-    public ProcessorFactory getFactory(Element specElement) {
-	return new APIConsumerProcessorFactory(new APIConsumerDefinition(specElement));
-    }
+	public ProcessorFactory getFactory(Element specElement) {
+		return new APIConsumerProcessorFactory(new APIConsumerDefinition(
+			specElement));
+	}
 
-    public Processor loadProcessorFromXML(Element processorNode, ScuflModel model, String name) 
-	throws ProcessorCreationException,
-	       DuplicateProcessorNameException,
-	       XScuflFormatException {
-	Element spec = processorNode.getChild("apiconsumer");
-	return new APIConsumerProcessor(model, name, new APIConsumerDefinition(spec));
-    }
+	public Processor loadProcessorFromXML(Element processorNode,
+		ScuflModel model, String name) throws ProcessorCreationException,
+		DuplicateProcessorNameException, XScuflFormatException {
+		Element spec = processorNode.getChild("apiconsumer");
+		APIConsumerProcessor proc =
+			new APIConsumerProcessor(model, name, new APIConsumerDefinition(
+				spec));
+		DependencyXMLHandler.loadDependencies(proc, spec);
+		return proc;
+	}
 
 }
