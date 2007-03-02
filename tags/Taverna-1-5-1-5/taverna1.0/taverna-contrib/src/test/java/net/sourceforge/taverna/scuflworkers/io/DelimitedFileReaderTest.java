c465fa2ce7276457b32868d279c556fcd84f939b
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-03-02 15:57:00 +0000

    This commit was manufactured by cvs2svn to create tag 'Taverna-1-5-1-5'.
    git-svn-id: http://taverna.googlecode.com/svn@2805 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/Taverna-1-5-1-5/taverna1.0/taverna-contrib/src/test/java/net/sourceforge/taverna/scuflworkers/io/DelimitedFileReaderTest.java
@@ -0,0 +1,18 @@
+package net.sourceforge.taverna.scuflworkers.io;
+
+import junit.framework.TestCase;
+
+/**
+ * This class
+ * 
+ * Last edited by $Author: sowen70 $
+ * 
+ * @author Mark
+ * @version $Revision: 1.2 $
+ */
+public class DelimitedFileReaderTest extends TestCase {
+
+    public void testExecute() {
+    }
+
+}
