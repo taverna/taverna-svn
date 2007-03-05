1736db01c35a30b2cd487524807d1b77b05ea6ff
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-03-05 16:15:18 +0000

    This commit was manufactured by cvs2svn to create tag 'Taverna-1-5-1-6'.
    git-svn-id: http://taverna.googlecode.com/svn@2809 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/Taverna-1-5-1-6/taverna1.0/taverna-biomoby/src/test/java/org/biomoby/client/taverna/plugin/BiomobyParserProcessorTest.java
@@ -0,0 +1,11 @@
+package org.biomoby.client.taverna.plugin;
+
+import junit.framework.TestCase;
+
+;
+
+public class BiomobyParserProcessorTest extends TestCase {
+	public void testCreation() {
+
+	}
+}
