e81f5f1d3ea63928385dbf69d0925d4a47890388
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-05-18 18:14:29 +0000

    Removed getProperties(String owningProcess) - this didn't belong here. The Monitorable interface is now just a marker, the getProperties will be provided by each node in the tree acquired from the Monitor (this isn't in the API yet). The idea is that the Monitor exposes a limited subset of the state model to any given agent based on the entity to which that agent is bound - while some top level 'workflow engine admin' type agents will have access to the entire tree most should not.
    git-svn-id: http://taverna.googlecode.com/svn@2954 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/monitor/Monitorable.java
+++ trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/monitor/Monitorable.java
@@ -1,15 +1,15 @@
 package net.sf.taverna.t2.monitor;
 
-import java.util.List;
-
 /**
  * Implemented by entities that can be included in the monitor state tree
+ * <p>
+ * This is now a marker interface, the previous ability to get properties by
+ * process identifier was a security leak in that it allowed agents to access
+ * properties which didn't belong to them (bad idea)
  * 
  * @author Tom Oinn
  * 
  */
 public interface Monitorable {
 
-	public List<MonitorableProperty> getProperties(String owningProcess);
-	
 }
