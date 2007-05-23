97e53db8ca6a083dd15e654ff929a153672893b4
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-05-23 15:40:32 +0000

    TAV-18 Exception hierarchy, introduced MediaTypeException (when you get text/xml instead of expected text/plain - and then don't know how to parse it)
    git-svn-id: http://taverna.googlecode.com/svn@2981 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/taverna-rest-client/src/main/java/net/sf/taverna/service/rest/client/NotSuccessException.java
+++ trunk/taverna-service/taverna-rest-client/src/main/java/net/sf/taverna/service/rest/client/NotSuccessException.java
@@ -2,8 +2,12 @@ package net.sf.taverna.service.rest.client;
 
 import org.restlet.data.Status;
 
-public class NotSuccessException extends Exception {
+public class NotSuccessException extends RESTException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -594509501212057625L;
 	private Status status;
 
 	public NotSuccessException(Status status) {
