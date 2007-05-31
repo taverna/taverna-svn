7d443663e10dc6b90c5a46c45d61e0be2dec385b
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-05-31 09:42:08 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_Feta-1-1-2'.
    git-svn-id: http://taverna.googlecode.com/svn@3025 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/Root_Feta-1-1-2/feta-engine/src/main/java/uk/ac/man/cs/img/fetaEngine/store/impl/sesame2/SesameConvenience.java
@@ -0,0 +1,19 @@
+/*
+ * SesameConvenience.java
+ *
+ * Created on 04 December 2005, 18:05
+ */
+
+package uk.ac.man.cs.img.fetaEngine.store.impl.sesame2;
+
+/**
+ * 
+ * @author Pinar
+ */
+public class SesameConvenience {
+
+	/** Creates a new instance of SesameConvenience */
+	public SesameConvenience() {
+	}
+
+}
