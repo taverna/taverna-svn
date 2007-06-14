b2b8bdc191fdc7e1a55403978121479aade7c964
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-06-14 15:01:20 +0000

    TAV-18 Representation both as XML and directly
    git-svn-id: http://taverna.googlecode.com/svn@3072 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/DataResource.java
+++ trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/DataResource.java
@@ -2,8 +2,11 @@ package net.sf.taverna.service.rest.resources;
 
 import net.sf.taverna.service.datastore.bean.DataDoc;
 import net.sf.taverna.service.datastore.dao.DataDocDAO;
+import net.sf.taverna.service.xml.DataDocument;
 
 import org.apache.log4j.Logger;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
 import org.restlet.Context;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
@@ -23,6 +26,7 @@ public class DataResource extends AbstractResource {
 		dataDoc = dataDocDao.read(data_id);
 		checkEntity(dataDoc);
 		addRepresentation(new Baclava());
+		addRepresentation(new XML());
 	}
 
 	class Baclava extends AbstractText {
@@ -35,6 +39,23 @@ public class DataResource extends AbstractResource {
 		public MediaType getMediaType() {
 			return baclavaType;
 		}
+	}
+	
+	class XML extends AbstractXML {
+
+		@Override
+		public XmlObject getXML() {
+			DataDocument doc = DataDocument.Factory.newInstance();
+			doc.addNewData().addNewOwner().setHref(uriFactory.getURI(dataDoc.getOwner()));
+			XmlObject xml;
+			try {
+				xml = XmlObject.Factory.parse(dataDoc.getBaclava());
+				doc.getData().addNewBaclava().set(xml);
+			} catch (XmlException e) {
+				logger.warn("Could not parse baclava for " + dataDoc, e);
+			}
+			return doc;
+		}		
 	}
 
 }
