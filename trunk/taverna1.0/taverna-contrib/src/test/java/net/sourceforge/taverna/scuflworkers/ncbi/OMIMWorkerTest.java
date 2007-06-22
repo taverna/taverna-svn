42c59dc35f1f97db36edb36e54dca31e9e1951a0
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-06-22 13:57:50 +0000

    This test has been unstable lately due to the external service its relying on (http://www.ncbi.nlm.nih.gov/entrez/query.fcgi) Put in a catch that prevents the test failing if the problem is genuinely with the external service rather than the worker itself.
    git-svn-id: http://taverna.googlecode.com/svn@3144 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/taverna-contrib/src/test/java/net/sourceforge/taverna/scuflworkers/ncbi/OMIMWorkerTest.java
+++ trunk/taverna1.0/taverna-contrib/src/test/java/net/sourceforge/taverna/scuflworkers/ncbi/OMIMWorkerTest.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import net.sourceforge.taverna.baclava.DataThingAdapter;
+import net.sourceforge.taverna.io.TransmitterException;
 
+import org.apache.log4j.Logger;
 import org.embl.ebi.escience.scuflworkers.java.LocalWorker;
 import org.w3c.dom.Element;
+
+import uk.ac.soton.itinnovation.taverna.enactor.entities.TaskExecutionException;
 
 import junit.framework.TestCase;
 
@@ -16,17 +20,32 @@ import junit.framework.TestCase;
  * Last edited by $Author: sowen70 $
  * 
  * @author Mark
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class OMIMWorkerTest extends AbstractXmlWorkerTest {
+	
+	private static Logger logger = Logger.getLogger(OMIMWorkerTest.class);
 
     public void testExecute() throws Exception{
         LocalWorker worker = new OMIMWorker();
         Map inputMap = new HashMap();
         DataThingAdapter inAdapter = new DataThingAdapter(inputMap);
         inAdapter.putString("term","brca2");
+        Map outputMap = null;
         
-        Map outputMap = worker.execute(inputMap);
+        try {
+        	outputMap = worker.execute(inputMap);
+        }
+        catch(TaskExecutionException e) { 
+        	if (e.getCause() instanceof TransmitterException) { //don't fail the test, there is something wrong with the external service not the worker.
+        		logger.error("There is a problem with the http://www.ncbi.nlm.nih.gov/entrez/query.fcgi endpoint for OMIWorker");
+        		return;
+        	}
+        	else {
+        		throw e;
+        	}
+        }
+        
         DataThingAdapter outAdapter = new DataThingAdapter(outputMap);
         
         String results = outAdapter.getString("resultsXml");
@@ -35,8 +54,7 @@ public class OMIMWorkerTest extends AbstractXmlWorkerTest {
         this.writeFile("test_omim.xml", results);
         Element root = this.parseXml(results);
         this.testXmlNotEmpty(root);
-
-
+      
     }
 
 }
