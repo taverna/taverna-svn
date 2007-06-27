779acdaf3a183b081a700ba08aa7a85ebe1ecfa9
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-06-27 09:35:06 +0000

    TAV-18 Added icon (package_network.png from KDE Crystals at http://everaldo.com/crystal/ - LGPL) and perspective.
    git-svn-id: http://taverna.googlecode.com/svn@3156 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/execute-remotely-plugin/src/main/java/net/sf/taverna/service/executeremotely/ui/ExecuteRemotelyFactory.java
+++ trunk/taverna-service/execute-remotely-plugin/src/main/java/net/sf/taverna/service/executeremotely/ui/ExecuteRemotelyFactory.java
@@ -1,13 +1,21 @@
 package net.sf.taverna.service.executeremotely.ui;
 
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 
 import org.apache.log4j.Logger;
-import org.embl.ebi.escience.scuflui.TavernaIcons;
 import org.embl.ebi.escience.scuflui.spi.UIComponentFactorySPI;
 import org.embl.ebi.escience.scuflui.spi.UIComponentSPI;
 
 public class ExecuteRemotelyFactory implements UIComponentFactorySPI {
+
+	private static int ICON_SIZE = 16;
+	
+	static ImageIcon ICON =
+		new ImageIcon(
+			ExecuteRemotelyPerspective.ICON.getImage().getScaledInstance(ICON_SIZE,
+				ICON_SIZE, Image.SCALE_SMOOTH));
 
 	private static Logger logger =
 		Logger.getLogger(ExecuteRemotelyFactory.class);
@@ -19,11 +27,11 @@ public class ExecuteRemotelyFactory implements UIComponentFactorySPI {
 
 	// FIXME: Replace with a "Run remotely" icon
 	public ImageIcon getIcon() {
-		return TavernaIcons.runIcon;
+		return ICON;
 	}
 
 	public String getName() {
-		return "Execute remotely";
+		return ExecuteRemotelyPerspective.NAME;
 	}
 
 }
