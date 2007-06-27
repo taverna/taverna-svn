16ea4f7674699737c9e865c50cb8e15779e001c1
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-06-27 14:01:53 +0000

    TAV-18 updated DataDoc tests, tests for empty and blank baclava POST
    git-svn-id: http://taverna.googlecode.com/svn@3166 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/taverna-rest/src/test/java/net/sf/taverna/service/rest/TestDataDoc.java
+++ trunk/taverna-service/taverna-rest/src/test/java/net/sf/taverna/service/rest/TestDataDoc.java
@@ -1,17 +1,19 @@
 package net.sf.taverna.service.rest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
 import net.sf.taverna.service.interfaces.ParseException;
 import net.sf.taverna.service.xml.Data;
+import net.sf.taverna.service.xml.DataDocument;
 import net.sf.taverna.service.xml.DatasDocument;
 
 import org.apache.xmlbeans.XmlException;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.restlet.Client;
 import org.restlet.data.MediaType;
@@ -21,16 +23,17 @@ import org.restlet.data.Protocol;
 import org.restlet.data.Reference;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.w3c.dom.Node;
 
 /**
  * Unit tests for datadoc resource creation and retrieval
  * 
  * @author Stian Soiland
- *
  */
 
 public class TestDataDoc extends ClientTest {
-	
+
 	private static Reference justCreated;
 
 	@BeforeClass
@@ -38,41 +41,39 @@ public class TestDataDoc extends ClientTest {
 		justCreated = null;
 	}
 
-	@Ignore
 	@Test
 	public void getAll() throws IOException {
-		Request request = new Request();
+		Request request = makeAuthRequest();
 		Client client = new Client(Protocol.HTTP);
-		request.setResourceRef(BASE_URL + "data");
+		request.setResourceRef(useruri + "/data");
 		request.setMethod(Method.GET);
 		Response response = client.handle(request);
-		assertTrue(response.getStatus().isSuccess());
-		//response.getEntity().write(System.out);
+		assertEquals(Status.SUCCESS_OK, response.getStatus());
+		assertNotNull(response.getEntity().getText());
 	}
 
-	@Ignore
 	@Test
 	public void getAllXML() throws IOException, ParseException, XmlException {
-		Request request = new Request();
+		Request request = makeAuthRequest();
 		Client client = new Client(Protocol.HTTP);
-		request.setResourceRef(BASE_URL + "data");
+		request.setResourceRef(useruri + "/data");
 		request.setMethod(Method.GET);
 		request.getClientInfo().getAcceptedMediaTypes().add(
 			new Preference<MediaType>(restType));
 		Response response = client.handle(request);
 		assertTrue(response.getStatus().isSuccess());
-		DatasDocument doc = DatasDocument.Factory.parse(response.getEntity().getStream());
+		DatasDocument doc =
+			DatasDocument.Factory.parse(response.getEntity().getStream());
 		for (Data data : doc.getDatas().getDataArray()) {
-			assertTrue(data.getHref().startsWith(BASE_URL + "users"));
+			assertTrue(data.getHref().startsWith(BASE_URL + "data"));
 		}
 	}
 
-	@Ignore
 	@Test
 	public void create() throws IOException {
-		Request request = new Request();
+		Request request = makeAuthRequest();
 		Client client = new Client(Protocol.HTTP);
-		request.setResourceRef(BASE_URL + "data");
+		request.setResourceRef(useruri + "/data");
 		request.setMethod(Method.POST);
 		request.setEntity(datadoc, baclavaType);
 		Response response = client.handle(request);
@@ -80,33 +81,36 @@ public class TestDataDoc extends ClientTest {
 		justCreated = response.getRedirectRef();
 	}
 
-	@Ignore
 	@Test
-	public void readJustCreated() throws IOException {
-		if (justCreated == null) {
-			create();
-		}
-		assertNotNull("create() must be run first", justCreated);
-		Request request = new Request();
+	public void createEmpty() throws IOException {
+		Request request = makeAuthRequest();
 		Client client = new Client(Protocol.HTTP);
-		request.setResourceRef(justCreated);
-		request.setMethod(Method.GET);
+		request.setResourceRef(useruri + "/data");
+		request.setMethod(Method.POST);
+		request.setEntity("", baclavaType);
 		Response response = client.handle(request);
-		assertTrue(response.getStatus().isSuccess());
-		String result = response.getEntity().getText();
-		// Should do text/plain as fallback
-		assertTrue(MediaType.TEXT_PLAIN.includes(response.getEntity().getMediaType()));
-		assertTrue(result.startsWith("Data document "));
+		assertEquals(Status.CLIENT_ERROR_BAD_REQUEST, response.getStatus());
+		assertNull(response.getRedirectRef());
 	}
 
-	@Ignore
 	@Test
-	public void readJustCreatedXML() throws IOException {
+	public void createMissing() throws IOException {
+		Request request = makeAuthRequest();
+		Client client = new Client(Protocol.HTTP);
+		request.setResourceRef(useruri + "/data");
+		request.setMethod(Method.POST);
+		Response response = client.handle(request);
+		assertEquals(Status.CLIENT_ERROR_BAD_REQUEST, response.getStatus());
+		assertNull(response.getRedirectRef());
+	}
+
+	@Test
+	public void readJustCreatedXML() throws IOException, XmlException {
 		if (justCreated == null) {
 			create();
 		}
 		assertNotNull("create() must be run first", justCreated);
-		Request request = new Request();
+		Request request = makeAuthRequest();
 		Client client = new Client(Protocol.HTTP);
 		request.setResourceRef(justCreated);
 		request.setMethod(Method.GET);
@@ -115,6 +119,33 @@ public class TestDataDoc extends ClientTest {
 		Response response = client.handle(request);
 		assertTrue(response.getStatus().isSuccess());
 		assertTrue(restType.includes(response.getEntity().getMediaType()));
+		DataDocument doc =
+			DataDocument.Factory.parse(response.getEntity().getStream());
+		assertEquals(useruri, doc.getData().getOwner().getHref());
+		Node baclavaNode = doc.getData().getBaclava().getDomNode();
+		baclavaNode.normalize();
+		Node a = baclavaNode.getChildNodes().item(1);
+		assertEquals("dataThingMap", a.getLocalName());
+		assertEquals("http://org.embl.ebi.escience/baclava/0.1alpha",
+			a.getNamespaceURI());
+		assertEquals(3, a.getChildNodes().getLength());
+	}
+
+	@Test
+	public void readJustCreatedBaclava() throws IOException {
+		if (justCreated == null) {
+			create();
+		}
+		assertNotNull("create() must be run first", justCreated);
+		Request request = makeAuthRequest();
+		Client client = new Client(Protocol.HTTP);
+		request.setResourceRef(justCreated);
+		request.setMethod(Method.GET);
+		request.getClientInfo().getAcceptedMediaTypes().add(
+			new Preference<MediaType>(baclavaType));
+		Response response = client.handle(request);
+		assertTrue(response.getStatus().isSuccess());
+		assertTrue(baclavaType.includes(response.getEntity().getMediaType()));
 		assertTrue(response.getEntity().getText().equals(datadoc));
 	}
 
