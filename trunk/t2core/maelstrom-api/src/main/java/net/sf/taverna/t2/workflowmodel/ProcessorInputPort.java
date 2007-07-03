3cb3d35aad9aab1aa847b114a26b8b69635c5413
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-07-03 13:18:50 +0000

    Abstracted getProcessor method to ProcessorPort, made processorinputport and processoroutputport marker or aggregator interfaces
    git-svn-id: http://taverna.googlecode.com/svn@3200 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/workflowmodel/ProcessorInputPort.java
+++ trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/workflowmodel/ProcessorInputPort.java
@@ -1,16 +1,12 @@
 package net.sf.taverna.t2.workflowmodel;
 
 /**
- * Input port on a Processor
+ * Input port on a Processor, is both a filtering input port and a processor
+ * port
  * 
  * @author Tom Oinn
- *
+ * 
  */
-public interface ProcessorInputPort extends FilteringInputPort {
+public interface ProcessorInputPort extends FilteringInputPort, ProcessorPort {
 
-	/**
-	 * Get the Processor to which this port belongs
-	 */
-	public Processor getProcessor();
-	
 }
--- trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/workflowmodel/ProcessorOutputPort.java
+++ trunk/t2core/maelstrom-api/src/main/java/net/sf/taverna/t2/workflowmodel/ProcessorOutputPort.java
@@ -1,16 +1,13 @@
 package net.sf.taverna.t2.workflowmodel;
 
 /**
- * Input port on a Processor
+ * Input port on a Processor, is both an event forwarding output port and a
+ * processor port
  * 
  * @author Tom Oinn
- *
+ * 
  */
-public interface ProcessorOutputPort extends EventForwardingOutputPort {
+public interface ProcessorOutputPort extends EventForwardingOutputPort,
+		ProcessorPort {
 
-	/**
-	 * Get the Processor to which this port belongs
-	 */
-	public Processor getProcessor();
-	
 }
