a629abc9875f975f8b171c652e4e196073c42459
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-07-10 08:55:06 +0000

    Redirection to the correct page after adding or deleting workers.
    git-svn-id: http://taverna.googlecode.com/svn@3223 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/WorkersResource.java
+++ trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/WorkersResource.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 
 public class WorkersResource extends AbstractResource {
 
@@ -27,12 +28,18 @@ public class WorkersResource extends AbstractResource {
 	}
 
 	public void handlePost() {
-		Worker worker = new Worker();
-		worker.setPassword("Bob"); //FIXME: hard-coded password
-		worker.setQueue(daoFactory.getQueueDAO().defaultQueue());
-		DAOFactory.getFactory().getWorkerDAO().create(worker);
-		DAOFactory.getFactory().commit();
-		
+		try {
+			Worker worker = new Worker();
+			worker.setPassword("Bob"); //FIXME: hard-coded password
+			worker.setQueue(daoFactory.getQueueDAO().defaultQueue());
+			DAOFactory.getFactory().getWorkerDAO().create(worker);
+			DAOFactory.getFactory().commit();
+			getResponse().setRedirectRef(getRequest().getReferrerRef());
+			getResponse().setStatus(Status.REDIRECTION_FOUND);
+		}
+		catch(Throwable e) {
+			getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "An error occurred creating the worker: "+e.getCause().getMessage());
+		}
 	}
 
 	class WorkersVelocityRepresentation extends VelocityRepresentation {
