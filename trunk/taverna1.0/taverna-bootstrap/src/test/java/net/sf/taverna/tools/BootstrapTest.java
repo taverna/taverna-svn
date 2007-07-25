7c1f042463e91e057a2e3a753e8909e0a2af4ed4
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-07-25 10:55:27 +0000

    Fixed tests that were broken by changing the taverna.home to -SNAPSHOT
    git-svn-id: http://taverna.googlecode.com/svn@3258 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/taverna-bootstrap/src/test/java/net/sf/taverna/tools/BootstrapTest.java
+++ trunk/taverna1.0/taverna-bootstrap/src/test/java/net/sf/taverna/tools/BootstrapTest.java
@@ -65,7 +65,7 @@ public class BootstrapTest extends TestCase {
 		System.setProperty("os.name", "Mac OS X");
 		Bootstrap.findUserDir();
 		File dir = new File(System.getProperty("taverna.home"));
-		File shouldBe = new File(tempHome, "Library/Application Support/Taverna-1.5.2");
+		File shouldBe = new File(tempHome, "Library/Application Support/Taverna-1.5.2-SNAPSHOT");
 		assertEquals(shouldBe, dir);
 		assertTrue(dir.isDirectory());
 
@@ -82,9 +82,9 @@ public class BootstrapTest extends TestCase {
 		String APPDATA = System.getenv("APPDATA");
 		if (APPDATA == null) {
 			// Likely on Non-Windows platform
-			shouldBe = new File(tempHome, "Taverna-1.5.2");
+			shouldBe = new File(tempHome, "Taverna-1.5.2-SNAPSHOT");
 		} else {
-			shouldBe = new File(APPDATA, "Taverna-1.5.2");
+			shouldBe = new File(APPDATA, "Taverna-1.5.2-SNAPSHOT");
 		}
 		assertEquals(shouldBe, dir);
 		assertTrue(dir.isDirectory());	
@@ -98,7 +98,7 @@ public class BootstrapTest extends TestCase {
 		System.setProperty("os.name", "Linn0x");
 		Bootstrap.findUserDir();
 		File dir = new File(System.getProperty("taverna.home"));
-		File shouldBe = new File(tempHome, ".taverna-1.5.2");
+		File shouldBe = new File(tempHome, ".taverna-1.5.2-snapshot");
 		assertEquals(shouldBe, dir);
 		assertTrue(dir.isDirectory());
 	}
