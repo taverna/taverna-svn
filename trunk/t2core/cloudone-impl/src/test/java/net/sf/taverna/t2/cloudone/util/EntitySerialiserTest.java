cc0fb7062a02017e5e02f1d5d6ac12c3465b6a03
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-04 13:08:21 +0000

    Removed redundant tests and added test for creating from File
    git-svn-id: http://taverna.googlecode.com/svn@3545 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-impl/src/test/java/net/sf/taverna/t2/cloudone/util/EntitySerialiserTest.java
+++ trunk/t2core/cloudone-impl/src/test/java/net/sf/taverna/t2/cloudone/util/EntitySerialiserTest.java
@@ -1,37 +1,30 @@
 package net.sf.taverna.t2.cloudone.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 
-import net.sf.taverna.t2.cloudone.EntityNotFoundException;
-import net.sf.taverna.t2.cloudone.EntityRetrievalException;
-import net.sf.taverna.t2.cloudone.EntityStorageException;
 import net.sf.taverna.t2.cloudone.LocationalContext;
-import net.sf.taverna.t2.cloudone.ReferenceScheme;
 import net.sf.taverna.t2.cloudone.bean.SillyBean;
 import net.sf.taverna.t2.cloudone.datamanager.memory.InMemoryDataManager;
-import net.sf.taverna.t2.cloudone.entity.DataDocument;
-import net.sf.taverna.t2.cloudone.entity.EntityList;
-import net.sf.taverna.t2.cloudone.identifier.DataDocumentIdentifier;
-import net.sf.taverna.t2.cloudone.identifier.EntityListIdentifier;
 import net.sf.taverna.t2.cloudone.impl.url.URLReferenceScheme;
 
 import org.jdom.Element;
 import org.jdom.JDOMException;
-import org.jdom.output.XMLOutputter;
 import org.junit.Test;
 
 
 public class EntitySerialiserTest {
 	
+	private ClassLoader classLoader = EntitySerialiserTest.class.getClassLoader();
+	
 	InMemoryDataManager dManager = new InMemoryDataManager("dataNS",
 			new HashSet<LocationalContext>());
 
-	XMLOutputter xo = new XMLOutputter();
-
-	
 	@Test
 	public void serialiseSillyBean() throws JDOMException, IOException {
 		SillyBean silly = new SillyBean();
@@ -41,33 +34,32 @@ public class EntitySerialiserTest {
 		assertEquals("I'm silly", silly2.getName());
 	}
 	
-	
 	@Test
-	public void serialiseDataDocument() throws EntityNotFoundException, JDOMException, IOException, EntityStorageException, EntityRetrievalException {
-		DataDocumentIdentifier docid = dManager.registerDocument(new HashSet<ReferenceScheme>());
-		System.out.println(docid);
-		DataDocument doc = (DataDocument) dManager.getEntity(docid);
-		Element elem = EntitySerialiser.toXML(doc);
-		System.out.println(xo.outputString(elem));
+	public void serialiseAsFile() throws IOException, JDOMException {
+		File file = File.createTempFile("test", ".xml");
+		file.deleteOnExit();
+		file.delete();
+		assertFalse(file.exists());
+		SillyBean silly = new SillyBean();
+		silly.setName("I'm silly");
+		EntitySerialiser.toXMLFile(silly, file);
+		assertTrue(file.exists());
+		// Should be somewhere between 50 (it's XML) and 1024 :-)
+		assertTrue("Serialised file too small", file.length() > 50);
+		assertTrue("Serialised file too big", file.length() < 1024);
+		SillyBean silly2 = (SillyBean) EntitySerialiser.fromXMLFile(file);
+		assertEquals("I'm silly", silly2.getName());
 	}
 	
 	
-	@Test
-	public void serialiseEmptyList() throws EntityNotFoundException, JDOMException, IOException, EntityStorageException, EntityRetrievalException {
-		int depth = 3;
-		EntityListIdentifier list = dManager.registerEmptyList(depth);
-		assertEquals(depth, list.getDepth());
-		EntityList entList = (EntityList) dManager.getEntity(list);
-		Element elem = EntitySerialiser.toXML(entList);
-		System.out.println(xo.outputString(elem));
-	}
 	
 	@Test
 	public void serialiseURLRefScheme() throws JDOMException, IOException {
 		URLReferenceScheme urlRef = new URLReferenceScheme();
-		urlRef.setFromBean("http://taverna.sf.net/");
+		final String bean = "http://taverna.sf.net/";
+		urlRef.setFromBean(bean);
 		Element elem = EntitySerialiser.toXML(urlRef.getAsBean());
-		System.out.println( xo.outputString(elem));
+		assertEquals(bean, EntitySerialiser.fromXML(elem, classLoader));
 	}
 	
 
