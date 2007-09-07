aee1c6d2f5b740652b7525f2787ae542ac8372f0
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-07 10:43:36 +0000

    Renamed Entity(.*)Exception to just $1Exception so they can also be used by BlobStore
    git-svn-id: http://taverna.googlecode.com/svn@3551 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/datamanager/NotFoundException.java
@@ -0,0 +1,31 @@
+package net.sf.taverna.t2.cloudone.datamanager;
+
+/**
+ * Thrown when a request is made for resolution of an entity or blob that cannot
+ * be found by the store to which the request is made.
+ * 
+ * @author Tom Oinn
+ * @author Ian Dunlop
+ * @author Stian Soiland
+ * 
+ */
+public class NotFoundException extends Exception {
+
+	private static final long serialVersionUID = -1069998094174721609L;
+
+	public NotFoundException() {
+	}
+
+	public NotFoundException(String msg) {
+		super(msg);
+	}
+
+	public NotFoundException(Throwable throwable) {
+		super(throwable);
+	}
+
+	public NotFoundException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+}
---
+++ trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/datamanager/RetrievalException.java
@@ -0,0 +1,32 @@
+package net.sf.taverna.t2.cloudone.datamanager;
+
+import net.sf.taverna.t2.cloudone.DataManager;
+
+/**
+ * Thrown when an entity or blob can't be retrieved from a {@link DataManager},
+ * for instance because of access failure.
+ * 
+ * @author Ian Dunlop
+ * @author Stian Soiland
+ * 
+ */
+public class RetrievalException extends RuntimeException {
+
+	private static final long serialVersionUID = -3674091401600650755L;
+
+	public RetrievalException() {
+	}
+
+	public RetrievalException(String message) {
+		super(message);
+	}
+
+	public RetrievalException(Throwable cause) {
+		super(cause);
+	}
+
+	public RetrievalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
