4c0d2b64be2ce0c3e53ec3e640c52ea46cec708b
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-21 12:54:16 +0000

    A very simple DataPeer implementation for use in URLLocationalContext tests
    git-svn-id: http://taverna.googlecode.com/svn@3605 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-impl/src/main/java/net/sf/taverna/t2/cloudone/datamanager/DataPeerImpl.java
@@ -0,0 +1,42 @@
+package net.sf.taverna.t2.cloudone.datamanager;
+
+import java.util.List;
+import java.util.Set;
+
+import net.sf.taverna.t2.cloudone.DataManager;
+import net.sf.taverna.t2.cloudone.DataPeer;
+import net.sf.taverna.t2.cloudone.LocationalContext;
+import net.sf.taverna.t2.cloudone.entity.DataDocument;
+import net.sf.taverna.t2.cloudone.identifier.DataDocumentIdentifier;
+
+public class DataPeerImpl implements DataPeer {
+
+	private DataManager dataManager;
+	
+	public DataPeerImpl(DataManager dataManager) {
+		this.dataManager = dataManager;
+	}
+	public DataDocument exportDataDocument(
+			Set<LocationalContext> remoteContext,
+			DataDocumentIdentifier identifier) throws NotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCurrentNamespace() {
+		return this.dataManager.getCurrentNamespace();
+	}
+
+	public DataManager getDataManager() {
+		return this.dataManager;
+	}
+
+	public Set<LocationalContext> getLocationalContexts() {
+		return this.dataManager.getLocationalContexts();
+	}
+
+	public List<String> getManagedNamespaces() {
+		return this.dataManager.getManagedNamespaces();
+	}
+
+}
