f9a871fe91ea94eff06a88de8ef5ed7b9359c0d4
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-27 08:55:02 +0000

    Removed unneccessary TODOs
    git-svn-id: http://taverna.googlecode.com/svn@3626 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/DereferenceException.java
+++ trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/DereferenceException.java
@@ -3,36 +3,32 @@ package net.sf.taverna.t2.cloudone;
 /**
  * Thrown if an attempt to dereference a reference scheme instance fails for
  * whatever reason. This failure may be anticipated or unexpected.
- * 
+ *
  * @author Tom Oinn
  * @author Matthew Pocock
- * 
+ *
  */
 public class DereferenceException extends Exception {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public DereferenceException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public DereferenceException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
+	public DereferenceException(String msg, Throwable cause) {
+		super(msg, cause);
 	}
 
-	public DereferenceException(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
+	public DereferenceException(String msg) {
+		super(msg);
 	}
 
-	public DereferenceException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
+	public DereferenceException(Throwable cause) {
+		super(cause);
 	}
 
 }
