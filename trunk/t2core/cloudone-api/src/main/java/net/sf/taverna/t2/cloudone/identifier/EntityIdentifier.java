7ab379a9433030101c6934d132b0068e39510564
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-27 10:47:25 +0000

    id -> identifier
    Reformat, organised imports, sorted methods
    
    
    git-svn-id: http://taverna.googlecode.com/svn@3632 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/identifier/EntityIdentifier.java
+++ trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/identifier/EntityIdentifier.java
@@ -5,14 +5,18 @@ import net.sf.taverna.t2.cloudone.bean.Beanable;
 /**
  * Identifier within the data cloud system for a single DataDocument, error or
  * list object.
- * 
+ *
  * @author Tom Oinn
  * @author Matthew Pocock
- * 
+ *
  */
 public abstract class EntityIdentifier implements Beanable<String> {
 
-	private String id;
+	public final static boolean isValidName(String name) {
+		return name != null && name.matches("[a-zA-Z0-9\\-_\\.]+");
+	}
+
+	private String identifier;
 
 	private IDType type;
 
@@ -20,16 +24,16 @@ public abstract class EntityIdentifier implements Beanable<String> {
 
 	/**
 	 * For use in combination with {@link #setFromBean(EntityIdentifierBean)}
-	 * 
+	 *
 	 */
 	public EntityIdentifier() {
-		id = null;
+		identifier = null;
 		type = null;
 		namespace = null;
 	}
 
-	public EntityIdentifier(String id) throws MalformedIdentifierException {
-		setFromBean(id);
+	public EntityIdentifier(String identifier) throws MalformedIdentifierException {
+		setFromBean(identifier);
 	}
 
 	@Override
@@ -37,42 +41,47 @@ public abstract class EntityIdentifier implements Beanable<String> {
 		if (!(o instanceof EntityIdentifier)) {
 			return false;
 		}
-		return ((EntityIdentifier) o).id.equals(this.id);
+		return ((EntityIdentifier) o).identifier.equals(identifier);
 	}
 
 	public String getAsBean() {
-		return this.id;
+		return identifier;
 	}
 
 	/**
 	 * Return the conceptual depth of the entity identified by this identifier
-	 * 
+	 *
 	 * @return
 	 */
 	public abstract int getDepth();
 
 	/**
+	 * Get the local name of the entity
+	 */
+	public abstract String getName();
+
+	/**
 	 * Each identifier has a namespace allocated by the datamanager that created
 	 * it
-	 * 
+	 *
 	 * @return
 	 */
 	public String getNamespace() {
-		return this.namespace;
+		return namespace;
 	}
 
 	/**
 	 * Get the type of this identifier
-	 * 
+	 *
 	 * @return
 	 */
 	public IDType getType() {
-		return this.type;
+		return type;
 	}
 
 	@Override
 	public int hashCode() {
-		return id.hashCode();
+		return identifier.hashCode();
 	}
 
 	public void setFromBean(String id) throws MalformedIdentifierException {
@@ -84,38 +93,28 @@ public abstract class EntityIdentifier implements Beanable<String> {
 		}
 
 		String[] pathParts = split[3].split("/", 4);
-		this.namespace = pathParts[2];
-		if (! isValidName(namespace)) {
+		namespace = pathParts[2];
+		if (!isValidName(namespace)) {
 			throw new MalformedIdentifierException("Invalid namespace '"
 					+ namespace + "' in " + id);
 		}
 		validate(pathParts[3]);
-		this.id = id;
+		identifier = id;
 	}
 
-	public final static boolean isValidName(String name) {
-		return name != null && name.matches("[a-zA-Z0-9\\-_\\.]+");
-	}
-	
 	@Override
 	public String toString() {
-		return this.id;
+		return identifier;
 	}
-
-	/**
-	 * Get the local name of the entity
-	 */
-	public abstract String getName();
 
 	/**
 	 * Called in the constructor to validate the specified identifier. Do not
 	 * screw around with it!
-	 * 
+	 *
 	 * @param identifierString
 	 * @throws MalformedIdentifierException
 	 */
 	protected abstract void validate(String identifierString)
 			throws MalformedIdentifierException;
-
 
 }
