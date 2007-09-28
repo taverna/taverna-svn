eeb8dabe8570f91866f548d89043229273e71b2c
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-28 16:50:53 +0000

    Added catch statements for explicit exceptions.
    git-svn-id: http://taverna.googlecode.com/svn@3678 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2activities/beanshell-activity/src/main/java/net/sf/taverna/t2/activities/beanshell/BeanshellActivity.java
+++ trunk/t2activities/beanshell-activity/src/main/java/net/sf/taverna/t2/activities/beanshell/BeanshellActivity.java
@@ -1,9 +1,12 @@
 package net.sf.taverna.t2.activities.beanshell;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.sf.taverna.t2.cloudone.datamanager.DataFacade;
+import net.sf.taverna.t2.cloudone.datamanager.DataManagerException;
+import net.sf.taverna.t2.cloudone.datamanager.NotFoundException;
 import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 import net.sf.taverna.t2.workflowmodel.processor.activity.AbstractAsynchronousActivity;
 import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
@@ -11,11 +14,12 @@ import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityPortBuilder;
 import net.sf.taverna.t2.workflowmodel.processor.activity.AsynchronousActivityCallback;
 import net.sf.taverna.t2.workflowmodel.processor.activity.config.ActivityOutputPortDefinitionBean;
 import net.sf.taverna.t2.workflowmodel.processor.activity.impl.ActivityPortBuilderImpl;
+import bsh.EvalError;
 import bsh.Interpreter;
 
 /**
  * <p>
- * An Activity providing Beanshell functionality.
+ * An Activity providing Beanshell functionality. 
  * </p>
  * 
  * @author David Withers
@@ -58,7 +62,7 @@ public class BeanshellActivity extends
 		callback.requestRun(new Runnable() {
 
 			public void run() {
-				DataFacade dataManager = new DataFacade(callback
+				DataFacade dataFacade = new DataFacade(callback
 						.getLocalDataManager());
 
 				Map<String, EntityIdentifier> outputData = new HashMap<String, EntityIdentifier>();
@@ -67,7 +71,7 @@ public class BeanshellActivity extends
 					synchronized (interpreter) {
 						// set inputs
 						for (String inputName : data.keySet()) {
-							Object input = dataManager.resolve(data
+							Object input = dataFacade.resolve(data
 									.get(inputName));
 							interpreter.set(inputName, input);
 						}
@@ -76,12 +80,13 @@ public class BeanshellActivity extends
 						// get and clear outputs
 						for (ActivityOutputPortDefinitionBean outputBean : configurationBean
 								.getOutputPortDefinitions()) {
+							String name = outputBean.getName();
 							Object value = interpreter
-									.get(outputBean.getName());
+									.get(name);
 							if (value != null) {
-								dataManager.register(value);
+								outputData.put(name, dataFacade.register(value));
 							}
-							interpreter.unset(outputBean.getName());
+							interpreter.unset(name);
 						}
 						// clear inputs
 						for (String inputName : data.keySet()) {
@@ -89,8 +94,14 @@ public class BeanshellActivity extends
 						}
 					}
 					callback.receiveResult(outputData, new int[0]);
-				} catch (Exception ex) {
-					callback.fail("", ex);
+				} catch (EvalError e) {
+					callback.fail("Error evaluating the beanshell script", e);
+				} catch (DataManagerException e) {
+					callback.fail("Error accessing beanshell input/output data", e);
+				} catch (NotFoundException e) {
+					callback.fail("Error accessing beanshell input/output data", e);
+				} catch (IOException e) {
+					callback.fail("Error accessing beanshell input/output data", e);
 				}
 			}
 		});
