7e3d908a1c71bf58616d3c5e27f8cc8e5c502ece
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-09-28 13:16:34 +0000

    Removed empty javadoc
    git-svn-id: http://taverna.googlecode.com/svn@3668 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/DereferenceException.java
+++ trunk/t2core/cloudone-api/src/main/java/net/sf/taverna/t2/cloudone/DereferenceException.java
@@ -3,16 +3,13 @@ package net.sf.taverna.t2.cloudone;
 /**
  * Thrown if an attempt to dereference a reference scheme instance fails for
  * whatever reason. This failure may be anticipated or unexpected.
- * 
+ *
  * @author Tom Oinn
  * @author Matthew Pocock
- * 
+ *
  */
 public class DereferenceException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public DereferenceException() {
