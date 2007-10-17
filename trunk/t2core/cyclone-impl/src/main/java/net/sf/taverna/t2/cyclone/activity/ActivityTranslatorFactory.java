f80d7977f1e1ebf115b74b55055f8fb43646136f
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-10-17 12:33:22 +0000

    Added a translator cache
    git-svn-id: http://taverna.googlecode.com/svn@3791 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cyclone-impl/src/main/java/net/sf/taverna/t2/cyclone/activity/ActivityTranslatorFactory.java
+++ trunk/t2core/cyclone-impl/src/main/java/net/sf/taverna/t2/cyclone/activity/ActivityTranslatorFactory.java
@@ -2,7 +2,9 @@ package net.sf.taverna.t2.cyclone.activity;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.taverna.raven.repository.Repository;
 import net.sf.taverna.raven.repository.impl.LocalRepository;
@@ -14,59 +16,73 @@ import org.embl.ebi.escience.scufl.Processor;
 
 /**
  * <p>
- * A Factory class responsible for providing the appropriate {@link ActivityTranslator} for a given
- * class of a Taverna 1 style Processor. This translator is responsible for providing a {@link Activity} that
- * has similar capabilities of the original Processor.
+ * A Factory class responsible for providing the appropriate
+ * {@link ActivityTranslator} for a given class of a Taverna 1 style Processor.
+ * This translator is responsible for providing a {@link Activity} that has
+ * similar capabilities of the original Processor.
  * </p>
+ * 
  * @author Stuart Owen
  * @author David Withers
- *
+ * 
  */
 public class ActivityTranslatorFactory {
-	
+
 	private static InstanceRegistry<ActivityTranslator<?>> instanceRegistry = null;
-	
-	
+
+	private static Map<Class<?>, ActivityTranslator<?>> translatorMap = new HashMap<Class<?>, ActivityTranslator<?>>();
+
 	/**
 	 * <p>
-	 * Given a particular Processor class it returns an appropriate ActivityTranslator
+	 * Given a particular Processor class it returns an appropriate
+	 * ActivityTranslator
 	 * </p>
 	 * 
-	 * @param processor - the Processor requiring an ActivityTranslator
+	 * @param processor -
+	 *            the Processor requiring an ActivityTranslator
 	 * @return an appropriate ActivityTranslator
-	 * @throws ActivityTranslatorNotFoundException 
+	 * @throws ActivityTranslatorNotFoundException
 	 */
-	public static ActivityTranslator<?> getTranslator(Processor processor) throws ActivityTranslatorNotFoundException {
-		ActivityTranslator<?> result=null;
-		List<ActivityTranslator<?>> translators = getTranslators();
-		for (ActivityTranslator<?> translator : translators) {
-			if (translator.canHandle(processor)) {
-				result=translator;
-				break;
+	public static ActivityTranslator<?> getTranslator(Processor processor)
+			throws ActivityTranslatorNotFoundException {
+		if (!translatorMap.containsKey(processor.getClass())) {
+			boolean foundTranslator = false;
+			List<ActivityTranslator<?>> translators = getTranslators();
+			for (ActivityTranslator<?> translator : translators) {
+				if (translator.canHandle(processor)) {
+					translatorMap.put(processor.getClass(), translator);
+					foundTranslator = true;
+					break;
+				}
+			}
+			if (!foundTranslator) {
+				throw new ActivityTranslatorNotFoundException(
+						"Unable to find Activity Translator for:"
+								+ processor.getClass());
 			}
 		}
-	
-		if (result == null) {
-			throw new ActivityTranslatorNotFoundException("Unable to find Activity Translator for:"+processor.getClass());
-		}
-		return result;
+
+		return translatorMap.get(processor.getClass());
 	}
-	
+
 	private static List<ActivityTranslator<?>> getTranslators() {
 		return getRegistry().getInstances();
 	}
-	
+
 	private synchronized static InstanceRegistry<ActivityTranslator<?>> getRegistry() {
-		if (instanceRegistry==null) {
-			SpiRegistry registry = new SpiRegistry(getRepository(),ActivityTranslator.class.getName(),ActivityTranslatorFactory.class.getClassLoader());
-			instanceRegistry = new InstanceRegistry<ActivityTranslator<?>>(registry, new Object[0]);
+		if (instanceRegistry == null) {
+			SpiRegistry registry = new SpiRegistry(getRepository(),
+					ActivityTranslator.class.getName(),
+					ActivityTranslatorFactory.class.getClassLoader());
+			instanceRegistry = new InstanceRegistry<ActivityTranslator<?>>(
+					registry, new Object[0]);
 		}
 		return instanceRegistry;
 	}
 
 	private static Repository getRepository() {
-		//FIXME: How do we get the repository correctly?
-		File tmpDir=null;
+		// FIXME: How do we get the repository correctly?
+		File tmpDir = null;
 		try {
 			tmpDir = File.createTempFile("taverna", "raven");
 		} catch (IOException e) {
