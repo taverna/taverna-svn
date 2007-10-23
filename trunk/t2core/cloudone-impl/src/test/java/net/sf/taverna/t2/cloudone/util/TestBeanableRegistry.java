f7359ac2227edaf874f76af30e594464ee65498a
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-10-23 14:22:27 +0000

    Beanable registry. Can look up a Beanable from classname.
    git-svn-id: http://taverna.googlecode.com/svn@3845 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-impl/src/test/java/net/sf/taverna/t2/cloudone/util/TestBeanableRegistry.java
@@ -0,0 +1,44 @@
+package net.sf.taverna.t2.cloudone.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import net.sf.taverna.t2.cloudone.bean.Beanable;
+import net.sf.taverna.t2.cloudone.entity.EntityList;
+import net.sf.taverna.t2.cloudone.entity.ErrorDocument;
+import net.sf.taverna.t2.cloudone.entity.Literal;
+import net.sf.taverna.t2.cloudone.entity.impl.DataDocumentImpl;
+import net.sf.taverna.t2.cloudone.identifier.DataDocumentIdentifier;
+import net.sf.taverna.t2.cloudone.identifier.EntityListIdentifier;
+import net.sf.taverna.t2.cloudone.identifier.ErrorDocumentIdentifier;
+import net.sf.taverna.t2.cloudone.impl.BlobReferenceSchemeImpl;
+import net.sf.taverna.t2.cloudone.impl.url.URLReferenceScheme;
+
+@SuppressWarnings("unchecked")
+public class TestBeanableRegistry {
+
+	BeanableRegistry registry = BeanableRegistry.getInstance();
+
+	Class<? extends Beanable>[] beanableClasses = new Class[]{
+			DataDocumentIdentifier.class,
+			EntityListIdentifier.class,
+			ErrorDocumentIdentifier.class,
+			Literal.class,
+			URLReferenceScheme.class,
+			BlobReferenceSchemeImpl.class,
+			EntityList.class,
+			ErrorDocument.class,
+			DataDocumentImpl.class,
+	};
+	
+	@Test
+	public void getBeanables() {
+		for (Class<? extends Beanable> c : beanableClasses) {
+			Beanable<?> beanable = registry.getBeanable(c.getCanonicalName());
+			assertTrue("Was not an instance of " + c + ": " + beanable, 
+					c.isInstance(beanable));
+		}
+	}
+
+}
