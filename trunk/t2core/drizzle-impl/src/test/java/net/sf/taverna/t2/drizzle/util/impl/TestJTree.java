a6c9544dfbe3b2299caa90499249393dc490ec5e
alaninmcr <alaninmcr@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-10-23 16:26:05 +0000

    Start of addition of table
    git-svn-id: http://taverna.googlecode.com/svn@3856 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/drizzle-impl/src/test/java/net/sf/taverna/t2/drizzle/util/impl/TestJTree.java
+++ trunk/t2core/drizzle-impl/src/test/java/net/sf/taverna/t2/drizzle/util/impl/TestJTree.java
@@ -10,11 +10,15 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTree;
 import java.util.Timer;
+
+import javax.swing.table.DefaultTableModel;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
@@ -32,30 +36,54 @@ import net.sf.taverna.t2.utility.TreeModelAdapter;
  */
 public final class TestJTree extends JFrame {
 	private PropertiedTreeModel<StringObject> testImpl = new PropertiedTreeModelImpl<StringObject>();
+
 	private PropertiedObjectSet<StringObject> testSet = new PropertiedObjectSetImpl<StringObject>();
+
 	private StringObject service1 = new StringObject("blast service");
+
 	private StringObject service2 = new StringObject("emma service");
+
 	private StringObject service3 = new StringObject("fetchPdb service");
+
 	private StringObject service4 = new StringObject("renderPdb service");
 
 	private PropertyKey domainKey = new StringKey("domain");
+
 	private PropertyKey nameKey = new StringKey("name");
+
 	private PropertyKey providerKey = new StringKey("provider");
+
 	private PropertyKey typeKey = new StringKey("type");
 
 	private PropertyValue ebiValue = new StringValue("ebi");
+
 	private PropertyValue manchesterValue = new StringValue("manchester");
+
 	private PropertyValue soaplabValue = new StringValue("soaplab");
+
 	private PropertyValue wsdlValue = new StringValue("wsdl");
+
 	private PropertyValue geneticsValue = new StringValue("genetics");
+
 	private PropertyValue structureValue = new StringValue("structure");
+
 	private PropertyValue blastValue = new StringValue("blast");
+
 	private PropertyValue emmaValue = new StringValue("emma");
+
 	private PropertyValue fetchPdbValue = new StringValue("fetchPdb");
+
 	private PropertyValue renderPdbValue = new StringValue("renderPdb");
-	
+
 	private JTree tree;
 
+	List<PropertyKeySetting> keySettings = new ArrayList<PropertyKeySetting>();
+	
+	/**
+	 * Populate the PropertiedObjectSet and create a TreeModel over it
+	 * 
+	 * @return
+	 */
 	public TreeModel createTree() {
 		PropertiedGraphView<StringObject> graphView = new PropertiedGraphViewImpl<StringObject>();
 
@@ -81,7 +109,6 @@ public final class TestJTree extends JFrame {
 
 		graphView.setPropertiedObjectSet(testSet);
 
-		List<PropertyKeySetting> keySettings = new ArrayList<PropertyKeySetting>();
 		PropertyKeySetting typeSetting = new PropertyKeySettingImpl();
 		typeSetting.setPropertyKey(typeKey);
 		keySettings.add(typeSetting);
@@ -95,20 +122,37 @@ public final class TestJTree extends JFrame {
 		return untypedView;
 	}
 
-	private void expandAll (JTree tree) {
+	private void expandAll(JTree tree) {
 		for (int i = 0; i <= tree.getRowCount(); i++) {
 			tree.expandRow(i);
 		}
 	}
-	
+
+	/**
+	 * Construct a TestJTree that takes a PropertiedObjectSet populated with
+	 * example data and shows it as a JTree. The effects of altering the
+	 * PropertiedObjectSet are shown to be mirrored in the JTree.
+	 * 
+	 * @throws InterruptedException
+	 */
 	public TestJTree() throws InterruptedException {
 		TreeModel model = createTree();
 		Container content = getContentPane();
 		tree = new JTree(model);
-		content.add(new JScrollPane(tree), BorderLayout.CENTER);
+		content.setLayout(new BorderLayout());
+		content.add(tree, BorderLayout.CENTER);
 		setSize(275, 300);
-		setVisible(true);
 		expandAll(tree);
+		setVisible(true);
+		Vector<String> keyNames = new Vector<String>();
+		for (PropertyKeySetting pks : keySettings) {
+			keyNames.add (pks.getPropertyKey().toString());
+		}
+		DefaultTableModel tableModel = new DefaultTableModel (keyNames, 
+				2);
+		JTable table = new JTable(tableModel);
+		content.add(table.getTableHeader(), BorderLayout.NORTH);
+		validate();
 		Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
 
@@ -117,7 +161,7 @@ public final class TestJTree extends JFrame {
 				testSet.removeObject(service1);
 				expandAll(tree);
 			}
-			
+
 		}, 10000);
 		timer.schedule(new TimerTask() {
 
@@ -126,7 +170,7 @@ public final class TestJTree extends JFrame {
 				testSet.setProperty(service2, typeKey, wsdlValue);
 				expandAll(tree);
 			}
-			
+
 		}, 20000);
 		timer.schedule(new TimerTask() {
 
@@ -135,7 +179,7 @@ public final class TestJTree extends JFrame {
 				testSet.addObject(service1);
 				expandAll(tree);
 			}
-			
+
 		}, 30000);
 		timer.schedule(new TimerTask() {
 
@@ -147,10 +191,16 @@ public final class TestJTree extends JFrame {
 				testSet.setProperty(service1, nameKey, blastValue);
 				expandAll(tree);
 			}
-			
+
 		}, 40000);
 	}
 
+	/**
+	 * Show the test JTree.
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 */
 	public static void main(String[] args) throws InterruptedException {
 		new TestJTree();
 	}
