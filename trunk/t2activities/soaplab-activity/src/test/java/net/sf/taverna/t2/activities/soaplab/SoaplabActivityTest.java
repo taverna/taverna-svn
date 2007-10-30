5299269e188e35000bb135b1d851e764c1065dbd
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-10-30 15:31:44 +0000

    Added unit tests
    git-svn-id: http://taverna.googlecode.com/svn@3947 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2activities/soaplab-activity/src/test/java/net/sf/taverna/t2/activities/soaplab/SoaplabActivityTest.java
+++ trunk/t2activities/soaplab-activity/src/test/java/net/sf/taverna/t2/activities/soaplab/SoaplabActivityTest.java
@@ -1,6 +1,7 @@
 package net.sf.taverna.t2.activities.soaplab;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -23,35 +24,47 @@ import org.junit.Test;
  * @author David Withers
  */
 public class SoaplabActivityTest {
-	
+
 	private SoaplabActivity activity;
-	
+
 	private SoaplabActivityConfigurationBean configurationBean;
 
 	@Before
 	public void setUp() throws Exception {
 		activity = new SoaplabActivity();
 		configurationBean = new SoaplabActivityConfigurationBean();
-		configurationBean.setEndpoint("http://www.ebi.ac.uk/soaplab/emboss4/services/utils_misc.embossversion");
+		configurationBean
+				.setEndpoint("http://www.ebi.ac.uk/soaplab/emboss4/services/utils_misc.embossversion");
 	}
 
 	@Test
 	public void testExecuteAsynch() throws Exception {
-		activity.configure(configurationBean);
-		Map<String,Object> inputs = new HashMap<String, Object>();
-//		inputs.put("full", "true");
+		Map<String, Object> inputs = new HashMap<String, Object>();
+		// inputs.put("full", "true");
 		List<String> expectedOutputs = new ArrayList<String>();
 		expectedOutputs.add("report");
 		expectedOutputs.add("outfile");
 
 		activity.configure(configurationBean);
 
-		Map<String,Object> outputs = ActivityInvoker.invokeAsyncActivity(activity, inputs, expectedOutputs);
+		Map<String, Object> outputs = ActivityInvoker.invokeAsyncActivity(
+				activity, inputs, expectedOutputs);
 		assertTrue(outputs.containsKey("report"));
-//		assertTrue(outputs.get("report") instanceof String);
+		// assertTrue(outputs.get("report") instanceof String);
 		assertTrue(outputs.containsKey("outfile"));
 		assertTrue(outputs.get("outfile") instanceof String);
 		System.out.println(outputs.get("outfile"));
+
+		// test with polling
+		configurationBean.setPollingInterval(5);
+		configurationBean.setPollingIntervalMax(6);
+		configurationBean.setPollingBackoff(1.2);
+		activity.configure(configurationBean);
+
+		outputs = ActivityInvoker.invokeAsyncActivity(activity, inputs,
+				expectedOutputs);
+		assertTrue(outputs.containsKey("report"));
+		assertTrue(outputs.containsKey("outfile"));
 	}
 
 	@Test
@@ -60,17 +73,29 @@ public class SoaplabActivityTest {
 	}
 
 	@Test
-	public void testConfigureSoaplabActivityConfigurationBean() throws Exception {
+	public void testConfigureSoaplabActivityConfigurationBean()
+			throws Exception {
 		Set<String> expectedOutputs = new HashSet<String>();
 		expectedOutputs.add("report");
 		expectedOutputs.add("outfile");
-		
+
 		activity.configure(configurationBean);
 		Set<OutputPort> ports = activity.getOutputPorts();
 		assertEquals(expectedOutputs.size(), ports.size());
 		for (OutputPort outputPort : ports) {
-			assertTrue("Wrong output : " + outputPort.getName(), expectedOutputs.remove(outputPort.getName()));
+			assertTrue("Wrong output : " + outputPort.getName(),
+					expectedOutputs.remove(outputPort.getName()));
 		}
+	}
+
+	@Test
+	public void testIsPollingDefined() throws Exception {
+		assertFalse(activity.isPollingDefined());
+		activity.configure(configurationBean);
+		assertFalse(activity.isPollingDefined());
+		configurationBean.setPollingInterval(1000);
+		activity.configure(configurationBean);
+		assertTrue(activity.isPollingDefined());
 	}
 
 }
