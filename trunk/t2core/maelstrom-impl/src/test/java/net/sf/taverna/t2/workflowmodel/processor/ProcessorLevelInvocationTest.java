61f83b4258fc236624f9e313b75fba600265a65d
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-10-31 10:33:58 +0000

    junit4 style test, avoid sysout
    git-svn-id: http://taverna.googlecode.com/svn@3953 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/maelstrom-impl/src/test/java/net/sf/taverna/t2/workflowmodel/processor/ProcessorLevelInvocationTest.java
+++ trunk/t2core/maelstrom-impl/src/test/java/net/sf/taverna/t2/workflowmodel/processor/ProcessorLevelInvocationTest.java
@@ -1,5 +1,8 @@
 package net.sf.taverna.t2.workflowmodel.processor;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.util.HashSet;
 
@@ -20,19 +23,19 @@ import net.sf.taverna.t2.workflowmodel.impl.Tools;
 import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
 
 import org.jdom.JDOMException;
+import org.junit.Before;
+import org.junit.Test;
 
-import junit.framework.TestCase;
-
-public class ProcessorLevelInvocationTest extends TestCase {
+public class ProcessorLevelInvocationTest {
 
 	// Create a diagnostic event receiver
-	DiagnosticEventHandler deh = new DiagnosticEventHandler();
-
+	DiagnosticEventHandler deh;
 	ProcessorImpl processor;
 
 	DataManager dManager = new InMemoryDataManager("dataNS",
 			new HashSet<LocationalContext>());
 
+	@Before
 	public void createProcessor() throws EditException, JDOMException,
 			IOException, ActivityConfigurationException,
 			MalformedIdentifierException {
@@ -41,6 +44,7 @@ public class ProcessorLevelInvocationTest extends TestCase {
 		AsynchEchoActivity activity = new AsynchEchoActivity();
 		activity.configure(new EchoConfig("blah"));
 		processor = Tools.buildFromActivity(activity);
+		deh = new DiagnosticEventHandler();
 
 		// Set up data manager
 		ContextManager.baseManager = this.dManager;
@@ -55,23 +59,23 @@ public class ProcessorLevelInvocationTest extends TestCase {
 
 	}
 
-	public void testSingleDataTokenBehaviour()
+	@Test
+	public synchronized void singleDataTokenBehaviour()
 			throws MalformedIdentifierException, EditException, JDOMException,
 			IOException, ActivityConfigurationException {
-		System.out.println("Single token, 'A string'");
-		createProcessor();
+		//System.out.println("Single token, 'A string'");
 		// Build an input data token
 		WorkflowDataToken token = new WorkflowDataToken("outerProcess1",
 				new int[0], Literal.buildLiteral("A string"));
 		processor.getInputPorts().get(0).receiveEvent(token);
-		assertTrue(deh.getEventCount() == 1);
+		assertEquals(1, deh.getEventCount());
 	}
 
-	public void testSingleEmptyListDataTokenBehaviour()
+	@Test
+	public void singleEmptyListDataTokenBehaviour()
 			throws MalformedIdentifierException, EditException, JDOMException,
-			IOException, ActivityConfigurationException {
-		createProcessor();
-		System.out.println("Empty list");
+			IOException, ActivityConfigurationException, InterruptedException {
+		//System.out.println("Empty list");
 		EntityIdentifier listIdentifier = ContextManager.baseManager
 				.registerEmptyList(1);
 		// Build an input data token of a single list
@@ -91,14 +95,14 @@ public class ProcessorLevelInvocationTest extends TestCase {
 		// 0 so it ignores this list which has depth 1. It will now, however,
 		// correctly iterate over what it thinks is an empty set and return a
 		// single empty result on the output
-		assertTrue(deh.getEventCount() == 1);
+		assertEquals(1, deh.getEventCount());
 	}
 
-	public void testSingleListDataTokenWithFilterConfigured()
+	@Test
+	public void singleListDataTokenWithFilterConfigured()
 			throws EditException, JDOMException, IOException,
-			ActivityConfigurationException, MalformedIdentifierException {
-		createProcessor();
-		System.out.println("List with two items, 'foo' and 'bar'");
+			ActivityConfigurationException, MalformedIdentifierException, InterruptedException {
+		//System.out.println("List with two items, 'foo' and 'bar'");
 		EntityIdentifier listIdentifier = ContextManager.baseManager
 				.registerList(new EntityIdentifier[] {
 						Literal.buildLiteral("foo"),
@@ -114,14 +118,14 @@ public class ProcessorLevelInvocationTest extends TestCase {
 		processor.getInputPorts().get(0).receiveEvent(token);
 
 		// Should produce two outputs followed by a collection
-		assertTrue(deh.getEventCount() == 3);
+		assertEquals(3, deh.getEventCount());
 	}
 
-	public void testTopLevelEmptyCollection() throws EditException,
+	@Test
+	public void topLevelEmptyCollection() throws EditException,
 			JDOMException, IOException, ActivityConfigurationException,
-			MalformedIdentifierException {
-		createProcessor();
-		System.out.println("Top level empty list (depth 2)");
+			MalformedIdentifierException, InterruptedException {
+		//System.out.println("Top level empty list (depth 2)");
 		EntityIdentifier listIdentifier = ContextManager.baseManager
 				.registerEmptyList(2);
 		((ProcessorInputPortImpl) (processor.getInputPorts().get(0)))
@@ -138,16 +142,16 @@ public class ProcessorLevelInvocationTest extends TestCase {
 		processor.resultWrappingDepth = 2;
 
 		processor.getInputPorts().get(0).receiveEvent(token);
-		assertTrue(deh.getEventCount() == 1);
+		assertEquals(1, deh.getEventCount());
 	}
 
+	@Test
 	public void testPartiallyEmptyCollection() throws EditException,
 			JDOMException, IOException, ActivityConfigurationException,
-			MalformedIdentifierException {
-		createProcessor();
+			MalformedIdentifierException, InterruptedException {
 		EntityIdentifier emptyListIdentifier = ContextManager.baseManager
 				.registerEmptyList(1);
-		System.out.println("Partially empty collection : {{}{foo, bar}}");
+		//System.out.println("Partially empty collection : {{}{foo, bar}}");
 		EntityIdentifier populatedListIdentifier = ContextManager.baseManager
 				.registerList(new EntityIdentifier[] {
 						Literal.buildLiteral("foo"),
@@ -169,7 +173,8 @@ public class ProcessorLevelInvocationTest extends TestCase {
 		processor.resultWrappingDepth = 2;
 		
 		processor.getInputPorts().get(0).receiveEvent(token);
-		assertTrue(deh.getEventCount() == 5);
+		Thread.sleep(1);
+		assertEquals(5, deh.getEventCount());
 	}
 
 }
