f6e58187b9d701705292d019164aa09033195ad8
alaninmcr <alaninmcr@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-01 17:48:12 +0000

    Initial commitment
    git-svn-id: http://taverna.googlecode.com/svn@3964 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2activitypalette/drizzle-api/src/main/java/net/sf/taverna/t2/drizzle/activityregistry/PropertyDecoder.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.drizzle.activityregistry;
+
+/**
+ * @author alanrw
+ *
+ */
+public interface PropertyDecoder {
+
+	boolean canDecode (Object encodedObject);
+}
