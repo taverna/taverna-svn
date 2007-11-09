63638a5e255c87d750f33abbe25a0df218f83e44
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-09 11:10:28 +0000

    Added contructor that only takes a message
    git-svn-id: http://taverna.googlecode.com/svn@4018 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cyclone-api/src/main/java/net/sf/taverna/t2/cyclone/activity/ActivityTranslationException.java
+++ trunk/t2core/cyclone-api/src/main/java/net/sf/taverna/t2/cyclone/activity/ActivityTranslationException.java
@@ -4,11 +4,21 @@ package net.sf.taverna.t2.cyclone.activity;
  * An Exception indicating problem translating a Taverna 1 Processor instance into an equivalent Taverna 2 Activity
  * 
  * @author Stuart Owen
+ * @author David Withers
  *
  */
 public class ActivityTranslationException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructs the ActivityTranslationException instance with a message.
+	 * 
+	 * @param message the message explaining the cause of the problem.
+	 */
+	public ActivityTranslationException(String message) {
+		super(message);
+	}
 
 	/**
 	 * Constructs the ActivityTranslationException instance with the message and cause.
