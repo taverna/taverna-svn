11bc4bd91542aa41da58f7a3bb5dcbb8dd8bf818
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-09 14:59:00 +0000

    Generate JFrame
    git-svn-id: http://taverna.googlecode.com/svn@4025 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-gui/src/test/java/net/sf/taverna/t2/cloudone/gui/entity/view/RunDocumentView.java
+++ trunk/t2core/cloudone-gui/src/test/java/net/sf/taverna/t2/cloudone/gui/entity/view/RunDocumentView.java
@@ -1,21 +1,25 @@
 package net.sf.taverna.t2.cloudone.gui.entity.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.net.MalformedURLException;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModel;
 import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModelEvent;
+import net.sf.taverna.t2.cloudone.gui.entity.model.FileRefSchemeModel;
 import net.sf.taverna.t2.cloudone.gui.entity.model.HttpRefSchemeModel;
-import net.sf.taverna.t2.cloudone.gui.entity.view.DataDocumentView;
 import net.sf.taverna.t2.lang.observer.Observable;
 import net.sf.taverna.t2.lang.observer.Observer;
 
 public class RunDocumentView {
 
 	public static void main(String[] args) throws InterruptedException,
-			MalformedURLException {
+			IOException {
 		DataDocumentModel model = new DataDocumentModel();
 		DataDocumentView view = new DataDocumentView(model);
 
@@ -27,16 +31,28 @@ public class RunDocumentView {
 			}
 		});
 
-		view.setSize(new Dimension(500, 350));
-		view.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		view.setVisible(true);
+		JFrame frame = new JFrame("Document view");
+		frame.add(new JPanel(), BorderLayout.LINE_START);
+		frame.add(new JPanel(), BorderLayout.PAGE_START);
+		frame.add(view, BorderLayout.CENTER);
+		
+		frame.setSize(new Dimension(500, 400));
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-		Thread.sleep(5 * 1000);
+		Thread.sleep(3 * 1000);
+		FileRefSchemeModel fileModel = new FileRefSchemeModel(model);
+		model.addReferenceScheme(fileModel);
+
+		Thread.sleep(2 * 1000);
+
 		HttpRefSchemeModel httpModel = new HttpRefSchemeModel(model);
 		model.addReferenceScheme(httpModel);
-		Thread.sleep(5 * 1000);
+		Thread.sleep(2 * 1000);
+		fileModel.setFile(File.createTempFile("some", "test"));
+		Thread.sleep(2 * 1000);
 		httpModel.setURL("http://www.google.co.uk/");
-
+		
 		Thread.sleep(2 * 60 * 1000);
 		System.exit(0);
 	}
