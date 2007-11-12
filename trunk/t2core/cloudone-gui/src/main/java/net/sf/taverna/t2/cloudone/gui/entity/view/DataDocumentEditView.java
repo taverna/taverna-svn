35d14f3a058b5ca4e4af01f6a691b4efbbeb008c
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-12 16:27:14 +0000

    Data doc views handled by 2 different view states, one for editable and another for non editable.  Views are switched when "edit" button is clicked
    git-svn-id: http://taverna.googlecode.com/svn@4050 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/view/DataDocumentEditView.java
@@ -0,0 +1,295 @@
+package net.sf.taverna.t2.cloudone.gui.entity.view;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModel;
+import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModelEvent;
+import net.sf.taverna.t2.cloudone.gui.entity.model.FileRefSchemeModel;
+import net.sf.taverna.t2.cloudone.gui.entity.model.HttpRefSchemeModel;
+import net.sf.taverna.t2.cloudone.gui.entity.model.ReferenceSchemeModel;
+import net.sf.taverna.t2.cloudone.refscheme.file.FileReferenceScheme;
+import net.sf.taverna.t2.cloudone.refscheme.http.HttpReferenceScheme;
+
+import org.apache.log4j.Logger;
+
+/**
+ * View (in MVC terms) for a Data Document builder GUI.
+ * 
+ * @author Ian Dunlop
+ * @author Stian Soiland
+ */
+public class DataDocumentEditView
+		extends
+		EntityView<DataDocumentModel, ReferenceSchemeModel, DataDocumentModelEvent> {
+
+	private static Logger logger = Logger.getLogger(DataDocumentEditView.class);
+	private static final long serialVersionUID = 1L;
+
+	private JPanel actionPanel;
+
+	private JButton editButton;
+
+	private JButton fileButton;
+
+	private JButton httpButton;
+
+	/** What {@link RefSchemeView} was the last one to be edited */
+	private RefSchemeView lastEditedView;
+	private DataDocumentView nonEditPanel;
+	private JPanel outerPanel;
+	private JPanel refViews;
+
+	/**
+	 * Constructor creates a {@link ModelObserver} and registers it as an
+	 * observer with the {@link DataDocumentEditView} and calls
+	 * {@link #initialiseGui()} to create the GUI
+	 * 
+	 * @param model
+	 *            the {@link DataDocumentModel} model part in
+	 *            Model-View-Controller terms
+	 */
+	public DataDocumentEditView(DataDocumentModel model) {
+		super(model);
+		initialiseGui();
+	}
+
+	/**
+	 * Resets buttons/text fields etc to their correct editing states eg, if OK
+	 * is clicked and the input is valid then only the Edit & Remove buttons
+	 * should be enabled
+	 * 
+	 * @param model
+	 *            the {@link ReferenceSchemeModel} whose state has changed
+	 * @throws IllegalStateException
+	 *             If the current editable could not set it's fields to the
+	 *             model
+	 */
+	public void edit(ReferenceSchemeModel model) throws IllegalStateException {
+		// Might return null, which is OK
+		RefSchemeView view = (RefSchemeView) modelViews.get(model);
+		if (lastEditedView != null && lastEditedView != view) {
+			try {
+				lastEditedView.setEdit(false);
+			} catch (IllegalStateException ex) {
+				throw new IllegalStateException(ex);
+			}
+		}
+		lastEditedView = view;
+		if (view != null) {
+			view.setEdit(true);
+		}
+	}
+
+	private JPanel createdRefs() {
+		JPanel refs = new JPanel();
+		refs.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		// TODO: Add existing from model
+		// model.getReferenceSchemeModels();
+		return refs;
+	}
+
+	private void initialiseGui() {
+
+		// panel inside panel for on-click activation
+		setLayout(new GridBagLayout());
+		// JPanel addSchemes = addSchemeButtons();
+		GridBagConstraints outerConstraint = new GridBagConstraints();
+		outerConstraint.gridx = 0;
+		outerConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+		// add to outer panel first then the parent?
+
+		outerPanel();
+		add(outerPanel, outerConstraint);
+		// add(addSchemes, outerConstraint);
+
+		refViews = createdRefs();
+		add(refViews, outerConstraint);
+
+		outerConstraint.gridx = 1;
+		outerConstraint.gridy = 100;
+		outerConstraint.weighty = 0.1;
+		outerConstraint.weightx = 0.1;
+		outerConstraint.fill = GridBagConstraints.BOTH;
+		JPanel filler = new JPanel();
+		// filler.setBorder(BorderFactory.createEtchedBorder());
+		add(filler, outerConstraint);
+		// panel which is switched in/out on edit button click
+	}
+
+	private void outerPanel() {
+		outerPanel = new JPanel();
+		EditPanelAction editPanelAction = new EditPanelAction();
+		editButton = new JButton(editPanelAction);
+		editButton.setText("Edit");
+		outerPanel.add(editButton);
+		actionPanel = addSchemeButtons();
+		nonEditPanel = new DataDocumentView(getParentModel());
+		GridBagConstraints outerConstraint = new GridBagConstraints();
+		outerConstraint.gridx = 0;
+		outerConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+		outerPanel.add(actionPanel);
+		outerPanel.add(nonEditPanel);
+		nonEditPanel.setVisible(false);
+		actionPanel.setVisible(false);
+	}
+
+	protected void addRefToModel(ReferenceSchemeModel refModel) {
+		getParentModel().addReferenceScheme(refModel);
+		try {
+			// Make it editable
+			edit(refModel);
+		} catch (IllegalStateException ex) {
+			// Could not change editable, remove fresh model
+			getParentModel().removeReferenceScheme(refModel);
+		}
+	}
+
+	protected JPanel addSchemeButtons() {
+		JPanel addSchemes = new JPanel();
+		addSchemes.setLayout(new GridBagLayout());
+		GridBagConstraints cLabel = new GridBagConstraints();
+		cLabel.gridx = 0;
+		cLabel.weightx = 0.1;
+		cLabel.fill = GridBagConstraints.HORIZONTAL;
+
+		GridBagConstraints cButton = new GridBagConstraints();
+		cButton.gridx = 1;
+		cButton.fill = GridBagConstraints.HORIZONTAL;
+
+		JLabel httpRefLabel = new JLabel("Http Reference Scheme");
+		JLabel fileRefLabel = new JLabel("File Reference Scheme");
+		CreateHttpAction createHttpAction = new CreateHttpAction(
+				getParentModel());
+		CreateFileAction createFileAction = new CreateFileAction(
+				getParentModel());
+		httpButton = new JButton(createHttpAction);
+		fileButton = new JButton(createFileAction);
+
+		addSchemes.add(httpRefLabel, cLabel);
+		addSchemes.add(httpButton, cButton);
+		addSchemes.add(fileRefLabel, cLabel);
+		addSchemes.add(fileButton, cButton);
+		return addSchemes;
+	}
+
+	@Override
+	protected JComponent createModelView(ReferenceSchemeModel refModel) {
+		RefSchemeView refView = null;
+		if (refModel instanceof HttpRefSchemeModel) {
+			refView = new HttpRefSchemeView((HttpRefSchemeModel) refModel, this);
+		} else if (refModel instanceof FileRefSchemeModel) {
+			refView = new FileRefSchemeView((FileRefSchemeModel) refModel, this);
+		} else {
+			logger.warn("Unsupported reference model " + refModel);
+		}
+		return refView;
+	}
+
+	@Override
+	protected void placeViewComponent(JComponent view) {
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.1;
+		refViews.add(view, c);
+		refViews.revalidate();
+
+	}
+
+	@Override
+	protected void removeViewComponent(JComponent view) {
+		if (view != null) {
+			refViews.remove(view);
+		}
+		if (lastEditedView == view) {
+			lastEditedView = null;
+		}
+		refViews.revalidate();
+
+	}
+
+	/**
+	 * The Controller (in Model-View-Controller terms) for adding a
+	 * {@link FileReferenceScheme} via clicking the appropriate button
+	 * 
+	 * @author Stian Soiland
+	 * @author Ian Dunlop
+	 * 
+	 */
+	public class CreateFileAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		private DataDocumentModel dataDocModel;
+
+		public CreateFileAction(DataDocumentModel dataDocModel) {
+			super("Create file reference");
+			this.dataDocModel = dataDocModel;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			FileRefSchemeModel refModel = new FileRefSchemeModel(dataDocModel);
+			addRefToModel(refModel);
+			if (refModel.getFile() == null) {
+				// User cancelled, remove it
+				refModel.remove();
+			}
+		}
+	}
+
+	/**
+	 * The Controller (in Model-View-Controller terms) for adding a
+	 * {@link HttpReferenceScheme} via clicking the appropriate button
+	 * 
+	 * @author Stian Soiland
+	 * @author Ian Dunlop
+	 * 
+	 */
+	public class CreateHttpAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		private DataDocumentModel dataDocModel;
+
+		public CreateHttpAction(DataDocumentModel dataDocModel) {
+			super("Create HTTP reference");
+			this.dataDocModel = dataDocModel;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			HttpRefSchemeModel refModel = new HttpRefSchemeModel(dataDocModel);
+			addRefToModel(refModel);
+		}
+	}
+
+	public class EditPanelAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		public EditPanelAction() {
+			super("Edit Data Document");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			if (editButton.getText().equalsIgnoreCase("Hide")) {
+				actionPanel.setVisible(false);
+				refViews.setVisible(false);
+				nonEditPanel.setVisible(true);
+				editButton.setText("Edit");
+			} else {
+				actionPanel.setVisible(true);
+				refViews.setVisible(true);
+				nonEditPanel.setVisible(false);
+				editButton.setText("Hide");
+			}
+		}
+	}
+
+}
--- trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/view/DataDocumentView.java
+++ trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/view/DataDocumentView.java
@@ -2,302 +2,70 @@ package net.sf.taverna.t2.cloudone.gui.entity.view;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import net.sf.taverna.t2.cloudone.gui.entity.model.BlobRefSchemeModel;
 import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModel;
 import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModelEvent;
-import net.sf.taverna.t2.cloudone.gui.entity.model.FileRefSchemeModel;
-import net.sf.taverna.t2.cloudone.gui.entity.model.HttpRefSchemeModel;
 import net.sf.taverna.t2.cloudone.gui.entity.model.ReferenceSchemeModel;
-import net.sf.taverna.t2.cloudone.gui.entity.model.DataDocumentModelEvent.EventType;
-import net.sf.taverna.t2.cloudone.refscheme.file.FileReferenceScheme;
-import net.sf.taverna.t2.cloudone.refscheme.http.HttpReferenceScheme;
-import net.sf.taverna.t2.lang.observer.Observable;
-import net.sf.taverna.t2.lang.observer.Observer;
 
 import org.apache.log4j.Logger;
 
-/**
- * View (in MVC terms) for a Data Document builder GUI.
- * 
- * @author Ian Dunlop
- * @author Stian Soiland
- */
-public class DataDocumentView extends EntityView {
-
-	private static final long serialVersionUID = 1L;
+public class DataDocumentView extends
+		EntityView<DataDocumentModel, ReferenceSchemeModel, DataDocumentModelEvent> {
+	
+	@SuppressWarnings("unused")
 	private static Logger logger = Logger.getLogger(DataDocumentView.class);
-	/**
-	 * A {@link Map} of {@link ReferenceSchemeModel} to
-	 * {@link RefSchemeView} to allow tracking of view state and what
-	 * model is associated with what view
-	 */
-	private Map<ReferenceSchemeModel, RefSchemeView> refModelViews = new HashMap<ReferenceSchemeModel, RefSchemeView>();
+	private JPanel views;
 
-	private DataDocumentModel model;
-
-	private ModelObserver modelObserver;
-
-	private JPanel refViews;
-
-	/** What {@link RefSchemeView} was the last one to be edited */
-	private RefSchemeView lastEditedView;
-
-	private JButton httpButton;
-	private JButton fileButton;
-
-	/**
-	 * Constructor creates a {@link ModelObserver} and registers it as an
-	 * observer with the {@link DataDocumentView} and calls
-	 * {@link #initialiseGui()} to create the GUI
-	 * 
-	 * @param model
-	 *            the {@link DataDocumentModel} model part in
-	 *            Model-View-Controller terms
-	 */
 	public DataDocumentView(DataDocumentModel model) {
-		this.model = model;
-		this.modelObserver = new ModelObserver();
-		model.registerObserver(modelObserver);
-		// TODO: removeObserver on window close
-		initialiseGui();
+		super(model);
+		initialise();
 	}
 
-	/**
-	 * Resets buttons/text fields etc to their correct editing states eg, if OK
-	 * is clicked and the input is valid then only the Edit & Remove buttons
-	 * should be enabled
-	 * 
-	 * @param model
-	 *            the {@link ReferenceSchemeModel} whose state has changed
-	 * @throws IllegalStateException
-	 *             If the current editable could not set it's fields to the
-	 *             model
-	 */
-	public void edit(ReferenceSchemeModel model) throws IllegalStateException {
-		// Might return null, which is OK
-		RefSchemeView view = refModelViews.get(model);
-		if (lastEditedView != null && lastEditedView != view) {
-			try {
-				lastEditedView.setEdit(false);
-			} catch (IllegalStateException ex) {
-				throw new IllegalStateException(ex);
-			}
-		}
-		lastEditedView = view;
-		if (view != null) {
-			view.setEdit(true);
-		}
-	}
-
-	/**
-	 * Refreshes the view after a model has been added
-	 * 
-	 * @param refView
-	 */
-	private void addReferenceView(RefSchemeView refView) {
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.1;
-		refViews.add(refView, c);
-		refViews.revalidate();
-	}
-
-	/**
-	 * Adds a {@link ReferenceSchemeModel} to the view. Triggered by the
-	 * {@link ModelObserver} being notified by the {@link DataDocumentModel}
-	 * that something has been added
-	 * 
-	 * @param refModel
-	 *            the type of {@link ReferenceSchemeModel} that has been added
-	 *            eg {@link HttpRefSchemeModel}
-	 */
-	private void addRefSchemeModel(ReferenceSchemeModel refModel) {
-		RefSchemeView refView = null;
-		if (refModel instanceof HttpRefSchemeModel) {
-			refView = new HttpRefSchemeView((HttpRefSchemeModel) refModel, this);
-		} else if (refModel instanceof FileRefSchemeModel) {
-			refView = new FileRefSchemeView((FileRefSchemeModel) refModel, this);
-		} else {
-			logger.warn("Unsupported reference model " + refModel);
-			return;
-		}
-		refModelViews.put(refModel, refView);
-		addReferenceView(refView);
-	}
-
-	private JPanel createdRefs() {
-		JPanel refs = new JPanel();
-		refs.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		// TODO: Add existing from model
-		model.getReferenceSchemeModels();
-		return refs;
-	}
-
-	private void initialiseGui() {
+	protected void initialise() {
 		setLayout(new GridBagLayout());
-
-		JPanel addSchemes = addSchemeButtons();
+		// JPanel addSchemes = addSchemeButtons();
 		GridBagConstraints outerConstraint = new GridBagConstraints();
 		outerConstraint.gridx = 0;
 		outerConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
-		add(addSchemes, outerConstraint);
-
-		refViews = createdRefs();
-		add(refViews, outerConstraint);
-
-		outerConstraint.gridx = 1;
-		outerConstraint.gridy = 100;
+		// add to outer panel first then the parent?
+		views = new JPanel();
+		views.setLayout(new GridBagLayout());
 		outerConstraint.weighty = 0.1;
 		outerConstraint.weightx = 0.1;
 		outerConstraint.fill = GridBagConstraints.BOTH;
+		add(views, outerConstraint);
+		for (ReferenceSchemeModel ref : getParentModel()
+				.getReferenceSchemeModels()) {
+			addModelView(ref);
+		}
 		JPanel filler = new JPanel();
 		// filler.setBorder(BorderFactory.createEtchedBorder());
 		add(filler, outerConstraint);
 	}
 
-	/**
-	 * Removes a {@link ReferenceSchemeModel} from the view. Triggered by the
-	 * {@link ModelObserver} being notified by the {@link DataDocumentModel}
-	 * that something has been removed. Refreshes the view.
-	 * 
-	 * @param refModel
-	 *            the type of {@link ReferenceSchemeModel} that has been removed
-	 *            eg {@link HttpRefSchemeModel}
-	 */
-	private void removeRefSchemeModel(ReferenceSchemeModel refModel) {
-		RefSchemeView view = refModelViews.remove(refModel);
-		refViews.remove(view);
-		if (lastEditedView == view) {
-			lastEditedView = null;
-		}
-		refViews.revalidate();
+	@Override
+	protected JComponent createModelView(ReferenceSchemeModel refModel) {
+		return new JLabel(refModel.toString());
 	}
 
-	protected void addCreatedRefModel(ReferenceSchemeModel refModel) {
-		model.addReferenceScheme(refModel);
-		try {
-			// Make it editable
-			edit(refModel);
-		} catch (IllegalStateException ex) {
-			// Could not change editable, remove fresh model
-			model.removeReferenceScheme(refModel);
-		}
+	@Override
+	protected void placeViewComponent(JComponent view) {
+		GridBagConstraints outerConstraint = new GridBagConstraints();
+		outerConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+		outerConstraint.gridx = 1;
+		outerConstraint.weighty = 0.1;
+		outerConstraint.weightx = 0.1;
+		outerConstraint.fill = GridBagConstraints.BOTH;
+		views.add(view, outerConstraint);
 	}
 
-	protected JPanel addSchemeButtons() {
-		JPanel addSchemes = new JPanel();
-		addSchemes.setLayout(new GridBagLayout());
-		GridBagConstraints cLabel = new GridBagConstraints();
-		cLabel.gridx = 0;
-		cLabel.weightx = 0.1;
-		cLabel.fill = GridBagConstraints.HORIZONTAL;
-
-		GridBagConstraints cButton = new GridBagConstraints();
-		cButton.gridx = 1;
-		cButton.fill = GridBagConstraints.HORIZONTAL;
-
-		JLabel httpRefLabel = new JLabel("Http Reference Scheme");
-		JLabel fileRefLabel = new JLabel("File Reference Scheme");
-		CreateHttpAction createHttpAction = new CreateHttpAction(model);
-		CreateFileAction createFileAction = new CreateFileAction(model);
-		httpButton = new JButton(createHttpAction);
-		fileButton = new JButton(createFileAction);
-
-		addSchemes.add(httpRefLabel, cLabel);
-		addSchemes.add(httpButton, cButton);
-		addSchemes.add(fileRefLabel, cLabel);
-		addSchemes.add(fileButton, cButton);
-		return addSchemes;
+	@Override
+	protected void removeViewComponent(JComponent refModel) {
+		views.remove(refModel);
 	}
 
-	/**
-	 * The Controller (in Model-View-Controller terms) for adding a
-	 * {@link HttpReferenceScheme} via clicking the appropriate button
-	 * 
-	 * @author Stian Soiland
-	 * @author Ian Dunlop
-	 * 
-	 */
-	public class CreateHttpAction extends AbstractAction {
-		private static final long serialVersionUID = 1L;
-
-		private DataDocumentModel dataDocModel;
-
-		public CreateHttpAction(DataDocumentModel dataDocModel) {
-			super("Create HTTP reference");
-			this.dataDocModel = dataDocModel;
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			HttpRefSchemeModel refModel = new HttpRefSchemeModel(dataDocModel);
-			addCreatedRefModel(refModel);
-		}
-	}
-	
-	/**
-	 * The Controller (in Model-View-Controller terms) for adding a
-	 * {@link FileReferenceScheme} via clicking the appropriate button
-	 * 
-	 * @author Stian Soiland
-	 * @author Ian Dunlop
-	 * 
-	 */
-	public class CreateFileAction extends AbstractAction {
-		private static final long serialVersionUID = 1L;
-
-		private DataDocumentModel dataDocModel;
-
-		public CreateFileAction(DataDocumentModel dataDocModel) {
-			super("Create file reference");
-			this.dataDocModel = dataDocModel;
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			FileRefSchemeModel refModel = new FileRefSchemeModel(dataDocModel);
-			addCreatedRefModel(refModel);
-			if (refModel.getFile() == null) { 
-				// User cancelled, remove it
-				refModel.remove();
-			}
-		}
-	}
-
-	/**
-	 * Observes the changes in a {@link DataDocumentModel} and is notified by
-	 * the {@link DataDocumentModel} whenit wants to inform it of a change to
-	 * the underlying data
-	 * 
-	 * @author Stian Soiland
-	 * @author Ian Dunlop
-	 * 
-	 */
-	private final class ModelObserver implements
-			Observer<DataDocumentModelEvent> {
-
-		public void notify(Observable<DataDocumentModelEvent> sender,
-				DataDocumentModelEvent event) {
-			EventType eventType = event.getEventType();
-			if (eventType.equals(EventType.ADDED)) {
-				ReferenceSchemeModel refModel = event.getRefSchemeModel();
-				addRefSchemeModel(refModel);
-			} else if (eventType.equals(EventType.REMOVED)) {
-				ReferenceSchemeModel refModel = event.getRefSchemeModel();
-				removeRefSchemeModel(refModel);
-			} else {
-				logger.warn("Unsupported event type " + eventType);
-			}
-		}
-	}
 }
