306b75004d5aa8165f9c6c8cb59871476331adfe
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-12 16:22:55 +0000

    Super class for events
    git-svn-id: http://taverna.googlecode.com/svn@4048 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/model/ModelEvent.java
@@ -0,0 +1,48 @@
+package net.sf.taverna.t2.cloudone.gui.entity.model;
+
+import org.apache.log4j.Logger;
+
+public class ModelEvent<Model> {
+
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(ModelEvent.class);
+
+	protected final Model entityModel;
+	protected final EventType eventType;
+
+	/**
+	 * @param eventType
+	 * @param entityModel
+	 */
+	public ModelEvent(
+			EventType eventType,
+			Model entityModel) {
+		this.eventType = eventType;
+		this.entityModel = entityModel;
+	}
+
+	public Model getModel() {
+		return entityModel;
+	}
+
+	/**
+	 * What actually happened to trigger the event
+	 * 
+	 * @return
+	 */
+	public EventType getEventType() {
+		return eventType;
+	}
+
+	/**
+	 * What is the type of event
+	 * 
+	 * @author Ian Dunlop
+	 * @author Stian Soiland
+	 * 
+	 */
+	public static enum EventType {
+		ADDED, REMOVED
+	}
+
+}
