832d4255e2ee1ba253f9a9ebcc67258715b8037e
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-19 15:45:40 +0000

    TAV-614 Updated to use a TreeModel which is populated as the data arrives from the dataflow outputs
    git-svn-id: http://taverna.googlecode.com/svn@4112 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/ResultComponent.java
+++ trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/ResultComponent.java
@@ -1,7 +1,7 @@
 package net.sf.taverna.t2.plugin;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,18 +10,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTree;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
 
-import net.sf.taverna.t2.cloudone.datamanager.NotFoundException;
-import net.sf.taverna.t2.cloudone.datamanager.RetrievalException;
-import net.sf.taverna.t2.cloudone.gui.entity.viewer.EntityViewer;
-import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 import net.sf.taverna.t2.facade.ResultListener;
 import net.sf.taverna.t2.facade.impl.WorkflowInstanceFacadeImpl;
 import net.sf.taverna.t2.workflowmodel.DataflowOutputPort;
 import net.sf.taverna.t2.workflowmodel.EditException;
-import net.sf.taverna.t2.workflowmodel.impl.ContextManager;
 
 import org.embl.ebi.escience.scuflui.spi.UIComponentSPI;
 
@@ -31,36 +30,14 @@ public class ResultComponent extends JPanel implements UIComponentSPI {
 
 	private Map<String, JComponent> componentMap = new HashMap<String, JComponent>();
 
-	private ResultListener resultListener;
-
+	List<ResultListener> resultListeners = new ArrayList<ResultListener>();
+	
 	public ResultComponent() {
 		setLayout(new BorderLayout());
 		add(new JLabel("Results"), BorderLayout.NORTH);
 
 		tabbedPane = new JTabbedPane();
 		add(tabbedPane, BorderLayout.CENTER);
-
-		resultListener = new ResultListener() {
-
-			public void resultTokenProduced(EntityIdentifier data, int[] index,
-					String port, String owningProcess) {
-				if (index.length == 0) {
-					try {
-						JComponent component = componentMap.get(port);
-						if (component != null) {
-							Component entityViewer = EntityViewer.getPanelForEntity(
-									ContextManager.baseManager, data);
-							component.add(entityViewer, BorderLayout.NORTH);
-							component.revalidate();
-						}
-					} catch (RetrievalException e) {
-						e.printStackTrace();
-					} catch (NotFoundException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
 	}
 
 	public void register(WorkflowInstanceFacadeImpl facade)
@@ -69,17 +46,49 @@ public class ResultComponent extends JPanel implements UIComponentSPI {
 		List<? extends DataflowOutputPort> dataflowOutputPorts = facade
 				.getDataflow().getOutputPorts();
 		for (DataflowOutputPort dataflowOutputPort : dataflowOutputPorts) {
+			String portName=dataflowOutputPort.getName();
 			JComponent outputPanel = new JPanel(new BorderLayout());
 			outputPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-			componentMap.put(dataflowOutputPort.getName(), outputPanel);
-			tabbedPane.add(dataflowOutputPort.getName(), outputPanel);
+			componentMap.put(portName, outputPanel);
+			tabbedPane.add(portName, outputPanel);
+			ResultTreeModel resultModel = new ResultTreeModel(portName,dataflowOutputPort.getDepth());
+			resultListeners.add(resultModel);
+			final JTree tree = new JTree(resultModel);
+			tree.setExpandsSelectedPaths(true);
+			resultModel.addTreeModelListener(new TreeModelListener() {
+
+				public void treeNodesChanged(TreeModelEvent e) {
+					tree.expandPath(e.getTreePath());
+					tree.scrollPathToVisible(e.getTreePath());
+					//tree.setSelectionPath(e.getTreePath());
+				}
+
+				public void treeNodesInserted(TreeModelEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void treeNodesRemoved(TreeModelEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void treeStructureChanged(TreeModelEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			});
+			outputPanel.add(new JScrollPane(tree));
+			facade.addResultListener(resultModel);
 		}
 		revalidate();
-		facade.addResultListener(resultListener);
 	}
 
 	public void deregister(WorkflowInstanceFacadeImpl facade) {
-		facade.removeResultListener(resultListener);
+		for (ResultListener listener : resultListeners) {
+			facade.removeResultListener(listener);
+		}
 	}
 
 	public ImageIcon getIcon() {
