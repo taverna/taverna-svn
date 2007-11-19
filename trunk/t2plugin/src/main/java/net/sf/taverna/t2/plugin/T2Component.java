9cbbf3b6109bbf8553de8207c28687877f404e1a
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-19 16:06:11 +0000

    Tabbed input dialog for T2 plugin
    git-svn-id: http://taverna.googlecode.com/svn@4114 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/T2Component.java
+++ trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/T2Component.java
@@ -4,11 +4,17 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
@@ -16,13 +22,18 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 
+import net.sf.taverna.t2.cloudone.datamanager.file.FileDataManager;
 import net.sf.taverna.t2.cloudone.datamanager.memory.InMemoryDataManager;
 import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 import net.sf.taverna.t2.cyclone.WorkflowModelTranslator;
 import net.sf.taverna.t2.cyclone.WorkflowTranslationException;
 import net.sf.taverna.t2.facade.ResultListener;
 import net.sf.taverna.t2.facade.impl.WorkflowInstanceFacadeImpl;
+import net.sf.taverna.t2.invocation.TokenOrderException;
+import net.sf.taverna.t2.plugin.input.InputComponent;
+import net.sf.taverna.t2.plugin.input.InputComponent.InputComponentCallback;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
+import net.sf.taverna.t2.workflowmodel.DataflowInputPort;
 import net.sf.taverna.t2.workflowmodel.DataflowValidationReport;
 import net.sf.taverna.t2.workflowmodel.EditException;
 import net.sf.taverna.t2.workflowmodel.Processor;
@@ -48,7 +59,7 @@ public class T2Component extends JPanel implements WorkflowModelViewSPI {
 
 	private ResultComponent resultComponent = (ResultComponent) new ResultComponentFactory()
 			.getComponent();
-	
+
 	private WorkflowInstanceFacadeImpl facade;
 
 	private int results = 0;
@@ -57,15 +68,22 @@ public class T2Component extends JPanel implements WorkflowModelViewSPI {
 		setLayout(new BorderLayout());
 
 		runButton = createRunButton();
-		stopButton = new JButton("Stop");
+		stopButton = new JButton(new AbstractAction("Stop") {
+
+			public void actionPerformed(ActionEvent e) {
+				// TODO: Actually stop the workflow
+				runButton.setEnabled(true);
+			}
+			
+		});
 
 		runStatus = new JTextArea();
 		runStatus.setSize(new Dimension(0, 200));
-		
+
 		JPanel runStatusPanel = new JPanel(new BorderLayout());
 		runStatusPanel.add(runStatus, BorderLayout.CENTER);
 		runStatusPanel.add(Box.createVerticalStrut(200), BorderLayout.EAST);
-		
+
 		runStatusScrollPane = new JScrollPane(runStatusPanel);
 
 		JPanel buttonPanel = new JPanel();
@@ -104,34 +122,44 @@ public class T2Component extends JPanel implements WorkflowModelViewSPI {
 								.checkValidity();
 						if (report.isValid()) {
 							updateStatus("done\n");
-							
-							facade = new WorkflowInstanceFacadeImpl(dataflow);
-							facade.addResultListener(new ResultListener() {
 
-								public void resultTokenProduced(
-										EntityIdentifier token,
-										int[] index, String portName, String owningProcess) {
-									if (index.length == 0) {
-										results++;
-										if (results == dataflow
-												.getOutputPorts()
-												.size()) {
-											resultComponent.deregister(facade);
-											facade.removeResultListener(this);
-											runButton.setEnabled(true);
-											results = 0;
-										}
-									}
-									updateStatus("Result "
-											+ indexString(index)
-											+ " for port "
-											+ portName + "\n");
-								}
+							List<? extends DataflowInputPort> inputPorts = dataflow
+									.getInputPorts();
+							if (!inputPorts.isEmpty()) {
+								JDialog dialog = new JDialog();
+								InputComponent inputComp = new InputComponent(
+										inputPorts,
+										new InputComponentCallback<DataflowInputPort>() {
 
-							});
-							resultComponent.register(facade);
-									
-							facade.fire();
+											public String getButtonText() {
+												return "Run workflow";
+											}
+
+											public void invoke(
+													Map<DataflowInputPort, EntityIdentifier> entities) {
+												try {
+													System.out
+															.println("Running with "
+																	+ entities);
+													runWorkflow(dataflow,
+															entities);
+												} catch (EditException e) {
+													logger.error(e);
+													updateStatus("failed\n");
+													showErrorDialog(
+															"Unable to translate workflow",
+															e.getMessage());
+													runButton.setEnabled(true);
+												}
+											}
+
+										});
+								dialog.add(inputComp);
+								dialog.setSize(640,480);
+								dialog.setVisible(true);
+								return;
+							}
+							runWorkflow(dataflow, null);
 						} else {
 							updateStatus("failed\n");
 							showErrorDialog("Unable to translate workflow",
@@ -177,7 +205,7 @@ public class T2Component extends JPanel implements WorkflowModelViewSPI {
 		result.append(']');
 		return result.toString();
 	}
-	
+
 	private void updateStatus(String status) {
 		runStatus.append(status);
 		JScrollBar scrollBar = runStatusScrollPane.getVerticalScrollBar();
@@ -203,6 +231,47 @@ public class T2Component extends JPanel implements WorkflowModelViewSPI {
 	}
 
 	public void onDispose() {
+
+	}
+
+	protected void runWorkflow(final Dataflow dataflow,
+			Map<DataflowInputPort, EntityIdentifier> entities)
+			throws EditException {
+		facade = new WorkflowInstanceFacadeImpl(dataflow);
+		facade.addResultListener(new ResultListener() {
+
+			public void resultTokenProduced(EntityIdentifier token,
+					int[] index, String portName, String owningProcess) {
+				if (index.length == 0) {
+					results++;
+					if (results == dataflow.getOutputPorts().size()) {
+						resultComponent.deregister(facade);
+						facade.removeResultListener(this);
+						runButton.setEnabled(true);
+						results = 0;
+					}
+				}
+				updateStatus("Result " + indexString(index) + " for port "
+						+ portName + "\n");
+			}
+
+		});
+		resultComponent.register(facade);
+		facade.fire();
+		if (entities != null) {
+			for (Entry<DataflowInputPort, EntityIdentifier> entry : entities
+					.entrySet()) {
+				DataflowInputPort inputPort = entry.getKey();
+				EntityIdentifier identifier = entry.getValue();
+				int[] index = new int[] {};
+				try {
+					facade.pushData(identifier, index, inputPort.getName());
+				} catch (TokenOrderException e) {
+					e.printStackTrace();
+					updateStatus("Could not submit data for port " + inputPort);
+				}
+			}
+		}
 
 	}
 
