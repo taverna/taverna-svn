5dc15762793ce3de70d6783a48868320d2b2fb56
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-11-28 13:28:48 +0000

    OPEN - issue TAV-662: Use BeanableFactory instead of BeanableRegistry http://www.mygrid.org.uk/jira/browse/TAV-662
    git-svn-id: http://taverna.googlecode.com/svn@4172 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2core/cloudone-impl/src/main/java/net/sf/taverna/t2/cloudone/bean/BeanableFactoryRegistry.java
@@ -0,0 +1,36 @@
+package net.sf.taverna.t2.cloudone.bean;
+
+import net.sf.taverna.t2.cloudone.bean.BeanableFactory;
+import net.sf.taverna.t2.spi.SPIRegistry;
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("unchecked")
+public class BeanableFactoryRegistry extends SPIRegistry<BeanableFactory> {
+	private static BeanableFactoryRegistry instance;
+
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger
+			.getLogger(BeanableFactoryRegistry.class);
+
+	protected BeanableFactoryRegistry() {
+		super(BeanableFactory.class);
+	}
+
+	public static synchronized BeanableFactoryRegistry getInstance() {
+		if (instance == null) {
+			instance = new BeanableFactoryRegistry();
+		}
+		return instance;
+	}
+
+	public BeanableFactory getFactory(String beanableClassName) {
+		for (BeanableFactory factory : getInstances()) {
+			if (factory.getBeanableType().getCanonicalName().equals(beanableClassName)) {
+				return factory;
+			}
+		}
+		throw new IllegalArgumentException("Can't find Beanable class " + beanableClassName);
+	}
+
+}
