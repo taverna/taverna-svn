981d9d63d0f8c735a18b5795ccfb3cc5025626f0
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-12-06 16:58:22 +0000

    TAV-673 an EntityModel can have a depth
    git-svn-id: http://taverna.googlecode.com/svn@4249 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/model/EntityModel.java
+++ trunk/t2core/cloudone-gui/src/main/java/net/sf/taverna/t2/cloudone/gui/entity/model/EntityModel.java
@@ -2,10 +2,21 @@ package net.sf.taverna.t2.cloudone.gui.entity.model;
 
 public class EntityModel {
 
+	public static final int UNKNOWN_DEPTH = -1;
+
+	private int depth = UNKNOWN_DEPTH;
+
 	private final EntityListModel parentModel;
+	private boolean removable = true;
 
 	public EntityModel(EntityListModel parentModel) {
 		this.parentModel = parentModel;
+		if (parentModel != null) {
+			int parentDepth = parentModel.getDepth();
+			if (parentDepth != UNKNOWN_DEPTH) {
+				setDepth(parentDepth - 1);
+			}
+		}
 	}
 
 	public EntityListModel getParentModel() {
@@ -13,10 +24,28 @@ public class EntityModel {
 	}
 
 	public void remove() {
-		EntityListModel parent = getParentModel();
-		if (parent != null) {
-			parent.removeEntityModel(this);
+		if (isRemovable()) {
+			getParentModel().removeEntityModel(this);
 		}
+	}
+
+	public boolean isRemovable() {
+		return removable && getParentModel() != null;
+	}
+
+	public void setRemovable(boolean removable) {
+		this.removable = removable;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		if (depth < UNKNOWN_DEPTH) {
+			throw new IllegalArgumentException("Invalid depth: " + depth);
+		}
+		this.depth = depth;
 	}
 
 }
