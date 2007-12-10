bb0ef935d0a439166b84cd0183ea0c9ea2874766
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-12-10 17:31:13 +0000

    Added a smaller t2 icon
    git-svn-id: http://taverna.googlecode.com/svn@4322 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/T2Perspective.java
+++ trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/T2Perspective.java
@@ -10,7 +10,7 @@ import net.sf.taverna.perspectives.AbstractPerspective;
 public class T2Perspective extends AbstractPerspective {
 
 	public ImageIcon getButtonIcon() {
-		URL iconURL = T2Perspective.class.getResource("/t2cogs.png");
+		URL iconURL = T2Perspective.class.getResource("/t2cogs_16x16.png");
 		if (iconURL == null) {
 			return null;
 		} else {
