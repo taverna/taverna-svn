714e1e4d8215206fe3a492f4245825429878884f
cvs2svn <cvs2svn@bf327186-88b3-11dd-a302-d386e5130c1c>
2007-12-14 16:21:06 +0000

    This commit was manufactured by cvs2svn to create tag 'wsdl-generic-1-0'.
    git-svn-id: http://taverna.googlecode.com/svn@4375 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ tags/wsdl-generic-1-0/wsdl-generic/src/main/java/net/sf/taverna/wsdl/parser/ArrayTypeDescriptor.java
@@ -0,0 +1,36 @@
+package net.sf.taverna.wsdl.parser;
+
+
+/**
+ * A TypeDescriptor that specifically describes an array type
+ * 
+ */
+public class ArrayTypeDescriptor extends TypeDescriptor {
+	private TypeDescriptor elementType;
+	private boolean wrapped;
+	
+	public boolean isWrapped() {
+		return wrapped;		
+	}
+
+	public void setWrapped(boolean wrapped) {
+		this.wrapped = wrapped;
+	}
+
+	public TypeDescriptor getElementType() {
+		return elementType;
+	}
+
+	public void setElementType(TypeDescriptor elementType) {
+		this.elementType = elementType;
+	}
+
+	public String getName() {
+		String name = super.getName();
+		if (name == null) {
+			return "ArrayOf" + getElementType().getType();
+		}
+		return name;
+	}
+
+}
