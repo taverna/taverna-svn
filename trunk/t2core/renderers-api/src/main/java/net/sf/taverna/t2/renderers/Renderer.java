017691dafe75b81c80d93a7b5cbe4604e500b724
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-02-19 14:35:50 +0000

    New canHandle method which takes DataFacade & EntityIdentifier
    git-svn-id: http://taverna.googlecode.com/svn@4556 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/renderers-api/src/main/java/net/sf/taverna/t2/renderers/Renderer.java
+++ trunk/t2core/renderers-api/src/main/java/net/sf/taverna/t2/renderers/Renderer.java
@@ -6,9 +6,13 @@ import net.sf.taverna.t2.cloudone.datamanager.DataFacade;
 import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 
 public interface Renderer {
-	
+
 	public boolean canHandle(String mimeType);
-	
-	public JComponent getComponent(EntityIdentifier entityIdentifier, DataFacade dataFacade);
+
+	public boolean canHandle(DataFacade facade,
+			EntityIdentifier entityIdentifier, String mimeType);
+
+	public JComponent getComponent(EntityIdentifier entityIdentifier,
+			DataFacade dataFacade);
 
 }
--- trunk/t2core/renderers-api/src/main/java/net/sf/taverna/t2/renderers/RendererRegistry.java
+++ trunk/t2core/renderers-api/src/main/java/net/sf/taverna/t2/renderers/RendererRegistry.java
@@ -3,6 +3,8 @@ package net.sf.taverna.t2.renderers;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.taverna.t2.cloudone.datamanager.DataFacade;
+import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 import net.sf.taverna.t2.spi.SPIRegistry;
 
 public class RendererRegistry extends SPIRegistry<Renderer>{
@@ -11,10 +13,10 @@ public class RendererRegistry extends SPIRegistry<Renderer>{
 		super(Renderer.class);
 	}
 	
-	public List<Renderer> getRenderersForMimeType(String mimeType) {
+	public List<Renderer> getRenderersForMimeType(DataFacade dataFacade, EntityIdentifier entityIdentifier, String mimeType) {
 		ArrayList<Renderer> list = new ArrayList<Renderer>();
 		for (Renderer renderer: getInstances()) {
-			if (renderer.canHandle(mimeType)) {
+			if (renderer.canHandle(dataFacade, entityIdentifier, mimeType)) {
 				list.add(renderer);
 			}
 		}
