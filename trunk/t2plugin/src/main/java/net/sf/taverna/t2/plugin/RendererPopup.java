bd5dcae8084b29299ad8f6b74a33ba7a457fd96e
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-02-22 14:49:46 +0000

    If there is a renderer error then catch the exception and display a text box outlining the problem.  Log it as well
    git-svn-id: http://taverna.googlecode.com/svn@4580 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/RendererPopup.java
+++ trunk/t2plugin/src/main/java/net/sf/taverna/t2/plugin/RendererPopup.java
@@ -7,18 +7,23 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTree;
 import javax.swing.tree.TreePath;
+
+import org.apache.log4j.Logger;
 
 import net.sf.taverna.t2.cloudone.datamanager.DataFacade;
 import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 import net.sf.taverna.t2.renderers.Renderer;
+import net.sf.taverna.t2.renderers.RendererException;
 import net.sf.taverna.t2.renderers.RendererRegistry;
 
 /**
@@ -33,6 +38,7 @@ import net.sf.taverna.t2.renderers.RendererRegistry;
  */
 public class RendererPopup extends MouseAdapter {
 
+	private static Logger logger = Logger.getLogger(RendererPopup.class);
 	private JTree tree;
 	private final DataFacade dataFacade;
 
@@ -69,7 +75,8 @@ public class RendererPopup extends MouseAdapter {
 				EntityIdentifier token = ((ResultTreeNode) lastSelectedPathComponent)
 						.getToken();
 				List<Renderer> allRenderers = new ArrayList<Renderer>();
-				//if there are no renderers then display these MIME types in a dialogue box
+				// if there are no renderers then display these MIME types in a
+				// dialogue box
 				String allMimeTypes = "";
 				for (String type : types) {
 					allMimeTypes = allMimeTypes + type + "\n";
@@ -119,8 +126,22 @@ public class RendererPopup extends MouseAdapter {
 
 			public void actionPerformed(ActionEvent e) {
 				JDialog resultDialog = new JDialog();
-				resultDialog.add(new JScrollPane(guiRenderer.getComponent(
-						guiEntityIdentifier, guiDataFacade)));
+				JComponent component = null;
+				try {
+					component = guiRenderer.getComponent(guiEntityIdentifier,
+							guiDataFacade);
+				} catch (RendererException e1) {
+					// show the use that something unexpected has happened but
+					// continue
+					component = new JTextArea(
+							"Could not render using renderer type "
+									+ guiRenderer.getClass().getName()
+									+ "\n"
+									+ "Please try with a different renderer if available and consult log for details of problem");
+					logger.warn("Couln not render using "
+							+ guiRenderer.getClass().getName(), e1);
+				}
+				resultDialog.add(new JScrollPane(component));
 				resultDialog.setSize(500, 500);
 				resultDialog.setVisible(true);
 			}
