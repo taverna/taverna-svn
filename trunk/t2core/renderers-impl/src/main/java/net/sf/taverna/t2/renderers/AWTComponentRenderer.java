e6ec2be6c02b7030835c0008c02971ec315bd2a6
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-03-05 15:47:59 +0000

    Returns a Component resolved from a DataManager using a EntityIdentifier
    git-svn-id: http://taverna.googlecode.com/svn@4598 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/renderers-impl/src/main/java/net/sf/taverna/t2/renderers/AWTComponentRenderer.java
+++ trunk/t2core/renderers-impl/src/main/java/net/sf/taverna/t2/renderers/AWTComponentRenderer.java
@@ -6,54 +6,66 @@
 package net.sf.taverna.t2.renderers;
 
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 import net.sf.taverna.t2.cloudone.datamanager.DataFacade;
+import net.sf.taverna.t2.cloudone.datamanager.NotFoundException;
+import net.sf.taverna.t2.cloudone.datamanager.RetrievalException;
 import net.sf.taverna.t2.cloudone.identifier.EntityIdentifier;
 
 /**
  * Class that renders objects which have a Java Component
  * subclass as their user object
  * @author Tom Oinn
+ * @author Ian Dunlop
  */
 public class AWTComponentRenderer implements Renderer{
     
-    private final String name;
 
-	public AWTComponentRenderer(String name) {
-		this.name = name;
-    }
-    
-    public boolean isTerminal() {
-	return true;
-    }
-    
-    public boolean canHandle(RendererRegistry renderers,
-			     Class dataClass) {
-	Class componentClass = Component.class;
-	return componentClass.isAssignableFrom(dataClass);
+	public AWTComponentRenderer() {
     }
 
 	public boolean canHandle(String mimeType) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public JComponent getComponent(EntityIdentifier entityIdentifier,
-			DataFacade dataFacade) {
-//		JPanel itemPanel = new JPanel(new BorderLayout());
-//		Component c = (Component)dataThing.getDataObject();
-//		itemPanel.add(c, BorderLayout.CENTER);
-//		return itemPanel;
-//		// TODO Auto-generated method stub
-		return null;
+			DataFacade dataFacade) throws RendererException {
+		JPanel itemPanel = new JPanel(new BorderLayout());
+		Component c = null;
+		try {
+			c = (Component)dataFacade.resolve(entityIdentifier, Component.class);
+		} catch (RetrievalException e) {
+			throw new RendererException(
+					"Could not resolve " + entityIdentifier, e);
+		} catch (NotFoundException e) {
+			throw new RendererException("Data Manager Could not find "
+					+ entityIdentifier, e);
+		}
+		itemPanel.add(c, BorderLayout.CENTER);
+		return itemPanel;
 	}
 
 	public boolean canHandle(DataFacade facade,
-			EntityIdentifier entityIdentifier, String mimeType) {
-		// TODO Auto-generated method stub
+			EntityIdentifier entityIdentifier, String mimeType) throws RendererException{
+		Object resolve = null;
+		try {
+			resolve = facade.resolve(entityIdentifier, Component.class);
+		} catch (RetrievalException e) {
+			throw new RendererException(
+					"Could not resolve " + entityIdentifier, e);
+		} catch (NotFoundException e) {
+			throw new RendererException("Data Manager Could not find "
+					+ entityIdentifier, e);
+		}
+		if (resolve instanceof Component) {
+			return true;
+		}
+
 		return false;
 	}
 
