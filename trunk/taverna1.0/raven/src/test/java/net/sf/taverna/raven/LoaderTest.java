b1c9f86e6f3df00e9fddcd318013f28846f57930
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-03-26 09:59:31 +0000

    Ignore integration tests that require internet connection
    git-svn-id: http://taverna.googlecode.com/svn@4701 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna1.0/raven/src/test/java/net/sf/taverna/raven/LoaderTest.java
+++ trunk/taverna1.0/raven/src/test/java/net/sf/taverna/raven/LoaderTest.java
@@ -2,6 +2,8 @@ package net.sf.taverna.raven;
 
 import static org.apache.commons.io.FileUtils.deleteDirectory;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -13,63 +15,57 @@ import java.net.URLClassLoader;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import net.sf.taverna.raven.repository.Artifact;
 import net.sf.taverna.raven.repository.ArtifactNotFoundException;
 import net.sf.taverna.raven.repository.ArtifactStateException;
 import net.sf.taverna.raven.repository.ArtifactStatus;
 import net.sf.taverna.raven.repository.RepositoryListener;
 
-public class LoaderTest extends TestCase {
+public class LoaderTest {
 
 	public static final String MAVEN_TOM_REPOSITORY = "http://www.ebi.ac.uk/~tmo/repository/";
 	public static final String MAVEN_MYGRID_REPOSITORY = "http://www.mygrid.org.uk/maven/repository/";
 	public static final String MAVEN_MYGRID_SNAPSHOT_REPOSITORY = "http://www.mygrid.org.uk/maven/snapshot-repository/";
 	public static final String MAVEN_MYGRID_PROXY_REPOSITORY = "http://www.mygrid.org.uk/maven/proxy/repository/";
 	public static final String MAVEN_UNIONTRANSIT_REPOSITORY = "http://www.uniontransit.com/apache/maven-repository/";
-	
-	public static final String repositoryLocation = 
-		MAVEN_MYGRID_REPOSITORY;
-	
+
+	public static final String repositoryLocation = MAVEN_MYGRID_REPOSITORY;
+
 	File dir;
-	
-	public void setUp() {
-		dir = createTempDirectory();
-		//dir = new File("C:\\testRaven");
-	}
 
-
-	public void tearDown() {
+	@Before
+	public void createTempDirectory() {
 		try {
-			deleteDirectory(dir);
-		} catch (IOException e) {
-			//
-		}
-	}
-
-	
-	private static File createTempDirectory() {
-		File tempFile;
-		try {
-			tempFile = File.createTempFile("raven", "");
-			// But we want a directory!
+			dir = File.createTempFile("raven", "");
 		} catch (IOException e) {
 			System.err.println("Could not create temporary directory");
 			e.printStackTrace();
-			return null;
+			dir = null;
+			return;
 		}
-		tempFile.delete();
-		assert tempFile.mkdir();
-		return tempFile;
+		// But we want a directory!
+		dir.delete();
+		assertTrue(dir.mkdir());
 	}
 
-  // fixme: name this back as testDynamic once this is deployed
-  // fixme: this is an integration test as it contacts a repo that relies on
-  // the code having already been successfully built and deployed - consider
-  // splitting into a unit test as above and a seperate integration test
-  /**
+	@After
+	public void deleteTempDirectory() throws IOException {
+		deleteDirectory(dir);
+	}
+
+	// fixme: name this back as testDynamic once this is deployed
+	// fixme: this is an integration test as it contacts a repo that relies on
+	// the code having already been successfully built and deployed - consider
+	// splitting into a unit test as above and a seperate integration test
+	/**
 	 * Test dynamic (reflection based) access to a remote raven repository
 	 * without using any reference to the raven API
+	 * 
 	 * @throws MalformedURLException
 	 * @throws ClassNotFoundException
 	 * @throws SecurityException
@@ -77,144 +73,125 @@ public class LoaderTest extends TestCase {
 	 * @throws IllegalArgumentException
 	 * @throws IllegalAccessException
 	 * @throws InvocationTargetException
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
-	public void shouldTestDynamic() throws MalformedURLException, ClassNotFoundException,
-	SecurityException, NoSuchMethodException, IllegalArgumentException, 
-	IllegalAccessException, InvocationTargetException, InterruptedException {
-		
-		// Create a remote classloader referencing the raven jar within a repository
+	@Ignore("Integration test")
+	@Test
+	public void shouldTestDynamic() throws MalformedURLException,
+			ClassNotFoundException, SecurityException, NoSuchMethodException,
+			IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException, InterruptedException {
 
-		String artifactLocation = 
-			"uk/org/mygrid/taverna/raven/raven/1.5.1/raven-1.5.1.jar";
-		ClassLoader c = new URLClassLoader(
-				new URL[]{new URL(repositoryLocation+artifactLocation)}
-				,null);
-		
+		// Create a remote classloader referencing the raven jar within a
+		// repository
+
+		String artifactLocation = "uk/org/mygrid/taverna/raven/raven/1.5.1/raven-1.5.1.jar";
+		ClassLoader c = new URLClassLoader(new URL[] { new URL(
+				repositoryLocation + artifactLocation) }, null);
+
 		// Reference to the Loader class within net.sf.taverna.raven
 		Class loaderClass = c.loadClass("net.sf.taverna.raven.Loader");
 		// Find the single static method provided by the loader
-		Method m = loaderClass.getDeclaredMethod(
-				"doRavenMagic",
-        String.class,
-        File.class,
-				URL[].class,
-        String.class,
-				String.class,
-        String.class,
-        String.class,
-        URL.class,
-        int.class);
-		
+		Method m = loaderClass.getDeclaredMethod("doRavenMagic", String.class,
+				File.class, URL[].class, String.class, String.class,
+				String.class, String.class, URL.class, int.class);
+
 		// Parameters for the Raven loader call
 		String ravenVersion = "1.5.1";
-		URL[] remoteRepositories = new URL[]{
-				new URL(repositoryLocation), 
-				new URL(MAVEN_MYGRID_PROXY_REPOSITORY)};
+		URL[] remoteRepositories = new URL[] { new URL(repositoryLocation),
+				new URL(MAVEN_MYGRID_PROXY_REPOSITORY) };
 		String groupID = "uk.org.mygrid.taverna";
 		String artifactID = "taverna-workbench";
 		String version = "1.5.1";
 		int minimumDisplayTime = 10 * 1000; // Ten seconds
 		String targetClassName = "org.embl.ebi.escience.scuflui.workbench.Workbench";
-		
+
 		// Construct array for dynamic invocation
-    //fixme: this will need changing when a mvn deploy is run
+		// fixme: this will need changing when a mvn deploy is run
 
 		// Call method via reflection, 'null' target as this is a static method
-		Class workbenchClass = (Class)m.invoke(
-            null,
-            ravenVersion,
-            dir,
-            remoteRepositories,
-            groupID,
-            artifactID,
-            version,
-            targetClassName,
-            null, //splashScreenImage,
-            minimumDisplayTime);
-		
+		Class workbenchClass = (Class) m.invoke(null, ravenVersion, dir,
+				remoteRepositories, groupID, artifactID, version,
+				targetClassName, null, // splashScreenImage,
+				minimumDisplayTime);
+
 		// Verify that the class is loaded and that the classloader is being
 		// driven off Raven's artifact system
-		assertEquals("org.embl.ebi.escience.scuflui.workbench.Workbench", 
+		assertEquals("org.embl.ebi.escience.scuflui.workbench.Workbench",
 				workbenchClass.getName());
-		String classLoaderName = workbenchClass.getClassLoader().toString().split(" ")[0];
+		String classLoaderName = workbenchClass.getClassLoader().toString()
+				.split(" ")[0];
 		assertEquals("loader{uk.org.mygrid.taverna:taverna-workbench:1.5.1}",
 				classLoaderName);
-		
+
 		// Could invoke the workbench's main method here as follows :
 		//
-		// Method workbenchMain = workbenchClass.getDeclaredMethod("main",String[].class);
+		// Method workbenchMain =
+		// workbenchClass.getDeclaredMethod("main",String[].class);
 		// workbenchMain.invoke(null,new Object[]{new String[0]});
 	}
 
-  public void testDynamicNonGui()
-          throws ClassNotFoundException, NoSuchMethodException,
-                 MalformedURLException, IllegalAccessException,
-                 InvocationTargetException, ArtifactNotFoundException,
-                 ArtifactStateException
-  {
+	@Ignore("Integration test")
+	@Test
+	public void testDynamicNonGui() throws ClassNotFoundException,
+			NoSuchMethodException, MalformedURLException,
+			IllegalAccessException, InvocationTargetException,
+			ArtifactNotFoundException, ArtifactStateException {
 
-    // Create a remote classloader referencing the raven jar within a repository
-    String artifactLocation = "uk/org/mygrid/taverna/raven/raven/1.5.1/raven-1.5.1.jar";
-    ClassLoader c = new URLClassLoader(new URL[]{new URL(repositoryLocation + artifactLocation)},
-                                       null);
+		// Create a remote classloader referencing the raven jar within a
+		// repository
+		String artifactLocation = "uk/org/mygrid/taverna/raven/raven/1.5.1/raven-1.5.1.jar";
+		ClassLoader c = new URLClassLoader(new URL[] { new URL(
+				repositoryLocation + artifactLocation) }, null);
 
-    // Reference to the Loader class within net.sf.taverna.raven
-    c.loadClass("net.sf.taverna.raven.Loader");
-    // Find the single static method provided by the loader
+		// Reference to the Loader class within net.sf.taverna.raven
+		c.loadClass("net.sf.taverna.raven.Loader");
+		// Find the single static method provided by the loader
 
-    // Parameters for the Raven loader call
-    URL[] remoteRepositories = new URL[]{
-            new URL(repositoryLocation),
-            new URL(MAVEN_MYGRID_PROXY_REPOSITORY) };
-    String groupID = "uk.org.mygrid.taverna";
-    String artifactID = "taverna-workbench";
-    String version = "1.5.1";
-    String targetClassName = "org.embl.ebi.escience.scuflui.workbench.Workbench";
+		// Parameters for the Raven loader call
+		URL[] remoteRepositories = new URL[] { new URL(repositoryLocation),
+				new URL(MAVEN_MYGRID_PROXY_REPOSITORY) };
+		String groupID = "uk.org.mygrid.taverna";
+		String artifactID = "taverna-workbench";
+		String version = "1.5.1";
+		String targetClassName = "org.embl.ebi.escience.scuflui.workbench.Workbench";
 
-    RepositoryListener listener = new RepositoryListener()
-    {
-      public void statusChanged(Artifact a, ArtifactStatus oldStatus, ArtifactStatus newStatus)
-      {
-        if(newStatus != null)
-        {
-        	/*
-          System.out.println(
-                  "change in status: " + a +
-                  " changed from " + oldStatus +
-                  " to " + newStatus);
-                  */
-        }
-      }
-    };
+		RepositoryListener listener = new RepositoryListener() {
+			public void statusChanged(Artifact a, ArtifactStatus oldStatus,
+					ArtifactStatus newStatus) {
+				if (newStatus != null) {
+					/*
+					 * System.out.println( "change in status: " + a + " changed
+					 * from " + oldStatus + " to " + newStatus);
+					 */
+				}
+			}
+		};
 
-    // Call method via reflection, 'null' target as this is a static method
-    Class workbenchClass = Loader.doRavenMagic(
-                                            dir,
-                                            remoteRepositories,
-                                            groupID,
-                                            artifactID,
-                                            version,
-                                            targetClassName,
-                                            listener);
+		// Call method via reflection, 'null' target as this is a static method
+		Class workbenchClass = Loader.doRavenMagic(dir, remoteRepositories,
+				groupID, artifactID, version, targetClassName, listener);
 
-    // Verify that the class is loaded and that the classloader is being
-    // driven off Raven's artifact system
-    assertEquals("org.embl.ebi.escience.scuflui.workbench.Workbench",
-            workbenchClass.getName());
+		// Verify that the class is loaded and that the classloader is being
+		// driven off Raven's artifact system
+		assertEquals("org.embl.ebi.escience.scuflui.workbench.Workbench",
+				workbenchClass.getName());
 
-	String classLoaderName = workbenchClass.getClassLoader().toString().split(" ")[0];
-	assertEquals("loader{uk.org.mygrid.taverna:taverna-workbench:1.5.1}",
-			classLoaderName);
+		String classLoaderName = workbenchClass.getClassLoader().toString()
+				.split(" ")[0];
+		assertEquals("loader{uk.org.mygrid.taverna:taverna-workbench:1.5.1}",
+				classLoaderName);
 
-    // Could invoke the workbench's main method here as follows :
-    //
-    // Method workbenchMain = workbenchClass.getDeclaredMethod("main",String[].class);
-    // workbenchMain.invoke(null,new Object[]{new String[0]});
-  }
+		// Could invoke the workbench's main method here as follows :
+		//
+		// Method workbenchMain =
+		// workbenchClass.getDeclaredMethod("main",String[].class);
+		// workbenchMain.invoke(null,new Object[]{new String[0]});
+	}
 
-  /**
+	/**
 	 * Test whether we can launch the workbench from a local raven jar
+	 * 
 	 * @throws MalformedURLException
 	 * @throws ArtifactNotFoundException
 	 * @throws ArtifactStateException
@@ -223,36 +200,31 @@ public class LoaderTest extends TestCase {
 	 * @throws IllegalAccessException
 	 * @throws UnsupportedLookAndFeelException
 	 */
-	public void testNonDynamic() throws MalformedURLException, ArtifactNotFoundException, ArtifactStateException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		UIManager.setLookAndFeel(UIManager
-				.getSystemLookAndFeelClassName());
-		Loader.doRavenMagic(
-				"1.5.1",
-				dir,
-				new URL[]{new URL(repositoryLocation), 
-				          new URL(MAVEN_MYGRID_PROXY_REPOSITORY)},
-        "uk.org.mygrid.taverna", "taverna-workbench", "1.5.1",
-        "org.embl.ebi.escience.scuflui.workbench.Workbench",
-        null, 10000
-    );
-		//System.out.println(workbenchClass.toString());
-		//System.out.println(workbenchClass.getClassLoader().toString());
+	@Ignore("Integration test")
+	@Test
+	public void testNonDynamic() throws MalformedURLException,
+			ArtifactNotFoundException, ArtifactStateException,
+			ClassNotFoundException, InstantiationException,
+			IllegalAccessException, UnsupportedLookAndFeelException {
+
+		// UIManager.setLookAndFeel(UIManager
+		// .getSystemLookAndFeelClassName());
+		Loader.doRavenMagic("1.5.1", dir, new URL[] {
+				new URL(repositoryLocation),
+				new URL(MAVEN_MYGRID_PROXY_REPOSITORY) },
+				"uk.org.mygrid.taverna", "taverna-workbench", "1.5.1",
+				"org.embl.ebi.escience.scuflui.workbench.Workbench", null,
+				10000);
+		// System.out.println(workbenchClass.toString());
+		// System.out.println(workbenchClass.getClassLoader().toString());
 		/**
-		System.out.println("\n\nRepository state dump : \n");
-		LocalRepository r = LocalRepository.getRepository(dir);
-		for (Artifact a : r.getArtifacts()) {
-			ArtifactImpl ai = (ArtifactImpl)a;
-			System.out.println(ai.toString()+"  ##  "+r.getStatus(ai).toString());
-			for (ArtifactImpl dep : ai.getDependencies()) {
-				System.out.println("    "+dep.toString()+"  --  "+r.getStatus(dep).toString());
-			}
-		}
-		*/
+		 * System.out.println("\n\nRepository state dump : \n"); LocalRepository
+		 * r = LocalRepository.getRepository(dir); for (Artifact a :
+		 * r.getArtifacts()) { ArtifactImpl ai = (ArtifactImpl)a;
+		 * System.out.println(ai.toString()+" ## "+r.getStatus(ai).toString());
+		 * for (ArtifactImpl dep : ai.getDependencies()) { System.out.println("
+		 * "+dep.toString()+" -- "+r.getStatus(dep).toString()); } }
+		 */
 	}
-	
-	public void testDummy() {
-		//
-	}
-	
-	
+
 }
