863f0339e116e268a19b790a4b896ad57802598f
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-04-02 13:12:32 +0000

    Removed not needed try-catch (Restlet catches Throwables as well)
    git-svn-id: http://taverna.googlecode.com/svn@4737 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/WorkersResource.java
+++ trunk/taverna-service/taverna-rest/src/main/java/net/sf/taverna/service/rest/resources/WorkersResource.java
@@ -19,35 +19,32 @@ public class WorkersResource extends AbstractResource {
 
 	@SuppressWarnings("unused")
 	private static Logger logger = Logger.getLogger(WorkersResource.class);
-	
+	private static final int MAX_WORKERS = 4;
+
 	public WorkersResource(Context context, Request request, Response response) {
 		super(context, request, response);
 		addRepresentation(new WorkersVelocityRepresentation());
 	}
-	
+
 	public boolean allowPost() {
 		return true;
 	}
 
 	public void handlePost() {
-		try {
-			if (getAuthUser().isAdmin() && daoFactory.getWorkerDAO().all().size()<4) {
-				
-				WorkerInitialisation.createNew();
-				getResponse().setRedirectRef(getRequest().getReferrerRef());
-				getResponse().setStatus(Status.REDIRECTION_FOUND);
-			}
-			else {
-				getResponse().setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			}
-		}
-		catch(Throwable e) {
-			getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "An error occurred creating the worker: "+e.getCause().getMessage());
+		if (getAuthUser().isAdmin()
+				&& daoFactory.getWorkerDAO().all().size() < MAX_WORKERS) {
+
+			WorkerInitialisation.createNew();
+			getResponse().setRedirectRef(getRequest().getReferrerRef());
+			getResponse().setStatus(Status.REDIRECTION_FOUND);
+		} else {
+			logger.warn("Can't create more than " + MAX_WORKERS + " workers");
+			getResponse().setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
 		}
 	}
 
 	class WorkersVelocityRepresentation extends VelocityRepresentation {
-		
+
 		@Override
 		protected Map<String, Object> getDataModel() {
 			List<Worker> workers = DAOFactory.getFactory().getWorkerDAO().all();
@@ -68,7 +65,6 @@ public class WorkersResource extends AbstractResource {
 		protected String templateName() {
 			return "workers.vm";
 		}
-		
-		
+
 	}
 }
