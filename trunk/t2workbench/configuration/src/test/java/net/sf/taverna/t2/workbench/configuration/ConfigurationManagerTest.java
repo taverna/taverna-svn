702d79c9ef56cd8cefa9602c3dad6b30798a9efb
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-04-18 09:19:38 +0000

    Test the ConfigurationManager
    git-svn-id: http://taverna.googlecode.com/svn@4838 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2workbench/configuration/src/test/java/net/sf/taverna/t2/workbench/configuration/ConfigurationManagerTest.java
@@ -0,0 +1,51 @@
+package net.sf.taverna.t2.workbench.configuration;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Map;
+
+import net.sf.taverna.t2.workbench.configuration.colour.ColourManager;
+
+import org.junit.Test;
+
+public class ConfigurationManagerTest {
+	
+	@Test
+	public void createConfigManager() {
+		ConfigurationManager instance = ConfigurationManager.getInstance();
+		assertNotNull("Config Manager should not be null", instance);
+	}
+	
+	@Test
+	public void populateConfigOfColourmanager() {
+		ColourManager manager=ColourManager.getInstance();
+		
+		manager.getPropertyMap().put("colour.first", 25);
+		manager.getPropertyMap().put("colour.second", 223);
+		
+		ConfigurationManager instance = ConfigurationManager.getInstance();
+		instance.setBaseConfigLocation(new File("/Users/Ian/scratch"));
+		try {
+			instance.store(manager);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		ColourManager manager2 = ColourManager.getInstance();
+		
+		try {
+			instance.populate(manager2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Map<String, Object> propertyMap = manager2.getPropertyMap();
+		
+		assertEquals("Properties do not match", propertyMap.get("colour.first"), manager.getPropertyMap().get("colour.first"));
+		assertEquals("Properties do not match", propertyMap.get("colour.second"), manager.getPropertyMap().get("colour.second"));
+		
+		
+	}
+
+}
