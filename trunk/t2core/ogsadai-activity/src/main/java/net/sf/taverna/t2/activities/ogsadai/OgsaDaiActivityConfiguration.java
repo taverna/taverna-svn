9deaf1878418502c9797560987d75dd214c6f3c9
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-04-22 15:22:41 +0000

    T2-78
    Renaming of funky modules.
    maelstrom -> workflowmodel
    cyclone -> compatibility
    cloudone -> data
    tsunami -> security
    
    
    git-svn-id: http://taverna.googlecode.com/svn@4856 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/ogsadai-activity/src/main/java/net/sf/taverna/t2/activities/ogsadai/OgsaDaiActivityConfiguration.java
+++
@@ -1,7 +0,0 @@
-package net.sf.taverna.t2.activities.ogsadai;
-
-import net.sf.taverna.t2.workflowmodel.processor.activity.config.ActivityPortsDefinitionBean;
-
-public class OgsaDaiActivityConfiguration extends ActivityPortsDefinitionBean{
-
-}
