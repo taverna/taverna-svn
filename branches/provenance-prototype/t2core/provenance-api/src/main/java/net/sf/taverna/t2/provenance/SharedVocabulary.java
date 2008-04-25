6b53bfac53e8b23e2148e40eaec8e6cf96eb2dd3
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-04-25 16:01:11 +0000

    Specifies the types of things happening
    git-svn-id: http://taverna.googlecode.com/svn@4915 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ branches/provenance-prototype/t2core/provenance-api/src/main/java/net/sf/taverna/t2/provenance/SharedVocabulary.java
@@ -0,0 +1,32 @@
+package net.sf.taverna.t2.provenance;
+
+/**
+* @author paolo
+*
+*/
+public interface SharedVocabulary {
+
+	/**
+	 * basic types
+	 */
+	public static String DATAFLOW_EVENT_TYPE = "workflow";
+	public static String PROCESS_EVENT_TYPE = "process";
+	
+	/**
+	 * correspond to each type in net.sf.taverna.t2.provenance
+	 */
+	public static String PROVENANCE_EVENT_TYPE = "provenanceEvent";
+	public static String ACTIVITY_EVENT_TYPE = "activity";
+	public static String DATA_EVENT_TYPE = "data";
+	public static String ERROR_EVENT_TYPE = "error";
+	public static String INMEMORY_EVENT_TYPE = "inmemory";
+	public static String INPUTDATA_EVENT_TYPE = "inputdata";
+	public static String ITERATION_EVENT_TYPE = "iteration";
+	public static String OUTPUTDATA_EVENT_TYPE = "outputdata";
+	public static String PROCESSOR_EVENT_TYPE = "processor";
+	public static String WEBSERVICE_EVENT_TYPE = "webservice";
+	public static String WORKFLOW_EVENT_TYPE = "workflow";
+	public static String END_WORKFLOW_EVENT_TYPE = "EOW";
+	
+	
+}
