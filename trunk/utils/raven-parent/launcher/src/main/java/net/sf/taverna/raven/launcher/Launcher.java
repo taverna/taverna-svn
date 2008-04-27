41c7bd6aa5c813422b554793c2761eab00f05e42
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-04-27 21:16:48 +0000

    Copied plugin manager and MyGridConfiguration/bootstrap stuff from taverna 1
    git-svn-id: http://taverna.googlecode.com/svn@4921 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/utils/raven-parent/launcher/src/main/java/net/sf/taverna/raven/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package net.sf.taverna.raven.launcher;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Launching..");
+	}
+	
+}
---
+++ trunk/utils/raven-parent/launcher/src/test/java/net/sf/taverna/raven/launcher/TestLauncher.java
@@ -0,0 +1,11 @@
+package net.sf.taverna.raven.launcher;
+
+import org.junit.Test;
+
+public class TestLauncher {
+	
+	@Test
+	public void launchSample() throws Exception {
+		Launcher.main(new String[]{});
+	}
+}
