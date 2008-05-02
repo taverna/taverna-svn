cbd3e6f6954bdf44407589f45d19b48f2c0efd9e
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-02 16:18:00 +0000

    Moved back ApplicationConfig to launcher/
    Renamed package from net.sf.taverna.raven.launcher to net.sf.taverna.raven.prelauncher
    
    Added ApplicationRuntime that keeps runtime stuff (such as home directory) that can be changed with set*() methods.
    
    
    git-svn-id: http://taverna.googlecode.com/svn@4943 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/utils/raven-parent/launcher/src/main/java/net/sf/taverna/raven/launcher/ApplicationRuntime.java
@@ -0,0 +1,170 @@
+package net.sf.taverna.raven.launcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.sf.taverna.raven.repository.Artifact;
+import net.sf.taverna.raven.repository.BasicArtifact;
+import net.sf.taverna.raven.repository.Repository;
+import net.sf.taverna.raven.repository.impl.DummyRepository;
+import net.sf.taverna.raven.repository.impl.LocalRepository;
+
+import org.apache.log4j.Logger;
+
+public class ApplicationRuntime {
+	private static final String REPOSITORY = "repository";
+
+	private static Logger logger = Logger.getLogger(ApplicationRuntime.class);
+
+	private static ApplicationRuntime instance;
+
+	private ApplicationConfig appConfig = ApplicationConfig.getInstance();
+
+	private File localRepositoryDir;
+
+	private File applicationHomeDir;
+
+	private ApplicationUserHome appUserHome;
+
+	private Repository ravenRepository;
+
+	/**
+	 * Protected constructor, use {@link #getInstance()}.
+	 */
+	protected ApplicationRuntime() {
+		appUserHome = new ApplicationUserHome(appConfig.getName(), appConfig
+				.getApplicationHome());
+	}
+
+	/**
+	 * Get the application runtime settings.
+	 * 
+	 * @return
+	 */
+	public static synchronized ApplicationRuntime getInstance() {
+		if (instance == null) {
+			instance = new ApplicationRuntime();
+		}
+		return instance;
+	}
+
+	/**
+	 * Get the local repository directory.
+	 * 
+	 * @return
+	 */
+	public synchronized File getLocalRepositoryDir() {
+		if (localRepositoryDir != null) {
+			return localRepositoryDir;
+		}
+
+		// Let's see if the application config says where it should be
+		String appRepository = appConfig.getLocalRavenRepository();
+		if (appRepository != null && !appRepository.equals("")) {
+			localRepositoryDir = new File(appRepository);
+		} else {
+			// We'll make it under the application home directory then
+			File appHome = getApplicationHomeDir();
+			localRepositoryDir = new File(appHome, REPOSITORY);
+		}
+
+		localRepositoryDir.mkdirs();
+		if (!localRepositoryDir.isDirectory()) {
+			throw new IllegalStateException("Could not make local repository "
+					+ localRepositoryDir);
+		}
+		return localRepositoryDir;
+
+	}
+
+	public synchronized File getApplicationHomeDir() {
+		if (applicationHomeDir != null) {
+			return applicationHomeDir;
+		}
+		File homeDir = appUserHome.getAppUserHome();
+		if (homeDir == null) {
+			try {
+				// Make a temporary home directory as a backup
+				homeDir = File.createTempFile(appConfig.getName(), "home");
+				homeDir.delete();
+				homeDir.mkdirs();
+			} catch (IOException e) {
+				throw new IllegalStateException(
+						"Can't create temporary application home", e);
+			}
+			logger.warn("Could not determine application's user home,"
+					+ " using temporary dir " + homeDir);
+
+		}
+		if (!homeDir.isDirectory()) {
+			throw new IllegalStateException(
+					"Could not create application home directory " + homeDir);
+		}
+		applicationHomeDir = homeDir;
+		return applicationHomeDir;
+	}
+
+	/**
+	 * Set (and if necessary create) the local repository directory to be
+	 * returned by {@link #getLocalRepositoryDir()}.
+	 * 
+	 * @param localRepositoryDir
+	 *            Directory that is to be the the new local repository
+	 * @throws IOException
+	 *             If the localRepositoryDir could not be created or accessed as
+	 *             a directory.
+	 */
+	public synchronized void setLocalRepositoryDir(File localRepositoryDir)
+			throws IOException {
+		localRepositoryDir.mkdirs();
+		if (localRepositoryDir.isDirectory()) {
+			this.localRepositoryDir = localRepositoryDir;
+		} else {
+			throw new IOException("Invalid directory " + localRepositoryDir);
+		}
+	}
+
+	public synchronized Repository getRavenRepository() {
+		if (ravenRepository != null) {
+			return ravenRepository;
+		}
+
+		if (!appConfig.isUsingRaven()) {
+			ravenRepository = new DummyRepository();
+			return ravenRepository;
+		}
+		ravenRepository = LocalRepository
+				.getRepository(getLocalRepositoryDir(), getClassLoader(),
+						getSystemArtifacts());
+		return ravenRepository;
+	}
+
+	public Set<Artifact> getSystemArtifacts() {
+		Set<Artifact> artifacts = new HashSet<Artifact>();
+		artifacts.add(new BasicArtifact("uk.org.mygrid.taverna.raven", "raven",
+				"1.7-SNAPSHOT"));
+		artifacts.add(new BasicArtifact("uk.org.mygrid.taverna.raven",
+				"raven-log4j", "1.7-SNAPSHOT"));
+		artifacts.add(new BasicArtifact("uk.org.mygrid.taverna.raven",
+				"prelauncher", "1.7-SNAPSHOT"));
+		artifacts.add(new BasicArtifact("uk.org.mygrid.taverna.raven",
+				"launcher", "1.7-SNAPSHOT"));
+		artifacts.add(new BasicArtifact("log4j", "log4j", "1.2.12"));
+		return artifacts;
+	}
+
+	public ClassLoader getClassLoader() {
+		ClassLoader ourClassLoader = getClass().getClassLoader();
+		if (ourClassLoader == null) {
+			ourClassLoader = ClassLoader.getSystemClassLoader();
+		}
+		return ourClassLoader;
+	}
+
+	public synchronized void setApplicationHomeDir(File applicationHomeDir) {
+		this.applicationHomeDir = applicationHomeDir;
+	}
+
+}
---
+++ trunk/utils/raven-parent/launcher/src/main/java/net/sf/taverna/raven/launcher/Launcher.java
@@ -0,0 +1,88 @@
+package net.sf.taverna.raven.launcher;
+
+import net.sf.taverna.raven.launcher.Launchable;
+import net.sf.taverna.raven.prelauncher.PreLauncher;
+import net.sf.taverna.raven.repository.Repository;
+import net.sf.taverna.raven.spi.SpiRegistry;
+
+/**
+ * Launcher called by the {@link PreLauncher} after making sure Raven etc. is on
+ * the classpath.
+ * 
+ * @author Stian Soiland-Reyes
+ * 
+ */
+public class Launcher {
+
+	/**
+	 * Calls the "real" application
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Launcher launcher = new Launcher();
+		int status = launcher.launchMain(args);
+		if (status != 0) {
+			System.exit(status);
+		}
+	}
+
+	private final ApplicationConfig appConfig;
+	private final ApplicationRuntime appRuntime;
+
+	public Launcher() {
+		appConfig = ApplicationConfig.getInstance();
+		appRuntime = ApplicationRuntime.getInstance();
+	}
+
+	public Launchable findMainClass(String className) throws InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		Repository localRepository = appRuntime.getRavenRepository();
+		localRepository.update();
+		// TODO: load plugin stuff
+		
+		SpiRegistry launchableSpi = new SpiRegistry(localRepository,
+				Launchable.class.getCanonicalName(), appRuntime
+						.getClassLoader());
+		for (Class<?> launchableClass : launchableSpi) {
+			System.out.println(launchableClass);
+			if (launchableClass.getCanonicalName().equals(className)) {
+				Launchable launchable = (Launchable) launchableClass
+						.newInstance();
+				return launchable;
+			}
+		}
+		throw new ClassNotFoundException("Could not find " + className);
+	}
+
+	public int launchMain(String[] args) {
+		Launchable launchable;
+		System.out.println(appConfig.getName());
+		String mainClass = appConfig.getMainClass();
+		try {
+			launchable = findMainClass(mainClass);
+		} catch (ClassNotFoundException e) {
+			System.err.println("Could not find class: " + mainClass);
+			e.printStackTrace();
+			return -1;
+		} catch (IllegalAccessException e) {
+			System.err.println("Could not access main() in class: "
+					+ mainClass);
+			e.printStackTrace();
+			return -2;
+		} catch (InstantiationException e) {
+			System.err.println("Could not instantiate class: " + mainClass);
+			e.printStackTrace();
+			return -3;
+		}
+		
+		try {
+			return launchable.launch(args);
+		} catch (Exception e) {
+			System.err.println("Error while executing main() of " + mainClass);
+			e.printStackTrace();
+			return -4;
+		}
+	}
+
+}
