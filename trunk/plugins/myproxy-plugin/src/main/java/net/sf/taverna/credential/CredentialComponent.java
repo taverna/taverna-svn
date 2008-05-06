4608d7a773c629899215c554aa27ffa01aa492b8
bkattamuri <bkattamuri@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-06 15:19:54 +0000

    After changing GUI bits
    git-svn-id: http://taverna.googlecode.com/svn@4948 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/plugins/myproxy-plugin/src/main/java/net/sf/taverna/credential/CredentialComponent.java
+++ trunk/plugins/myproxy-plugin/src/main/java/net/sf/taverna/credential/CredentialComponent.java
@@ -1,5 +1,7 @@
 package net.sf.taverna.credential;
 
+
+import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -24,7 +26,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
-import org.embl.ebi.escience.scufl.ScuflModel;
 import org.embl.ebi.escience.scuflui.spi.UIComponentSPI;
 import org.globus.gsi.GlobusCredential;
 import org.globus.gsi.GlobusCredentialException;
@@ -46,7 +47,7 @@ public class CredentialComponent extends JPanel implements
 	private static final Logger logger = Logger
 			.getLogger(CredentialComponent.class);
 	GlobusCredential gc = null;
-	String format = null;
+	String format = "pem";
 	CredentialCreator credentialCreator = null;
 	JRadioButton pk12 = null;
 	JRadioButton pem = null;
@@ -71,6 +72,7 @@ public class CredentialComponent extends JPanel implements
 
 	JTextField myproxyUserText = null;
 	JPasswordField myproxyPassText = null;
+	JPasswordField myproxyPassText2 = null;
 	JTextField myproxyServerText = null;
 	JTextField myproxyCredentialDaysText = null;
 	JTextField myproxyCredentialHoursText = null;
@@ -80,55 +82,36 @@ public class CredentialComponent extends JPanel implements
 	JPanel mainPanel = null;
 	JPanel gridPanel = null;
 	JPanel myproxyPanel = null;
+	JPanel cardPanel = null;
+	CardLayout cardLayout = null;
+	
+	JTabbedPane tabbedPane = null;
+	int myproxyTab = 0;
 
 	public CredentialComponent() {
 		super(new GridLayout(3, 3));
 
-		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.setPreferredSize(new Dimension(500, 400));
-		tabbedPane.setMinimumSize(new Dimension(500, 400));
-		tabbedPane.setMaximumSize(new Dimension(500, 400));
+		tabbedPane = new JTabbedPane();
+		tabbedPane.setPreferredSize(new Dimension(400, 400));
+		tabbedPane.setMinimumSize(new Dimension(400, 400));
+		tabbedPane.setMaximumSize(new Dimension(400, 400));
 
 		gridPanel = new JPanel(new GridLayout(6, 1));
 		credentialCreator = new CredentialCreator();
-
-		JPanel optionPanel = new JPanel(new GridLayout(1, 4));
-
-		JLabel lab1 = new JLabel("");
-		optionPanel.add(lab1);
-		pk12 = new JRadioButton("PK12");
-		pk12.addActionListener(this);
-		pem = new JRadioButton("PEM");
-		pem.addActionListener(this);
-		formatGroup = new ButtonGroup();
-		formatGroup.add(pk12);
-		formatGroup.add(pem);
-
-		optionPanel.add(pk12);
-		optionPanel.add(pem);
-		JLabel lab2 = new JLabel("");
-		optionPanel.add(lab2);
-		gridPanel.add(optionPanel);
-
-		usercertLabel = new JLabel("Usercert file:");
-		usercertText = new JTextField(10);
-		certButton = new JButton("...");
-		certButton.addActionListener(this);
-		this.addGridPanel(usercertLabel, usercertText, certButton);
-		userkeyLabel = new JLabel("Userkey file:");
-		userkeyText = new JTextField(10);
-		keyButton = new JButton("...");
-		keyButton.addActionListener(this);
-		this.addGridPanel(userkeyLabel, userkeyText, keyButton);
-
-		pk12Label = new JLabel("PK12 file:   ");
-		pk12Text = new JTextField(10);
-		pk12Button = new JButton("...");
-		pk12Button.addActionListener(this);
-		this.addGridPanel(pk12Label, pk12Text, pk12Button);
-
+	
 		tabbedPane.addTab("GridProxy", this.getGridPanel());
-		tabbedPane.addTab("MyProxy", this.getMyProxyPanel());
+		tabbedPane.addTab("MyProxy", this.getMyProxyPanel()); 
+		myproxyTab = tabbedPane.getTabCount() -1;
+		
+		try {
+			gc = credentialCreator.getGlobusCredentialFromFile();
+			if (gc==null) {
+				tabbedPane.setEnabledAt(myproxyTab, false);
+				deleteProxy.setEnabled(false);
+			}
+		} catch (GlobusCredentialException e) {
+			e.printStackTrace();
+		}
 		tabbedPane.addTab("Help", this.getHelpPanel());
 
 		add(tabbedPane);
@@ -163,27 +146,10 @@ public class CredentialComponent extends JPanel implements
 		logger.debug("source:" + e.getActionCommand());
 		if (e.getSource() == pk12) {
 			format = "pk12";
-			usercertLabel.setVisible(false);
-			usercertText.setVisible(false);
-			certButton.setVisible(false);
-			userkeyLabel.setVisible(false);
-			userkeyText.setVisible(false);
-			keyButton.setVisible(false);
-			pk12Label.setVisible(true);
-			pk12Text.setVisible(true);
-			pk12Button.setVisible(true);
+			cardLayout.show(cardPanel, "2");
 		}
 		if (e.getSource() == pem) {
-			format = "pem";
-			usercertLabel.setVisible(true);
-			usercertText.setVisible(true);
-			certButton.setVisible(true);
-			userkeyLabel.setVisible(true);
-			userkeyText.setVisible(true);
-			keyButton.setVisible(true);
-			pk12Label.setVisible(false);
-			pk12Text.setVisible(false);
-			pk12Button.setVisible(false);
+			cardLayout.show(cardPanel, "1");
 		}
 		if (e.getSource() == certButton) {
 			JFileChooser filechooser = null;
@@ -226,23 +192,33 @@ public class CredentialComponent extends JPanel implements
 			userkeyText.setText("");
 			pk12Text.setText("");
 			gridpassText.setText("");
+			gridcredentialTimeText.setText("12");
 		}
 
 		if (e.getSource() == deleteProxy) {
-
+			
 			boolean deletefile = credentialCreator.deleteCredential();
-			if (deletefile == true)
-				JOptionPane.showMessageDialog(this,
-						"Credential Deleted Successfully");
-			else
-				JOptionPane.showMessageDialog(this,
-						"Credential Not Existed to Delete");
-		}
+			int ret = JOptionPane.showConfirmDialog(gridPanel, "Do you want to Delete Proxy ?");
+			if( ret == JOptionPane.YES_OPTION ) {
+				if (deletefile){
+					
+					JOptionPane.showMessageDialog(this,
+							"Proxy Deleted Successfully");
+				    tabbedPane.setEnabledAt(myproxyTab, false);
+				    deleteProxy.setEnabled(false);
+				}
+				else
+					JOptionPane.showMessageDialog(this,
+							"Proxy Not Existed to Delete");
+			}
+			 		
+		} //deleteProxy
 
 		if (e.getSource() == gridproxyInit) {
 			if (format == null) {
 				JOptionPane.showMessageDialog(this,
 						"FailFormat not selected, select PEM or PK12 !");
+				
 			}
 
 			if (format.equals("pem")) {
@@ -257,20 +233,23 @@ public class CredentialComponent extends JPanel implements
 								gridpassText.getPassword()), usercertText
 								.getText(), userkeyText.getText(),
 								gridcredentialTimeText.getText());
-						if (gc != null)
+						if (gc != null){
 							JOptionPane.showMessageDialog(this,
-									"Credential created Successfully");
+									"Proxy created Successfully");
+							tabbedPane.setEnabledAt(myproxyTab, true);
+							deleteProxy.setEnabled(true);
+						}
 					} catch (Exception e1) {
 						e1.printStackTrace();
 						JOptionPane.showMessageDialog(this,
-								"Failed to create credential !\n\""
+								"Failed to create proxy !\n\""
 										+ e1.getMessage() + "\"", "Error",
 								JOptionPane.ERROR_MESSAGE);
 						e1.printStackTrace();
 					}
 				}
-
-			}
+			} //if pem
+			
 			if (format.equals("pk12")) {
 				if (pk12Text.getText().equals("")) {
 					JOptionPane.showMessageDialog(this,
@@ -281,21 +260,26 @@ public class CredentialComponent extends JPanel implements
 								gridpassText.getPassword()),
 								pk12Text.getText(), gridcredentialTimeText
 										.getText());
-						if (gc != null)
+						if (gc != null){
 							JOptionPane.showMessageDialog(this,
-									"Credential created Successfully");
+									"Proxy created Successfully");
+							tabbedPane.setEnabledAt(myproxyTab, true);
+							deleteProxy.setEnabled(true);
+						}
 					} catch (Exception e1) {
 						JOptionPane.showMessageDialog(this,
-								"Failed to create credential !\n\""
+								"Failed to create proxy !\n\""
 										+ e1.getMessage() + "\"", "Error",
 								JOptionPane.ERROR_MESSAGE);
 						e1.printStackTrace();
 					}
 				}
-			}
+			} //if pk12
+			
 		}
 
 		if (e.getSource() == myproxyInit) {
+			
 			boolean success = false;
 			try {
 				gc = credentialCreator.getGlobusCredentialFromFile();
@@ -308,11 +292,12 @@ public class CredentialComponent extends JPanel implements
 						"Credential not existed, do GridProxyInit ");
 			} else if (myproxyUserText.getText().equals("")
 					|| myproxyPassText.getPassword().length == 0
+					|| !(myproxyPassText2.getPassword().equals(myproxyPassText.getPassword()) )
 					|| myproxyServerText.getText().equals("")
 					|| (myproxyCredentialDaysText.getText().equals("") && myproxyCredentialHoursText
 							.getText().equals(""))) {
 				JOptionPane
-						.showMessageDialog(this, "Insufficient MyProxy Data");
+						.showMessageDialog(this, "Insufficient MyProxy Data or Passwords didn't match. ");
 			} else {
 
 				try {
@@ -349,9 +334,9 @@ public class CredentialComponent extends JPanel implements
 		if (e.getSource() == myproxyClear) {
 			myproxyUserText.setText("");
 			myproxyPassText.setText("");
-			myproxyServerText.setText("");
-			myproxyCredentialDaysText.setText("");
-			myproxyCredentialHoursText.setText("");
+			myproxyServerText.setText("myproxy.grid-support.ac.uk");
+			myproxyCredentialDaysText.setText("7");
+			myproxyCredentialHoursText.setText("0");
 		}
 
 		if (e.getSource() == myproxyDelete) {
@@ -373,15 +358,13 @@ public class CredentialComponent extends JPanel implements
 									myproxyPassText.getPassword()),
 							myproxyServerText.getText());
 				} catch (MyProxyException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (GSSException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 
 			}
-		}
+		} 
 
 	}
 
@@ -398,7 +381,10 @@ public class CredentialComponent extends JPanel implements
 	}
 
 	private JPanel getGridPanel() {
-		gridPanel = new JPanel(new GridLayout(6, 1));
+		gridPanel = new JPanel(); //new GridLayout(6, 1));
+		GridBagLayout ggb = new GridBagLayout();
+		gridPanel.setLayout(ggb);
+		GridBagConstraints ggbc = new GridBagConstraints();
 		credentialCreator = new CredentialCreator();
 
 		JPanel optionPanel = new JPanel(new GridLayout(1, 4));
@@ -410,78 +396,95 @@ public class CredentialComponent extends JPanel implements
 		optionPanel.add(lab1);
 		pk12 = new JRadioButton("PK12");
 		pk12.addActionListener(this);
-		pem = new JRadioButton("PEM");
+		pem = new JRadioButton("PEM", true);
 		pem.addActionListener(this);
 		formatGroup = new ButtonGroup();
 		formatGroup.add(pk12);
 		formatGroup.add(pem);
+		optionPanel.setBorder(BorderFactory.createTitledBorder("choose file format "));
 
 		optionPanel.add(pk12);
 		optionPanel.add(pem);
 		JLabel lab2 = new JLabel("");
 		optionPanel.add(lab2);
-		gridPanel.add(optionPanel);
-
+	
+		this.addToGridBagPanel(optionPanel, ggb, ggbc,0);
+		
+		cardPanel = new JPanel();
+		cardLayout = new CardLayout();
+		cardPanel.setLayout(cardLayout);
+		
+		
+		JPanel pemFilePanel = new JPanel(new GridLayout(2,1));
 		usercertLabel = new JLabel("Usercert file:");
 		usercertText = new JTextField(10);
-		certButton = new JButton("...");
+		certButton = new JButton("choose file");
 		certButton.addActionListener(this);
-		this.addGridPanel(usercertLabel, usercertText, certButton);
+		this.addToPanel(pemFilePanel,usercertLabel, usercertText, certButton);
 		userkeyLabel = new JLabel("Userkey file:");
 		userkeyText = new JTextField(10);
-		keyButton = new JButton("...");
+		keyButton = new JButton("choose file");
 		keyButton.addActionListener(this);
-		this.addGridPanel(userkeyLabel, userkeyText, keyButton);
+		this.addToPanel(pemFilePanel,userkeyLabel, userkeyText, keyButton);
 
+		JPanel pk12FilePanel = new JPanel();
 		pk12Label = new JLabel("PK12 file:   ");
 		pk12Text = new JTextField(10);
-		pk12Button = new JButton("...");
+		pk12Button = new JButton("choose file");
 		pk12Button.addActionListener(this);
-		this.addGridPanel(pk12Label, pk12Text, pk12Button);
+		this.addToPanel(pk12FilePanel,pk12Label, pk12Text, pk12Button);
+		
+		this.addToGridBagPanel(pemFilePanel, ggb, ggbc,1);
 
+		cardPanel.add(pemFilePanel,"1");
+		cardPanel.add(pk12FilePanel,"2");
+		cardLayout.first(cardPanel);
+		this.addToGridBagPanel(cardPanel, ggb, ggbc, 1);
+		
 		JPanel gridProxyPanel = new JPanel();
 		gb = new GridBagLayout();
 		gridProxyPanel.setLayout(gb);
 		gbc = new GridBagConstraints();
 		this.setGbc(gbc, 0, 0, 0.5, 0.5, 1, 1, GridBagConstraints.HORIZONTAL);
-		JLabel gridpassLabel = new JLabel("Gridpassphrase:");
+		JLabel gridpassLabel = new JLabel("Password:");
 		gb.setConstraints(gridpassLabel, gbc);
 		gridProxyPanel.add(gridpassLabel);
 		this.setGbc(gbc, 0, 1, 0.5, 0.5, 1, 1, GridBagConstraints.HORIZONTAL);
 		gbc.insets = new Insets(2, 10, 2, 10);
 		gridpassText = new JPasswordField(10);
 		gb.setConstraints(gridpassText, gbc);
-		gridProxyPanel.add(gridpassText); // ............2
+		gridProxyPanel.add(gridpassText); 
 		this.setGbc(gbc, 0, 2, 0.5, 0.5, 1, 1, GridBagConstraints.HORIZONTAL);
 		JLabel credentialTimeLabel = new JLabel("Credential Time (hrs):");
 		gbc.anchor = GridBagConstraints.CENTER;
 		gb.setConstraints(credentialTimeLabel, gbc);
 		gridProxyPanel.add(credentialTimeLabel);
 		this.setGbc(gbc, 0, 3, 0.5, 0.5, 1, 1, GridBagConstraints.REMAINDER);
-		gridcredentialTimeText = new JTextField("12");
+		gridcredentialTimeText = new JTextField(3);
+		gridcredentialTimeText.setText("12");
 		gbc.anchor = GridBagConstraints.WEST;
 		gb.setConstraints(gridcredentialTimeText, gbc);
 		gridProxyPanel.add(gridcredentialTimeText);
-		gridPanel.add(gridProxyPanel);
+		this.addToGridBagPanel(gridProxyPanel, ggb, ggbc,2);
 
-		JPanel gridButtonPanel = new JPanel(new GridLayout(1, 3));
-		gridproxyClear = new JButton("Clear");
+		JPanel gridButtonPanel = new JPanel();
+		gridproxyClear = new JButton("Default");
 		gridproxyClear.addActionListener(this);
 		deleteProxy = new JButton("DeleteProxy");
 		deleteProxy.addActionListener(this);
-		gridproxyInit = new JButton("GridProxyInit");
+		gridproxyInit = new JButton("CreateProxy");
 		gridproxyInit.addActionListener(this);
 		gridButtonPanel.add(gridproxyClear);
 		gridButtonPanel.add(deleteProxy);
 		gridButtonPanel.add(gridproxyInit);
-		gridPanel.add(gridButtonPanel);
+		this.addToGridBagPanel(gridButtonPanel, ggb, ggbc,3);
 
 		return gridPanel;
 	}
 
 	private JPanel getMyProxyPanel() {
 
-		myproxyPanel = new JPanel(new GridLayout(6, 1));
+		myproxyPanel = new JPanel(new GridLayout(8, 1));
 
 		JLabel myproxyUserLabel = new JLabel("MyProxy Username:");
 		myproxyUserText = new JTextField(10);
@@ -492,31 +495,36 @@ public class CredentialComponent extends JPanel implements
 		myproxyPassText = new JPasswordField(10);
 		this.addMyproxyPanel(myproxyPassphraseLabel, myproxyPassText);
 
+		JLabel myproxyPassphraseLabel2 = new JLabel("Confirm MyProxy Passphrase:");
+		myproxyPassText2 = new JPasswordField(10);
+		this.addMyproxyPanel(myproxyPassphraseLabel2, myproxyPassText2);
+		
 		JLabel myproxyServer = new JLabel("MyProxy Server:");
-		myproxyServerText = new JTextField("myproxy.grid-support.ac.uk");
-		myproxyServerText.setSize(10, 10);
+		myproxyServerText = new JTextField(30);
+		myproxyServerText.setText("myproxy.grid-support.ac.uk");
+		
 		this.addMyproxyPanel(myproxyServer, myproxyServerText);
 
 		JLabel myproxyCredentialDaysLabel = new JLabel(
 				"MyProxy Credential  (days):");
-		myproxyCredentialDaysText = new JTextField(5);
+		myproxyCredentialDaysText = new JTextField(3);
 		myproxyCredentialDaysText.setText("7");
 		this.addMyproxyPanel(myproxyCredentialDaysLabel,
 				myproxyCredentialDaysText);
 
 		JLabel myproxyCredentialHoursLabel = new JLabel(
 				"MyProxy Credential  (hrs):");
-		myproxyCredentialHoursText = new JTextField(5);
+		myproxyCredentialHoursText = new JTextField(3);
 		myproxyCredentialHoursText.setText("0");
 		this.addMyproxyPanel(myproxyCredentialHoursLabel,
 				myproxyCredentialHoursText);
 
-		JPanel myproxyButtonPanel = new JPanel(new GridLayout(1, 3));
-		myproxyClear = new JButton("Clear");
+		JPanel myproxyButtonPanel = new JPanel();
+		myproxyClear = new JButton("Default");
 		myproxyClear.addActionListener(this);
-		myproxyDelete = new JButton("MyProxy Delete");
+		myproxyDelete = new JButton("DeleteMyProxy");
 		myproxyDelete.addActionListener(this);
-		myproxyInit = new JButton("MyProxy Init");
+		myproxyInit = new JButton("CreateMyProxy");
 		myproxyInit.addActionListener(this);
 
 		myproxyButtonPanel.add(myproxyClear);
@@ -530,7 +538,7 @@ public class CredentialComponent extends JPanel implements
 
 	}
 
-	private void addGridPanel(JLabel label, JTextField textField, JButton button) {
+	private void addToPanel(JPanel apanel, JLabel label, JTextField textField, JButton button) {
 		JPanel panel = new JPanel();
 		GridBagLayout gb = new GridBagLayout();
 		panel.setLayout(gb);
@@ -547,11 +555,14 @@ public class CredentialComponent extends JPanel implements
 		panel.add(button);
 		panel.setBorder(BorderFactory.createTitledBorder(label.getText()));
 
-		gridPanel.add(panel);
+		apanel.add(panel);
 
 	}
 
 	private void addMyproxyPanel(JLabel label, JTextField textField) {
+		/*JPanel panel = new JPanel();
+		panel.add(label);
+		panel.add(textField); */
 
 		JPanel panel = new JPanel();
 		GridBagLayout gb = new GridBagLayout();
@@ -559,11 +570,11 @@ public class CredentialComponent extends JPanel implements
 		GridBagConstraints gbc = new GridBagConstraints();
 
 		this.setGbc(gbc, 0, 0, 0.5, 0.5, 1, 1, GridBagConstraints.HORIZONTAL);
-		gbc.anchor = GridBagConstraints.CENTER;
+	//	gbc.anchor = GridBagConstraints.CENTER;
 		gb.setConstraints(label, gbc);
 		panel.add(label);
 
-		this.setGbc(gbc, 0, 1, 0.5, 0.5, 1, 1, GridBagConstraints.WEST);
+		this.setGbc(gbc, 0, 1, 0.5, 0.5, 1, 1, GridBagConstraints.HORIZONTAL);
 		gbc.insets = new Insets(2, 10, 2, 10);
 		gb.setConstraints(textField, gbc);
 		panel.add(textField);
@@ -581,20 +592,27 @@ public class CredentialComponent extends JPanel implements
 
 		JPanel helpPanel = new JPanel(new GridLayout(1, 1));
 
-		String text = "1. GridProxyInit creates a X509 proxy certificate file in 'HOME/.taverna/' directory  \n which is valid for specified lifetime. \n \n"
-				+ "2. MyProxyInit sends proxy to specified MyProxy Server, which will live there  \n for specified time. \n \n"
+		String text = "1. CreateProxy creates a X509 proxy certificate file in 'HOME/.taverna/' directory,  \n which will be valid for specified lifetime. \n \n"
+				+ "2. CreateMyProxy sends proxy to specified MyProxy Server, which will live there  \n for specified time. \n \n"
 				+ "3. CA (Cerification Authority) root certificates, MyProxyServer CA, root certificates,  \n are required in "
-				+ System.getProperty("taverna.home")
-				+ "/grid-certificates directory. \n \n"
+				+ System.getProperty("taverna.home") + "/grid-certificates directory. \n \n"
 				+ "4. If CA root certificates are missing in the above directory."
-				+ "\n One can add them by downloading them  from  \n   'https://dist.eugridpma.org/distribution/igtf/current/accredited/tgz/'"
+				+ "\n One can add them by downloading them  from  \n 'https://dist.eugridpma.org/distribution/igtf/current/accredited/tgz/'"
 				+ "\n or obtain them from certificate's CA  \n\n"
 				+ "Errors: If you see 'java.security.InvalidKeyException: Illegal key size' error message, \n please donwload and add JCE Unlimited Strength"
-				+ "Policy  \nfiles into java installation as specified in ce_policy-1_x.x.zip \n\n ";
+				+ "Policy  \nfiles into java installation as specified in jce_policy-1_x.x.zip \n\n ";
 		JTextArea area = new JTextArea(text);
 
 		helpPanel.add(area);
 		return helpPanel;
 	}
 
+	private void addToGridBagPanel(JPanel  subPanel,GridBagLayout gb,  GridBagConstraints gbc, int row){
+	 
+		this.setGbc(gbc, row, 0, 0.5, 0.5, 1, 1, GridBagConstraints.REMAINDER);
+	 
+		gb.setConstraints(subPanel, gbc);
+		gridPanel.add(subPanel);
+	}
+	
 }
