0926c54c77b4b4e05abd833142884a7fa88fef8c
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-20 12:51:28 +0000

    Use getBootstrapDir instead of getBootstrapFile
    git-svn-id: http://taverna.googlecode.com/svn@4984 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/utils/raven-parent/prelauncher/src/main/java/net/sf/taverna/raven/prelauncher/PreLauncher.java
+++ trunk/utils/raven-parent/prelauncher/src/main/java/net/sf/taverna/raven/prelauncher/PreLauncher.java
@@ -84,7 +84,7 @@ public class PreLauncher {
 
 	protected List<URL> buildClassPath() throws IOException {
 		List<URL> classPath = new ArrayList<URL>();
-		File libDir = BootstrapLocation.getBootstrapFile(getClass()).getParentFile();
+		File libDir = BootstrapLocation.getBootstrapDir(getClass());
 
 		// The directory itself, when run from .class files
 		classPath.add(libDir.toURL());
