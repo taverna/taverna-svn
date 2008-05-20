3160df09d83f170afa634c6c7a11bec2d56f9bc7
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-20 14:10:06 +0000

    Auto-generated reformat
    git-svn-id: http://taverna.googlecode.com/svn@4986 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2platform/src/test/java/net/sf/taverna/platform/spring/RavenSpringIntegrationTest.java
+++ trunk/t2platform/src/test/java/net/sf/taverna/platform/spring/RavenSpringIntegrationTest.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
 public class RavenSpringIntegrationTest {
-	
+
 	@Test
 	public void testRavenEnabledBeanConstruction() {
 		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
 				"applicationContext.xml");
 		Object o = context.getBean("ravenTestBean");
-		assertEquals(o.getClass().getName(),"net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.ErrorBounce");
+		assertEquals(o.getClass().getName(),
+				"net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.ErrorBounce");
 	}
 
 }
