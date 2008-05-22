ab6ca293dfaa1653bc87f3efe75dcd6dc8e32e89
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-22 16:49:36 +0000

    more mime type views (by popup) - not quite 100% yet!
    git-svn-id: http://taverna.googlecode.com/svn@5023 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/contextual-views/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/BeanshellOutputViewer.java
+++ trunk/t2workbench/contextual-views/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/BeanshellOutputViewer.java
@@ -5,6 +5,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +18,9 @@ import javax.swing.AbstractAction;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -38,6 +42,10 @@ public class BeanshellOutputViewer extends JPanel{
 	private final JList mimeDropList = new JList();
 	private JButton addMimeTypeButton;
 	private JPanel mimeTypePanel;
+	private final JPopupMenu mimePopup = new JPopupMenu();
+	private final Vector<String> originalMimes = new Vector<String>();
+	private JList mimeList;
+	
 
 	public BeanshellOutputViewer(ActivityOutputPortDefinitionBean bean, boolean editable) {
 		this.bean = bean;
@@ -73,59 +81,8 @@ public class BeanshellOutputViewer extends JPanel{
 		add(granularDepthSpinner, outerConstraint);
 		
 		outerConstraint.gridx = 3;
-		mimeTypePanel = new JPanel();
-		mimeTypePanel.setLayout(new GridBagLayout());
-		GridBagConstraints mimeConstraint = new GridBagConstraints();
-		mimeConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
-		mimeConstraint.gridx = 0;
-		mimeConstraint.gridy = 0;
-		mimeConstraint.weighty = 0;
-		mimeConstraint.weightx = 0.1;
-		mimeConstraint.fill = GridBagConstraints.BOTH;
-		final Map<String, Object> propertyMap = MimeTypeManager.getInstance().getPropertyMap();
-		Set<Entry<String, Object>> mimeTypes = propertyMap.entrySet();
-		DefaultListModel mimeModel = new DefaultListModel();
-		for (Entry entry:mimeTypes) {
-			mimeModel.addElement((String) entry.getValue());
-		}
-		mimeDropList.setModel(mimeModel);
-		addMimeTypeButton = new JButton(new AbstractAction() {
-
-			public void actionPerformed(ActionEvent arg0) {
-				String text = mimeTypeText.getText();
-				text = text + "\n" + propertyMap.get(mimeDropList.getSelectedValue());
-				
-				// TODO add to original mime list
-				
-			}
-			
-		});
-		addMimeTypeButton.setText("Add");
-		mimeTypeText = new JTextArea();
-		String mimes = "";
-		List<String> originalMimes = new ArrayList<String>();
-		for (String mimeType:bean.getMimeTypes()) {
-			originalMimes.add(mimeType);
-			mimes = mimes + mimeType +"\n";
-		}
-		mimeTypeText.setText(mimes);
-		mimeTypeText.setEditable(false);
-		mimeTypePanel.add(mimeTypeText, mimeConstraint);
 		
-		JPanel mimeTypePanel2 = new JPanel();
-		GridBagConstraints mimeConstraint2 = new GridBagConstraints();
-		mimeConstraint2.anchor = GridBagConstraints.FIRST_LINE_START;
-		mimeConstraint2.gridx = 0;
-		mimeConstraint2.gridy = 0;
-		mimeConstraint2.weighty = 0;
-		mimeConstraint2.weightx = 0.1;
-		mimeConstraint2.fill = GridBagConstraints.BOTH;
-		mimeTypePanel2.add(mimeDropList, mimeConstraint2);
-		mimeConstraint2.gridx = 1;
-		mimeTypePanel2 .add(addMimeTypeButton, mimeConstraint2);
-		mimeTypePanel.add(mimeTypeText, mimeConstraint);
-		mimeTypePanel.add(mimeTypePanel2, mimeConstraint);
-		add(mimeTypePanel, outerConstraint);
+		add(setMimeTypePanel(), outerConstraint);
 	}
 
 	public JTextField getNameField() {
@@ -167,6 +124,84 @@ public class BeanshellOutputViewer extends JPanel{
 	}
 
 	public JPanel getMimeTypePanel() {
+		return mimeTypePanel;
+	}
+	
+	private JPanel setMimeTypePanel() {
+		
+		for (String mimeType:bean.getMimeTypes()) {
+			originalMimes.add(mimeType);
+		}
+		
+		final Map<String, Object> propertyMap = MimeTypeManager.getInstance().getPropertyMap();
+		Set<Entry<String, Object>> mimeTypes = propertyMap.entrySet();
+		DefaultListModel mimeModel = new DefaultListModel();
+		for (Entry entry:mimeTypes) {
+			final JMenuItem item = new JMenuItem();
+			item.setText((String) entry.getValue());
+			item.addActionListener(new AbstractAction() {
+
+				public void actionPerformed(ActionEvent arg0) {
+					System.out.println(item.getText());
+					originalMimes.add((String) propertyMap.get(item.getText()));
+					mimeList.revalidate();
+					mimePopup.setVisible(false);
+				}
+				
+			});
+			mimePopup.add(item);
+			
+		}
+		
+		mimeTypePanel = new JPanel();
+		mimeTypePanel.setLayout(new GridBagLayout());
+		GridBagConstraints mimeConstraint = new GridBagConstraints();
+		mimeConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+		mimeConstraint.gridx = 0;
+		mimeConstraint.gridy = 0;
+		mimeConstraint.weighty = 0;
+		mimeConstraint.weightx = 0.1;
+		mimeConstraint.fill = GridBagConstraints.BOTH;
+		
+		addMimeTypeButton = new JButton();
+		addMimeTypeButton.setText("Add");
+		addMimeTypeButton.addMouseListener(new MouseListener() {
+
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseReleased(MouseEvent arg0) {
+				mimePopup.show(arg0.getComponent(), arg0.getX(), arg0.getY());
+			}
+			
+		});
+		mimeList = new JList(originalMimes);
+		
+		mimeTypeText = new JTextArea();
+		String mimes = "";
+		mimeTypeText.setText(mimes);
+		mimeTypeText.setEditable(false);
+		
+		mimeTypePanel.add(mimeList, mimeConstraint);
+		mimeConstraint.gridx = 1;
+		mimeTypePanel.add(addMimeTypeButton, mimeConstraint);
 		return mimeTypePanel;
 	}
 
