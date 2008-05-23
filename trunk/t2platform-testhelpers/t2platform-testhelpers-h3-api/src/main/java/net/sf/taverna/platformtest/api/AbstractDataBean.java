5f1db94c1f1eeb4d31845f818bb8e6e91ce0119a
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-23 13:26:44 +0000

    Added get and set methods for the internal ID, although I'm not sure still why they're needed. Need to figure out how to switch hibernate into 'use private properties' mode.
    git-svn-id: http://taverna.googlecode.com/svn@5032 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2platform-testhelpers/t2platform-testhelpers-h3-api/src/main/java/net/sf/taverna/platformtest/api/AbstractDataBean.java
+++ trunk/t2platform-testhelpers/t2platform-testhelpers-h3-api/src/main/java/net/sf/taverna/platformtest/api/AbstractDataBean.java
@@ -5,10 +5,21 @@ public abstract class AbstractDataBean implements DataBeanSPI {
 	// Internal private key for use in mapping schemes
 	private int id;
 
+	public void setId(int newId) {
+		this.id = newId;
+	}
+	public int getId() {
+		return this.id;
+	}
+	
 	protected String name;
 	
 	public void setName(String newName) {
 		this.name = newName;
+	}
+	
+	public String getName() {
+		return this.name;
 	}
 	
 	/**
