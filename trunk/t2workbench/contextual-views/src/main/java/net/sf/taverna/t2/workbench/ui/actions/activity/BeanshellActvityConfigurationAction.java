146aa25c822aa516dab9197f8b08139a59082acb
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-23 13:35:27 +0000

    Action handler for clicking OK on a beanshell
    git-svn-id: http://taverna.googlecode.com/svn@5034 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2workbench/contextual-views/src/main/java/net/sf/taverna/t2/workbench/ui/actions/activity/BeanshellActvityConfigurationAction.java
@@ -0,0 +1,33 @@
+package net.sf.taverna.t2.workbench.ui.actions.activity;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import net.sf.taverna.t2.activities.beanshell.BeanshellActivity;
+import net.sf.taverna.t2.workbench.ui.views.contextualviews.BeanshellConfigView;
+
+public class BeanshellActvityConfigurationAction extends ActivityConfigurationAction<BeanshellActivity>{
+
+	private static final long serialVersionUID = 8462133163666495672L;
+
+	public BeanshellActvityConfigurationAction(BeanshellActivity activity) {
+		super(activity);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		BeanshellConfigView beanshellConfigView = new BeanshellConfigView((BeanshellActivity)getActivity());
+		final JFrame frame = new JFrame();
+		frame.add(beanshellConfigView);
+		frame.setVisible(true);
+		beanshellConfigView.setButtonClickedListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);				
+			}
+			
+		});
+	}
+
+}
