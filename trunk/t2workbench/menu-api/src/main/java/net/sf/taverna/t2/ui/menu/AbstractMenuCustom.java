cb14c333d4ac4b006f3622fc54d6d4343150faa9
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-28 15:22:46 +0000

    AbstractMenuCustom for providing custom components (like a hand-crafted JMenu )
    git-svn-id: http://taverna.googlecode.com/svn@5055 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2workbench/menu-api/src/main/java/net/sf/taverna/t2/ui/menu/AbstractMenuCustom.java
@@ -0,0 +1,20 @@
+package net.sf.taverna.t2.ui.menu;
+
+import java.awt.Component;
+import java.net.URI;
+
+public abstract class AbstractMenuCustom extends AbstractMenuItem {
+
+	public AbstractMenuCustom(URI parentId, int positionHint) {
+		this(parentId, positionHint, null);
+	}
+
+	public AbstractMenuCustom(URI parentId, int positionHint, URI id) {
+		super(MenuType.custom, parentId, id);
+		this.positionHint = positionHint;
+	}
+
+	@Override
+	public abstract Component getCustomComponent();
+
+}
