d640cca68c1ed9833107979d7151cb77763b2246
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-28 13:53:47 +0000

    Added some very important javadoc for the initView method
    git-svn-id: http://taverna.googlecode.com/svn@5047 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/contextual-views/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/ContextualView.java
+++ trunk/t2workbench/contextual-views/src/main/java/net/sf/taverna/t2/workbench/ui/views/contextualviews/ContextualView.java
@@ -10,44 +10,59 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- * An abstract class defining the base container to hold a contextual view over Dataflow element.
+ * An abstract class defining the base container to hold a contextual view over
+ * Dataflow element.
  * <p>
- * The specific implementation of this class to support a given dataflow element needs to implement the 
- * getMainFrame() and getViewTitle().
+ * The specific implementation of this class to support a given dataflow element
+ * needs to implement the getMainFrame() and getViewTitle().
  * </p>
  * <p>
- * If a view is associated with an action handler to configure this component, then the getConfigureAction handler must be over-ridden. If this returns null
- * then the configure button is left disabled and it is not possible to configure the element.
+ * If a view is associated with an action handler to configure this component,
+ * then the getConfigureAction handler must be over-ridden. If this returns null
+ * then the configure button is left disabled and it is not possible to
+ * configure the element.
  * </p>
  * 
  * @author Stuart Owen
  * @author Ian Dunlop
- *
+ * 
  */
 public abstract class ContextualView extends JFrame {
-	
+
 	/**
-	 * When implemented, this method should define the main frame that is placed in this container, and provides a static view of the Dataflow element.
+	 * When implemented, this method should define the main frame that is placed
+	 * in this container, and provides a static view of the Dataflow element.
+	 * 
 	 * @return a JComponent that represents the dataflow element.
 	 */
 	protected abstract JComponent getMainFrame();
-	
+
 	/**
 	 * @return a String providing a title for the view
 	 */
 	protected abstract String getViewTitle();
-	
+
 	/**
-	 * Allows the item to be configured, but returning an action handler that will be invoked when selecting to configure. By default this is provided by a button.
+	 * Allows the item to be configured, but returning an action handler that
+	 * will be invoked when selecting to configure. By default this is provided
+	 * by a button.
 	 * <p>
-	 * If there is no ability to configure the given item, then this should return null.
+	 * If there is no ability to configure the given item, then this should
+	 * return null.
 	 * </p>
+	 * 
 	 * @return an action that allows the element being viewed to be configured.
 	 */
 	public Action getConfigureAction() {
 		return null;
 	}
-	
+
+	/**
+	 * This MUST be called by any sub classes after they have initialised their
+	 * own view since it gets their main panel and adds it to the main
+	 * contextual view. If you don't do this you will get a very empty frame
+	 * popping up!
+	 */
 	protected void initView() {
 		setSize(800, 500);
 		setLayout(new BorderLayout());
@@ -56,28 +71,25 @@ public abstract class ContextualView extends JFrame {
 		JPanel buttonFrame = new JPanel();
 		add(buttonFrame, BorderLayout.SOUTH);
 		buttonFrame.setLayout(new BorderLayout());
-		
-		buttonFrame.add(createButtonPanel(),BorderLayout.EAST);
+
+		buttonFrame.add(createButtonPanel(), BorderLayout.EAST);
 	}
 
 	private JPanel createButtonPanel() {
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
-		
+
 		JButton configureButton = new JButton(getConfigureAction());
 		configureButton.setEnabled(false);
-		
-		if (getConfigureAction()!=null) {
+
+		if (getConfigureAction() != null) {
 			configureButton.setAction(getConfigureAction());
 			configureButton.setEnabled(true);
 		}
 		configureButton.setText("Configure");
 		buttonPanel.add(configureButton);
-		
+
 		return buttonPanel;
 	}
-
-	
-	
 
 }
