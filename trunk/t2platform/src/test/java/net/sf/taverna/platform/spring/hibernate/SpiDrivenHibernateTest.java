cc387f9dc85527a699bcaa2dbbb73bf76ef13c72
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-05-29 15:43:43 +0000

    Tidied up the test slightly, removed redundant assignments and imports (I just don't like the eclipse warning list)
    git-svn-id: http://taverna.googlecode.com/svn@5075 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2platform/src/test/java/net/sf/taverna/platform/spring/hibernate/SpiDrivenHibernateTest.java
+++ trunk/t2platform/src/test/java/net/sf/taverna/platform/spring/hibernate/SpiDrivenHibernateTest.java
@@ -1,15 +1,8 @@
 package net.sf.taverna.platform.spring.hibernate;
 
 import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
-import net.sf.taverna.raven.repository.Artifact;
-import net.sf.taverna.raven.repository.BasicArtifact;
-import net.sf.taverna.raven.repository.Repository;
-import net.sf.taverna.raven.spi.SpiRegistry;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 
 /**
  * Uses a patched version of hibernate to pull in bean definitions from Raven
@@ -19,8 +12,6 @@ import org.springframework.context.ApplicationContext;
  */
 public class SpiDrivenHibernateTest {
 
-	private static Log log = LogFactory.getLog(SpiDrivenHibernateTest.class);
-
 	@Test
 	/**
 	 * Works around the current raven issues by preloading a bean from the
@@ -28,7 +19,6 @@ public class SpiDrivenHibernateTest {
 	 * for existing artifacts!
 	 */
 	public void testSpiBasedDerbyInit() {
-		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
-				"applicationContext4.xml");
+		new RavenAwareClassPathXmlApplicationContext("applicationContext4.xml");
 	}
 }
