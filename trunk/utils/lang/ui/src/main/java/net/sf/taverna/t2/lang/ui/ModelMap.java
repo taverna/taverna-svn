4afcabccbd26f59f886430509f2ffb2614730b74
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-05 14:06:11 +0000

    Removed deprecated listener methods (that nobody have had a chance to use yet in this new package)
    git-svn-id: http://taverna.googlecode.com/svn@5108 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/utils/lang/ui/src/main/java/net/sf/taverna/t2/lang/ui/ModelMap.java
+++ trunk/utils/lang/ui/src/main/java/net/sf/taverna/t2/lang/ui/ModelMap.java
@@ -1,6 +1,5 @@
 package net.sf.taverna.t2.lang.ui;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -36,9 +35,6 @@ public class ModelMap implements Observable<ModelMapEvent> {
 		return instance;
 	}
 
-	@Deprecated
-	private List<ModelChangeListener> listeners = new ArrayList<ModelChangeListener>();
-
 	/**
 	 * At any given time there are zero or more named model objects over which
 	 * the workbench UI is acting.
@@ -52,27 +48,11 @@ public class ModelMap implements Observable<ModelMapEvent> {
 	private ModelMap() {
 	}
 
-	/**
-	 * @deprecated
-	 * @see #addObserver(Observer)
-	 */
-	@Deprecated
-	public void addModelListener(ModelChangeListener listener) {
-		listeners.add(listener);
-	}
 
 	public void addObserver(Observer<ModelMapEvent> observer) {
 		multiCaster.addObserver(observer);
 	}
 
-	/**
-	 * @deprecated
-	 * @see #getModel(String)
-	 */
-	@Deprecated
-	public Object getNamedModel(String modelName) {
-		return getModel(modelName);
-	}
 
 	public Object getModel(String modelName) {
 		return modelMap.get(modelName);
@@ -80,15 +60,6 @@ public class ModelMap implements Observable<ModelMapEvent> {
 
 	public List<Observer<ModelMapEvent>> getObservers() {
 		return multiCaster.getObservers();
-	}
-
-	/**
-	 * @deprecated
-	 * @see #removeObserver(Observer)
-	 */
-	@Deprecated
-	public void removeModelListener(ModelChangeListener listener) {
-		listeners.remove(listener);
 	}
 
 	public void removeObserver(Observer<ModelMapEvent> observer) {
@@ -134,110 +105,20 @@ public class ModelMap implements Observable<ModelMapEvent> {
 	private void modelChanged(String modelName, Object oldModel, Object newModel) {
 		multiCaster
 				.notify(new ModelChangedEvent(modelName, oldModel, newModel));
-		for (ModelChangeListener listener : listeners) {
-			if (!listener.canHandle(modelName, newModel)) {
-				continue;
-			}
-			try {
-				listener.modelChanged(modelName, oldModel, newModel);
-			} catch (Error er) {
-				logger.error("Could not notify model listener " + listener, er);
-			}
-		}
 	}
 
 	private void modelCreated(String modelName, Object model) {
 		multiCaster.notify(new ModelCreatedEvent(modelName, model));
-		for (ModelChangeListener listener : listeners) {
-			if (!listener.canHandle(modelName, model)) {
-				continue;
-			}
-			try {
-				listener.modelCreated(modelName, model);
-			} catch (Error er) {
-				logger.error("Could not notify model listener " + listener, er);
-			}
-		}
 	}
 
 	private void modelDestroyed(String modelName, Object oldModel) {
 		multiCaster.notify(new ModelDestroyedEvent(modelName, oldModel));
-
-		for (ModelChangeListener listener : listeners) {
-			if (!listener.canHandle(modelName, oldModel)) {
-				continue;
-			}
-			try {
-				listener.modelDestroyed(modelName, oldModel);
-			} catch (Error er) {
-				logger.error("Could not notify model listener " + listener, er);
-			}
-		}
 	}
 
 	public static class ModelChangedEvent extends ModelMapEvent {
 		ModelChangedEvent(String modelName, Object oldModel, Object newModel) {
 			super(modelName, oldModel, newModel);
 		}
-	}
-
-	/**
-	 * Register with the static class to inform workbench like systems that the
-	 * underlying map of named model objects has been altered in some way.
-	 * 
-	 * @deprecated See {@link ModelMap#addObserver(Observer)}
-	 * 
-	 * @author Tom Oinn
-	 * @author Stian Soiland-Reyes
-	 */
-	@Deprecated
-	public interface ModelChangeListener {
-
-		/**
-		 * Return true if the listener can handle events for the given modelname
-		 * and model. Called before firing model* events.
-		 * <p>
-		 * For modelChanged events, the canHandle() will be called with the
-		 * newModel object.
-		 * 
-		 * @param modelName
-		 *            name of the model ex: ModelMap.CURRENT_WORKFLOW
-		 * @param model
-		 *            Model that is being created, changed or destroyed
-		 * @return true if the listener want to receive events for such objects
-		 */
-		public boolean canHandle(String modelName, Object model);
-
-		/**
-		 * Called when the named model is updated
-		 * 
-		 * @param modelName
-		 *            name of the model that changed
-		 * @param newModel
-		 *            new model object
-		 * @param oldModel
-		 *            old model object it replaces
-		 */
-		public void modelChanged(String modelName, Object oldModel,
-				Object newModel);
-
-		/**
-		 * Called when a new model is created or inserted into the model map
-		 * under a previously absent key
-		 * 
-		 * @param modelName
-		 *            name of the new model
-		 * @param model
-		 *            the new model object
-		 */
-		public void modelCreated(String modelName, Object model);
-
-		/**
-		 * Called when the named model is removed from the model map
-		 * 
-		 * @param modelName
-		 */
-		public void modelDestroyed(String modelName, Object oldModel);
 	}
 
 	public static class ModelCreatedEvent extends ModelMapEvent {
