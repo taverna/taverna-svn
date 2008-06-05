b0a36f495b14457b4fc64131eadccf54d590b747
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-05 22:31:21 +0000

    Added sub-package for hibernate support - classes in this package are not public in the normal sense, but neither do they bring in the hibernate library dependencies. They provide appropriately accessible properties to be used by hibernate when persisting instances of reference set etc.
    git-svn-id: http://taverna.googlecode.com/svn@5118 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2reference/t2reference-api/src/main/java/net/sf/taverna/t2/reference/h3/ReferenceSetImpl.java
@@ -0,0 +1,101 @@
+package net.sf.taverna.t2.reference.h3;
+
+import java.util.Set;
+
+import net.sf.taverna.t2.reference.ExternalReferenceSPI;
+import net.sf.taverna.t2.reference.ReferenceSet;
+
+/**
+ * An implementation of ReferenceSet with the additional methods and metadata
+ * required by Hibernate3 to allow it to be persisted in a relational store. As
+ * with everything else in this package you shouldn't be using this class
+ * directly! Instead of this class you should use the registration methods on
+ * {@link net.sf.taverna.t2.reference.ReferenceSetService}, implementations of
+ * that interface will handle the construction of ReferenceSet implementations
+ * (including this one).
+ * 
+ * @author Tom Oinn
+ * 
+ */
+public class ReferenceSetImpl implements ReferenceSet {
+
+	private Set<ExternalReferenceSPI> externalReferences;
+	private ReferenceSetT2ReferenceImpl id;
+
+	/**
+	 * Construct a new ReferenceSetImpl with the given set of external
+	 * references and identifier.
+	 * 
+	 * @param references
+	 *            the set of ExternalReferenceSPI which this reference set
+	 *            should contain initially
+	 * @param id
+	 *            the T2Reference to use, must be an instance of
+	 *            ReferenceSetT2ReferenceImpl so hibernate can make use of it as
+	 *            a compound primary key component
+	 */
+	public ReferenceSetImpl(Set<ExternalReferenceSPI> references,
+			ReferenceSetT2ReferenceImpl id) {
+		this.id = id;
+		this.externalReferences = references;
+	}
+
+	/**
+	 * Default constructor, used by Hibernate when reconstructing this bean from
+	 * the database. If you call this directly from your code you must then call
+	 * both {@link #setExternalReferences(Set)} and
+	 * {@link #setId(ReferenceSetT2ReferenceImpl)} before any use of the
+	 * reference set. If you're not writing the reference manager implementation
+	 * you shouldn't be using this class anyway.
+	 */
+	public ReferenceSetImpl() {
+		//
+	}
+
+	/**
+	 * For debugging purposes, prints a summary of the contents and identifier
+	 * of this reference set.
+	 * 
+	 * @return human readable string representation of this object. This is not
+	 *         regarded as 'stable' and should not be parsed for any reason!
+	 */
+	@Override
+	public String toString() {
+		return "ReferenceSet " + id;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Set<ExternalReferenceSPI> getExternalReferences() {
+		return this.externalReferences;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ReferenceSetT2ReferenceImpl getId() {
+		return this.id;
+	}
+
+	/**
+	 * This method is only ever called from within Hibernate, and is used to
+	 * initialize the set of external references.
+	 */
+	public void setExternalReferences(Set<ExternalReferenceSPI> newReferences) {
+		this.externalReferences = newReferences;
+	}
+
+	/**
+	 * This method is only ever called from within Hibernate, and is used to
+	 * initialize the unique ID of this reference set.
+	 * 
+	 * @param newId
+	 *            an instance of ReferenceSetT2ReferenceImpl created from the
+	 *            database
+	 */
+	public void setId(ReferenceSetT2ReferenceImpl newId) {
+		this.id = newId;
+	}
+
+}
---
+++ trunk/t2reference/t2reference-api/src/main/java/net/sf/taverna/t2/reference/h3/ReferenceSetT2ReferenceImpl.java
@@ -0,0 +1,135 @@
+package net.sf.taverna.t2.reference.h3;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import net.sf.taverna.t2.reference.T2Reference;
+
+/**
+ * An implementation of T2Reference specific to the ReferenceSetImpl. This is
+ * needed because ReferenceSetImpl uses a component based primary key driven
+ * from the namespace and local parts of T2Reference. This in turn means we can
+ * query hibernate directly with a T2Reference instance in the data access
+ * object. Because this is only used as a component (i.e. a value type) we don't
+ * need to define a hibernate mapping file for it.
+ * 
+ * @author Tom Oinn
+ * 
+ */
+public class ReferenceSetT2ReferenceImpl implements T2Reference, Serializable {
+
+	private static final long serialVersionUID = 8363330461158750319L;
+	private URI cachedUri = null;
+	private String localPart;
+	private String namespacePart;
+
+	/**
+	 * Reference sets by definition do not contain errors, so this method always
+	 * returns false
+	 * 
+	 * @return false
+	 */
+	public boolean containsErrors() {
+		return false;
+	}
+
+	/**
+	 * Reference sets by definition have a depth of 0
+	 * 
+	 * @return 0
+	 */
+	public int getDepth() {
+		return 0;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getLocalPart() {
+		return this.localPart;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getNamespacePart() {
+		return namespacePart;
+	}
+
+	/**
+	 * This method is only ever called from within Hibernate when
+	 * re-constructing the identifier component to set the namespace part of the
+	 * identifier.
+	 */
+	public synchronized void setNamespacePart(String namespacePart) {
+		this.namespacePart = namespacePart;
+		cachedUri = null;
+	}
+
+	/**
+	 * This method is only ever called from within Hibernate when
+	 * re-constructing the identifier component to set the local part of the
+	 * identifier.
+	 */
+	public synchronized void setLocalPart(String localPart) {
+		this.localPart = localPart;
+		cachedUri = null;
+	}
+
+	/**
+	 * By default when printing an identifier we use {@link #toUri()}.{@link java.net.URI#toASCIIString() toASCIIString()}
+	 */
+	@Override
+	public String toString() {
+		return toUri().toASCIIString();
+	}
+
+	/**
+	 * Returns the identifier expressed as a {@link java.net.URI URI},
+	 * constructed from
+	 * <code>new URI("t2:ref//" + namespacePart + "?" + localPart)</code>
+	 * leading to URIs of the form <code>t2:ref//namespace?local</code>
+	 */
+	public synchronized URI toUri() {
+		if (cachedUri != null) {
+			return cachedUri;
+		} else {
+			try {
+				URI result = new URI("t2:ref//" + namespacePart + "?"
+						+ localPart);
+				cachedUri = result;
+				return result;
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
+	/**
+	 * Use the equality operator over the URI representation of this bean.
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other == this) {
+			return true;
+		}
+		if (other instanceof ReferenceSetT2ReferenceImpl) {
+			ReferenceSetT2ReferenceImpl otherRef = (ReferenceSetT2ReferenceImpl) other;
+			return (toUri().equals(otherRef.toUri()));
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Use hashcode method from the URI representation of this bean
+	 */
+	@Override
+	public int hashCode() {
+		return toUri().hashCode();
+	}
+
+}
