3f23304bb71b5207b01d3dbc49730d4824207511
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-06 10:02:57 +0000

    Code cleanup
    git-svn-id: http://taverna.googlecode.com/svn@5132 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2reference/t2reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/DatabaseSetupTest.java
+++ trunk/t2reference/t2reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/DatabaseSetupTest.java
@@ -1,23 +1,21 @@
 package net.sf.taverna.t2.reference.impl;
 
 import java.util.HashSet;
-import java.util.List;
 
-import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
 import net.sf.taverna.t2.reference.ExternalReferenceSPI;
 import net.sf.taverna.t2.reference.ReferenceSet;
 import net.sf.taverna.t2.reference.h3.ReferenceSetImpl;
 import net.sf.taverna.t2.reference.h3.ReferenceSetT2ReferenceImpl;
-import net.sf.taverna.t2.reference.impl.HibernateReferenceSetDao;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DatabaseSetupTest {
 
 	@Test
 	public void testDatabaseReadWriteWithoutPlugins() {
-		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
+		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"vanillaHibernateAppContext.xml");
 		HibernateReferenceSetDao o = (HibernateReferenceSetDao) context
 				.getBean("testDao");
@@ -26,11 +24,11 @@ public class DatabaseSetupTest {
 		id.setLocalPart("testLocal");
 		ReferenceSetImpl rs = new ReferenceSetImpl(
 				new HashSet<ExternalReferenceSPI>(), id);
-		o.storeOrUpdate(rs);
+		o.store(rs);
 
-		List<? extends ReferenceSet> returnedSet = o.getReferenceSetImpl(id);
-		for (ReferenceSet set : returnedSet) {
-			System.out.println("Found a set! : " + set.toString());
-		}
+		ReferenceSet returnedset = o.get(id);
+		System.out.println(returnedset);
+
 	}
+
 }
