383f63b8a27a26aa54d3f316d5bb636c6d80a81b
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-07 12:20:26 +0000

    Added inline docs
    git-svn-id: http://taverna.googlecode.com/svn@5145 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceSetDaoTest.java
+++ trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceSetDaoTest.java
@@ -12,17 +12,31 @@ public class ReferenceSetDaoTest {
 
 	@Test
 	public void doTest() {
+		// Initialize application context - there's a lot going on in this one,
+		// see the context definition itself as that's really the test case
+		// rather than this code
 		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
-		"referenceSetDaoTestContext1.xml");
-		ReferenceSetService rss = (ReferenceSetService) context.getBean("referenceSetService");
-		
+				"referenceSetDaoTestContext1.xml");
+
+		// Get the reference set service object from the context. At this point
+		// it will be wired and configured ready to use.
+		ReferenceSetService rss = (ReferenceSetService) context
+				.getBean("referenceSetService");
+
+		// Build a simple set of two external references, these have been
+		// defined in the application context definition and loaded with raven,
+		// the classes are not on the classpath for this test case.
 		Set<ExternalReferenceSPI> references = new HashSet<ExternalReferenceSPI>();
-		references.add((ExternalReferenceSPI) context.getBean("exampleUrlBean"));
-		references.add((ExternalReferenceSPI) context.getBean("exampleFileBean"));
-				
+		references
+				.add((ExternalReferenceSPI) context.getBean("exampleUrlBean"));
+		references.add((ExternalReferenceSPI) context
+				.getBean("exampleFileBean"));
+
+		// If all goes well we can register the set of external references and
+		// get a referenceset object back with an ID allocated appropriately
 		ReferenceSet rs = rss.registerReferenceSet(references);
 		System.out.println(rs);
-		
+
 	}
-	
+
 }
