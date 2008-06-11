1bff9f5a09ced74462186491050d5e664dbf36a8
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-11 15:16:58 +0000

    IN PROGRESS - issue T2-146: PartitionAlgorithms for WSDL and Soaplab http://www.mygrid.org.uk/dev/issues/browse/T2-146
    Added PartitionAlgorithmSet for Soaplab
    
    
    git-svn-id: http://taverna.googlecode.com/svn@5184 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2activities/soaplab-activity/src/main/java/net/sf/taverna/t2/activities/soaplab/partition/SoaplabPartitionAlgorithmSetSPI.java
@@ -0,0 +1,12 @@
+package net.sf.taverna.t2.activities.soaplab.partition;
+
+import net.sf.taverna.t2.partition.ActivityPartitionAlgorithmSet;
+import net.sf.taverna.t2.partition.algorithms.LiteralValuePartitionAlgorithm;
+
+public class SoaplabPartitionAlgorithmSetSPI extends ActivityPartitionAlgorithmSet {
+
+	public SoaplabPartitionAlgorithmSetSPI() {
+		partitionAlgorithms.add(new LiteralValuePartitionAlgorithm("category"));
+		partitionAlgorithms.add(new LiteralValuePartitionAlgorithm("operation"));
+	}
+}
