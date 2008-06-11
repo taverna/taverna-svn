631ea1041e8367e2929961bcb5e81c916570eeb4
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-11 16:45:50 +0000

    IN PROGRESS - issue T2-140: PropertyExtractorRegistry http://www.mygrid.org.uk/dev/issues/browse/T2-140
    added javadocs
    
    
    git-svn-id: http://taverna.googlecode.com/svn@5197 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/activity-palette-api/src/main/java/net/sf/taverna/t2/partition/PropertyExtractorSPIRegistry.java
+++ trunk/t2workbench/activity-palette-api/src/main/java/net/sf/taverna/t2/partition/PropertyExtractorSPIRegistry.java
@@ -5,11 +5,28 @@ import java.util.Map;
 
 import net.sf.taverna.t2.spi.SPIRegistry;
 
+/**
+ * A singleton SPI registry class for extracting properties for target "Objects".
+ * <p>
+ * Through an SPI identified by classes defined in a file
+ * </p>
+ * <pre>
+ * META-INF/services/net.sf.taverna.t2.partition.PropertyExtractorSPI
+ * </pre>
+ * PropertyExtractorSPIs are discovered, and then for each it then passes the target Object to retrieve
+ * a property map for that Object.
+ * <p>
+ * If the PropertyExtractorSPI is not relevant to that Object type, the Extractor simply responds with an empty Map.
+ * </p> 
+ * @author Stuart Owen
+ * @see PropertyExtractorSPI
+ * 
+ *
+ */
 public class PropertyExtractorSPIRegistry extends SPIRegistry<PropertyExtractorSPI> implements
 		PropertyExtractorRegistry {
 	
 	private static PropertyExtractorSPIRegistry instance = new PropertyExtractorSPIRegistry();
-
 
 	public static PropertyExtractorSPIRegistry getInstance() {
 		return instance;
@@ -18,9 +35,11 @@ public class PropertyExtractorSPIRegistry extends SPIRegistry<PropertyExtractorS
 		super(PropertyExtractorSPI.class);
 	}
 
+	/* (non-Javadoc)
+	 * @see net.sf.taverna.t2.partition.PropertyExtractorRegistry#getAllPropertiesFor(java.lang.Object)
+	 */
 	public Map<String, Object> getAllPropertiesFor(Object target) {
 		Map<String,Object> result = new HashMap<String, Object>();
-		//TODO:provide caching
 		for (PropertyExtractorSPI extractor : getInstances()) {
 			result.putAll(extractor.extractProperties(target));
 		}
