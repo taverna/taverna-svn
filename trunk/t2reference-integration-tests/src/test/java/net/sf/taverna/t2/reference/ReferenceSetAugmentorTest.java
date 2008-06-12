38a649ab9bb1f9f3727b8f033ac843cdedf9c9f6
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-12 21:30:57 +0000

    Updated to exercise at least some of the new augmentation implementation. All appears to work
    git-svn-id: http://taverna.googlecode.com/svn@5238 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceSetAugmentorTest.java
+++ trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceSetAugmentorTest.java
@@ -1,5 +1,10 @@
 package net.sf.taverna.t2.reference;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
 
 import org.junit.Test;
@@ -12,6 +17,7 @@ import org.springframework.context.ApplicationContext;
  */
 public class ReferenceSetAugmentorTest {
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void doTest() {
 		// Initialize application context - there's a lot going on in this one,
@@ -23,8 +29,22 @@ public class ReferenceSetAugmentorTest {
 		// Get the pre-baked reference set
 		ReferenceSet rs = (ReferenceSet) context.getBean("referenceSet");
 
-		// Print out the reference set for debug
-		System.out.println(rs);
+		ReferenceSetAugmentor aug = (ReferenceSetAugmentor) context.getBean("referenceSetAugmentor");
+		
+		ReferenceContext refContext = new ReferenceContext() {
+			public <T> List<? extends T> getEntities(Class<T> arg0) {
+				return new ArrayList<T>();
+			}
+		};
+		
+		Set<Class<ExternalReferenceSPI>> redTarget = new HashSet<Class<ExternalReferenceSPI>>();
+		redTarget.add((Class<ExternalReferenceSPI>) context.getBean("redBean").getClass());
+		
+		aug.augmentReferenceSet(rs, redTarget, refContext);
+		
+		
 	}
 
+	
+	
 }
