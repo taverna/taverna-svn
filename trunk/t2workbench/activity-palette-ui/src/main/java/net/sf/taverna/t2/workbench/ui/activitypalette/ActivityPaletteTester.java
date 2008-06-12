73373a6ed452c25a2f8ffc62a087f044491330b1
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-12 09:05:24 +0000

    New activity palette
    git-svn-id: http://taverna.googlecode.com/svn@5202 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2workbench/activity-palette-ui/src/main/java/net/sf/taverna/t2/workbench/ui/activitypalette/ActivityPaletteTester.java
@@ -0,0 +1,16 @@
+package net.sf.taverna.t2.workbench.ui.activitypalette;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class ActivityPaletteTester {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		ActivityPaletteComponent palette = new ActivityPaletteComponent();
+		frame.add(palette);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
