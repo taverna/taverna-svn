449b9d90d89b1459d733d79a484c8bdc602ee63e
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-12 02:03:30 +0000

    Checkin of helper module for t2reference integration tests. Consists of simple test implementations of e.g. ExternalReferenceSPI, ExternalReferenceBuilderSPI, ExternalReferenceTranslatorSPI and similar which are then used by the integration tests to selectively exercise different aspects of the implementation package within t2reference/t2reference-impl
    git-svn-id: http://taverna.googlecode.com/svn@5201 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2reference-testhelpers/src/main/java/net/sf/taverna/t2referencetest/YellowReference.java
@@ -0,0 +1,17 @@
+package net.sf.taverna.t2referencetest;
+
+import java.io.InputStream;
+
+import net.sf.taverna.t2.reference.AbstractExternalReference;
+import net.sf.taverna.t2.reference.ExternalReferenceSPI;
+import net.sf.taverna.t2.reference.ReferenceContext;
+
+public class YellowReference extends AbstractExternalReference implements
+		ExternalReferenceSPI {
+
+	public InputStream openStream(ReferenceContext arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
