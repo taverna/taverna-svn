9219255a12245f0f10dc73c7723f317b63e15123
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-16 17:20:31 +0000

    Added test for full ReferenceService, writing a URL and pulling back the ReferenceSet, works!
    git-svn-id: http://taverna.googlecode.com/svn@5280 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceServiceTest.java
+++ trunk/t2reference-integration-tests/src/test/java/net/sf/taverna/t2/reference/ReferenceServiceTest.java
@@ -1,5 +1,10 @@
 package net.sf.taverna.t2.reference;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
 
 import org.junit.Test;
@@ -13,15 +18,37 @@ import org.springframework.context.ApplicationContext;
  */
 public class ReferenceServiceTest {
 
-	private ReferenceService rs;
+	private ReferenceContext dummyContext = new ReferenceContext() {
+		public <T> List<? extends T> getEntities(Class<T> arg0) {
+			return new ArrayList<T>();
+		}
+	};
 
 	@Test
 	public void testInit() {
 		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
 				"referenceServiceTestContext1.xml");
-		rs = (ReferenceService) context.getBean("referenceService");
+		ReferenceService rs = (ReferenceService) context
+				.getBean("referenceService");
 		System.out.println("Created reference service implementation :"
 				+ rs.getClass().getCanonicalName());
+	}
+
+	@Test
+	public void testURLRegistration() throws MalformedURLException {
+		URL testUrl = new URL("http://www.ebi.ac.uk/~tmo/patterns.xml");
+		ApplicationContext context = new RavenAwareClassPathXmlApplicationContext(
+				"referenceServiceTestContext1.xml");
+		ReferenceService rs = (ReferenceService) context
+				.getBean("referenceService");
+		for (int i = 0; i < 10; i++) {
+			long startTime = System.currentTimeMillis();
+			T2Reference ref = rs.register(testUrl, 0, true, dummyContext);
+			ReferenceSet refSet = (ReferenceSet) rs.resolveIdentifier(ref,
+					null, dummyContext);
+			System.out.println(refSet.toString() + "  -  "
+					+ (System.currentTimeMillis() - startTime) + "ms");
+		}
 	}
 
 }
