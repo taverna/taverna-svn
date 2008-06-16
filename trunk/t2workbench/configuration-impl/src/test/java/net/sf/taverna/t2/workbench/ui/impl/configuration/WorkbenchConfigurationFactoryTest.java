b24cdb555706b2dd638d8785b69d6c2f6fa2060e
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-16 10:03:15 +0000

    set manager baselocation to temp dir when running tests
    git-svn-id: http://taverna.googlecode.com/svn@5258 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/configuration-impl/src/test/java/net/sf/taverna/t2/workbench/ui/impl/configuration/WorkbenchConfigurationFactoryTest.java
+++ trunk/t2workbench/configuration-impl/src/test/java/net/sf/taverna/t2/workbench/ui/impl/configuration/WorkbenchConfigurationFactoryTest.java
@@ -1,20 +1,22 @@
 package net.sf.taverna.t2.workbench.ui.impl.configuration;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
 import java.util.List;
 
 import net.sf.taverna.t2.workbench.configuration.ConfigurationManager;
 import net.sf.taverna.t2.workbench.configuration.ConfigurationUIFactory;
 import net.sf.taverna.t2.workbench.configuration.ConfigurationUIRegistry;
-import net.sf.taverna.t2.workbench.ui.impl.configuration.WorkbenchConfigurationUIFactory;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 public class WorkbenchConfigurationFactoryTest {
 	
 	@Before
 	public void setup() {
 		ConfigurationManager manager = ConfigurationManager.getInstance();
+		manager.setBaseConfigLocation(new File(System.getProperty("java.io.tmpdir")));
 	}
 	
 	@Test
