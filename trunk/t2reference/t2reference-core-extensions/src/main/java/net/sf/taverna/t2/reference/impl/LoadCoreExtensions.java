e993521fbb2a8c2e28af7e808dfa247500f1e984
mereden <mereden@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-16 16:19:41 +0000

    File->FileReference and URL->HttpReference implementations of ValueToReferenceConverterSPI and associated services file
    git-svn-id: http://taverna.googlecode.com/svn@5273 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ trunk/t2reference/t2reference-core-extensions/src/main/java/net/sf/taverna/t2/reference/impl/LoadCoreExtensions.java
@@ -0,0 +1,21 @@
+package net.sf.taverna.t2.reference.impl;
+
+/**
+ * Trivial bean, use a reference to this in your spring configuration along with
+ * the appropriate raven decorators to force a load of the core extensions
+ * package and enable the functionality herein.
+ * 
+ * @author Tom Oinn
+ * 
+ */
+public class LoadCoreExtensions {
+
+	/**
+	 * Default constructor, does about as much as this bean does, that is to say
+	 * nothing at all
+	 */
+	public LoadCoreExtensions() {
+		//
+	}
+
+}
