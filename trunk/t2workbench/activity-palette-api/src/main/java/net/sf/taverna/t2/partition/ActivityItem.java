c07198b0bb911b7264906e1dbefa6e163d7c6d4c
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-17 13:24:35 +0000

    The activity item now has a transferable for drag and drop operations
    git-svn-id: http://taverna.googlecode.com/svn@5289 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/activity-palette-api/src/main/java/net/sf/taverna/t2/partition/ActivityItem.java
+++ trunk/t2workbench/activity-palette-api/src/main/java/net/sf/taverna/t2/partition/ActivityItem.java
@@ -1,13 +1,25 @@
 package net.sf.taverna.t2.partition;
 
+import java.awt.datatransfer.Transferable;
+
 /**
- * An interface that defines an "item" that is the result of performing a query for an Activity type.
+ * An interface that defines an "item" that is the result of performing a query
+ * for an Activity type.
  * 
  * @author Stuart Owen
+ * @author Ian Dunlop
+ * 
  * @see ActivityQuery
  * @see Query
  * 
  */
 public interface ActivityItem {
+	/**
+	 * Used during drag and drop operations. The activity palette (ie the Tree
+	 * which has the {@link RootPartition} stuff) is made up of
+	 * {@link ActivityItem}s each of which will return a {@link Transferable}.
+	 * Inside this will be a wrapper containing the activity and a bean
+	 */
+	public Transferable getActivityTransferable();
 
 }
