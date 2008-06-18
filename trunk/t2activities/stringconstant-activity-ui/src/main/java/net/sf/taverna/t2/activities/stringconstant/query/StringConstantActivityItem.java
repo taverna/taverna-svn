8165e30808774b61f0c686fdb85d59c7d8c1a27f
iandunlop <iandunlop@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-18 18:07:40 +0000

    Has compareTo method for comparable when sorting members of a partition in the activity palette
    git-svn-id: http://taverna.googlecode.com/svn@5327 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2activities/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/query/StringConstantActivityItem.java
+++ trunk/t2activities/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/query/StringConstantActivityItem.java
@@ -1,12 +1,19 @@
 package net.sf.taverna.t2.activities.stringconstant.query;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 import net.sf.taverna.t2.activities.stringconstant.StringConstantActivity;
 import net.sf.taverna.t2.activities.stringconstant.StringConstantConfigurationBean;
 import net.sf.taverna.t2.partition.AbstractActivityItem;
+import net.sf.taverna.t2.partition.ActivityItem;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper;
 
 public class StringConstantActivityItem extends AbstractActivityItem {
 
@@ -18,12 +25,72 @@ public class StringConstantActivityItem extends AbstractActivityItem {
 	public String toString() {
 		return getType();
 	}
-	
 
-	public Icon getIcon() {
-		return new ImageIcon(StringConstantActivityItem.class.getResource("/stringconstant.png"));
+	/**
+	 * Returns a {@link Transferable} containing an
+	 * {@link ActivityAndBeanWrapper} with an {@link Activity} and its
+	 * configuration bean inside. To get the Transferable you call
+	 * {@link ActivityItem#getActivityTransferable()}, with this you call this
+	 * method which returns an {@link ActivityAndBeanWrapper}. From the callers
+	 * side you ask for a DataFlavor of
+	 * {@link DataFlavor#javaJVMLocalObjectMimeType} of type
+	 * {@link ActivityAndBeanWrapper} <code>
+	 * new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper"));
+	 * </code>
+	 */
+	public Transferable getActivityTransferable() {
+		Transferable transferable = new Transferable() {
+
+			public Object getTransferData(DataFlavor flavor)
+					throws UnsupportedFlavorException, IOException {
+				ActivityAndBeanWrapper wrapper = new ActivityAndBeanWrapper();
+				StringConstantConfigurationBean bean = new StringConstantConfigurationBean();
+				bean.setValue("Add your own value here");
+				StringConstantActivity activity = new StringConstantActivity();
+				wrapper.setActivity(activity);
+				wrapper.setBean(bean);
+				return wrapper;
+
+			}
+
+			public DataFlavor[] getTransferDataFlavors() {
+				DataFlavor[] flavors = new DataFlavor[1];
+				DataFlavor flavor = null;
+				try {
+					flavor = new DataFlavor(
+							DataFlavor.javaJVMLocalObjectMimeType
+									+ ";class=net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper");
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				flavors[0] = flavor;
+				return flavors;
+			}
+
+			public boolean isDataFlavorSupported(DataFlavor flavor) {
+				DataFlavor thisFlavor = null;
+				try {
+					thisFlavor = new DataFlavor(
+							DataFlavor.javaJVMLocalObjectMimeType
+									+ ";class=net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper");
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return flavor.equals(flavor);
+			}
+
+		};
+		return transferable;
 	}
 
+	public Icon getIcon() {
+		return new ImageIcon(StringConstantActivityItem.class
+				.getResource("/stringconstant.png"));
+	}
+
+	
 	@Override
 	protected Object getConfigBean() {
 		StringConstantConfigurationBean configbean = new StringConstantConfigurationBean();
@@ -36,6 +103,13 @@ public class StringConstantActivityItem extends AbstractActivityItem {
 	protected Activity<?> getUnconfiguredActivity() {
 		return new StringConstantActivity();
 	}
-	
-	
+
+	public int compareTo(ActivityItem o) {
+		if (toString().compareTo(o.toString()) > 0) {
+
+			return 1;
+		}
+		return 0;
+	}
+
 }
