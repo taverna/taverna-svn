f99d41e2a8120d94454bb55c9e25a498f1dbc8e8
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-18 16:15:27 +0000

    Organized import ;)
    git-svn-id: http://taverna.googlecode.com/svn@5319 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/file-impl/src/main/java/net/sf/taverna/t2/workbench/file/impl/menu/WorkflowsMenu.java
+++ trunk/t2workbench/file-impl/src/main/java/net/sf/taverna/t2/workbench/file/impl/menu/WorkflowsMenu.java
@@ -10,12 +10,11 @@ import javax.swing.JRadioButtonMenuItem;
 
 import net.sf.taverna.t2.lang.observer.Observable;
 import net.sf.taverna.t2.lang.observer.Observer;
-import net.sf.taverna.t2.lang.ui.ModelMap;
 import net.sf.taverna.t2.ui.menu.AbstractMenuCustom;
 import net.sf.taverna.t2.ui.menu.DefaultMenuBar;
 import net.sf.taverna.t2.workbench.file.FileManager;
-import net.sf.taverna.t2.workbench.file.events.FileManagerEvent;
 import net.sf.taverna.t2.workbench.file.events.AbstractDataflowEvent;
+import net.sf.taverna.t2.workbench.file.events.FileManagerEvent;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 
 public class WorkflowsMenu extends AbstractMenuCustom {
