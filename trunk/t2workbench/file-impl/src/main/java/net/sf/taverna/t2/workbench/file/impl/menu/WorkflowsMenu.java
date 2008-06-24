c4b5ef3dcadf735f4f84d5f2d83e61f2e0451937
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-06-24 10:53:49 +0000

    Workflows menu listens to edit manager changes as well
    git-svn-id: http://taverna.googlecode.com/svn@5396 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/file-impl/src/main/java/net/sf/taverna/t2/workbench/file/impl/menu/WorkflowsMenu.java
+++ trunk/t2workbench/file-impl/src/main/java/net/sf/taverna/t2/workbench/file/impl/menu/WorkflowsMenu.java
@@ -7,11 +7,15 @@ import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.SwingUtilities;
 
 import net.sf.taverna.t2.lang.observer.Observable;
 import net.sf.taverna.t2.lang.observer.Observer;
 import net.sf.taverna.t2.ui.menu.AbstractMenuCustom;
 import net.sf.taverna.t2.ui.menu.DefaultMenuBar;
+import net.sf.taverna.t2.workbench.edits.EditManager;
+import net.sf.taverna.t2.workbench.edits.EditManager.AbstractDataflowEditEvent;
+import net.sf.taverna.t2.workbench.edits.EditManager.EditManagerEvent;
 import net.sf.taverna.t2.workbench.file.FileManager;
 import net.sf.taverna.t2.workbench.file.events.AbstractDataflowEvent;
 import net.sf.taverna.t2.workbench.file.events.FileManagerEvent;
@@ -19,48 +23,17 @@ import net.sf.taverna.t2.workflowmodel.Dataflow;
 
 public class WorkflowsMenu extends AbstractMenuCustom {
 
+	private EditManager editManager = EditManager.getInstance();
+	private EditManagerObserver editManagerObserver = new EditManagerObserver();
 	private FileManager fileManager = FileManager.getInstance();
 	private FileManagerObserver fileManagerObserver = new FileManagerObserver();
+
 	private JMenu workflowsMenu;
-
-	private final class NoWorkflowsOpen extends AbstractAction {
-		private NoWorkflowsOpen() {
-			super("No workflows open");
-			setEnabled(false);
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			// No-op
-		}
-	}
-
-	private final class SwitchWorkflowAction extends AbstractAction {
-		private final Dataflow dataflow;
-
-		private SwitchWorkflowAction(String name, Dataflow dataflow) {
-			super(name);
-			this.dataflow = dataflow;
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			fileManager.setCurrentDataflow(dataflow);
-		}
-	}
-
-	private final class FileManagerObserver implements
-			Observer<FileManagerEvent> {
-		public void notify(Observable<FileManagerEvent> sender,
-				FileManagerEvent message) throws Exception {
-			if (message instanceof AbstractDataflowEvent) {
-				updateWorkflowsMenu();
-				// TODO: Don't rebuild whole menu
-			}
-		}
-	}
 
 	public WorkflowsMenu() {
 		super(DefaultMenuBar.DEFAULT_MENU_BAR, 900);
 		fileManager.addObserver(fileManagerObserver);
+		editManager.addObserver(editManagerObserver);
 	}
 
 	@Override
@@ -70,7 +43,15 @@ public class WorkflowsMenu extends AbstractMenuCustom {
 		return workflowsMenu;
 	}
 
-	protected void updateWorkflowsMenu() {
+	public void updateWorkflowsMenu() {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				updateWorkflowsMenuUI();
+			}
+		});
+	}
+
+	protected void updateWorkflowsMenuUI() {
 		workflowsMenu.removeAll();
 		ButtonGroup workflowsGroup = new ButtonGroup();
 
@@ -99,5 +80,50 @@ public class WorkflowsMenu extends AbstractMenuCustom {
 		}
 
 		workflowsMenu.revalidate();
+	}
+
+	private final class EditManagerObserver implements
+			Observer<EditManagerEvent> {
+		public void notify(Observable<EditManagerEvent> sender,
+				EditManagerEvent message) throws Exception {
+			if (message instanceof AbstractDataflowEditEvent) {
+				updateWorkflowsMenu();
+			}
+		}
+	}
+
+	private final class FileManagerObserver implements
+			Observer<FileManagerEvent> {
+		public void notify(Observable<FileManagerEvent> sender,
+				FileManagerEvent message) throws Exception {
+			if (message instanceof AbstractDataflowEvent) {
+				updateWorkflowsMenu();
+				// TODO: Don't rebuild whole menu
+			}
+		}
+	}
+
+	private final class NoWorkflowsOpen extends AbstractAction {
+		private NoWorkflowsOpen() {
+			super("No workflows open");
+			setEnabled(false);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			// No-op
+		}
+	}
+
+	private final class SwitchWorkflowAction extends AbstractAction {
+		private final Dataflow dataflow;
+
+		private SwitchWorkflowAction(String name, Dataflow dataflow) {
+			super(name);
+			this.dataflow = dataflow;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			fileManager.setCurrentDataflow(dataflow);
+		}
 	}
 }
