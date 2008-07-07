c41c6578eb7572e87f34fa51e93efc6c068fe68e
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-07-07 16:51:14 +0000

    added colour for Localworker activity
    git-svn-id: http://taverna.googlecode.com/svn@5569 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/configuration-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/configuration/colour/ColourManager.java
+++ trunk/t2workbench/configuration-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/configuration/colour/ColourManager.java
@@ -88,6 +88,7 @@ public class ColourManager implements Configurable {
 		defaultPropertyMap.put("net.sf.taverna.t2.activities.soaplab.SoaplabActivity", "#fafad2");//lightgoldenrodyellow
 		defaultPropertyMap.put("net.sf.taverna.t2.activities.stringconstant.StringConstantActivity", "#b0c4de");//lightsteelblue
 		defaultPropertyMap.put("net.sf.taverna.t2.activities.wsdl.WSDLActivity", "#a2cd5a");//darkolivegreen3
+		defaultPropertyMap.put("net.sf.taverna.t2.activities.localworker.LocalworkerActivity", "#d15fee"); //mediumorchid2
 	}
 
 	/**
