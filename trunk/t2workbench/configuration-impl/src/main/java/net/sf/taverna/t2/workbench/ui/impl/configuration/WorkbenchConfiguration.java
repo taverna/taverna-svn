77702d2e5d7edd99a1aa2692c0d7e9eb423b6abc
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-07-25 13:25:47 +0000

    OS-defaults for Dot location
    git-svn-id: http://taverna.googlecode.com/svn@5797 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/configuration-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/configuration/WorkbenchConfiguration.java
+++ trunk/t2workbench/configuration-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/configuration/WorkbenchConfiguration.java
@@ -1,9 +1,11 @@
 package net.sf.taverna.t2.workbench.ui.impl.configuration;
 
-import java.util.Collections;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.sf.taverna.raven.appconfig.ApplicationConfig;
 import net.sf.taverna.t2.workbench.configuration.AbstractConfigurable;
 
 /**
@@ -15,13 +17,26 @@ import net.sf.taverna.t2.workbench.configuration.AbstractConfigurable;
  */
 public class WorkbenchConfiguration extends AbstractConfigurable {
 
+
+	public static final String TAVERNA_DOTLOCATION = "taverna.dotlocation";
+	private static final String BIN = "bin";
+	private static final String BUNDLE_CONTENTS = "Contents";
+	private static final String BUNDLE_MAC_OS = "MacOS";
+	private static final String DOT_EXE = "dot.exe";
+	private static final String DOT_FALLBACK = "dot";
+
 	public static String uuid = "c14856f0-5967-11dd-ae16-0800200c9a66";
+
 	private static WorkbenchConfiguration instance = new WorkbenchConfiguration();
+	private static final String MAC_OS_X = "Mac OS X";
+	private static final String WIN32I386 = "win32i386";
+	private static final String WINDOWS = "Windows";
 
 	public static WorkbenchConfiguration getInstance() {
 		return instance;
 	}
 
+	private ApplicationConfig appConfig = ApplicationConfig.getInstance();
 
 	Map<String, String> defaultWorkbenchProperties = null;
 	Map<String, String> workbenchProperties = new HashMap<String, String>();
@@ -33,10 +48,10 @@ public class WorkbenchConfiguration extends AbstractConfigurable {
 	public Map<String, String> getDefaultPropertyMap() {
 		if (defaultWorkbenchProperties == null) {
 			defaultWorkbenchProperties = new HashMap<String, String>();
-			String dotLocation = System.getProperty("taverna.dotlocation") != null ? System
-					.getProperty("taverna.dotlocation")
-					: "/Applications/Taverna-1.7.1.app/Contents/MacOS/dot";
-			defaultWorkbenchProperties.put("taverna.dotlocation", dotLocation);
+			String dotLocation = System.getProperty(TAVERNA_DOTLOCATION) != null ? System
+					.getProperty(TAVERNA_DOTLOCATION)
+					: getDefaultDotLocation();
+			defaultWorkbenchProperties.put(TAVERNA_DOTLOCATION, dotLocation);
 		}
 		return defaultWorkbenchProperties;
 	}
@@ -49,5 +64,33 @@ public class WorkbenchConfiguration extends AbstractConfigurable {
 		return uuid;
 	}
 
-	
+	private String getDefaultDotLocation() {
+		File startupDir;
+		try {
+			startupDir = appConfig.getStartupDir();
+		} catch (IOException e) {
+			return DOT_FALLBACK;
+		}
+		String os = System.getProperty("os.name");
+		if (os.equals(MAC_OS_X)) {
+			if (startupDir.getParentFile() != null) {
+				File contentsDir = startupDir.getParentFile().getParentFile();
+				if (contentsDir != null
+						&& contentsDir.getName().equalsIgnoreCase(BUNDLE_CONTENTS)) {
+					File dot = new File(new File(contentsDir, BUNDLE_MAC_OS), DOT_FALLBACK);
+					if (dot.exists()) {
+						return dot.getAbsolutePath();
+					}
+				}
+			}
+		} else if (os.startsWith(WINDOWS)) {
+			File binWin386Dir = new File(new File(startupDir, BIN), WIN32I386);
+			File dot = new File(binWin386Dir, DOT_EXE);
+			if (dot.exists()) {
+				return dot.getAbsolutePath();
+			}
+		}
+		return DOT_FALLBACK;
+	}
+
 }
