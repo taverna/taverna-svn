b2aab41f8ebc2406417a1ea45b06bbc4cdcd700d
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-07-25 14:38:17 +0000

    Moved to position 1000 so it's after Workflows and before Help
    git-svn-id: http://taverna.googlecode.com/svn@5803 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/ui-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/menu/AdvancedMenu.java
+++ trunk/t2workbench/ui-impl/src/main/java/net/sf/taverna/t2/workbench/ui/impl/menu/AdvancedMenu.java
@@ -7,7 +7,7 @@ import net.sf.taverna.t2.ui.menu.DefaultMenuBar;
 
 public class AdvancedMenu extends AbstractMenu {
 	public AdvancedMenu() {
-		super(DefaultMenuBar.DEFAULT_MENU_BAR, 200, URI
+		super(DefaultMenuBar.DEFAULT_MENU_BAR, 1000, URI
 				.create("http://taverna.sf.net/2008/t2workbench/menu#advanced"),
 				"Advanced");
 	}
