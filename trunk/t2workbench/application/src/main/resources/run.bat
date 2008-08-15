1cd9294a5c5ec0409feee2f2158975b8b94dc2ec
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-08-15 14:07:10 +0000

    More memory (300 MB)
    git-svn-id: http://taverna.googlecode.com/svn@5990 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/application/src/main/resources/run.bat
+++ trunk/t2workbench/application/src/main/resources/run.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -Draven.profile=file:conf/current-profile.xml -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader -jar lib/prelauncher-1.7-SNAPSHOT.jar
+java -Xmx300m -Draven.profile=file:conf/current-profile.xml -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader -jar lib/prelauncher-1.7-SNAPSHOT.jar
