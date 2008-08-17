e4b1bc6b20a16441b2c13dc2b9a4a386aa9410f6
petarj <petarj@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-08-17 18:01:35 +0000

    modified gui
    git-svn-id: http://taverna.googlecode.com/svn@5993 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/sandbox/mat-plugin/mat-server/mat-server-java/src/main/java/net/sf/taverna/matserver/Application.java
+++ trunk/sandbox/mat-plugin/mat-server/mat-server-java/src/main/java/net/sf/taverna/matserver/Application.java
@@ -2,12 +2,15 @@ package net.sf.taverna.matserver;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
@@ -19,41 +22,58 @@ public class Application extends JFrame {
 
     private static final long serialVersionUID = 7434716524883186229L;
     private static MatServer server;
-    private JTextField portField;
     private JButton startButton;
     private JButton stopButton;
+    private JButton statusButton;
+    private JTextArea logArea;
+    private boolean running = false;
 
     public Application() {
         super("mat-server");
         init();
         server = new MatServer();
-        server.setPort(server.DEFAULT_PORT);
+        server.setPort(MatServer.DEFAULT_PORT);
     }
 
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         Container cp = getContentPane();
-        cp.setLayout(new FlowLayout()); //TODO: make this grid layout
+        cp.setLayout(new GridBagLayout()); //TODO: make this grid layout
 
-        portField = new JTextField();
-        portField.setColumns(10);
-        add(portField);
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridheight = GridBagConstraints.REMAINDER;
+
+        logArea = new JTextArea(16, 32);
+        logArea.setEditable(false);
+        cp.add(logArea, constraints);
 
         startButton = new JButton("Start");
         startButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                int port = Integer.parseInt(portField.getText());
-                server.setPort(port);
                 try {
                     server.start();
+                    running = true;
+                    logArea.append(">Server started.\n");
                 } catch (Exception ex) {
                     Logger.getLogger(Application.class.getName()).
                             log(Level.SEVERE, null, ex);
+                    running = false;
+                    logArea.append("Server startup failed.\n");
                 }
             }
         });
-        add(startButton);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridheight = 1;
+        constraints.gridwidth = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+
+        add(startButton, constraints);
 
         stopButton = new JButton("Stop");
         stopButton.addActionListener(new ActionListener() {
@@ -61,14 +81,35 @@ public class Application extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 try {
                     server.stop();
+                    running = false;
+                    logArea.append(">Server stopped.\n");
                 } catch (Exception ex) {
                     Logger.getLogger(Application.class.getName()).
                             log(Level.SEVERE, null, ex);
                 }
             }
         });
-        add(stopButton);
+
+        constraints.gridy = 1;
+        add(stopButton, constraints);
+
+        statusButton = new JButton("Status");
+        statusButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                if (running) {
+                    logArea.append(">Server is running.\n");
+                } else {
+                    logArea.append(">Server is inactive.\n");
+                }
+            }
+        });
+
+        constraints.gridy = 2;
+        add(statusButton, constraints);
+
         pack();
+        setResizable(false);
     }
 
     public static void main(String[] args) {
