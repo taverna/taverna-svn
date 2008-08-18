67bd4e79e5c4a23c4067dcf852de91bc12ba1222
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-08-18 15:29:10 +0000

    Throws InvalidDataflowException if the workflow was not valid
    git-svn-id: http://taverna.googlecode.com/svn@6013 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2core/workflowmodel-impl/src/main/java/net/sf/taverna/t2/facade/impl/WorkflowInstanceFacadeImpl.java
+++ trunk/t2core/workflowmodel-impl/src/main/java/net/sf/taverna/t2/facade/impl/WorkflowInstanceFacadeImpl.java
@@ -18,21 +18,47 @@ import net.sf.taverna.t2.utility.TypedTreeModel;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 import net.sf.taverna.t2.workflowmodel.DataflowInputPort;
 import net.sf.taverna.t2.workflowmodel.DataflowOutputPort;
+import net.sf.taverna.t2.workflowmodel.DataflowValidationReport;
+import net.sf.taverna.t2.workflowmodel.InvalidDataflowException;
 
+import org.apache.log4j.Logger;
+
+/**
+ * Implementation of {@link WorkflowInstanceFacade}
+ * 
+ * @author Tom Oinn
+ * @author Stian Soiland-Reyes
+ * 
+ */
 public class WorkflowInstanceFacadeImpl implements WorkflowInstanceFacade {
 
+	private static Logger logger = Logger
+			.getLogger(WorkflowInstanceFacadeImpl.class);
+
+	protected static AtomicLong owningProcessId = new AtomicLong(0);
+
+	private InvocationContext context;
+	public InvocationContext getContext() {
+		return context;
+	}
+
 	private Dataflow dataflow;
+	private ResultListener facadeResultListener;
+	private String instanceOwningProcessId;
+	private String localName;
+	private MonitorManager monitorManager = MonitorManager.getInstance();
+	private boolean pushDataCalled = false;
 	protected List<FailureListener> failureListeners = new ArrayList<FailureListener>();
 	protected List<ResultListener> resultListeners = new ArrayList<ResultListener>();
-	protected static AtomicLong owningProcessId = new AtomicLong(0);
-	private String instanceOwningProcessId;
-	private boolean pushDataCalled = false;
-	private ResultListener facadeResultListener;
-	private InvocationContext context;
-	private String localName;
 
 	public WorkflowInstanceFacadeImpl(final Dataflow dataflow,
-			InvocationContext context, String parentProcess) {
+			InvocationContext context, String parentProcess)
+			throws InvalidDataflowException {
+		DataflowValidationReport report = dataflow.checkValidity();
+		if (!report.isValid()) {
+			throw new InvalidDataflowException(dataflow, report);
+		}
+
 		this.dataflow = dataflow;
 		this.context = context;
 		this.localName = "facade" + owningProcessId.getAndIncrement();
@@ -41,37 +67,9 @@ public class WorkflowInstanceFacadeImpl implements WorkflowInstanceFacade {
 		} else {
 			this.instanceOwningProcessId = parentProcess + ":" + localName;
 		}
-		facadeResultListener = new ResultListener() {
-			private int portsToComplete = dataflow.getOutputPorts().size();
-
-			public void resultTokenProduced(WorkflowDataToken token,
-					String portName) {
-				if (instanceOwningProcessId.equals(token.getOwningProcess())) {
-					synchronized (this) {
-						if (token.getIndex().length == 0) {
-							portsToComplete--;
-						}
-						if (portsToComplete == 0) {
-							// Received complete events on all ports, can
-							// un-register this node from the monitor
-							MonitorManager.getInstance().deregisterNode(
-									instanceOwningProcessId.split(":"));
-						}
-					}
-					for (ResultListener resultListener : resultListeners
-							.toArray(new ResultListener[resultListeners.size()])) {
-						resultListener.resultTokenProduced(token
-								.popOwningProcess(), portName);
-					}
-				}
-			}
-
-		};
-
-		MonitorManager.getInstance().registerNode(this,
-				instanceOwningProcessId.split(":"),
+		facadeResultListener = new FacadeResultListener(dataflow);
+		monitorManager.registerNode(this, instanceOwningProcessId.split(":"),
 				new HashSet<MonitorableProperty<?>>());
-
 	}
 
 	public void addFailureListener(FailureListener listener) {
@@ -79,7 +77,7 @@ public class WorkflowInstanceFacadeImpl implements WorkflowInstanceFacade {
 	}
 
 	public synchronized void addResultListener(ResultListener listener) {
-		if (resultListeners.size() == 0) {
+		if (resultListeners.isEmpty()) {
 			for (DataflowOutputPort port : dataflow.getOutputPorts()) {
 				port.addResultListener(facadeResultListener);
 			}
@@ -92,6 +90,10 @@ public class WorkflowInstanceFacadeImpl implements WorkflowInstanceFacade {
 			throw new IllegalStateException(
 					"Data has already been pushed, fire must be called first!");
 		dataflow.fire(instanceOwningProcessId, context);
+	}
+
+	public Dataflow getDataflow() {
+		return dataflow;
 	}
 
 	public TypedTreeModel<MonitorNode> getStateModel() {
@@ -117,15 +119,46 @@ public class WorkflowInstanceFacadeImpl implements WorkflowInstanceFacade {
 
 	public synchronized void removeResultListener(ResultListener listener) {
 		resultListeners.remove(listener);
-		if (resultListeners.size() == 0) {
+		if (resultListeners.isEmpty()) {
 			for (DataflowOutputPort port : dataflow.getOutputPorts()) {
 				port.removeResultListener(facadeResultListener);
 			}
 		}
 	}
 
-	public Dataflow getDataflow() {
-		return dataflow;
+	protected class FacadeResultListener implements ResultListener {
+		private int portsToComplete;
+
+		public FacadeResultListener(Dataflow dataflow) {
+			portsToComplete = dataflow.getOutputPorts().size();
+		}
+
+		public void resultTokenProduced(WorkflowDataToken token, String portName) {
+			if (instanceOwningProcessId.equals(token.getOwningProcess())) {
+				synchronized (this) {
+					if (token.getIndex().length == 0) {
+						portsToComplete--;
+					}
+					if (portsToComplete == 0) {
+						// Received complete events on all ports, can
+						// un-register this node from the monitor
+						MonitorManager.getInstance().deregisterNode(
+								instanceOwningProcessId.split(":"));
+					}
+				}
+				ArrayList<ResultListener> copyOfListeners = new ArrayList<ResultListener>(
+						resultListeners);
+				for (ResultListener resultListener : copyOfListeners) {
+					try {
+						resultListener.resultTokenProduced(token
+								.popOwningProcess(), portName);
+					} catch (RuntimeException ex) {
+						logger.warn("Could not notify result listener "
+								+ resultListener, ex);
+					}
+				}
+			}
+		}
 	}
 
 }
