ded18f5b73a7a2efea4d2fef84b7d3b32e73d1e2
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-08-27 10:36:18 +0000

    Added ability to add multiple components to be verified
    git-svn-id: http://taverna.googlecode.com/svn@6061 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/utils/lang/ui/src/main/java/net/sf/taverna/t2/lang/ui/ValidatingUserInputDialog.java
+++ trunk/utils/lang/ui/src/main/java/net/sf/taverna/t2/lang/ui/ValidatingUserInputDialog.java
@@ -3,28 +3,27 @@ package net.sf.taverna.t2.lang.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.FocusTraversalPolicy;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Set;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.JTextComponent;
 
 import net.sf.taverna.t2.lang.ui.icons.Icons;
 
@@ -38,17 +37,7 @@ public class ValidatingUserInputDialog extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
-	private Set<String> invalidInputs;
-
-	private String invalidInputsMessage;
-
-	private String inputRegularExpression;
-
-	private String inputRegularExpressionMessage;
-
 	private String inputTitle;
-
-	private String inputMessage;
 
 	private JButton okButton;
 
@@ -56,17 +45,33 @@ public class ValidatingUserInputDialog extends JDialog {
 
 	private JTextArea inputText;
 
-	private JTextField inputField;
+	private JPanel inputPanel;
 
 	private JLabel iconLabel;
 
-	private String defaultValue;
+	private boolean valid = true;
 
-	private String inputValue;
+	private boolean result = false;
 
 	/**
 	 * Constructs a new instance of ValidatingUserInputDialog.
 	 * 
+	 * @param inputTitle
+	 *            the title for the dialog.
+	 * @param inputMessage
+	 *            the message describing what the user should input.
+	 */
+	public ValidatingUserInputDialog(String inputTitle, JPanel inputPanel) {
+		this.inputTitle = inputTitle;
+		this.inputPanel = inputPanel;
+		initialize();
+	}
+
+	/**
+	 * Adds a text component and the rules for a valid user entry.
+	 * 
+	 * @param textComponent
+	 *            the text component to validate
 	 * @param invalidInputs
 	 *            a set of inputs that are not valid. This is typically a set of
 	 *            already used identifiers to avoid name clashes. Can be an
@@ -80,25 +85,60 @@ public class ValidatingUserInputDialog extends JDialog {
 	 * @param inputRegularExpressionMessage
 	 *            the message to display if the user enters a value that doesn't
 	 *            match the inputRegularExpression.
-	 * @param inputTitle
-	 *            the title for the dialog.
-	 * @param inputMessage
-	 *            the message describing what the user should input.
-	 * @param defaultValue
-	 *            the default input value. Can be null.
 	 */
-	public ValidatingUserInputDialog(Set<String> invalidInputs,
-			String invalidInputsMessage, String inputRegularExpression,
-			String inputRegularExpressionMessage, String inputTitle,
-			String inputMessage, String defaultValue) {
-		this.invalidInputs = invalidInputs;
-		this.invalidInputsMessage = invalidInputsMessage;
-		this.inputRegularExpression = inputRegularExpression;
-		this.inputRegularExpressionMessage = inputRegularExpressionMessage;
-		this.inputTitle = inputTitle;
-		this.inputMessage = inputMessage;
-		this.defaultValue = defaultValue;
-		initialize();
+	public void addTextComponentValidation(final JTextComponent textComponent,
+			final String inputMessage, final Set<String> invalidInputs,
+			final String invalidInputsMessage,
+			final String inputRegularExpression,
+			final String inputRegularExpressionMessage) {
+		textComponent.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+			}
+
+			public void insertUpdate(DocumentEvent e) {
+				verify(textComponent.getText(), inputMessage, invalidInputs,
+						invalidInputsMessage, inputRegularExpression,
+						inputRegularExpressionMessage);
+			}
+
+			public void removeUpdate(DocumentEvent e) {
+				verify(textComponent.getText(), inputMessage, invalidInputs,
+						invalidInputsMessage, inputRegularExpression,
+						inputRegularExpressionMessage);
+			}
+		});
+		textComponent.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if (okButton.isEnabled() && e.getKeyCode() == KeyEvent.VK_ENTER) {
+					okButton.doClick();
+				}
+			}
+		});
+		textComponent.addFocusListener(new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				if (valid) {
+					setMessage(inputMessage);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Adds a component and a message to display when the component is in focus.
+	 * 
+	 * @param component
+	 *            the component to add
+	 * @param message
+	 *            the message to display when the component is in focus
+	 */
+	public void addMessageComponent(Component component, final String message) {
+		component.addFocusListener(new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				if (valid) {
+					setMessage(message);
+				}
+			}
+		});
 	}
 
 	private void initialize() {
@@ -115,41 +155,18 @@ public class ValidatingUserInputDialog extends JDialog {
 		inputLabel.setFont(inputLabel.getFont().deriveFont(Font.BOLD));
 		messagePanel.add(inputLabel, BorderLayout.NORTH);
 
-		inputText = new JTextArea(inputMessage);
+		inputText = new JTextArea();
 		inputText.setMargin(new Insets(5, 10, 10, 10));
 		inputText.setMinimumSize(new Dimension(0, 30));
 		inputText.setFont(inputText.getFont().deriveFont(11f));
 		inputText.setEditable(false);
+		inputText.setFocusable(false);
 		messagePanel.add(inputText, BorderLayout.CENTER);
-		
+
 		iconLabel = new JLabel();
 		messagePanel.add(iconLabel, BorderLayout.WEST);
-		
-		JPanel inputPanel = new JPanel(new BorderLayout());
-		inputPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
 		add(inputPanel, BorderLayout.CENTER);
-
-		inputField = new JTextField();
-		inputField.getDocument().addDocumentListener(new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {
-			}
-
-			public void insertUpdate(DocumentEvent e) {
-				verify();
-			}
-
-			public void removeUpdate(DocumentEvent e) {
-				verify();
-			}
-		});
-		inputField.addKeyListener(new KeyAdapter() {
-			public void keyReleased(KeyEvent e) {
-				if (okButton.isEnabled() && e.getKeyCode() == KeyEvent.VK_ENTER) {
-					okButton.doClick();
-				}
-			}
-		});
-		inputPanel.add(inputField, BorderLayout.NORTH);
 
 		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		add(buttonPanel, BorderLayout.SOUTH);
@@ -165,7 +182,7 @@ public class ValidatingUserInputDialog extends JDialog {
 		okButton = new JButton("OK");
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				inputValue = inputField.getText();
+				result = valid;
 				setVisible(false);
 			}
 		});
@@ -174,115 +191,56 @@ public class ValidatingUserInputDialog extends JDialog {
 
 		setModal(true);
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
-
-		setSize(new Dimension(400, 200));
-
-		setFocusTraversalPolicy(new FocusTraversalPolicy() {
-
-			@Override
-			public Component getComponentAfter(Container container,
-					Component component) {
-				if (component == inputField) {
-					if (okButton.isEnabled()) {
-						return okButton;
-					} else {
-						return cancelButton;
-					}
-				} else if (component == okButton) {
-					return cancelButton;
-				} else {
-					return inputField;
-				}
-			}
-
-			@Override
-			public Component getComponentBefore(Container container,
-					Component component) {
-				if (component == inputField) {
-					return cancelButton;
-				} else if (component == okButton) {
-					return inputField;
-				} else {
-					if (okButton.isEnabled()) {
-						return okButton;
-					} else {
-						return inputField;
-					}
-				}
-			}
-
-			@Override
-			public Component getDefaultComponent(Container container) {
-				return inputField;
-			}
-
-			@Override
-			public Component getFirstComponent(Container container) {
-				return inputField;
-			}
-
-			@Override
-			public Component getLastComponent(Container container) {
-				return cancelButton;
-			}
-
-		});
-
-		if (defaultValue != null) {
-			inputField.setText(defaultValue);
-		}
-
 	}
 
 	public void setMessage(String message) {
 		iconLabel.setIcon(null);
-		inputText.setText(message);		
+		inputText.setText(message);
 	}
-	
+
 	public void setWarningMessage(String message) {
 		iconLabel.setIcon(Icons.warningIcon);
-		inputText.setText(message);		
+		inputText.setText(message);
 	}
-	
+
 	public void setErrorMessage(String message) {
 		iconLabel.setIcon(Icons.severeIcon);
-		inputText.setText(message);		
+		inputText.setText(message);
 	}
-	
-	private void verify() {
-		if (invalidInputs != null
-				&& invalidInputs.contains(inputField.getText())) {
+
+	private void verify(String text, String inputMessage,
+			Set<String> invalidInputs, String invalidInputsMessage,
+			String inputRegularExpression, String inputRegularExpressionMessage) {
+		if (invalidInputs != null && invalidInputs.contains(text)) {
 			setErrorMessage(invalidInputsMessage);
-			okButton.setEnabled(false);
-			okButton.setSelected(false);
+			valid = false;
 		} else if (inputRegularExpression != null
-				&& !inputField.getText().matches(inputRegularExpression)) {
+				&& !text.matches(inputRegularExpression)) {
 			setErrorMessage(inputRegularExpressionMessage);
-			okButton.setEnabled(false);
-			okButton.setSelected(false);
+			valid = false;
 		} else {
 			setMessage(inputMessage);
-			okButton.setEnabled(true);
-			okButton.setSelected(true);
+			valid = true;
 		}
+		okButton.setEnabled(valid);
+		okButton.setSelected(valid);
 	}
 
 	/**
 	 * Show the dialog relative to the component. If the component is null then
 	 * the dialog is shown in the centre of the screen.
 	 * 
-	 * The value input by the user is returned. If the user clicks cancel or
-	 * closes the window null is returned.
+	 * Returns true if the user input is valid.
 	 * 
 	 * @param component
 	 *            the component that the dialog is shown relative to
-	 * @return the value input by the user
+	 * @return true if the user input is valid
 	 */
-	public String show(Component component) {
+	public boolean show(Component component) {
 		setLocationRelativeTo(component);
 		setVisible(true);
 		dispose();
-		return inputValue;
+		return result;
 	}
 
 }
