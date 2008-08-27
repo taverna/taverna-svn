7c8a4b15094f389ccf77b162ac7c3fc878b2dd0c
davidwithers <davidwithers@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-08-27 10:50:04 +0000

    Renamed Dataflow Input/Output actions
    git-svn-id: http://taverna.googlecode.com/svn@6062 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/design-ui/src/main/java/net/sf/taverna/t2/workbench/design/actions/EditDataflowOutputPortAction.java
+++ trunk/t2workbench/design-ui/src/main/java/net/sf/taverna/t2/workbench/design/actions/EditDataflowOutputPortAction.java
@@ -1,32 +1,38 @@
 package net.sf.taverna.t2.workbench.design.actions;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.HashSet;
 import java.util.Set;
 
 import net.sf.taverna.t2.lang.ui.ValidatingUserInputDialog;
+import net.sf.taverna.t2.workbench.design.ui.DataflowOutputPortPanel;
 import net.sf.taverna.t2.workbench.icons.WorkbenchIcons;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 import net.sf.taverna.t2.workflowmodel.DataflowOutputPort;
 import net.sf.taverna.t2.workflowmodel.EditException;
-import net.sf.taverna.t2.workflowmodel.Processor;
 
 import org.apache.log4j.Logger;
 
-public class RenameDataflowOutputPortAction extends DataflowEditAction {
+/**
+ * Action for editing a dataflow output port.
+ *
+ * @author David Withers
+ */
+public class EditDataflowOutputPortAction extends DataflowEditAction {
 
 	private static final long serialVersionUID = 1L;
 
-	private static Logger logger = Logger.getLogger(RenameDataflowOutputPortAction.class);
+	private static Logger logger = Logger.getLogger(EditDataflowOutputPortAction.class);
 
 	private DataflowOutputPort port;
 
-	public RenameDataflowOutputPortAction(Dataflow dataflow, DataflowOutputPort port, Component component) {
+	public EditDataflowOutputPortAction(Dataflow dataflow, DataflowOutputPort port, Component component) {
 		super(dataflow, component);
 		this.port = port;
 		putValue(SMALL_ICON, WorkbenchIcons.renameIcon);
-		putValue(NAME, "Rename Output Port...");		
+		putValue(NAME, "Edit Output Port...");		
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -37,11 +43,21 @@ public class RenameDataflowOutputPortAction extends DataflowEditAction {
 					usedOutputPorts.add(usedOutputPort.getName());
 				}
 			}
-			ValidatingUserInputDialog vuid = new ValidatingUserInputDialog(usedOutputPorts, "Duplicate output port.",
-					"[\\p{L}\\p{Digit}_.]+", "Invalid output port name.", "Output Port Name", "Set the output port name.",
-					port.getName());
-			String portName = vuid.show(component);
-			if (portName != null && !portName.equals(port.getName())) {
+
+			DataflowOutputPortPanel inputPanel = new DataflowOutputPortPanel();
+			
+			ValidatingUserInputDialog vuid = new ValidatingUserInputDialog(
+					"Edit Workflow Output Port", inputPanel);
+			vuid.addTextComponentValidation(inputPanel.getPortNameField(),
+					"Set the output port name.", usedOutputPorts,
+					"Duplicate output port.", "[\\p{L}\\p{Digit}_.]+",
+					"Invalid output port name.");
+			vuid.setSize(new Dimension(400, 200));
+
+			inputPanel.setPortName(port.getName());
+
+			if (vuid.show(component)) {
+				String portName = inputPanel.getPortName();
 				editManager.doDataflowEdit(dataflow, edits.getRenameDataflowOutputPortEdit(port, portName));
 			}
 		} catch (EditException e1) {
--- trunk/t2workbench/menu-impl/src/main/java/net/sf/taverna/t2/ui/menu/impl/ContextMenuFactory.java
+++ trunk/t2workbench/menu-impl/src/main/java/net/sf/taverna/t2/ui/menu/impl/ContextMenuFactory.java
@@ -8,13 +8,13 @@ import javax.swing.JPopupMenu;
 import net.sf.taverna.t2.lang.ui.ShadedLabel;
 import net.sf.taverna.t2.workbench.design.actions.AddDataflowInputAction;
 import net.sf.taverna.t2.workbench.design.actions.AddDataflowOutputAction;
+import net.sf.taverna.t2.workbench.design.actions.EditDataflowInputPortAction;
+import net.sf.taverna.t2.workbench.design.actions.EditDataflowOutputPortAction;
 import net.sf.taverna.t2.workbench.design.actions.RemoveDataflowInputPortAction;
 import net.sf.taverna.t2.workbench.design.actions.RemoveDataflowOutputPortAction;
 import net.sf.taverna.t2.workbench.design.actions.RemoveDatalinkAction;
 import net.sf.taverna.t2.workbench.design.actions.RemoveMergeAction;
 import net.sf.taverna.t2.workbench.design.actions.RemoveProcessorAction;
-import net.sf.taverna.t2.workbench.design.actions.RenameDataflowInputPortAction;
-import net.sf.taverna.t2.workbench.design.actions.RenameDataflowOutputPortAction;
 import net.sf.taverna.t2.workbench.design.actions.RenameProcessorAction;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 import net.sf.taverna.t2.workflowmodel.DataflowInputPort;
@@ -44,20 +44,20 @@ public class ContextMenuFactory {
 			popupMenu.add(new JMenuItem(new RemoveProcessorAction(dataflow, processor, component)));
 		} else if (dataflowObject instanceof Merge) {
 			Merge merge = (Merge) dataflowObject;
-			popupMenu.add(new ShadedLabel("Merge : " + merge.getLocalName(), ShadedLabel.GREEN));
+			popupMenu.add(new ShadedLabel("Merge", ShadedLabel.GREEN));
 			popupMenu.addSeparator();
 			popupMenu.add(new JMenuItem(new RemoveMergeAction(dataflow, merge, component)));
 		} else if (dataflowObject instanceof DataflowInputPort) {
 			DataflowInputPort dataflowInputPort = (DataflowInputPort) dataflowObject;
 			popupMenu.add(new ShadedLabel("Workflow Input : " + dataflowInputPort.getName(), ShadedLabel.GREEN));
 			popupMenu.addSeparator();
-			popupMenu.add(new JMenuItem(new RenameDataflowInputPortAction(dataflow, dataflowInputPort, component)));		
+			popupMenu.add(new JMenuItem(new EditDataflowInputPortAction(dataflow, dataflowInputPort, component)));		
 			popupMenu.add(new JMenuItem(new RemoveDataflowInputPortAction(dataflow, dataflowInputPort, component)));		
 		} else if (dataflowObject instanceof DataflowOutputPort) {
 			DataflowOutputPort dataflowOutputPort = (DataflowOutputPort) dataflowObject;
 			popupMenu.add(new ShadedLabel("Workflow Output : " + dataflowOutputPort.getName(), ShadedLabel.GREEN));
 			popupMenu.addSeparator();
-			popupMenu.add(new JMenuItem(new RenameDataflowOutputPortAction(dataflow, dataflowOutputPort, component)));		
+			popupMenu.add(new JMenuItem(new EditDataflowOutputPortAction(dataflow, dataflowOutputPort, component)));		
 			popupMenu.add(new JMenuItem(new RemoveDataflowOutputPortAction(dataflow, dataflowOutputPort, component)));		
 		} else if (dataflowObject instanceof Datalink) {
 			popupMenu.add(new ShadedLabel("Link", ShadedLabel.GREEN));
