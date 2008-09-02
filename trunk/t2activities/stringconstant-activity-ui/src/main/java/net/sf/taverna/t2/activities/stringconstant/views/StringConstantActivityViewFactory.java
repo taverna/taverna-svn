d333e7c02b7dbb4bb7a189aad6e236d854559183
anenadic <anenadic@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-09-02 10:38:19 +0000

    Small update to the workbench's icons.
    git-svn-id: http://taverna.googlecode.com/svn@6116 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2activities/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/views/StringConstantActivityViewFactory.java
+++ trunk/t2activities/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/views/StringConstantActivityViewFactory.java
@@ -3,7 +3,6 @@ package net.sf.taverna.t2.activities.stringconstant.views;
 import net.sf.taverna.t2.activities.stringconstant.StringConstantActivity;
 import net.sf.taverna.t2.workbench.ui.actions.activity.ActivityContextualView;
 import net.sf.taverna.t2.workbench.ui.views.contextualviews.activity.ContextualViewFactory;
-import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 
 public class StringConstantActivityViewFactory implements ContextualViewFactory<StringConstantActivity> {
 
