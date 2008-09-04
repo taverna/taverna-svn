db40ae61e4fa499ae6848405e1def0c4b7da13bf
sowen70 <sowen70@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-09-04 14:52:19 +0000

    T2-266 Added copyright notice header
    git-svn-id: http://taverna.googlecode.com/svn@6144 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/utils/raven-parent/plugins-api/src/main/java/net/sf/taverna/raven/plugins/PluginManager.java
+++ trunk/utils/raven-parent/plugins-api/src/main/java/net/sf/taverna/raven/plugins/PluginManager.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2007 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
 /*
  * Copyright (C) 2003 The University of Manchester 
  *
@@ -25,10 +45,10 @@
  * Source code information
  * -----------------------
  * Filename           $RCSfile: PluginManager.java,v $
- * Revision           $Revision: 1.3 $
+ * Revision           $Revision: 1.4 $
  * Release status     $State: Exp $
- * Last modified on   $Date: 2008-06-03 12:50:35 $
- *               by   $Author: stain $
+ * Last modified on   $Date: 2008-09-04 14:52:04 $
+ *               by   $Author: sowen70 $
  * Created on 23 Nov 2006
  *****************************************************************/
 package net.sf.taverna.raven.plugins;
--- trunk/utils/raven-parent/plugins-gui/src/main/java/net/sf/taverna/raven/plugins/ui/PluginManagerFrame.java
+++ trunk/utils/raven-parent/plugins-gui/src/main/java/net/sf/taverna/raven/plugins/ui/PluginManagerFrame.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2007 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
 /*
  * Copyright (C) 2003 The University of Manchester 
  *
@@ -25,10 +45,10 @@
  * Source code information
  * -----------------------
  * Filename           $RCSfile: PluginManagerFrame.java,v $
- * Revision           $Revision: 1.2 $
+ * Revision           $Revision: 1.3 $
  * Release status     $State: Exp $
- * Last modified on   $Date: 2008-07-28 09:40:05 $
- *               by   $Author: stain $
+ * Last modified on   $Date: 2008-09-04 14:51:52 $
+ *               by   $Author: sowen70 $
  * Created on 27 Nov 2006
  *****************************************************************/
 package net.sf.taverna.raven.plugins.ui;
