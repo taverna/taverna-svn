38f104cd34589605affa26d46b4e1d0202e19854
stain <stain@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-09-12 14:40:35 +0000

    for Taverna 1 compatability
    git-svn-id: http://taverna.googlecode.com/svn@6164 bf327186-88b3-11dd-a302-d386e5130c1c

--- trunk/t2workbench/application/src/main/resources/run.sh
+++ trunk/t2workbench/application/src/main/resources/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 java -Xmx300m -Draven.profile=file:conf/current-profile.xml \
+  -Dtaverna.startup=`pwd` \
   -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
   -Dapple.laf.useScreenMenuBar=true \
   -jar lib/prelauncher-1.7-SNAPSHOT.jar
