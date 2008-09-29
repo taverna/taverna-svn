4275d1e0ae34f8700448f69203828fd7f75fefbf
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2008-09-29 09:26:21 +0000

    Restructuring
    git-svn-id: http://taverna.googlecode.com/svn@6361 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna2/provenance/tags/provenance-core/webservice/src/main/java/net/sf/taverna/t2/lineageService/types/ProvenanceEventType.java
@@ -0,0 +1,5 @@
+package net.sf.taverna.t2.lineageService.types;
+
+public interface ProvenanceEventType {
+
+}
---
+++ taverna2/provenance/tags/provenance-core/webservice/src/test/java/net/sf/taverna/t2/service/test/ProvenanceAnalysisTest1.java
@@ -0,0 +1,157 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.service.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import net.sf.taverna.t2.lineageService.ProvenanceAnalysis;
+import net.sf.taverna.t2.lineageService.ProvenanceQuery;
+import net.sf.taverna.t2.lineageService.util.Arc;
+import net.sf.taverna.t2.lineageService.util.ProvenanceProcessor;
+import net.sf.taverna.t2.lineageService.util.Var;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author paolo
+ *
+ */
+public class ProvenanceAnalysisTest1 {
+
+	private static final String annotationsFileName = null;
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	@Test
+	public final void testComputeLineagePaths() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		String[] annotationFiles = 
+		{ "webservice/src/test/resources/simple_workflow2_with_2_inputs.xml",  //0
+		  "webservice/src/test/resources/test_iterate_list_of_lists.xml",  //1
+		  "webservice/src/test/resources/test1.annot.xml",  //2
+		  "webservice/src/test/resources/test2.annot.xml",  //3
+		  "webservice/src/test/resources/test3.annot.xml",  //4
+		  "webservice/src/test/resources/test4.annot.xml",  //5
+		  "webservice/src/test/resources/test5.annot.xml",  //6
+		  "webservice/src/test/resources/test6.annot.xml"};  //7
+
+		
+		String proc = "_OUTPUT_"; // we test from the outputs of the workflow
+		
+		Set<String> selectedProcessors = new HashSet<String>();
+
+		// this is what we certainly want to test
+		String targetIteration = "0,0,1,1";
+		String targetProc = "ALL";  // this is a magic keyword -- it stands for "all" :-)
+		
+		// use annotations?
+		boolean useAnnotations = false;
+		
+		// do this for one wfInstances -- there should actually be only one if clear() is used
+		ProvenanceAnalysis pa = new ProvenanceAnalysis();
+		
+		if (useAnnotations)  pa.setAnnotationFile(annotationFiles[5]);
+		else {
+			System.out.println("WARNING: no annotations have been loaded");
+		}
+		
+		ArrayList<String> IDs = (ArrayList<String>) pa.getWFInstanceIDs();  // ordered by timestamp
+		
+		if (IDs.size()>0)  {
+			
+			String WFID = IDs.get(0);
+			
+			System.out.println("computing lineage paths on instance "+WFID);
+			
+			// get outputs for this workflow
+			// retrieve all Arcs ending with (var,proc)
+			Map<String, String>  varsQueryConstraints = new HashMap<String, String>();
+
+			varsQueryConstraints.put("wfInstanceRef", WFID);
+			varsQueryConstraints.put("PNameRef", proc);  
+			
+			ProvenanceQuery pq = new ProvenanceQuery();		
+			
+			List<Var> outVars = pq.getVars(varsQueryConstraints);
+			
+			if (outVars.isEmpty()) {
+				assertFalse("no output vars to trace -- terminating", true);
+			}
+			
+			// collect all processors and select a random subset
+			Map<String, String>  procConstraints = new HashMap<String, String>();
+
+			procConstraints.put("wfInstanceRef", WFID);
+
+			List<ProvenanceProcessor> processors = pq.getProcessors(procConstraints);
+
+			List<String> procNames = new ArrayList<String>();
+			for (ProvenanceProcessor p:processors) { procNames.add(p.getPname()); }
+
+			if (targetProc.equals("ALL")) {
+
+				selectedProcessors.addAll(procNames);
+
+			} else {
+
+				selectedProcessors.add(targetProc);
+
+				Random r = new Random();					
+
+				for (int i=0; i<procNames.size() / 2; i++) {
+					int  n = r.nextInt(procNames.size());
+					selectedProcessors.add(procNames.get(n));
+				}
+
+			}
+
+			System.out.println("selected processors:");
+			for (String s:selectedProcessors) {
+				System.out.print(s+" ");
+			}
+			System.out.println();
+			
+			for (int i=0; i<outVars.size(); i++) {
+
+				pa.clp2(WFID, 
+						outVars.get(i).getVName(), 
+						outVars.get(i).getPName(), 
+						targetIteration, 
+						selectedProcessors);
+
+			}
+
+			assertTrue("lineage tree should have been printed above", true);
+		}
+		
+		
+	}
+	
+
+}
