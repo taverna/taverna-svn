dd7d12465d14d6b7d4a25ed6def589b796be1631
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-02-17 17:30:33 +0000

    Moved to top level directories
    git-svn-id: http://taverna.googlecode.com/svn@6920 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna2/workbench/workbench-application/trunk/src/main/resources/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-java -Xmx300m -Draven.profile=file:conf/current-profile.xml \
-  -Dtaverna.startup=. \
-  -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
-  -Dapple.laf.useScreenMenuBar=true \
-  -Dsun.swing.enableImprovedDragGesture \
-  -jar lib/prelauncher-1.9.jar
