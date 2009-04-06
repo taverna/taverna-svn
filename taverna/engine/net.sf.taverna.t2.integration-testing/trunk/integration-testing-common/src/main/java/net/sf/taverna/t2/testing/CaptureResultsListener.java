9c02656ae27cb8556f57f1da2cc01b9c25bce8df
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-06 13:31:05 +0000

    Don't fail on errors - in fact - print them on stderr
    git-svn-id: http://taverna.googlecode.com/svn@7219 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/engine/net.sf.taverna.t2.integration-testing/trunk/integration-testing-common/src/main/java/net/sf/taverna/t2/testing/CaptureResultsListener.java
+++ taverna/engine/net.sf.taverna.t2.integration-testing/trunk/integration-testing-common/src/main/java/net/sf/taverna/t2/testing/CaptureResultsListener.java
@@ -26,6 +26,11 @@ import java.util.Map;
 import net.sf.taverna.t2.facade.ResultListener;
 import net.sf.taverna.t2.invocation.InvocationContext;
 import net.sf.taverna.t2.invocation.WorkflowDataToken;
+import net.sf.taverna.t2.reference.ErrorDocument;
+import net.sf.taverna.t2.reference.Identified;
+import net.sf.taverna.t2.reference.IdentifiedList;
+import net.sf.taverna.t2.reference.ReferenceServiceException;
+import net.sf.taverna.t2.reference.StackTraceElementBean;
 import net.sf.taverna.t2.reference.T2Reference;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 
@@ -44,15 +49,66 @@ public class CaptureResultsListener implements ResultListener {
 	public void resultTokenProduced(WorkflowDataToken dataToken, String portname) {
 		if (dataToken.getIndex().length == 0) {
 			T2Reference reference = dataToken.getData();
-			System.out.println("Output reference = " + reference);
-			resultMap.put(portname, context.getReferenceService()
-					.renderIdentifier(reference, Object.class, context));
-			outputCount--;
+			System.out.println("Output reference made = " + reference);
+			Object value;
+			if (reference.containsErrors()) {
+				System.out.println("Contains errors!");
+				printAllErrors(context.getReferenceService().resolveIdentifier(reference, null, context));				
+			}
+			try {
+				value = context.getReferenceService().renderIdentifier(
+						reference, Object.class, context);
+			} catch (ReferenceServiceException ex) {
+				ex.printStackTrace();
+				value = reference;
+			}
+			resultMap.put(portname, value);
+			synchronized (this) {
+				outputCount--;
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void printAllErrors(Identified identified) {
+		if (! identified.getId().containsErrors()) {
+			return;
+		}
+		if (identified instanceof ErrorDocument) {
+			ErrorDocument errorDoc = (ErrorDocument)identified;
+			System.err.println("ERROR: " + identified.getId());
+			System.err.println(" message=" + errorDoc.getMessage());
+			System.err.println(" exceptionMessage=" + errorDoc.getExceptionMessage());
+			if (! errorDoc.getStackTraceStrings().isEmpty()) {
+				System.err.println(" stacktrace=");
+	//			System.err.println("Exception: ");
+				for (StackTraceElementBean stackTrace : errorDoc.getStackTraceStrings()) {
+					System.err.println("	at " + stackTrace.getClassName() + "." + stackTrace.getMethodName() + "(" + stackTrace.getFileName() + ":" + stackTrace.getLineNumber() + ")");
+				}
+			}
+			if (!errorDoc.getErrorReferences().isEmpty()) {
+				System.err.print("Caused by ");
+				for (T2Reference errorRef : errorDoc.getErrorReferences()) {
+					printAllErrors(context.getReferenceService().resolveIdentifier(errorRef, null, context));
+				}
+			}
+			System.err.println("(end of workflow error " + identified.getId() + ")");
+		} else if (identified instanceof IdentifiedList) {
+			IdentifiedList list = (IdentifiedList) identified;
+			for (Object object : list) {
+				if (object instanceof Identified) {
+					printAllErrors((Identified)object);
+				}
+			}			
+		} else {
+			// OK, no errors to report
 		}
 	}
 
 	public boolean isFinished() {
-		return outputCount == 0;
+		synchronized (this) {
+			return outputCount == 0;
+		}
 	}
 
 	public Object getResult(String outputName) {
