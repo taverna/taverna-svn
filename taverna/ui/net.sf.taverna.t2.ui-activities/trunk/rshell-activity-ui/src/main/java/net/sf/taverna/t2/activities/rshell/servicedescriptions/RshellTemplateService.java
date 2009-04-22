980689741d7098491d6b754b1cb4e4424f540a8f
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-22 16:24:47 +0000

    remove unused import
    git-svn-id: http://taverna.googlecode.com/svn@7336 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/ui/net.sf.taverna.t2.ui-activities/trunk/rshell-activity-ui/src/main/java/net/sf/taverna/t2/activities/rshell/servicedescriptions/RshellTemplateService.java
+++ taverna/ui/net.sf.taverna.t2.ui-activities/trunk/rshell-activity-ui/src/main/java/net/sf/taverna/t2/activities/rshell/servicedescriptions/RshellTemplateService.java
@@ -7,7 +7,6 @@ import net.sf.taverna.t2.activities.rshell.RshellActivity;
 import net.sf.taverna.t2.activities.rshell.RshellActivityConfigurationBean;
 import net.sf.taverna.t2.activities.rshell.query.RshellActivityItem;
 import net.sf.taverna.t2.servicedescriptions.AbstractTemplateService;
-import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 
 public class RshellTemplateService extends AbstractTemplateService<RshellActivityConfigurationBean>{
 
