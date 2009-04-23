085d92b75577a04fcc27148712cc98ba0e89b782
ian@mygrid.org.uk <ian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-23 16:22:48 +0000

    removed redundant files (pdfs, databases and other things!).  tupelo dependency out of the pom for the moment
    git-svn-id: http://taverna.googlecode.com/svn@7389 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-testing/src/test/resources/provenance-testing/resolvedData.sql
+++ taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-testing/src/test/resources/provenance-testing/resolvedData.sql
@@ -1,2 +1,2 @@
-CREATE VIEW `T2Provenance`.`resolvedData` AS
+REATE VIEW `T2Provenance`.`resolvedData` AS
     SELECT * FROM VarBinding VB join Data D on (VB.wfInstanceRef = D.wfInstanceID and VB.value = D.dataReference)
