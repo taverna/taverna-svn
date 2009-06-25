f125bfb33a92176f6c9ceb5463d41f4cab5e842d
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-06-25 11:36:55 +0000

    [maven-release-plugin]  copy for tag ui-activities-0.10
    git-svn-id: http://taverna.googlecode.com/svn@8001 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/ui/net.sf.taverna.t2.ui-activities/tags/ui-activities-0.10/biomoby-activity-ui/src/main/java/net/sf/taverna/t2/activities/biomoby/servicedescriptions/BiomobyServiceProvider.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (C) 2007 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.biomoby.servicedescriptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.Icon;
+
+import net.sf.taverna.t2.activities.biomoby.actions.BiomobyAdvancedMenuAction;
+import net.sf.taverna.t2.activities.biomoby.query.BiomobyActivityIcon;
+import net.sf.taverna.t2.activities.biomoby.query.BiomobyQueryHelper;
+import net.sf.taverna.t2.activities.biomoby.ui.AddBiomobyDialogue;
+import net.sf.taverna.t2.lang.observer.Observable;
+import net.sf.taverna.t2.lang.observer.Observer;
+import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceProvider;
+import net.sf.taverna.t2.servicedescriptions.CustomizedConfigurePanelProvider;
+import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionRegistry;
+import net.sf.taverna.t2.servicedescriptions.events.RemovedProviderEvent;
+import net.sf.taverna.t2.servicedescriptions.events.ServiceDescriptionRegistryEvent;
+import net.sf.taverna.t2.servicedescriptions.impl.ServiceDescriptionRegistryImpl;
+
+import org.biomoby.client.CentralImpl;
+import org.biomoby.shared.MobyException;
+
+public class BiomobyServiceProvider extends
+		AbstractConfigurableServiceProvider<BiomobyServiceProviderConfig>
+		implements
+		CustomizedConfigurePanelProvider<BiomobyServiceProviderConfig> {
+
+	public BiomobyServiceProvider() {
+		super(new BiomobyServiceProviderConfig());
+	}
+
+	public void findServiceDescriptionsAsync(
+			FindServiceDescriptionsCallBack callBack) {
+		try {
+			// constructor throws exception if it cannot communicate
+			// with the registry
+			BiomobyQueryHelper helper = new BiomobyQueryHelper(
+					getConfiguration().getEndpoint().toASCIIString(),
+					getConfiguration().getNamespace().toASCIIString());
+			helper.findServiceDescriptionsAsync(callBack);
+			// if it gets this far, the registry should be valid
+			BiomobyAdvancedMenuAction.addRegistry(
+					getConfiguration().getEndpoint().toASCIIString(),
+					getConfiguration().getNamespace().toASCIIString());
+		} catch (MobyException ex) {
+			callBack.fail("Could not connect to Biomoby endpoint "
+					+ getConfiguration().getEndpoint(), ex);
+		}
+		
+		// here we add a listener for events regarding BiomobyServiceProvider
+		// listening for the remove provider event
+		ServiceDescriptionRegistry serviceReg = ServiceDescriptionRegistryImpl.getInstance();
+		serviceReg.addObserver(new Observer<ServiceDescriptionRegistryEvent>(){
+			public void notify(
+					Observable<ServiceDescriptionRegistryEvent> subject,
+					ServiceDescriptionRegistryEvent event) throws Exception {
+				// check for removed provider events
+				if (event instanceof RemovedProviderEvent) {
+					if (((RemovedProviderEvent) event).getProvider() instanceof BiomobyServiceProvider) {
+						BiomobyServiceProvider bsp = (BiomobyServiceProvider) ((RemovedProviderEvent) event).getProvider();
+						// remove our menu item
+						BiomobyAdvancedMenuAction.removeRegistry(
+								bsp.getConfiguration().getEndpoint().toASCIIString(),
+								bsp.getConfiguration().getNamespace().toASCIIString());
+					}
+				}
+			}});
+	}
+
+	@Override
+	public List<BiomobyServiceProviderConfig> getDefaultConfigurations() {
+		return Arrays.asList(new BiomobyServiceProviderConfig(
+				CentralImpl.DEFAULT_ENDPOINT,
+				CentralImpl.DEFAULT_NAMESPACE));
+	}
+
+	public String getName() {
+		return "Biomoby service";
+	}
+
+	public Icon getIcon() {
+		return BiomobyActivityIcon.getBiomobyIcon();
+	}
+
+	@Override
+	public String toString() {
+		return getName() + " " + getConfiguration().getEndpoint();
+	}
+
+	@SuppressWarnings("serial")
+	public void createCustomizedConfigurePanel(
+			final CustomizedConfigureCallBack<BiomobyServiceProviderConfig> callBack) {
+		AddBiomobyDialogue addBiomobyDialogue = new AddBiomobyDialogue() {
+			@Override
+			protected void addRegistry(String registryEndpoint,
+					String registryURI) {
+				BiomobyServiceProviderConfig providerConfig = new BiomobyServiceProviderConfig(
+						registryEndpoint, registryURI);
+				callBack.newProviderConfiguration(providerConfig);
+			}
+		};
+		addBiomobyDialogue.setVisible(true);
+	}
+
+	@Override
+	protected List<? extends Object> getIdentifyingData() {
+		List<String> result;
+		result = Arrays.asList(getConfiguration().getEndpoint().toString());
+		return result;
+	}
+
+}
