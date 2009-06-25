7cad4e98397e589a86286104fe7a75b486b1077d
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-06-25 11:04:01 +0000

    [maven-release-plugin]  copy for tag ui-api-0.6
    git-svn-id: http://taverna.googlecode.com/svn@7992 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/ui/net.sf.taverna.t2.ui-api/tags/ui-api-0.6/activity-palette-api/src/main/java/net/sf/taverna/t2/servicedescriptions/ServiceDescription.java
@@ -0,0 +1,64 @@
+package net.sf.taverna.t2.servicedescriptions;
+
+import java.util.List;
+
+import javax.swing.Icon;
+
+import net.sf.taverna.t2.lang.beans.PropertyAnnotation;
+import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+
+import org.apache.log4j.Logger;
+
+public abstract class ServiceDescription<ConfigType> extends IdentifiedObject {
+
+	public static final String SERVICE_TEMPLATES = "Service templates";
+	private static final String NAME = "Name";
+	private static final String SERVICE_CONFIGURATION = "Service configuration";
+	private static final String SERVICE_IMPLEMENTATION_CLASS = "Service implementation class";
+	private static final String DESCRIPTION = "Description";
+	
+	public static final String LOCAL_SERVICES = "Local services";
+
+	private String description = "";
+
+	private static Logger logger = Logger.getLogger(ServiceDescription.class);
+
+	@PropertyAnnotation(expert = true, displayName = SERVICE_IMPLEMENTATION_CLASS)
+	public abstract Class<? extends Activity<ConfigType>> getActivityClass();
+
+	@PropertyAnnotation(expert = true, displayName = SERVICE_CONFIGURATION)
+	public abstract ConfigType getActivityConfiguration();
+
+	@PropertyAnnotation(displayName = DESCRIPTION)
+	public String getDescription() {
+		return this.description;
+	}
+
+	@PropertyAnnotation(expert = true)
+	public abstract Icon getIcon();
+
+	@PropertyAnnotation(displayName = NAME)
+	public abstract String getName();
+	
+
+	@PropertyAnnotation(expert = true)
+	@SuppressWarnings("unchecked")
+	public abstract List<? extends Comparable> getPath();
+
+	@PropertyAnnotation(hidden = true)
+	public boolean isTemplateService() {
+		return false;
+	}
+	
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String toString() {
+		return "Service description " +  getName();
+	}
+	
+}
