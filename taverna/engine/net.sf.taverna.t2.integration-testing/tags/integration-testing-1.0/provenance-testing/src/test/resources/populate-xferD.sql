727ae4c17bbed16e152d492ba3470ef2690e0536
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-12-16 15:33:03 +0000

    T2-936 updated integration tests
    git-svn-id: http://taverna.googlecode.com/svn@9601 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/engine/net.sf.taverna.t2.integration-testing/tags/integration-testing-1.0/provenance-testing/src/test/resources/populate-xferD.sql
+++
@@ -1,5 +0,0 @@
-insert into xferd (pto, vto, valto, pfrom, vfrom, valfrom)
-(select pto, vto, valto, pfrom, vfrom, valfrom
- from   DD 
- where pto <> pfrom)
-
--- taverna/engine/net.sf.taverna.t2.integration-testing/tags/integration-testing-1.0/provenance-testing/src/test/resources/populate-xformD.sql
+++
@@ -1,5 +0,0 @@
-insert into xformd (p, vto, valto, vfrom, valfrom, iteration)
-(select pto, vto, valto, vfrom, valfrom, iteration
- from   DD 
- where pto = pfrom)
-
