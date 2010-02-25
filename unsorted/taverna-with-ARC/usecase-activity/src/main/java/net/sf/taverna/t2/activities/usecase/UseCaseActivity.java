8163897a54129545dabd738bfc1326dd500b7039
fxtentacle <fxtentacle@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-02-25 13:31:57 +0000

    Modified use case activity to properly set MIME Types on its ports
    git-svn-id: http://taverna.googlecode.com/svn@9722 bf327186-88b3-11dd-a302-d386e5130c1c

--- unsorted/taverna-with-ARC/usecase-activity/src/main/java/net/sf/taverna/t2/activities/usecase/UseCaseActivity.java
+++ unsorted/taverna-with-ARC/usecase-activity/src/main/java/net/sf/taverna/t2/activities/usecase/UseCaseActivity.java
@@ -23,17 +23,27 @@ package net.sf.taverna.t2.activities.usecase;
 
 import java.io.IOException;
 import java.rmi.ServerException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.sf.taverna.t2.annotation.Annotated;
+import net.sf.taverna.t2.annotation.annotationbeans.MimeType;
+import net.sf.taverna.t2.reference.ExternalReferenceSPI;
 import net.sf.taverna.t2.reference.ReferenceService;
 import net.sf.taverna.t2.reference.ReferenceServiceException;
 import net.sf.taverna.t2.reference.T2Reference;
+import net.sf.taverna.t2.workflowmodel.EditException;
+import net.sf.taverna.t2.workflowmodel.EditsRegistry;
+import net.sf.taverna.t2.workflowmodel.OutputPort;
+import net.sf.taverna.t2.workflowmodel.processor.activity.AbstractActivity;
 import net.sf.taverna.t2.workflowmodel.processor.activity.AbstractAsynchronousActivity;
 import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityInputPort;
 import net.sf.taverna.t2.workflowmodel.processor.activity.AsynchronousActivityCallback;
 
+import org.apache.log4j.Logger;
 import org.globus.ftp.exception.ClientException;
 
 import de.uni_luebeck.inb.knowarc.gui.ProgressDisplayImpl;
@@ -42,35 +52,63 @@ import de.uni_luebeck.inb.knowarc.usecases.ScriptInputUser;
 import de.uni_luebeck.inb.knowarc.usecases.ScriptOutput;
 import de.uni_luebeck.inb.knowarc.usecases.UseCaseDescription;
 import de.uni_luebeck.inb.knowarc.usecases.UseCaseEnumeration;
-import de.uni_luebeck.inb.knowarc.usecases.invocation.GridUseCaseInvokation;
 import de.uni_luebeck.inb.knowarc.usecases.invocation.UseCaseInvokation;
 
 public class UseCaseActivity extends AbstractAsynchronousActivity<UseCaseActivityConfigurationBean> {
 	private UseCaseActivityConfigurationBean configurationBean;
 	private UseCaseDescription mydesc;
 
+	private void addMimeTypes(Annotated<?> annotated, List<String> mimeTypes) {
+		for (String mimeType : mimeTypes) {
+			MimeType mimeTypeAnnotation = new MimeType();
+			mimeTypeAnnotation.setText(mimeType);
+			try {
+				EditsRegistry.getEdits().getAddAnnotationChainEdit(annotated, mimeTypeAnnotation).doEdit();
+			} catch (EditException e) {
+				Logger.getLogger(UseCaseActivity.class).error(e);
+			}
+		}
+	}
+
+	private void addInputWithMime(String portName, int portDepth, Class<?> translatedElementClass, List<String> mimeTypes) {
+		List<Class<? extends ExternalReferenceSPI>> handledReferenceSchemes = Collections.emptyList();
+		ActivityInputPort inputPort = EditsRegistry.getEdits().createActivityInputPort(portName, portDepth, true, handledReferenceSchemes,
+				translatedElementClass);
+		inputPorts.add(inputPort);
+		addMimeTypes(inputPort, mimeTypes);
+	}
+
+	private void addOutputWithMime(String portName, int portDepth, List<String> mimeTypes) {
+		OutputPort outputPort = EditsRegistry.getEdits().createActivityOutputPort(portName, portDepth, portDepth);
+		outputPorts.add(outputPort);
+		addMimeTypes(outputPort, mimeTypes);
+	}
+
 	@Override
 	public void configure(UseCaseActivityConfigurationBean bean) throws ActivityConfigurationException {
 		this.configurationBean = bean;
-		
+
 		try {
-			List<UseCaseDescription> usecases = UseCaseEnumeration.enumerateXmlFile(new ProgressDisplayImpl(KnowARCConfigurationFactory.getConfiguration()), bean.getRepositoryUrl());
-			for(UseCaseDescription usecase : usecases ) {
-				if(!usecase.usecaseid.equalsIgnoreCase(bean.getUsecaseid())) continue;
+			List<UseCaseDescription> usecases = UseCaseEnumeration.enumerateXmlFile(new ProgressDisplayImpl(KnowARCConfigurationFactory.getConfiguration()),
+					bean.getRepositoryUrl());
+			for (UseCaseDescription usecase : usecases) {
+				if (!usecase.usecaseid.equalsIgnoreCase(bean.getUsecaseid()))
+					continue;
 				mydesc = usecase;
 				break;
 			}
-			
+
 			for (Map.Entry<String, ScriptInput> cur : mydesc.inputs.entrySet()) {
-				addInput(cur.getKey(), ((ScriptInputUser) cur.getValue()).list ? 2 : 1, true, null, cur.getValue().binary ? byte[].class : String.class);
+				ScriptInputUser scriptInputUser = (ScriptInputUser) cur.getValue();
+				addInputWithMime(cur.getKey(), scriptInputUser.list ? 1 : 0, cur.getValue().binary ? byte[].class : String.class, scriptInputUser.mime);
 
 			}
 			for (Map.Entry<String, ScriptOutput> cur : mydesc.outputs.entrySet()) {
-				addOutput(cur.getKey(), 1);
+				addOutputWithMime(cur.getKey(), 0, cur.getValue().mime);
 			}
 
-			addOutput("STDOUT", 1);
-			addOutput("STDERR", 1);
+			addOutput("STDOUT", 0);
+			addOutput("STDERR", 0);
 		} catch (Exception e) {
 			throw new ActivityConfigurationException("Couldn't create UseCase Activity", e);
 		}
@@ -95,10 +133,12 @@ public class UseCaseActivity extends AbstractAsynchronousActivity<UseCaseActivit
 						Object value = referenceService.renderIdentifier(data.get(cur), invoke.getType(cur), callback.getContext());
 						invoke.setInput(cur, value);
 					}
-					HashMap<String, Object> downloads = invoke.Submit();
-					HashMap<String, T2Reference> result = new HashMap<String, T2Reference>();
+					Map<String, Object> downloads = invoke.Submit();
+					Map<String, T2Reference> result = new HashMap<String, T2Reference>();
 					for (Map.Entry<String, Object> cur : downloads.entrySet()) {
-						result.put(cur.getKey(), referenceService.register(cur.getValue(), 1, true, callback.getContext()));
+						Object value = cur.getValue();
+						T2Reference reference = referenceService.register(value, 0, true, callback.getContext());
+						result.put(cur.getKey(), reference);
 					}
 					callback.receiveResult(result, new int[0]);
 				} catch (ServerException e) {
