f7f2e0bf6ea1445c28f2d18a825719523f10cbb9
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-03-29 12:30:04 +0000

    T2-1256 re-tagging 1.1
    git-svn-id: http://taverna.googlecode.com/svn@9914 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/engine/net.sf.taverna.t2.security/tags/security-1.1/credential-manager/src/main/java/net/sf/taverna/t2/security/credentialmanager/UsernamePassword.java
@@ -0,0 +1,69 @@
+package net.sf.taverna.t2.security.credentialmanager;
+
+import java.util.Arrays;
+
+public class UsernamePassword {
+	private char[] password;
+	private boolean shouldSave = false;
+	private String username;
+
+	@Override
+	protected UsernamePassword clone() {
+		UsernamePassword up = new UsernamePassword();
+		up.setUsername(getUsername());
+		up.setPassword(getPassword().clone());
+		up.setShouldSave(isShouldSave());
+		return up;
+	}
+	
+	public UsernamePassword() {
+	}
+	
+	public UsernamePassword(String username, String password) {
+		this.username = username;
+		this.password = password.toCharArray();
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+	
+	public String getPasswordAsString() {
+		return String.valueOf(password);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public boolean isShouldSave() {
+		return shouldSave;
+	}
+
+	public void resetPassword() {
+		if (this.password == null) {
+			return;
+		}
+		Arrays.fill(this.password, '\u0000');
+	}
+
+	public void setPassword(char[] password) {
+		resetPassword();
+		this.password = password;
+	}
+
+	public void setShouldSave(boolean shouldSave) {
+		this.shouldSave = shouldSave;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		resetPassword();
+	}
+
+
+}
