9ecf5fb3e1894240618c57733f02143ac4c26d93
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-04-21 15:39:29 +0000

    T2-1307 Move to package .database
    git-svn-id: http://taverna.googlecode.com/svn@10000 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-unit-tests/src/test/java/net/sf/taverna/t2/provenance/query/AbstractDatabaseTestHelper.java
+++ taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-unit-tests/src/test/java/net/sf/taverna/t2/provenance/query/AbstractDatabaseTestHelper.java
@@ -1,4 +1,4 @@
-package net.sf.taverna.t2.provenance.capture;
+package net.sf.taverna.t2.provenance.query;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
--- taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-unit-tests/src/test/java/net/sf/taverna/t2/provenance/query/CapturedToDatabaseNested2Test.java
+++ taverna/engine/net.sf.taverna.t2.integration-testing/trunk/provenance-unit-tests/src/test/java/net/sf/taverna/t2/provenance/query/CapturedToDatabaseNested2Test.java
@@ -1,4 +1,4 @@
-package net.sf.taverna.t2.provenance.capture;
+package net.sf.taverna.t2.provenance.query;
 
 import java.util.HashMap;
 import java.util.Map;
