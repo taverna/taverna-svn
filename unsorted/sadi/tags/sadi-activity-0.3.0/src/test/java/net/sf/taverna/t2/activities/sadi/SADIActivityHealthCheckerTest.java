73fed0f37a475260ef24b0fcbd51f49ee5861f79
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-07-09 13:16:46 +0000

    0.3.0 release
    git-svn-id: http://taverna.googlecode.com/svn@11185 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ unsorted/sadi/tags/sadi-activity-0.3.0/src/test/java/net/sf/taverna/t2/activities/sadi/SADIActivityHealthCheckerTest.java
@@ -0,0 +1,192 @@
+/*******************************************************************************
+ * Copyright (C) 2009 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.sadi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import net.sf.taverna.t2.visit.VisitReport;
+import net.sf.taverna.t2.visit.VisitReport.Status;
+import net.sf.taverna.t2.workflowmodel.health.HealthCheck;
+import net.sf.taverna.t2.workflowmodel.processor.activity.AbstractActivity;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
+
+import org.apache.commons.configuration.BaseConfiguration;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ca.wilkinsonlab.sadi.client.RegistryImpl;
+import ca.wilkinsonlab.sadi.client.Service.ServiceStatus;
+import ca.wilkinsonlab.sadi.common.SADIException;
+
+/**
+ * Unit tests for {@link SADIActivityHealthChecker}.
+ * 
+ * @author David Withers
+ */
+public class SADIActivityHealthCheckerTest {
+
+	private SADIActivity activity;
+	
+	private SADIActivityHealthChecker activityHealthChecker;
+	
+	private ServiceStatus serviceStatus;
+		
+	@Before
+	public void setUp() throws Exception {
+		activity = new SADIActivity() {
+			@Override
+			public RegistryImpl getRegistry() throws IOException {
+				return new RegistryImpl(new BaseConfiguration() {}) {
+					@Override
+					public ServiceStatus getServiceStatus(String serviceURI) throws SADIException {
+						return serviceStatus;
+					}
+				};
+			}
+
+			@Override
+			public SADIActivityConfigurationBean getConfiguration() {
+				return new SADIActivityConfigurationBean() {
+					public String getServiceURI() {
+						return "http://example.com/test";
+					}					
+				};
+			}
+		};
+		activityHealthChecker = new SADIActivityHealthChecker();
+	}
+
+	@Test
+	public void testCanVisit() {
+		assertFalse(activityHealthChecker.canVisit(null));
+		assertFalse(activityHealthChecker.canVisit(new Object()));
+		assertFalse(activityHealthChecker.canVisit(new AbstractActivity<Object>() {
+			public void configure(Object conf) throws ActivityConfigurationException {
+			}
+			public Object getConfiguration() {
+				return null;
+			}
+		}));
+		assertTrue(activityHealthChecker.canVisit(activity));
+	}
+
+	@Test
+	@Ignore
+	public void testVisit() {
+		serviceStatus = ServiceStatus.OK;
+		VisitReport healthReport = activityHealthChecker.visit(activity, new ArrayList<Object>());
+		assertNotNull(healthReport);
+		assertEquals(HealthCheck.NO_PROBLEM, healthReport.getResultId());
+		assertEquals(Status.WARNING, healthReport.getStatus());		
+		assertEquals(2, healthReport.getSubReports().size());
+		Iterator<VisitReport> iterator = healthReport.getSubReports().iterator();
+		iterator.next();
+		VisitReport subReport = iterator.next();
+		System.out.println(subReport.getMessage());
+		assertEquals(Status.OK, subReport.getStatus());
+
+		serviceStatus = ServiceStatus.SLOW;
+		healthReport = activityHealthChecker.visit(activity, new ArrayList<Object>());
+		assertNotNull(healthReport);
+		assertEquals(HealthCheck.NO_PROBLEM, healthReport.getResultId());
+		assertEquals(Status.WARNING, healthReport.getStatus());		
+		assertEquals(2, healthReport.getSubReports().size());
+		iterator = healthReport.getSubReports().iterator();
+		iterator.next();
+		subReport = iterator.next();
+		subReport = healthReport.getSubReports().iterator().next();
+		assertEquals(Status.WARNING, subReport.getStatus());
+
+		serviceStatus = ServiceStatus.DEAD;
+		healthReport = activityHealthChecker.visit(activity, new ArrayList<Object>());
+		assertNotNull(healthReport);
+		assertEquals(HealthCheck.NO_PROBLEM, healthReport.getResultId());
+		assertEquals(Status.SEVERE, healthReport.getStatus());		
+		assertEquals(2, healthReport.getSubReports().size());
+		healthReport.getSubReports().iterator().next();
+		iterator = healthReport.getSubReports().iterator();
+		iterator.next();
+		subReport = iterator.next();
+		assertEquals(Status.SEVERE, subReport.getStatus());
+
+		healthReport = activityHealthChecker.visit(new SADIActivity() {
+			public RegistryImpl getRegistry() throws IOException {
+				throw new IOException();
+			}
+			public SADIActivityConfigurationBean getConfiguration() {
+				return new SADIActivityConfigurationBean() {
+					public String getServiceURI() {
+						return "http://example.com/test";
+					}					
+				};
+			}
+		}, new ArrayList<Object>());
+		assertNotNull(healthReport);
+		assertEquals(HealthCheck.NO_PROBLEM, healthReport.getResultId());
+		assertEquals(Status.WARNING, healthReport.getStatus());		
+		assertEquals(2, healthReport.getSubReports().size());
+		healthReport.getSubReports().iterator().next();
+		iterator = healthReport.getSubReports().iterator();
+		iterator.next();
+		subReport = iterator.next();
+		assertEquals(Status.WARNING, subReport.getStatus());
+
+		healthReport = activityHealthChecker.visit(new SADIActivity() {
+			public RegistryImpl getRegistry() throws IOException {
+				return new RegistryImpl(new BaseConfiguration() {}) {
+					public ServiceStatus getServiceStatus(String serviceURI) throws SADIException {
+						throw new SADIException("");
+					}
+				};
+			}
+			public SADIActivityConfigurationBean getConfiguration() {
+				return new SADIActivityConfigurationBean() {
+					public String getServiceURI() {
+						return "http://example.com/test";
+					}					
+				};
+			}
+		}, new ArrayList<Object>());
+		assertNotNull(healthReport);
+		assertEquals(HealthCheck.NO_PROBLEM, healthReport.getResultId());
+		assertEquals(Status.WARNING, healthReport.getStatus());		
+		assertEquals(2, healthReport.getSubReports().size());
+		healthReport.getSubReports().iterator().next();
+		iterator = healthReport.getSubReports().iterator();
+		iterator.next();
+		subReport = iterator.next();
+		assertEquals(Status.WARNING, subReport.getStatus());
+	}
+
+	@Test
+	public void testIsTimeConsuming() {
+		assertTrue(activityHealthChecker.isTimeConsuming());
+	}
+
+}
---
+++ unsorted/sadi/tags/sadi-activity-0.3.0/src/test/java/net/sf/taverna/t2/activities/sadi/SADIUtilsIT.java
@@ -0,0 +1,234 @@
+/*******************************************************************************
+ * Copyright (C) 2010 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.sadi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ca.wilkinsonlab.sadi.client.Service;
+import ca.wilkinsonlab.sadi.client.ServiceImpl;
+
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+/**
+ * Unit tests for SADIUtils
+ *
+ * @author David Withers
+ */
+public class SADIUtilsIT {
+
+	private <T> List<List<T>> listOfLists(T... values) {
+		List<List<T>> listOfList = new ArrayList<List<T>>();
+		for (T value : values) {
+			List<T> list = new ArrayList<T>();
+			list.add(value);
+			listOfList.add(list);
+		}
+		return listOfList;
+	}
+	
+	@Test
+	public void testGetInputResources() throws Exception {
+		String serviceURI = "http://sadiframework.org/examples/ermineJgo";
+		String serviceId = serviceURI + UUID.randomUUID();
+		Service service = new ServiceImpl(serviceURI);
+		
+		List<List<String>> hasGoTerm = new ArrayList<List<String>>();
+		List<String> goTerms = new ArrayList<String>();
+		Collections.addAll(goTerms, "0000166", "0003677", "0017111", "0005515", "0005524",
+				"0005663", "0005634", "0003689", "0006260");
+		hasGoTerm.add(goTerms);
+		goTerms = new ArrayList<String>();
+		Collections.addAll(goTerms, "0016740", "0006468", "0007155", "0005515", "0004672",
+				"0007169", "0016301", "0004713", "0004872", "0004714", "0000166", "0005524",
+				"0016020", "0005887", "0016021");
+		hasGoTerm.add(goTerms);
+		goTerms = new ArrayList<String>();
+		Collections.addAll(goTerms, "0006986", "0006950", "0005524", "0000166");
+		hasGoTerm.add(goTerms);
+		
+		List<List<String>> expressionLevels = listOfLists("0.903370423", "1.903370423", "1.2");
+
+		RestrictionNode node = SADIUtils.buildInputRestrictionTree(service.getInputClass());
+		for (List<String> path : SADIUtils.getDefaultRestrictionPaths(node)) {
+			RestrictionNode restriction = SADIUtils.getRestriction(node, path);
+			restriction.setSelected();
+		}
+		RestrictionNode firstLeaf = (RestrictionNode) node.getFirstLeaf();
+		RestrictionNode nextLeaf = (RestrictionNode) firstLeaf.getNextLeaf();
+		System.out.println(firstLeaf.getOntProperty().getLocalName());
+		System.out.println(nextLeaf.getOntProperty().getLocalName());
+		if (firstLeaf.getOntProperty().getLocalName().equals("hasGOTerm")) {
+			firstLeaf.setValues(serviceId, hasGoTerm);
+			nextLeaf.setValues(serviceId, expressionLevels);
+		} else {
+			firstLeaf.setValues(serviceId, expressionLevels);
+			nextLeaf.setValues(serviceId, hasGoTerm);
+		}
+		
+		List<Resource> inputResources = SADIUtils.getInputResources(node, serviceId);
+		
+		Collection<Triple> outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+		
+		RestrictionNode outputNode = SADIUtils.buildOutputRestrictionTree(service.getInputClass(), service.getOutputClass());
+		SADIUtils.putOutputResources(outputNode, outputs, inputResources, serviceId);
+		SADIUtils.printTreeValues(serviceId, outputNode);
+	}
+	
+	@Test
+	public void testGetInputResourcesLinear() throws Exception {
+		String serviceURI = "http://sadiframework.org/examples/linear";
+		String serviceId = serviceURI + UUID.randomUUID();
+		Service service = new ServiceImpl(serviceURI);
+		
+		List<List<String>> x = listOfLists("1", "3");
+		List<List<String>> y = listOfLists("2", "5");
+		List<List<String>> date = listOfLists("2009-01-01", "2009-01-03");
+		List<List<String>> value = listOfLists("3", "5");
+
+		RestrictionNode inputRestrictionTree = SADIUtils.buildInputRestrictionTree(service.getInputClass());
+		Map<String, RestrictionNode> map = new HashMap<String, RestrictionNode>();
+		RestrictionNode leaf = (RestrictionNode) inputRestrictionTree.getFirstLeaf();
+		map.put(leaf.getOntProperty().getLocalName(), leaf);
+		leaf = (RestrictionNode) leaf.getNextLeaf();
+		map.put(leaf.getOntProperty().getLocalName(), leaf);
+		leaf = (RestrictionNode) leaf.getNextLeaf();
+		map.put(leaf.getOntProperty().getLocalName(), leaf);
+		leaf = (RestrictionNode) leaf.getNextLeaf();
+		map.put(leaf.getOntProperty().getLocalName(), leaf);
+
+		map.get("x").setSelected();
+		map.get("x").setValues(serviceId, x);
+		map.get("y").setSelected();
+		map.get("y").setValues(serviceId, y);
+		System.out.println(SADIUtils.printTree(inputRestrictionTree));
+				
+		Collection<Resource> inputResources = SADIUtils.getInputResources(inputRestrictionTree, serviceId);
+		
+		Collection<Triple> outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+
+		inputRestrictionTree.clearSelected();
+
+		map.get("date").setSelected();
+		map.get("date").setValues(serviceId, date);
+		map.get("value").setSelected();
+		map.get("value").setValues(serviceId, value);
+		System.out.println(SADIUtils.printTree(inputRestrictionTree));
+	
+		inputResources = SADIUtils.getInputResources(inputRestrictionTree, serviceId);
+		
+		outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+
+	}
+
+	@Test
+	public void testGetInputResources2() throws Exception {
+		String serviceURI = "http://sadiframework.org/examples/uniprotInfo";
+		String serviceId = serviceURI + UUID.randomUUID();
+		Service service = new ServiceImpl(serviceURI);
+		
+		List<String> uniprot = Arrays.asList("P68871", "Q7Z591");
+
+		RestrictionNode inputRestrictionTree = SADIUtils.buildInputRestrictionTree(service.getInputClass());
+		for (List<String> path : SADIUtils.getDefaultRestrictionPaths(inputRestrictionTree)) {
+			RestrictionNode restriction = SADIUtils.getRestriction(inputRestrictionTree, path);
+			restriction.setSelected();
+		}
+		RestrictionNode firstLeaf = (RestrictionNode) inputRestrictionTree.getFirstLeaf();
+		firstLeaf.setValues(serviceId, uniprot);
+		
+		Collection<Resource> inputResources = SADIUtils.getInputResources(inputRestrictionTree, serviceId);
+		
+		Collection<Triple> outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+	}
+
+	@Test
+	@Ignore(value="service fails")
+	public void testGetInputResources3() throws Exception {
+		String serviceURI = "http://sadiframework.org/services/getMolecularInteractions";
+		String serviceId = serviceURI + UUID.randomUUID();
+		Service service = new ServiceImpl(serviceURI);
+		
+		List<String> uniprot = Arrays.asList("P68871", "Q7Z591");
+
+		RestrictionNode inputRestrictionTree = SADIUtils.buildInputRestrictionTree(service.getInputClass());
+		for (List<String> path : SADIUtils.getDefaultRestrictionPaths(inputRestrictionTree)) {
+			RestrictionNode restriction = SADIUtils.getRestriction(inputRestrictionTree, path);
+			restriction.setSelected();
+		}
+		RestrictionNode firstLeaf = (RestrictionNode) inputRestrictionTree.getFirstLeaf();
+		firstLeaf.setValues(serviceId, uniprot);
+		
+		Collection<Resource> inputResources = SADIUtils.getInputResources(inputRestrictionTree, serviceId);
+		System.out.println(inputResources);
+		Collection<Triple> outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+	}
+	
+	@Test
+	public void testGetInputResources4() throws Exception {
+		String serviceURI = "http://sadiframework.org/services/getKEGGIDFromUniProt";
+		String serviceId = serviceURI + UUID.randomUUID();
+		Service service = new ServiceImpl(serviceURI);
+		
+		List<String> uniprot = Arrays.asList("P68871", "Q7Z591");
+
+		RestrictionNode inputRestrictionTree = SADIUtils.buildInputRestrictionTree(service.getInputClass());
+		for (List<String> path : SADIUtils.getDefaultRestrictionPaths(inputRestrictionTree)) {
+			RestrictionNode restriction = SADIUtils.getRestriction(inputRestrictionTree, path);
+			restriction.setSelected();
+		}
+		RestrictionNode firstLeaf = (RestrictionNode) inputRestrictionTree.getFirstLeaf();
+		firstLeaf.setValues(serviceId, uniprot);
+		
+		Collection<Resource> inputResources = SADIUtils.getInputResources(inputRestrictionTree, serviceId);
+		System.out.println(inputResources);
+		Collection<Triple> outputs = service.invokeService(inputResources);
+		for (Triple triple : outputs) {
+			System.out.println(triple);
+		}
+	}
+	
+}
