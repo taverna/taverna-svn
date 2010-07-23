35a11c83785c58b88f0c61d981e1b787b27167e9
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-07-23 20:43:08 +0000

    RESOLVED - issue TAVSERV-91: Unneccessary XML wrapping http://www.mygrid.org.uk/dev/issues/browse/TAVSERV-91
    git-svn-id: http://taverna.googlecode.com/svn@11284 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/products/uk.org.taverna.server/tags/server-2.2a1/server-webapp/src/main/java/org/taverna/server/master/common/Workflow.java
+++ taverna/products/uk.org.taverna.server/tags/server-2.2a1/server-webapp/src/main/java/org/taverna/server/master/common/Workflow.java
@@ -1,39 +1,22 @@
 package org.taverna.server.master.common;
 
-import java.util.Map;
-
-import javax.xml.bind.annotation.XmlAnyAttribute;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.namespace.QName;
 
 import org.w3c.dom.Element;
 
 /**
- * Encapsulation of a SCUFL2 or T2flow document.
+ * Encapsulation of a T2flow document.
  * 
  * @author dkf
  */
-@XmlRootElement(name = "abstractWorkflow")
-@XmlType(name = "Taverna2Workflow")
-@XmlSeeAlso({Workflow.T2Flow.class})
-public abstract class Workflow {
-	/** Literal document. */
-	@XmlAnyElement
-	public Element[] content;
-	/** Literal attributes. */
-	@XmlAnyAttribute
-	public Map<QName, Object> args;
-
+@XmlRootElement(name = "workflow")
+@XmlType(name = "Workflow")
+public class Workflow {
 	/**
-	 * Old-style Taverna 2 workflow.
-	 * 
-	 * @author dkf
+	 * Literal document.
 	 */
-	@XmlRootElement(name = "workflow", namespace = "http://taverna.sf.net/2008/xml/t2flow")
-	@XmlType(name = "t2flow", namespace = "http://taverna.sf.net/2008/xml/t2flow")
-	public static class T2Flow extends Workflow {
-	}
+	@XmlAnyElement(lax = true)
+	public Element[] content;
 }
