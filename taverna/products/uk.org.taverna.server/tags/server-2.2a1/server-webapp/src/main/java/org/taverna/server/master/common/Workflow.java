dd0401abdf5370c6497d59cf491d85ee6f50612a
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-07-23 10:56:51 +0000

    Working on TAVSERV-91
    git-svn-id: http://taverna.googlecode.com/svn@11271 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/products/uk.org.taverna.server/tags/server-2.2a1/server-webapp/src/main/java/org/taverna/server/master/common/Workflow.java
@@ -0,0 +1,22 @@
+package org.taverna.server.master.common;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.w3c.dom.Element;
+
+/**
+ * Encapsulation of a SCUFL2 or T2flow document.
+ * 
+ * @author dkf
+ */
+@XmlRootElement(name = "workflow")
+@XmlType(name = "Workflow")
+public class Workflow {
+	/**
+	 * Literal document.
+	 */
+	@XmlAnyElement(lax = true)
+	public Element[] content;
+}
