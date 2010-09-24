19bab821cc1089f4600d7f5847fe9c269b784aa9
donal.k.fellows@man.ac.uk <donal.k.fellows@man.ac.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-24 12:27:33 +0000

    Add in large-file download support
    git-svn-id: http://taverna.googlecode.com/svn@11620 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/products/uk.org.taverna.server/branches/app4andy-intermediate-fixes/server-webapp/src/main/java/org/taverna/server/master/TavernaServerImpl.java
+++ taverna/products/uk.org.taverna.server/branches/app4andy-intermediate-fixes/server-webapp/src/main/java/org/taverna/server/master/TavernaServerImpl.java
@@ -628,7 +628,7 @@ public class TavernaServerImpl implements TavernaServerSOAP, TavernaServerREST {
 			Object result;
 			if (v.getMediaType().equals(APPLICATION_OCTET_STREAM_TYPE))
 				// Only for files...
-				result = ((File) de).getContents();
+				result = de;
 			else if (v.getMediaType().equals(APPLICATION_ZIP_TYPE))
 				// Only for directories...
 				result = ((Directory) de).getContentsAsZip();
@@ -684,7 +684,7 @@ public class TavernaServerImpl implements TavernaServerSOAP, TavernaServerREST {
 			Object result;
 			if (wanted.equals(APPLICATION_OCTET_STREAM_TYPE))
 				// Only for files...
-				result = ((File) de).getContents();
+				result = de;
 			else if (wanted.equals(APPLICATION_ZIP_TYPE))
 				// Only for directories...
 				result = ((Directory) de).getContentsAsZip();
@@ -924,7 +924,8 @@ public class TavernaServerImpl implements TavernaServerSOAP, TavernaServerREST {
 	public byte[] getRunFileContents(String runName, DirEntryReference d)
 			throws UnknownRunException, FilesystemAccessException {
 		invokes++;
-		return getFile(getRun(runName), d).getContents();
+		File f = getFile(getRun(runName), d);
+		return f.getContents(0, -1);
 	}
 
 	@Override
