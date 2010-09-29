cfb9bcd5b3f6312ba689c8fd5cd93e3482f0742d
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-29 14:44:47 +0000

    Added new class for workflow outputs.
    git-svn-id: http://taverna.googlecode.com/svn@11636 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ unsorted/portlet168-workfow-submission/trunk/src/java/net/sf/taverna/t2/portal/WorkflowOutputPort.java
@@ -0,0 +1,76 @@
+
+package net.sf.taverna.t2.portal;
+
+/**
+ * Represents all information we have about a workflow output
+ * port (such as name, depth and data value), parsed from the
+ * result Baclava file.
+ *
+ * @author Alex Nenadic
+ */
+
+
+public class WorkflowOutputPort {
+
+    // Output port name
+    private String name;
+
+    // Output port depth;
+    private int depth;
+
+    // Data value on the output port
+    private Object value;
+
+
+    public WorkflowOutputPort(){
+    }
+
+    public WorkflowOutputPort(String name, int depth){
+        this.name = name;
+        this.depth = depth;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the depth
+     */
+    public int getDepth() {
+        return depth;
+    }
+
+    /**
+     * @param depth the depth to set
+     */
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    /**
+     * @return the value
+     */
+    public Object getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+}
+
