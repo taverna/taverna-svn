8594ca90c2d7727238a6e6fa2c1e87ed83993809
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-10-21 16:26:51 +0000

    Tagged version
    git-svn-id: http://taverna.googlecode.com/svn@11738 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ unsorted/taverna-with-ARC/tags/0.2/usecase-activity-ui/src/main/java/net/sf/taverna/t2/activities/usecase/views/UseCaseActivityContextualView.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (C) 2010 Hajo Nils Krabbenhšft, INB, University of Luebeck   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+
+package net.sf.taverna.t2.activities.usecase.views;
+
+import java.awt.Frame;
+
+import javax.swing.Action;
+
+import net.sf.taverna.t2.activities.usecase.UseCaseActivity;
+import net.sf.taverna.t2.activities.usecase.UseCaseActivityConfigurationBean;
+import net.sf.taverna.t2.activities.usecase.actions.UseCaseActivityConfigureAction;
+import net.sf.taverna.t2.workbench.ui.actions.activity.HTMLBasedActivityContextualView;
+import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+
+/**
+ * UseCaseActivityContextualView displays the use case information in a HTML table.
+ * Currently, this is only the use case ID.
+ * 
+ * @author Hajo Nils Krabbenhšft
+ */
+public class UseCaseActivityContextualView extends HTMLBasedActivityContextualView<UseCaseActivityConfigurationBean> {
+	private static final long serialVersionUID = 1L;
+
+	public UseCaseActivityContextualView(Activity<?> activity) {
+		super(activity);
+	}
+
+	@Override
+	protected String getRawTableRowsHtml() {
+		String html = "<b>Usecase: </b>" + getConfigBean().getUsecaseid();
+		return html;
+	}
+
+	@Override
+	public String getViewTitle() {
+		return "use case activity";
+	}
+
+	@Override
+	public Action getConfigureAction(final Frame owner) {
+		return new UseCaseActivityConfigureAction((UseCaseActivity) getActivity(), owner);
+	}
+
+	@Override
+	public int getPreferredPosition() {
+		return 100;
+	}
+
+}
