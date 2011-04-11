4cdb24b1e05e7bcb2f2e4844eb14a3f014f46218
rishi@mygrid.org.uk <rishi@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-04-11 16:08:25 +0000

    e-LICO Utilities - Repository Browser & Plugin Configuration (for RapidAnalytics & flora)
    git-svn-id: http://taverna.googlecode.com/svn@12888 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ unsorted/taverna-elico/branches/version2dev/elico-utilities/src/main/java/uk/ac/manchester/cs/elico/utilities/configuration/RapidMinerConfigurationUIFactory.java
@@ -0,0 +1,21 @@
+package uk.ac.manchester.cs.elico.utilities.configuration;
+
+import javax.swing.JPanel;
+
+import net.sf.taverna.t2.workbench.configuration.Configurable;
+import net.sf.taverna.t2.workbench.configuration.ConfigurationUIFactory;
+
+public class RapidMinerConfigurationUIFactory implements ConfigurationUIFactory {
+
+	public boolean canHandle(String uuid) {
+		return uuid.equals(getConfigurable().getUUID());	}
+
+	public Configurable getConfigurable() {
+		return RapidMinerPluginConfiguration.getInstance();
+	}
+
+	public JPanel getConfigurationPanel() {
+		return new RapidMinerPluginPreferencesPlugin();
+	}
+
+}
