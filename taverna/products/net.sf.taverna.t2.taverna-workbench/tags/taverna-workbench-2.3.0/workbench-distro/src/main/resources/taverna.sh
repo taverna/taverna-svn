1feb11b5be84bcfbb6c0f0c0096cc80b208ea0f1
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-07-12 12:44:45 +0000

    Retagging 2.3.0
    git-svn-id: http://taverna.googlecode.com/svn@13563 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/products/net.sf.taverna.t2.taverna-workbench/tags/taverna-workbench-2.3.0/workbench-distro/src/main/resources/taverna.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+## resolve links - $0 may be a symlink
+prog="$0"
+
+real_path() {
+    readlink -m "$1" 2>/dev/null || python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$1"
+}
+
+realprog=`real_path "$prog"`
+taverna_home=`dirname "$realprog"`
+javabin=java
+if test -x "$JAVA_HOME/bin/java"; then
+    javabin="$JAVA_HOME/bin/java"
+fi
+
+# 400 MB memory, 140 MB for classes
+exec "$javabin" -Xmx400m -XX:MaxPermSize=140m \
+  "-Draven.profile=file://$taverna_home/conf/current-profile.xml" \
+  "-Dtaverna.startup=$taverna_home" \
+  -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
+  -Dapple.laf.useScreenMenuBar=true \
+  -Dapple.awt.graphics.UseQuartz=false \
+  -Dsun.swing.enableImprovedDragGesture \
+  -jar "$taverna_home/lib/"prelauncher-*.jar \
+  ${1+"$@"}
+  
