585c68af89e395644a380e40fd090abdb179e140
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-07-18 14:16:56 +0000

    Updated version to 2.0-SNAPSHOT
    git-svn-id: http://taverna.googlecode.com/svn@13638 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/engine/net.sf.taverna.t2.activities/trunk/usecase-activity/src/main/java/net/sf/taverna/t2/activities/usecase/UseCaseActivity.java
+++ taverna/engine/net.sf.taverna.t2.activities/trunk/usecase-activity/src/main/java/net/sf/taverna/t2/activities/usecase/UseCaseActivity.java
@@ -14,6 +14,7 @@ import net.sf.taverna.t2.activities.externaltool.ExternalToolActivityConfigurati
 import net.sf.taverna.t2.activities.externaltool.manager.InvocationGroupManager;
 import net.sf.taverna.t2.annotation.AnnotationChain;
 import net.sf.taverna.t2.workflowmodel.Edit;
+import net.sf.taverna.t2.workflowmodel.Edits;
 import net.sf.taverna.t2.workflowmodel.OutputPort;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
@@ -107,6 +108,11 @@ public class UseCaseActivity implements SupersededActivity<UseCaseActivityConfig
 	@Override
 	public UseCaseActivityConfigurationBean getConfiguration() {
 		return null;
+	}
+
+	@Override
+	public void setEdits(Edits edits) {
+		
 	}
 
 }
