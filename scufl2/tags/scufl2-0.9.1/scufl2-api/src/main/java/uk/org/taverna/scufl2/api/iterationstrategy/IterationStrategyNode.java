0482d7bf8f8638de7b11e5711511f68c61128b97
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-01-31 12:39:46 +0000

    [maven-release-plugin]  copy for tag scufl2-0.9.1
    git-svn-id: http://taverna.googlecode.com/svn@14451 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2/tags/scufl2-0.9.1/scufl2-api/src/main/java/uk/org/taverna/scufl2/api/iterationstrategy/IterationStrategyNode.java
@@ -0,0 +1,12 @@
+package uk.org.taverna.scufl2.api.iterationstrategy;
+
+import uk.org.taverna.scufl2.api.common.Child;
+
+/**
+ * @author Alan R Williams
+ *
+ */
+public interface IterationStrategyNode extends
+		Child<IterationStrategyParent> {
+
+}
---
+++ scufl2/tags/scufl2-0.9.1/scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/SenderPort.java
@@ -0,0 +1,10 @@
+package uk.org.taverna.scufl2.api.port;
+
+/**
+ * A <code>Port</code> that sends data out.
+ * 
+ * @author Alan R Williams
+ */
+public interface SenderPort extends Port {
+
+}
