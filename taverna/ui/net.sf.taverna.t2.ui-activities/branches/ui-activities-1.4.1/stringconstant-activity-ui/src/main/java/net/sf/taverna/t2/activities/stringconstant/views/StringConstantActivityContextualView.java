1e4a0e95ad6d4387161a5145bd076352c8883e14
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-05-29 09:51:55 +0000

    Updates to version numbers for 2.4.1 and tweak to text constant contextual view
    git-svn-id: http://taverna.googlecode.com/svn@14978 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/ui/net.sf.taverna.t2.ui-activities/branches/ui-activities-1.4.1/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/views/StringConstantActivityContextualView.java
+++ taverna/ui/net.sf.taverna.t2.ui-activities/branches/ui-activities-1.4.1/stringconstant-activity-ui/src/main/java/net/sf/taverna/t2/activities/stringconstant/views/StringConstantActivityContextualView.java
@@ -24,6 +24,9 @@ import java.awt.Frame;
 
 import javax.swing.Action;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
+
 import net.sf.taverna.t2.activities.stringconstant.StringConstantActivity;
 import net.sf.taverna.t2.activities.stringconstant.StringConstantConfigurationBean;
 import net.sf.taverna.t2.activities.stringconstant.actions.StringConstantActivityConfigurationAction;
@@ -33,6 +36,7 @@ import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
 public class StringConstantActivityContextualView extends HTMLBasedActivityContextualView<StringConstantConfigurationBean> {
 
 	private static final long serialVersionUID = -553974544001808511L;
+	private static final int MAX_LENGTH = 100;
 
 	public StringConstantActivityContextualView(Activity<?> activity) {
 		super(activity);
@@ -46,7 +50,9 @@ public class StringConstantActivityContextualView extends HTMLBasedActivityConte
 
 	@Override
 	protected String getRawTableRowsHtml() {
-		String html = "<tr><td>Value</td><td>"+getConfigBean().getValue()+"</td></tr>";
+		String v = StringUtils.abbreviate(getConfigBean().getValue(), MAX_LENGTH);
+		v = StringEscapeUtils.escapeHtml(v);
+		String html = "<tr><td>Value</td><td>"+v+"</td></tr>";
 		return html;
 	}
 
