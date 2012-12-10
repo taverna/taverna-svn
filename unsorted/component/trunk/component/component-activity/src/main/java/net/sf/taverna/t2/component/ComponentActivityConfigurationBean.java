21b176c9e94bc75a117b8df7bd7c19575bb3ad89
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-12-10 11:39:07 +0000

    Changed to use ComponentVersionIdentification which is also used by the service description
    git-svn-id: http://taverna.googlecode.com/svn@15418 bf327186-88b3-11dd-a302-d386e5130c1c

--- unsorted/component/trunk/component/component-activity/src/main/java/net/sf/taverna/t2/component/ComponentActivityConfigurationBean.java
+++ unsorted/component/trunk/component/component-activity/src/main/java/net/sf/taverna/t2/component/ComponentActivityConfigurationBean.java
@@ -7,7 +7,9 @@ import net.sf.taverna.t2.component.registry.Component;
 import net.sf.taverna.t2.component.registry.ComponentFamily;
 import net.sf.taverna.t2.component.registry.ComponentRegistry;
 import net.sf.taverna.t2.component.registry.ComponentRegistryException;
+import net.sf.taverna.t2.component.registry.ComponentUtil;
 import net.sf.taverna.t2.component.registry.ComponentVersion;
+import net.sf.taverna.t2.component.registry.ComponentVersionIdentification;
 import net.sf.taverna.t2.component.registry.local.LocalComponentRegistry;
 import net.sf.taverna.t2.component.registry.myexperiment.MyExperimentComponentRegistry;
 import net.sf.taverna.t2.workflowmodel.Dataflow;
@@ -16,101 +18,32 @@ import net.sf.taverna.t2.workflowmodel.Dataflow;
  * Component activity configuration bean.
  * 
  */
-public class ComponentActivityConfigurationBean implements Serializable {
+public class ComponentActivityConfigurationBean extends ComponentVersionIdentification implements Serializable {
 	
-	private URL registryBase;
-	
-	private String familyName;
-	
-	private String componentName;
-	
-	private Integer componentVersion;
 	
 	private transient Dataflow dataflow;
 
 	public ComponentActivityConfigurationBean(
-			URL registryBase, String familyName, String componentName, Integer componentVersion) {
-		super();
-		this.registryBase = registryBase;
-		this.familyName = familyName;
-		this.componentName = componentName;
-		this.componentVersion = componentVersion;
+			ComponentVersionIdentification toBeCopied) {
+		super(toBeCopied);
+		
 	}
 
 
 	public Dataflow getDataflow() throws ComponentRegistryException {
 		if (dataflow == null) {
-			ComponentVersion version = calculateComponentVersion();
+			ComponentVersion version = ComponentUtil.calculateComponentVersion(this);
 			dataflow = version.getDataflow();
 		}
 		return dataflow;
 	}
 
 
-	public ComponentVersion calculateComponentVersion() throws ComponentRegistryException {
-		Component component = calculateComponent();
-		ComponentVersion version = component.getComponentVersion(componentVersion);
-		return version;
-	}
 
 
-	public Component calculateComponent() throws ComponentRegistryException {
-		ComponentFamily family = calculateFamily();
-		Component component = family.getComponent(componentName);
-		return component;
-	}
 
 
-	public ComponentFamily calculateFamily() throws ComponentRegistryException {
-		ComponentRegistry registry;
-		registry = calculateRegistry();
-		ComponentFamily family = registry.getComponentFamily(familyName);
-		return family;
-	}
 
-
-	public ComponentRegistry calculateRegistry() {
-		ComponentRegistry registry;
-		if (registryBase.getProtocol().startsWith("http")) {
-			registry = MyExperimentComponentRegistry.getComponentRegistry(registryBase);
-		}
-		else {
-			registry = LocalComponentRegistry.getComponentRegistry(registryBase);
-		}
-		return registry;
-	}
-
-
-	/**
-	 * @return the registryBase
-	 */
-	public URL getRegistryBase() {
-		return registryBase;
-	}
-
-
-	/**
-	 * @return the familyName
-	 */
-	public String getFamilyName() {
-		return familyName;
-	}
-
-
-	/**
-	 * @return the componentName
-	 */
-	public String getComponentName() {
-		return componentName;
-	}
-
-
-	/**
-	 * @return the componentVersion
-	 */
-	public Integer getComponentVersion() {
-		return componentVersion;
-	}
 	
 	
 }
