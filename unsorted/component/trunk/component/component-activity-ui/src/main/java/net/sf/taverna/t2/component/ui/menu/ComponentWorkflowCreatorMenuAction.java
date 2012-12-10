d9ebcfebb866d462e2f70c58b70083c1d49fe435
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-12-10 11:49:53 +0000

    Changed to use singleton action
    git-svn-id: http://taverna.googlecode.com/svn@15426 bf327186-88b3-11dd-a302-d386e5130c1c

--- unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/menu/ComponentWorkflowCreatorMenuAction.java
+++ unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/menu/ComponentWorkflowCreatorMenuAction.java
@@ -20,8 +20,10 @@ public class ComponentWorkflowCreatorMenuAction extends AbstractMenuAction {
 	
 	private static final URI COMPONENT_WORKFLOW_CREATE_URI = URI.create("http://taverna.sf.net/2008/t2workbench/menu#componentWorkflowCreate");
 	
+	private static Action creatorAction = new ComponentWorkflowCreatorAction();
+	
 	public ComponentWorkflowCreatorMenuAction() {
-		super(FILE_RUN_SECTION_URI, 10, COMPONENT_WORKFLOW_CREATE_URI);
+		super(ComponentMenu.COMPONENT, 200, COMPONENT_WORKFLOW_CREATE_URI);
 	}
 
 	/* (non-Javadoc)
@@ -29,7 +31,7 @@ public class ComponentWorkflowCreatorMenuAction extends AbstractMenuAction {
 	 */
 	@Override
 	protected Action createAction() {
-		return new ComponentWorkflowCreatorAction();
+		return creatorAction;
 	}
 
 }
