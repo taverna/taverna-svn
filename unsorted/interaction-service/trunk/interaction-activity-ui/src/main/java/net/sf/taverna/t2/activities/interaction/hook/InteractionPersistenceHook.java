d48dcabdb2423a420f46a6426d0271686fa703b5
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-02-07 14:59:21 +0000

    Code to load and save usage information
    git-svn-id: http://taverna.googlecode.com/svn@15581 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ unsorted/interaction-service/trunk/interaction-activity-ui/src/main/java/net/sf/taverna/t2/activities/interaction/hook/InteractionPersistenceHook.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.activities.interaction.hook;
+
+import net.sf.taverna.t2.activities.interaction.InteractionRecorder;
+import net.sf.taverna.t2.activities.interaction.InteractionRunDeletionListener;
+import net.sf.taverna.t2.workbench.ShutdownSPI;
+import net.sf.taverna.t2.workbench.StartupSPI;
+
+/**
+ * @author alanrw
+ *
+ */
+public class InteractionPersistenceHook implements StartupSPI, ShutdownSPI {
+
+	/* (non-Javadoc)
+	 * @see net.sf.taverna.t2.workbench.ShutdownSPI#positionHint()
+	 */
+	@Override
+	public int positionHint() {
+		return 900;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.taverna.t2.workbench.ShutdownSPI#shutdown()
+	 */
+	@Override
+	public boolean shutdown() {
+		InteractionRecorder.persist();
+		return true;
+	}
+
+	@Override
+	public boolean startup() {
+		InteractionRecorder.load();
+		return true;
+	}
+
+}
