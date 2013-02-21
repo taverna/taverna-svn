abe2d47c02317ad5de3999d68cf4cc0ff89dbfe3
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-02-21 09:56:08 +0000

    Put in family deletion usage check
    git-svn-id: http://taverna.googlecode.com/svn@15611 bf327186-88b3-11dd-a302-d386e5130c1c

--- unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/menu/family/ComponentFamilyDeleteAction.java
+++ unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/menu/family/ComponentFamilyDeleteAction.java
@@ -15,9 +15,15 @@ import javax.swing.JPanel;
 import net.sf.taverna.t2.component.registry.ComponentFamily;
 import net.sf.taverna.t2.component.registry.ComponentRegistry;
 import net.sf.taverna.t2.component.registry.ComponentRegistryException;
+import net.sf.taverna.t2.component.registry.ComponentVersionIdentification;
 import net.sf.taverna.t2.component.ui.panel.FamilyChooserPanel;
 import net.sf.taverna.t2.component.ui.panel.RegistryChooserPanel;
 import net.sf.taverna.t2.component.ui.serviceprovider.ComponentServiceIcon;
+import net.sf.taverna.t2.component.ui.serviceprovider.ComponentServiceProviderConfig;
+import net.sf.taverna.t2.component.ui.util.Utils;
+import net.sf.taverna.t2.workbench.file.FileManager;
+import net.sf.taverna.t2.workflowmodel.ConfigurationException;
+import net.sf.taverna.t2.workflowmodel.Dataflow;
 
 import org.apache.log4j.Logger;
 
@@ -39,6 +45,8 @@ public class ComponentFamilyDeleteAction extends AbstractAction {
 
 	
 	private static final String DELETE_FAMILY = "Delete family...";
+	
+	private static FileManager fm = FileManager.getInstance();
 	
 	public ComponentFamilyDeleteAction() {
 		super (DELETE_FAMILY, ComponentServiceIcon.getIcon());
@@ -86,17 +94,42 @@ public class ComponentFamilyDeleteAction extends AbstractAction {
 				JOptionPane.showMessageDialog(null, "Unable to determine family", "Component Family Problem", JOptionPane.ERROR_MESSAGE);
 				return;
 			}
+			if (familyIsInUse(chosenRegistry, chosenFamily)) {
+				JOptionPane.showMessageDialog(null, "Components in the family are open", "Component Family Problem", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
 			int confirmation = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete " + chosenFamily.getName(), "Delete Component Family Confirmation", JOptionPane.YES_NO_OPTION);
 			if (confirmation == JOptionPane.YES_OPTION) {
 				try {
 					chosenRegistry.removeComponentFamily(chosenFamily);
+					ComponentServiceProviderConfig config = new ComponentServiceProviderConfig();
+					config.setFamilyName(chosenFamily.getName());
+					config.setRegistryBase(chosenRegistry.getRegistryBase());
+					Utils.removeComponentServiceProvider(config);
 				} catch (ComponentRegistryException e) {
 					JOptionPane.showMessageDialog(null, "Unable to delete " + chosenFamily.getName(), "Component Family Deletion Error", JOptionPane.ERROR_MESSAGE);
+					logger.error(e);
+				} catch (ConfigurationException e) {
 					logger.error(e);
 				}
 			}
 		}
 
+	}
+
+
+	private static boolean familyIsInUse(ComponentRegistry chosenRegistry, ComponentFamily chosenFamily) {
+		for (Dataflow d : fm.getOpenDataflows()) {
+			Object dataflowSource = fm.getDataflowSource(d);
+			if (dataflowSource instanceof ComponentVersionIdentification) {
+				ComponentVersionIdentification ident = (ComponentVersionIdentification) dataflowSource;
+				if (ident.getRegistryBase().equals(chosenRegistry.getRegistryBase()) &&
+						ident.getFamilyName().equals(chosenFamily.getName())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
