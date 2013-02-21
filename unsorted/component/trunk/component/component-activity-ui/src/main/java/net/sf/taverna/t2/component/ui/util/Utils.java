959d92450ad52dd12545edd8b7c5d40addf890af
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-02-21 09:57:04 +0000

    Changed to allow deletion
    git-svn-id: http://taverna.googlecode.com/svn@15613 bf327186-88b3-11dd-a302-d386e5130c1c

--- unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/util/Utils.java
+++ unsorted/component/trunk/component/component-activity-ui/src/main/java/net/sf/taverna/t2/component/ui/util/Utils.java
@@ -23,15 +23,24 @@ public class Utils {
 	// From http://stackoverflow.com/questions/163360/regular-expresion-to-match-urls-in-java
 	public static String URL_PATTERN = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 
+	private static ServiceDescriptionRegistry registry = ServiceDescriptionRegistryImpl.getInstance();
+
 	public static void refreshComponentServiceProvider(ComponentServiceProviderConfig config)
 			throws ConfigurationException {
 		ComponentServiceProvider provider = new ComponentServiceProvider();
 		provider.configure(config);
-		ServiceDescriptionRegistry registry = ServiceDescriptionRegistryImpl.getInstance();
 		registry.removeServiceDescriptionProvider(provider);
 		registry.addServiceDescriptionProvider(provider);
 	}
 	
+	public static void removeComponentServiceProvider(
+			ComponentServiceProviderConfig config)
+			throws ConfigurationException {
+		ComponentServiceProvider provider = new ComponentServiceProvider();
+		provider.configure(config);
+		registry.removeServiceDescriptionProvider(provider);
+	}
+
 	public static boolean dataflowIsComponent(Dataflow d) {
 		if (d == null) {
 			return false;
