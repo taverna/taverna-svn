45e28fc5f08e52fa0c39a697105b20e60b9fb4ab
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-06-28 16:37:34 +0000

    git-svn-id: http://taverna.googlecode.com/svn@15812 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/ui/net.sf.taverna.t2.ui-activities/branches/ui-activities-1.4.2/biomart-activity-ui/src/main/java/net/sf/taverna/t2/activities/biomart/actions/BiomartConfigurationPanel.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ * Copyright (C) 2007 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.biomart.actions;
+
+import java.awt.Dimension;
+import java.io.File;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+
+import net.sf.taverna.raven.appconfig.ApplicationRuntime;
+import net.sf.taverna.t2.activities.biomart.BiomartActivity;
+import net.sf.taverna.t2.workbench.ui.views.contextualviews.activity.ActivityConfigurationPanel;
+
+import org.apache.log4j.Logger;
+import org.biomart.martservice.MartQuery;
+import org.biomart.martservice.MartService;
+import org.biomart.martservice.MartServiceException;
+import org.biomart.martservice.MartServiceXMLHandler;
+import org.biomart.martservice.config.QueryConfigController;
+import org.biomart.martservice.config.ui.MartServiceQueryConfigUIFactory;
+import org.biomart.martservice.config.ui.QueryConfigUIFactory;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+
+public class BiomartConfigurationPanel extends ActivityConfigurationPanel<BiomartActivity, Element> {
+	private static Logger logger = Logger
+			.getLogger(BiomartConfigurationPanel.class);
+	
+	private static final long serialVersionUID = 1884045346293327621L;
+	
+	private Element configuration;
+	private String configurationString;
+
+	private MartQuery biomartQuery;
+
+	private BiomartActivity activity;
+	
+	private static XMLOutputter outputter = new XMLOutputter();
+
+	public BiomartConfigurationPanel(BiomartActivity activity) {
+		this.activity = activity;
+		initialise();
+	}
+
+	private void initialise() {
+		this.configuration = activity.getConfiguration();
+		this.configurationString = outputter.outputString(this.configuration);
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		biomartQuery = MartServiceXMLHandler.elementToMartQuery(configuration, null);
+		MartService service = biomartQuery.getMartService();
+		
+		File homeRoot=ApplicationRuntime.getInstance().getApplicationHomeDir();
+		if (homeRoot==null) {
+			logger.error("unable to determine application home for biomart cache");
+			homeRoot=new File(System.getProperty("java.io.tmpdir"));
+		}
+		File cache=new File(homeRoot,"t2-biomart-activity");
+		service.setCacheDirectory(new File(cache,"cache"));
+		logger.info("Biomart is using cache directory:"+cache.getAbsolutePath());
+		
+		QueryConfigController controller = new QueryConfigController(biomartQuery);
+		try {
+			QueryConfigUIFactory queryConfigUIFactory = new MartServiceQueryConfigUIFactory(
+					service, controller, biomartQuery.getMartDataset());
+			add(queryConfigUIFactory.getDatasetConfigUI());
+			add(Box.createGlue());
+		} catch (MartServiceException e) {
+			add(new JLabel("Error reading configuration properties"));
+			add(new JLabel(e.getMessage()));
+			add(Box.createGlue());
+		}
+		this.setPreferredSize(new Dimension(900,500));
+		this.validate();
+	}
+	
+	public Element getQuery() {
+		return MartServiceXMLHandler.martQueryToElement(biomartQuery, null);
+	}
+
+	@Override
+	public Element getConfiguration() {
+		return configuration;
+	}
+
+	@Override
+	public boolean isConfigurationChanged() {
+		String queryString = outputter.outputString(getQuery());
+		return !queryString.equals(configurationString);
+	}
+
+	@Override
+	public void noteConfiguration() {
+		configuration = (Element) getQuery().clone();
+		configurationString = outputter.outputString(configuration);
+	}
+
+	@Override
+	public void refreshConfiguration() {
+		removeAll();
+		initialise();
+	}
+
+	@Override
+	public boolean checkValues() {
+		// TODO Not yet done
+		return true;
+	}
+}
---
+++ taverna/ui/net.sf.taverna.t2.ui-activities/branches/ui-activities-1.4.2/disabled-activity-ui/src/main/java/net/sf/taverna/t2/activities/disabled/views/DisabledContextualView.java
@@ -0,0 +1,141 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.activities.disabled.views;
+
+import net.sf.taverna.t2.activities.disabled.actions.DisabledActivityConfigurationAction;
+import net.sf.taverna.t2.workbench.edits.EditManager;
+import net.sf.taverna.t2.workbench.file.FileManager;
+import net.sf.taverna.t2.workbench.ui.actions.activity.HTMLBasedActivityContextualView;
+import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+import net.sf.taverna.t2.workflowmodel.processor.activity.DisabledActivity;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityInputPort;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityOutputPort;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper;
+import net.sf.taverna.t2.workflowmodel.Dataflow;
+import net.sf.taverna.t2.workflowmodel.Edit;
+import net.sf.taverna.t2.workflowmodel.EditException;
+import net.sf.taverna.t2.workflowmodel.Edits;
+import net.sf.taverna.t2.workflowmodel.EditsRegistry;
+import net.sf.taverna.t2.workflowmodel.OutputPort;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.beans.Introspector;
+import java.beans.IntrospectionException;
+import java.beans.BeanInfo;
+import java.beans.PropertyDescriptor;
+
+import java.lang.reflect.Method;
+import java.lang.IllegalAccessException;
+import java.lang.IllegalArgumentException;
+import java.lang.NoSuchMethodException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.Action;
+
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.taverna.t2.lang.uibuilder.UIBuilder;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * A DisabledContextualView displays information about a DisabledActivity
+ * 
+ * @author alanrw
+ * 
+ */
+public class DisabledContextualView extends
+		HTMLBasedActivityContextualView<Object> {
+
+	private static Logger logger = Logger
+			.getLogger(DisabledContextualView.class);
+
+    private List<String> fieldNames;
+
+	public DisabledContextualView(DisabledActivity activity) {
+		super(activity);
+		init();
+	}
+
+	private void init() {
+	}
+
+	/**
+	 * The table for the DisabledActivity shows its ports and the information
+	 * within the offline Activity's configuration.
+	 * 
+	 * @return
+	 */
+	@Override
+	protected String getRawTableRowsHtml() {
+		String html = "";
+		html = html + "<tr><th>Input Port Name</th><th>Port depth</th>"
+				+ "</tr>";
+		for (ActivityInputPort aip : getActivity().getInputPorts()) {
+			html = html + "<tr><td>" + aip.getName() + "</td><td>"
+					+ aip.getDepth() + "</td></tr>";
+		}
+		html = html + "<tr><th>Output Port Name</th><th>Port depth</th>"
+				+ "</tr>";
+		for (OutputPort aop : getActivity().getOutputPorts()) {
+			html = html + "<tr><td>" + aop.getName() + "</td><td>"
+					+ aop.getDepth() + "</td></tr>";
+		}
+
+		Object config = ((DisabledActivity) getActivity()).getConfiguration()
+				.getBean();
+		try {
+			html += "<tr><th>Property Name</th><th>Property Value</th></tr>";
+			BeanInfo beanInfo = Introspector.getBeanInfo(config.getClass());
+			for (PropertyDescriptor pd : beanInfo.getPropertyDescriptors()) {
+				Method readMethod = pd.getReadMethod();
+				if ((readMethod != null) && !(pd.getName().equals("class"))) {
+					try {
+						html += "<tr><td>" + pd.getName() + "</td><td>"
+								+ readMethod.invoke(config) + "</td></tr>";
+						if (fieldNames == null) {
+						    fieldNames = new ArrayList<String>();
+						}
+						fieldNames.add(pd.getName());
+					} catch (IllegalAccessException ex) {
+						// ignore
+					} catch (IllegalArgumentException ex) {
+						// ignore
+					} catch (InvocationTargetException ex) {
+						// ignore
+					}
+				}
+			}
+		} catch (IntrospectionException e) {
+			// ignore
+		}
+		return html;
+	}
+
+	@Override
+	public String getViewTitle() {
+		return "Unavailable service";
+	}
+
+	@Override
+	public int getPreferredPosition() {
+		return 100;
+	}
+
+	@Override
+	public Action getConfigureAction(Frame owner) {
+		return new DisabledActivityConfigurationAction(
+				(DisabledActivity) getActivity(), owner);
+	}
+
+}
