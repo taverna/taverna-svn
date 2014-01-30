1e6d9cabc8c1bd66f7ddc8ec432fc4550b9ebb94
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-01-30 12:03:07 +0000

    webdav_worker_names
    git-svn-id: http://taverna.googlecode.com/svn@16652 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna/ui/net.sf.taverna.t2.ui-activities/branches/maintenance/webdav-activity-ui/src/main/java/net/sf/taverna/t2/webdav/ui/serviceprovider/WebDAVServiceProvider.java
+++ taverna/ui/net.sf.taverna.t2.ui-activities/branches/maintenance/webdav-activity-ui/src/main/java/net/sf/taverna/t2/webdav/ui/serviceprovider/WebDAVServiceProvider.java
@@ -34,7 +34,7 @@ import net.sf.taverna.t2.workflowmodel.serialization.xml.ActivityXMLDeserializer
 
 public class WebDAVServiceProvider implements ServiceDescriptionProvider {
 	
-	private static final String LOCALWORKER_NAMES = "/sardineworker_names";
+	private static final String LOCALWORKER_NAMES = "/webdav_worker_names";
 	
 	private static final String LOCALWORKER_SERVICE = "Local service";
 
