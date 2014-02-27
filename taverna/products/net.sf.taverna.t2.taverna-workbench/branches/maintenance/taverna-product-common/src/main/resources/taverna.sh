c0c120ef48d83da396800c758626507ae3f1f585
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-02-27 12:08:03 +0000

    restored https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/branches/maintenance/taverna-product-common
    git-svn-id: http://taverna.googlecode.com/svn@16785 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/products/net.sf.taverna.t2.taverna-workbench/branches/maintenance/taverna-product-common/src/main/resources/taverna.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+## resolve links - $0 may be a symlink
+prog="$0"
+
+real_path() {
+    readlink -m "$1" 2>/dev/null || python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$1"
+}
+
+realprog=`real_path "$prog"`
+taverna_home=`dirname "$realprog"`
+javabin=java
+if test -x "$JAVA_HOME/bin/java"; then
+    javabin="$JAVA_HOME/bin/java"
+fi
+
+# 32-bit compatible memory settings:
+# 1000 MB memory, 200 MB for classes
+exec "$javabin" -Xmx1000m -XX:MaxPermSize=200m \
+  "-Draven.profile=file://$taverna_home/conf/current-profile.xml" \
+  "-Dtaverna.startup=$taverna_home" \
+  "-Dtaverna.dotlocation=$taverna_home/bin/dot.sh" \
+  -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
+  -Dapple.laf.useScreenMenuBar=true \
+  -Dapple.awt.graphics.UseQuartz=false \
+  -Dcom.sun.net.ssl.enableECC=false \
+  -Djsse.enableSNIExtension=false \
+  -Dsun.swing.enableImprovedDragGesture \
+  -jar "$taverna_home/lib/"prelauncher-*.jar \
+  ${1+"$@"}
+  
