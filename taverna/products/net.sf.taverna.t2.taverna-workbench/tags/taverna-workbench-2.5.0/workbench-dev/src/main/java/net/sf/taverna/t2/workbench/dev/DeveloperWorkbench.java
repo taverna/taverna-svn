983acb7fec5b6b61f24b2b541bc724e2a8195b14
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-03-21 16:35:00 +0000

    [maven-release-plugin]  copy for tag taverna-workbench-2.5.0
    git-svn-id: http://taverna.googlecode.com/svn@17015 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ taverna/products/net.sf.taverna.t2.taverna-workbench/tags/taverna-workbench-2.5.0/workbench-dev/src/main/java/net/sf/taverna/t2/workbench/dev/DeveloperWorkbench.java
@@ -0,0 +1,24 @@
+package net.sf.taverna.t2.workbench.dev;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import net.sf.taverna.raven.launcher.Launcher;
+
+
+public class DeveloperWorkbench {
+
+	/**
+	 * Remember running with VM parameters:
+	 * 
+	 * -Xmx400m -XX:MaxPermSize=140m
+	 * 
+	 */
+	public static void main(String[] args) throws URISyntaxException {
+		URL dir = DeveloperWorkbench.class.getResource("/conf/current-profile.xml");
+		System.setProperty("taverna.startup", dir.toURI().resolve("../").getPath());
+		System.setProperty("raven.launcher.app.name", "taverna-2.5.0-dev");
+		Launcher.main(args);
+	}
+
+}
